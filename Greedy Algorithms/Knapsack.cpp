f0146cd5e1901259c3d456a6b91adacd0b6ffa1e
AnupKumarPanwar <1anuppanwar@gmail.com>
2017-02-20 09:26:23 +0530

    Greedy Algorithms

--- Greedy Algorithms/Knapsack.cpp
+++ Greedy Algorithms/Knapsack.cpp
@@ -8,7 +8,87 @@ struct Item
 };
 
 
-int main(int argc, char const *argv[])
+float profitPerUnit(Item x)
 {
+	return (float)(x.profit/x.weight);
+}
+
+int partition (Item arr[], int low, int high)
+{
+    Item pivot = arr[high];    // pivot
+    int i = (low - 1);  // Index of smaller element
+ 
+    for (int j = low; j <high; j++)
+    {
+        // If current element is smaller than or
+        // equal to pivot
+        if (profitPerUnit(arr[j]) <= profitPerUnit(pivot))
+        {
+            i++;    // increment index of smaller element
+            Item temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+        }
+    }
+    Item temp=arr[i+1];
+    arr[i+1]=arr[high];
+    arr[high]=temp;
+    return (i + 1);
+}
+ 
+
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+       
+        int p= partition(arr, low, high);
+
+        quickSort(arr, low, p - 1);
+        quickSort(arr, p + 1, high);
+    }
+}
+ 
+
+
+int main()
+{
+	cout<<"\nEnter the capacity of the knapsack : ";
+	int capacity;
+	cin<<capacity;
+	cout<<"\n Enter the number of Items : ";
+	int n;
+	cin<<n;
+	Items itemArray[n];
+	for (int i = 0; i < n; i++)
+	{
+		cout<<"\nEnter the weight and profit of item "<<i<<" : ";
+		cin>itemArray[i].weight;
+		cin>>itemArray[i].profit;
+	}
+
+	quickSort(itemArray, 0, n);
+
+	float maxProfit=0;
+	int i=0;
+	while(capacity>0 && i<n)
+	{
+		if(capacity>itemArray[i].weight)
+		{
+			maxProfit+=itemArray[i].profit;
+			capacity-=itemArray[i].weight;
+			cout<<"\n\t"<<itemArray[i].weight<<"\t"<<itemArray[i].profit;
+		}
+		else
+		{
+			maxProfit+=profitPerUnit(itemArray[i])*capacity;
+			capacity=0;
+			cout<<"\n\t"<<capacity<<"\t"<<profitPerUnit(itemArray[i])*capacity;
+			break;
+		}
+	}
+
+	cout<<"\nMax Profit : "<<maxProfit;
+
 	return 0;
 }
