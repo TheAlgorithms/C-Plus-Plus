3b5ed7ae752294f4918b054c0225272a21aab5ca
rudrasohan <sohanrudra@gmail.com>
2017-09-11 00:58:45 +0530

    New Function descriptions added

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -15,13 +15,30 @@ using namespace std;
 	.
 	.
 */
-int a[2][2] = {{1,1},{1,0}};
-int I[2][2] = {{1,0},{0,1}};
-int ans[2][2] = {{1,1},{1,0}};
+int a[2][2] = {{1,1},{1,0}};//fibonacci matrix
+int ans[2][2] = {{1,1},{1,0}};//final ans matrix
+/*
+	Working Principal:
+
+	[F(k+1)	  F(k)]   [1  1]^k
+	|             | = |    |
+	[F(k)	F(k-1)]   [1  0]
+
+	where F(k) is the kth term of the Fibonacci Sequence. 
+
+
+*/
 
 void product(int b[][2],int k[][2])
 {
-	int c[2][2];
+	/*
+		Function for computing product of the two matrices b and k
+		and storing them into the variable ans.
+
+		Implementation :
+		Simple matrix multiplication of two (2X2) matrices.
+	*/
+	int c[2][2];//temporary stores the answer
 	c[0][0] = b[0][0]*k[0][0]+b[0][1]*k[1][0];
 	c[0][1] = b[0][0]*k[0][1]+b[0][1]*k[1][1];
 	c[1][0] = b[1][0]*k[0][0]+b[1][1]*k[1][0];
@@ -36,6 +53,13 @@ void product(int b[][2],int k[][2])
 void power_rec(int n)
 {	
 
+	/*
+		Function for calculating A^n(exponent) in a recursive fashion
+
+		Implementation:
+		A^n = { A^(n/2)*A^(n/2) 			if n is even
+			  { A^((n-1)/2)*A^((n-1)/2)*A   if n is odd
+	*/
 	if((n == 1)||(n==0))
 		return;
 	else
@@ -58,6 +82,7 @@ void power_rec(int n)
 
 int main()
 {
+	//Main Function
 	cout <<"Enter the value of n\n";
 	int n;
 	cin >>n;
