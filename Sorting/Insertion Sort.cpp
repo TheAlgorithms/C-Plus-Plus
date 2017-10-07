0310bd2cc781db737e7f113cf5c3f404d7d4fb69
Joseph Lyons <JosephTLyons@users.noreply.github.com>
2017-10-07 04:56:13 -0400

    Formatting and added some cout statements

--- Sorting/Insertion Sort.cpp
+++ Sorting/Insertion Sort.cpp
@@ -6,33 +6,40 @@ using namespace std;
 int main()
 {
 	int n;
+    
+    cout << "Enter an array size: ";
     cin >> n;
+    
 	int *arrayPtr = new int[n];
+	cout << "\nEnter any " << n << " Numbers for Unsorted Array: \n";
 	
 	//Input
-	for(int i=0; i<n; i++)
+	for (int i = 0; i < n; i++)
 	{
-		cin>>arrayPtr[i];
+        cout << i + 1 << ") ";
+		cin >> arrayPtr[i];
 	}
 	
 	//Sorting
-	for(int i=1; i<n; i++)
+	for (int i = 1; i < n; i++)
 	{
-		int temp=arrayPtr[i];
-		int j=i-1;
-		while(j>=0 && temp<arrayPtr[j])
+		int temp = arrayPtr[i];
+		int j = i-1;
+        
+        for (; j >= 0 && temp < arrayPtr[j]; j--)
 		{
-			arrayPtr[j+1]=arrayPtr[j];
-			j--;
+			arrayPtr[j + 1] = arrayPtr[j];
 		}
-		arrayPtr[j+1]=temp;
+        
+		arrayPtr[j + 1] = temp;
 	}
 	
 	//Output
-	cout<<"\nSorted Array : ";
-	for(int i=0; i<n; i++)
+	cout << "\nSorted Array: ";
+    
+	for (int i = 0; i < n; i++)
 	{
-		cout<<arrayPtr[i]<<"\t";
+		cout << arrayPtr[i] << " ";
 	}
 }
 
