bc64e18edbb12db8333736da7966f506161709ed
Ayaan Faiz <ayaan2103@gmail.com>
2017-10-08 11:39:29 +0530

    To find the height of tree

---
+++ Trees/Height of tree.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <cstddef>
+
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d){
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            }
+            else {
+                Node* cur;
+                if(data <= root->data){
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                }
+                else{
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+
+               return root;
+           }
+        }
+int height(Node* root) {
+        if(root == NULL)
+        {
+            return -1;
+        }
+        int l = height(root->left);
+        int r = height(root->right);
+        if(l>r)
+            return(l+1);
+        else
+            return(r+1);
+    }
+};
+
+int main() {
+    Solution myTree;
+    Node* root = NULL;
+    int t;
+    int data;
+
+    cin >> t;
+
+    while(t-- > 0){
+        cin >> data;
+        root = myTree.insert(root, data);
+    }
+    int height = myTree.height(root);
+    cout << height;
+
+    return 0;
+}
