a6641b573460362046fce68dc795e0d69d8d271f
Baigel <43929690+Baigel@users.noreply.github.com>
2018-10-08 21:18:44 +1000

    Update Bubble Sort.cpp

--- Sorting/Bubble Sort.cpp
+++ Sorting/Bubble Sort.cpp
@@ -1,39 +1,35 @@
-//Bubble Sort
+// Bubble Sort
 
 #include<iostream>
+
 using namespace std;
 
-int main()
+int main ()
 {
+	// Input
 	int n;
+	cout << "Enter Length of Array" << endl;
 	cin >> n;
 	int Array[n];
-	cout<<"\nEnter any 6 Numbers for Unsorted Array : ";
+	cout << "Enter any " << n << " Numbers for Unsorted Array : " << endl;
+	for (int i = 0; i < n; i++) { cin >> Array[i]; }
 
-	//Input
-	for(int i=0; i<n; i++)
-	{
-		cin>>Array[i];
-	}
-
-	//Bubble Sorting
-	for(int i=0; i<n; i++)
-	{
-		for(int j=0; j<n-1; j++)
-		{
-			if(Array[j]>Array[j+1])
-			{
-				int temp=Array[j];
-				Array[j]=Array[j+1];
-				Array[j+1]=temp;
+	// Bubble Sorting
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n-1; j++) {
+			if (Array[j] > Array[j+1]) {
+				int temp = Array[j];
+				Array[j] = Array[j+1];
+				Array[j+1] = temp;
 			}
 		}
 	}
 
-	//Output
-	cout<<"\nSorted Array : ";
-	for(int i=0; i<n; i++)
-	{
-		cout<<Array[i]<<"\t";
+	// Output
+	cout << "Sorted Array : " << endl;
+	for(int i = 0; i < n; i++) {
+		cout << Array[i] << "  ";
 	}
+
+    return 0;
 }
