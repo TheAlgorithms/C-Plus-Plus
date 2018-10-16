d086371fef8f0cf3ffb0b293464b5b3b27b71243
Rakesh <rsenwar448@gmail.com>
2018-10-16 18:10:26 +0530

    Update Decimal To Binary.cpp
    Improved code with proper output and User friendly

--- Decimal To Binary.cpp
+++ Decimal To Binary.cpp
@@ -1,20 +1,20 @@
 #include<iostream>
 using namespace std;
+
 int main()
 {
 	int number;
+	cout << "Enter Number: ";
 	cin>>number;
 	int remainder,binary=0,var=1;
 
-do{
-		
-	 remainder=number%2;
-	number=number/2;
-	binary=binary+(remainder*var);
-	var=var*10;
-
-
-}
-while(number>0);
-	cout<<binary;
+	while(number>0) {
+		remainder = number%2;
+		number /= 2;
+		binary = binary + (remainder*var);
+		var=var*10;
+	}
+	cout << "Binary representation: " << binary; 
+	
+	return 0;
 }
