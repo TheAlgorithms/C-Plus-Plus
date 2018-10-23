798993010709b9e4a59268964e40a9ad5a8f28b9
Ashwek Swamy <39827514+ashwek@users.noreply.github.com>
2018-10-23 18:01:24 +0530

    Added Proper Indentation
    Added proper indentation and using shorthand.

--- Decimal To Binary.cpp
+++ Decimal To Binary.cpp
@@ -1,20 +1,17 @@
 #include<iostream>
 using namespace std;
-int main()
-{
+int main(){
 	int number;
+	cout <<"Enter number to find its binary : ";
 	cin>>number;
 	int remainder,binary=0,var=1;
 
-do{
-		
-	 remainder=number%2;
-	number=number/2;
-	binary=binary+(remainder*var);
-	var=var*10;
-
-
-}
-while(number>0);
+	do{
+		remainder = number%2;
+		number /= 2;
+		binary += (remainder*var);
+		var *= 10;
+	}while(number>0);
 	cout<<binary;
+	return 0;
 }
