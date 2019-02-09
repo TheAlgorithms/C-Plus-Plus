6a22ba7499ee50a19a8c488f35a34f02a8ec1166
Ashwek Swamy <39827514+ashwek@users.noreply.github.com>
2019-02-09 15:45:31 +0530

    Update Decimal To Binary.cpp

--- Decimal To Binary.cpp
+++ Decimal To Binary.cpp
@@ -5,14 +5,14 @@ int main()
 {
 	int number;
 	cout << "Enter Number: ";
-	cin>>number;
-	int remainder,binary=0,var=1;
+	cin >> number;
+	int remainder, binary = 0, var = 1;
 
 	while(number>0) {
-		remainder = number%2;
+		remainder = number % 2;
 		number /= 2;
-		binary = binary + (remainder*var);
-		var=var*10;
+		binary = binary + (remainder * var);
+		var = var * 10;
 	}
 	cout << "Binary representation: " << binary; 
 	
