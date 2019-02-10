9e6cd3bb7ceba514ecd510fbf633ee13dcfd3a4b
Ashwek Swamy <39827514+ashwek@users.noreply.github.com>
2019-02-10 12:10:29 +0530

    Update Palindromeofnumber.cpp

--- Palindromeofnumber.cpp
+++ Palindromeofnumber.cpp
@@ -1,20 +1,23 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+
 using namespace std;
 
 int main()
 {
-int num;
-cin>>num;
+	int num;
+	cout << "Enter number = ";
+	cin >> num;
 
-string s1 = to_string(num);
-string s2 = s1;
+	string s1 = to_string(num);
+	string s2 = s1;
 
-reverse(s1.begin(),s1.end());
+	reverse(s1.begin(),s1.end());
 
-if(s1==s2)
-  cout<<"true";
-else
-  cout<<"false";
+	if(s1 == s2)
+		cout<<"true";
+	else
+		cout<<"false";
 
-return 0;
+	return 0;
 }
