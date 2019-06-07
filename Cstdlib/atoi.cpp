853ebf09c9fae39d21053efe1bb83dc610b71ebe
sinkyoungdeok <tlsrudejr123@nate.com>
2019-06-07 22:40:37 +0900

    add atoi in stdlib header

---
+++ Cstdlib/atoi.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int atoi(char *input)
+{
+	int result = 0;
+	for (int i = 0; input[i]; i++)
+	{
+		result *= 10;
+		result += input[i] - '0';
+	}
+	return result;
+}
+int main()
+{
+	char input[10000];
+	cin >> input;
+	cout << atoi(input) << endl;
+	return 0;
+}
+
