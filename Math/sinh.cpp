2409f3d999a4e08350943ceec989e1d2136eb2c5
ljw322 <30587502+ljw322@users.noreply.github.com>
2019-06-07 22:57:39 +0900

    Create sinh.cpp

---
+++ Math/sinh.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+const double E = 2.71828;
+
+double sinh(int y)
+{
+	double temp, e1, e2;
+
+	if (y == 0)
+		return 1;
+
+	temp = sinh( y / 2);
+
+	if (y % 2 == 0)
+	{
+		e1 = temp * temp;
+		e2 = 1 / temp * temp;
+		return e1 - e2 / 2;
+	}
+	else
+	{
+		if (y > 0)
+		{
+			e1 = E*temp * temp;
+			e2 = 1 / E*temp * temp;
+			return e1 - e2 / 2;
+		}
+		else
+		{
+			e1 = (temp*temp) / E;
+				e2 = 1/((temp*temp) / E);
+			return e1 - e2 / 2;
+		}
+	}
+}
+
+
+int main()
+{
+	double a, result;
+
+	cout << "This is the sinh(x).\nEnter the value of x: ";
+	cin >> a ;
+
+	cout << sinh(a);
+
+	return 0;
+}
