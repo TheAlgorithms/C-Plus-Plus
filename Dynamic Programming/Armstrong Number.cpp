cd06b34460034e2ace34e2f36cb3319cc4036432
travellers <828180@gmail.com>
2019-07-07 15:38:11 +0800

    Update Armstrong Number.cpp
    So any positive integer can be checked.

--- Dynamic Programming/Armstrong Number.cpp
+++ Dynamic Programming/Armstrong Number.cpp
@@ -1,20 +1,47 @@
 //program to check whether a number is an armstrong number or not
-#include<iostream.h>
+#include<iostream>
 #include<Math.h>
+using namespace std;
+
 int main()
 {
-  int n,k,d,s=0;
-  cout<<"Enter a number:";
-  cin>>n;
-  k=n;
-  while(k!=0)
-  {
-    d=k%10;
-    s+=(int)pow(d,3);
-    k/=10;
-  }
+	int n, tmp, numPlace=1, k, s=0, unit;
+	cout << "Enter a decimal number: ";
+	cin >> n;
+	if(n<=0)
+    {
+        cout << "n must > 0";
+        return -1;
+    }else if(n<10)
+    {
+		cout << n << " is an armstrong number!";
+        return 0;
+    }
+
+    tmp = n;
+    while(1)
+    {
+        tmp /= 10;
+        if(tmp != 0)
+        {
+            numPlace++;
+        }else
+        {
+            break;
+        }
+    }
+
+    tmp = n;
+    while(tmp>0)
+    {
+        unit = tmp % 10;
+        s += (int)pow(unit, numPlace);
+        tmp /= 10;
+    }
+
   if(s==n)
-   cout<<n<<"is an armstrong number";
+   cout<<n<<" is an armstrong number";
   else
-   cout<<n<<"is not an armstrong number";
+   cout<<n<<" is not an armstrong number";
 }
+
