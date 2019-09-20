0cd9ace6772d1966e7f290f98a82e672da4228ea
shubham mouar <53973064+mouar@users.noreply.github.com>
2019-09-21 01:10:06 +0530

    create randomised_quicksort
    this sorting is faster than other sorting

---
+++ Sorting/candomised_quicksort.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<stdlib.h>
+#include<time.h>
+using namespace std;
+int partition(int *a,int start,int end)
+{
+    int pindex=0;
+    int pivot=a[end];
+    for(int i=0;i<=end-1;i++)
+    {
+        if(a[i]<=pivot)
+        {
+            swap(a[i],a[pindex]);
+            pindex=pindex+1;
+        }
+    }
+    swap(a[pindex],a[end]);
+    return pindex;
+}
+int random_partition(int *a,int start,int end) //it will choose the random element as pivot element
+{
+    int r;
+    srand(time(NULL));
+    r=start+rand()%(end - start);
+    swap(a[end],a[r]);
+    return partition(a,start,end);
+}
+
+void quicksort(int *a,int start,int end)
+{
+    if(start<end)
+    {
+        int pindex=random_partition(a,start,end);
+        quicksort(a,start,pindex-1);
+        quicksort(a,pindex+1,end);
+    }
+}
+
+int main()
+{
+    int i,n;
+    cout<<"ENTER THE SIZE OF LIST YOU WANT TO SORT : "<<endl;
+    cin>>n;
+    int a[n];
+    cout<<"ENTER ELEMENTS"<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    quicksort(a,0,n-1);
+    cout<<"THE SORTED LIST IS: "<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cout<<" "<<a[i];
+    }
+    return 0;
+}
