ed2ca024994a5987638e1619afd2664d37de2d75
Aayush Srivastava <49818988+srivastavaayu@users.noreply.github.com>
2019-10-03 18:17:41 +0530

    Update Insertion Sort
    Use dynamic memory allocation to create an array of size N, then ask the user to input the array and sort the dynamically allocated array with insertion sort.

--- Sorting/Insertion Sort.cpp
+++ Sorting/Insertion Sort.cpp
@@ -1,40 +1,39 @@
-//Insertion Sort
-
-#include <iostream>
+#include<iostream>
+#include<cstdlib>
 using namespace std;
-
 int main()
 {
-	int n;
-	cout << "\nEnter the length of your array : ";
-	cin >> n;
-	int Array[n];
-	cout << "\nEnter any " << n << " Numbers for Unsorted Array : ";
-
-	//Input
-	for (int i = 0; i < n; i++)
-	{
-		cin >> Array[i];
-	}
-
-	//Sorting
-	for (int i = 1; i < n; i++)
-	{
-		int temp = Array[i];
-		int j = i - 1;
-		while (j >= 0 && temp < Array[j])
-		{
-			Array[j + 1] = Array[j];
-			j--;
-		}
-		Array[j + 1] = temp;
-	}
-
-	//Output
-	cout << "\nSorted Array : ";
-	for (int i = 0; i < n; i++)
-	{
-		cout << Array[i] << "\t";
-	}
-	return 0;
+    int n,i,j,mini,temp;
+    int * arr;
+    cout<<"Enter the size of array you want to create: ";
+    cin>>n;
+    arr=new int(n*sizeof(int));
+    for(i=0;i<n;i++)
+    {
+        cout<<"Enter element "<<i+1<<": ";
+        cin>>*(arr+i);
+    }
+    cout<<"\nThe array before insertion sort is: ";
+    for(i=0;i<n;i++)
+    {
+        cout<<*(arr+i)<<"   ";
+    }
+    for(i=0;i<n;i++)
+    {
+        mini=i;
+        for(j=i+1;j<n;j++)
+        {
+            if((*(arr+j))<(*(arr+mini)))
+                mini=j;
+        }
+        temp=(*(arr+mini));
+        *(arr+mini)=*(arr+i);
+        *(arr+i)=temp;
+    }
+    cout<<"\nThe array after insertion sort is: ";
+    for(i=0;i<n;i++)
+    {
+        cout<<*(arr+i)<<"   ";
+    }
+    return 0;
 }
