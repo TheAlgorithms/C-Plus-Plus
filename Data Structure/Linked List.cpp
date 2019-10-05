013118a0093236cce5749cdf372bd110879790b1
manolo esparza <manoloesparta@gmail.com>
2019-10-04 20:09:04 -0700

    Replaced struct implementation with a class one, less like C more like C++ code

--- Data Structure/Linked List.cpp
+++ Data Structure/Linked List.cpp
@@ -1,158 +1,136 @@
 #include <iostream>
-using namespace std;
+#include <string>
 
-struct node
+class Node
 {
-	int val;
-	node *next;
+public:
+    int val;
+    Node* next;
+
+    Node(int val)
+    {
+        this->val = val;
+    }
 };
 
-node *start;
-
-void insert(int x)
+class LinkedList
 {
-	node *t = start;
-	if (start != NULL)
-	{
-		while (t->next != NULL)
-		{
-			t = t->next;
-		}
-		node *n = new node;
-		t->next = n;
-		n->val = x;
-		n->next = NULL;
-	}
-	else
-	{
-		node *n = new node;
-		n->val = x;
-		n->next = NULL;
-		start = n;
-	}
-}
+public:
+    Node* root;
+    int size;
 
-void remove(int x)
+    LinkedList(int val)
+    {
+        root = new Node{ val };
+    }
+
+    void insert(int val)
+    {
+        if(root == nullptr)
+        {
+            root = new Node{ val };
+            return;
+        }
+
+        Node* temp = root;
+
+        while(temp->next != nullptr)
+            temp = temp->next;
+
+        temp->next = new Node{ val };
+    }
+
+    void remove(int val)
+    {
+        Node* temp = root;
+        Node* prev;
+
+        if(temp != nullptr && temp->val == val)
+        {
+            root = temp->next;
+            return;
+        }
+
+        while(temp != nullptr && temp->val != val)
+        {
+            prev = temp;
+            temp = temp->next;
+        }
+
+        if(temp == nullptr)
+            return;
+
+        prev->next = temp->next;
+    }
+
+    Node* search(int val)
+    {
+        if(root->val == val)
+            return root;
+
+        Node* temp = root;
+        while(temp->next != nullptr)
+        {
+            if(temp->val == val)
+                return temp;
+            temp = temp->next;
+        }
+    }
+
+    void print()
+    {
+        Node* temp = root;
+        while(temp != nullptr)
+        {
+            std::cout << temp->val << " ";
+            temp = temp->next;
+        }
+    }
+};
+
+void print_menu()
 {
-
-	if (start == NULL)
-	{
-		cout << "\nLinked List is empty\n";
-		return;
-	}
-	else if (start->val == x)
-	{
-		node *temp = start;
-		start = start->next;
-		delete temp;
-		return;
-	}
-
-	node *temp = start, *parent = start;
-
-	while (temp != NULL && temp->val != x)
-	{
-		parent = temp;
-		temp = temp->next;
-	}
-
-	if (temp == NULL)
-	{
-		cout << endl
-			 << x << " not found in list\n";
-		return;
-	}
-
-	parent->next = temp->next;
-	delete temp;
-}
-
-void search(int x)
-{
-	node *t = start;
-	int found = 0;
-	while (t != NULL)
-	{
-		if (t->val == x)
-		{
-			cout << "\nFound";
-			found = 1;
-			break;
-		}
-		t = t->next;
-	}
-	if (found == 0)
-	{
-		cout << "\nNot Found";
-	}
-}
-
-void show()
-{
-	node *t = start;
-	while (t != NULL)
-	{
-		cout << t->val << "\t";
-		t = t->next;
-	}
-}
-
-void reverse()
-{
-	node *first = start;
-	node *second = first->next;
-	while (second != NULL)
-	{
-		node *tem = second->next;
-		second->next = first;
-		first = second;
-		second = tem;
-	}
-
-	start->next = NULL;
-	start = first;
+    std::cout << "\n1. Insert";
+    std::cout << "\n2. Delete" ;
+    std::cout << "\n3. Search";
+    std::cout << "\n4. Print" ;
+    std::cout << "\n0. Exit" ;
+    std::cout << "\n\nEnter you choice : " ;
 }
 
 int main()
 {
-	int choice, x;
+
+    int init;
+    std::cout << "Initial value: ";
+    std::cin >> init;
+    LinkedList ll{ init };
+
+
+	int choice, target;
 	do
 	{
-		cout << "\n1. Insert";
-		cout << "\n2. Delete";
-		cout << "\n3. Search";
-		cout << "\n4. Print";
-		cout << "\n5. Reverse";
-		cout << "\n0. Exit";
-		cout << "\n\nEnter you choice : ";
-		cin >> choice;
+        print_menu();
+        std::cin >> choice;
 		switch (choice)
 		{
 		case 1:
-			cout << "\nEnter the element to be inserted : ";
-			cin >> x;
-			;
-			insert(x);
+            std::cout << "\nEnter the element to be inserted : ";
+			std::cin >> target;
+            ll.insert(target);
 			break;
 		case 2:
-			cout << "\nEnter the element to be removed : ";
-			cin >> x;
-			remove(x);
+            std::cout << "\nEnter the element to be removed : ";
+			std::cin >> target;
+            ll.remove(target);
 			break;
 		case 3:
-			cout << "\nEnter the element to be searched : ";
-			cin >> x;
-			search(x);
+            std::cout << "\nEnter the element to be searched : ";
+			std::cin >> target;
 			break;
 		case 4:
-			show();
-			cout << "\n";
-			break;
-		case 5:
-			cout << "The reversed list: \n";
-			reverse();
-			show();
-			cout << "\n";
+            ll.print();
+            std::cout << "\n";
 			break;
 		}
 	} while (choice != 0);
