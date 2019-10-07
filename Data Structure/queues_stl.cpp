c83a27312a9c4987def828fc3f8cc79dee9988ef
Yogesh Nimangre <31822326+yogeshnimangre007@users.noreply.github.com>
2019-10-07 15:58:49 +0530

    create queues_stl.cpp
    this code shows how we can implement queues using stl in an easy way

---
+++ Data Structure/queues_stl.cpp
@@ -0,0 +1,37 @@
+
+#include <iostream> 
+#include <queue> 
+  
+using namespace std; 
+  
+void showq(queue <int> gq) 
+{ 
+    queue <int> g = gq; 
+    while (!g.empty()) 
+    { 
+        cout << '\t' << g.front(); 
+        g.pop(); 
+    } 
+    cout << '\n'; 
+} 
+  
+int main() 
+{ 
+    queue <int> gquiz; 
+    gquiz.push(10); 
+    gquiz.push(20); 
+    gquiz.push(30); 
+  
+    cout << "The queue gquiz is : "; 
+    showq(gquiz); 
+  
+    cout << "\ngquiz.size() : " << gquiz.size(); 
+    cout << "\ngquiz.front() : " << gquiz.front(); 
+    cout << "\ngquiz.back() : " << gquiz.back(); 
+  
+    cout << "\ngquiz.pop() : "; 
+    gquiz.pop(); 
+    showq(gquiz); 
+  
+    return 0; 
+} 
