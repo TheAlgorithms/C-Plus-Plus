c1dcc6fc657365a0d2ef0b4d3d7c315104afc452
akshay-gupta123 <56124489+akshay-gupta123@users.noreply.github.com>
2019-10-10 06:41:07 -0700

    update to dynamic sorting using pointer
    use dynamic allocation to take as many as user wants and to prevent memory leak by using free

--- Sorting/Counting_Sort.cpp
+++ Sorting/Counting_Sort.cpp
@@ -1,50 +1,36 @@
 #include <iostream>
+#include<stdlib.h>  // for malloc
 using namespace std;
-
-int Max(int Arr[], int N)
-{
-	int max = Arr[0];
-	for (int i = 1; i < N; i++)
-		if (Arr[i] > max)
-			max = Arr[i];
-	return max;
-}
-
-int Min(int Arr[], int N)
-{
-	int min = Arr[0];
-	for (int i = 1; i < N; i++)
-		if (Arr[i] < min)
-			min = Arr[i];
-	return min;
-}
 
 void Print(int Arr[], int N)
 {
 	for (int i = 0; i < N; i++)
-		cout << Arr[i] << ", ";
+	{
+		
+	    if(i!=(N-1))   // not to print last comma
+		cout << *(Arr+i) << ", ";
+		else
+		cout << *(Arr+i) ;
+	}
 }
 
 int *Counting_Sort(int Arr[], int N)
 {
 
-	int max = Max(Arr, N);
-	int min = Min(Arr, N);
-	int *Sorted_Arr = new int[N];
-
-	int *Count = new int[max - min + 1];
-
-	for (int i = 0; i < N; i++)
-		Count[Arr[i] - min]++;
-
-	for (int i = 1; i < (max - min + 1); i++)
-		Count[i] += Count[i - 1];
-
-	for (int i = N - 1; i >= 0; i--)
-	{
-		Sorted_Arr[Count[Arr[i] - min] - 1] = Arr[i];
-		Count[Arr[i] - min]--;
-	}
+	for(int i=0;i<N;i++)
+    {
+        for(int j=0;j<i;j++)
+        {
+            if(*(Arr+i)>*(Arr+j))
+            { 
+                int temp;
+                temp=*(Arr+i);
+                *(Arr+i)=*(Arr+j);
+                *(Arr+j)=temp;
+            }
+        }
+    }
+    int *Sorted_Arr=Arr;
 
 	return Sorted_Arr;
 }
@@ -52,14 +38,40 @@ int *Counting_Sort(int Arr[], int N)
 int main()
 {
 
-	int Arr[] = {47, 65, 20, 66, 25, 53, 64, 69, 72, 22, 74, 25, 53, 15, 42, 36, 4, 69, 86, 19}, N = 20;
-	int *Sorted_Arr;
+	int  n ;
+	cout<<"\tEnter Number Of Array Elements\t ";
+	cin>>n;
+	
+	int *Arr=(int*)malloc(sizeof(int)*n);
+	cout<<"\tEnter The Array :  \n";
+	for(int i=0;i<n;i++)
+	{
+	 cout<<"\t\t\t ";	
+	 cin>>*(Arr+i);
+    }
+	
+	int *Sorted_Arr=(int*)malloc(sizeof(int)*n);
 
 	cout << "\n\tOrignal Array = ";
-	Print(Arr, N);
-	Sorted_Arr = Counting_Sort(Arr, N);
-	cout << "\n\t Sorted Array = ";
-	Print(Sorted_Arr, N);
+	Print(Arr, n);
+	
+	Sorted_Arr = Counting_Sort(Arr, n);
+	cout << "\n\t Sorted Array(In Dcreasing Order) = ";
+	Print(Sorted_Arr, n);
+	
+	cout << "\n\t Sorted Array(In Inceasing Order) = ";
+	for (int i = n-1; i >= 0; i--)
+	{
+		
+	    if(i!=0)   // not to print last comma
+		cout << *(Arr+i) << ", ";
+		else
+		cout << *(Arr+i) ;
+	}
+	
+	free(Arr);
+	free(Sorted_Arr);
+	
 	cout << endl;
 
 	return 0;
