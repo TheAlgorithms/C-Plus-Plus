3fc0038f961434cfa804b5041baf88f21cdeebf4
akshay-gupta123 <56124489+akshay-gupta123@users.noreply.github.com>
2019-10-11 00:22:49 -0700

    added prime factorization using recursion

---
+++ Others/prime factorization.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+int prime(int i);
+int sum=0,product=1,k;     //sum is used for not printing last comma
+int main()
+{
+	int n;
+	cout<<"\t Enter A Number ";
+	cin>>n;
+	k=n;
+	cout<<"\tPrime Factorization Of "<<n<<" = ";
+	prime(n);
+	return 0;
+}
+int prime(int n)
+{   
+	for(int i=2;i<=n;i++)
+	{    
+	    int flag=1;  //to check whether a no. is prime or not
+		for(int j=2;j<i;j++)
+		{
+			if(i%j==0)
+			{
+			  flag=0;
+			  break;	
+			}
+			
+		}
+		if(flag==1&&n%i==0&&product!=k)
+		{  
+			if(n%i==0&&sum==0)   //for not printing last comma
+			{
+			  cout<<i;
+			  sum++;
+		    }
+		   else if(n%i==0&&sum!=0)
+			{
+			  cout<<" * "<<i;
+			  sum++;
+		    }
+		    product*=i;
+		    prime(n/i);
+		}
+		
+	}
+  
+  return 0; 
+}
