c1a875081b1ddfe8495b1f27a9077255b0e6bb47
naman011 <56451068+naman011@users.noreply.github.com>
2019-10-11 23:27:40 +0530

    Created Prim.cpp

---
+++ Graph/Prim.cpp
@@ -0,0 +1,40 @@
+//Logic: https://www.hackerearth.com/practice/algorithms/graphs/minimum-spanning-tree/tutorial/
+
+int dist[N], parent[N];
+bool vis[N];
+vector<pair<int, int> > g[N], tree[N];
+
+int primsMST(int source) //Finds the cost and makes the MST
+{
+	for(int i=1;i<=n;i++)
+		dist[i]=1e18;
+	set<pair<int, int> > s;
+	s.insert({0, source});
+	int cost=0;
+	dist[source]=0;
+	while(!s.empty())
+	{
+		auto x = *(s.begin());
+		s.erase(x);
+		vis[x.second]=1;
+		cost+=x.first;
+		int u=x.second;
+		int v=parent[x.second];
+		int w=x.first;
+		tree[u].push_back({v, w});
+		tree[v].push_back({u, w});
+		for(auto it:g[x.second])
+		{
+			if(vis[it.first])
+				continue;
+			if(dist[it.first] > it.second)
+			{
+				s.erase({dist[it.first], it.first});
+				dist[it.first]=it.second;
+				s.insert({dist[it.first], it.first});
+				parent[it.first]=x.second;
+			}
+		}
+	}
+	return cost;
+}
