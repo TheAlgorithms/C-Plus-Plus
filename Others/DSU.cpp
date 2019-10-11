99495a7b74e4b4608eba2f73a5e1ec13ba77f5a2
amangupta01 <amangupta151299@gmail.com>
2019-10-11 22:26:08 +0530

    Created Disjoint Set Union.cpp

---
+++ Others/DSU.cpp
@@ -0,0 +1,95 @@
+Without Struct:
+
+int connected;
+int root[N], sz[N];
+
+void init()
+{
+	for(int i=1;i<=n;i++)
+	{
+		root[i]=i;
+		sz[i]=1;
+	}
+	connected=n;
+}
+
+int rt(int k)
+{
+	while(k!=root[k])
+	{
+		root[k]=root[root[k]];
+		k=root[k];
+	}
+	return k;
+}
+
+void merge(int u, int v)
+{
+	int rt1=rt(u);
+	int rt2=rt(v);
+
+	if(rt1==rt2)
+		return;
+
+	connected--;
+
+	if(sz[rt1]>sz[rt2])
+		swap(rt1, rt2);
+
+	sz[rt2]+=sz[rt1];
+	sz[rt1]=0;
+	root[rt1]=root[rt2];
+}
+
+---
+ 
+Struct Implementation:
+
+struct DSU
+{
+	int connected;
+	int par[N], sz[N];
+
+	DSU() {} 
+
+	DSU(int n) 
+	{
+		for(int i=1;i<=n;i++)
+		{
+			par[i]=i;
+			sz[i]=1;
+		}
+		connected=n;
+	}
+
+	int getPar(int k)
+	{
+		while(k!=par[k])
+		{
+			par[k]=par[par[k]];
+			k=par[k];
+		}
+		return k;
+	}
+
+	int getSize(int k)
+	{
+		return sz[getPar(k)];
+	}
+
+	void unite(int u, int v)
+	{
+		int par1=getPar(u), par2=getPar(v);
+
+		if(par1==par2)
+			return;
+
+		connected--;
+
+		if(sz[par1]>sz[par2])
+			swap(par1, par2);
+
+		sz[par2]+=sz[par1];
+		sz[par1]=0;
+		par[par1]=par[par2];
+	}
