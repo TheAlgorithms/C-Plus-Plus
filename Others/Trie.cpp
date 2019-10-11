c89e231e1c2f66707bbea56eca9d99079413e0ed
naman011 <56451068+naman011@users.noreply.github.com>
2019-10-11 23:20:43 +0530

    Created Trie.cpp

---
+++ Others/Trie.cpp
@@ -0,0 +1,49 @@
+typedef struct data
+{
+	data* bit[2];
+	int cnt = 0;
+}trie;
+
+trie* head;
+
+void insert(int x)
+{
+	trie* cur = head;
+	for(int i=30;i>=0;i--)
+	{
+		int b = (x>>i) & 1;
+		if(!cur->bit[b])
+			cur->bit[b] = new trie();
+		cur = cur->bit[b];
+		cur->cnt++;
+	}
+}
+
+void remove(int x)
+{
+	trie* cur = head;
+	for(int i=30;i>=0;i--)
+	{
+		int b = (x>>i) & 1;
+		cur = cur->bit[b];
+		cur->cnt--;
+	}
+}
+
+int maxxor(int x)
+{
+	trie* cur = head;
+	int ans = 0;
+	for(int i=30;i>=0;i--)
+	{
+		int b = (x>>i)&1;
+		if(cur->bit[!b] && cur->bit[!b]->cnt>0)
+		{
+			ans += (1LL<<i);
+			cur = cur->bit[!b];
+		}
+		else
+			cur = cur->bit[b];
+	}
+	return ans;
+}
