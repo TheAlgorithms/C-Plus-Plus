6c38fa861bd05640902dcf90c2359603f180f4f2
SURYANSH SINGH RATHORE <rathoresuryansh196@gmail.com>
2019-10-13 22:27:20 +0530

    Update PriorityQueue.cpp

--- Data Structure/PriorityQueue.cpp
+++ Data Structure/PriorityQueue.cpp
@@ -32,6 +32,17 @@ int main ()
     cout << "\n pq.pop() : "; 
     pq.pop(); 
     printpq(pq); 
-  
+    
+    //OUTPUT:
+    //The priority queue pq is: 30 20 10 5 1
+    //
+    //pq.size() : 5
+    //pq.top() : 30
+    //pq.pop() : 20 10 5 1
+    //
+    //By default priority_queue in STL is used to implement max heap. To use priority queue in STL as min heap we need to add extra arguments
+    // : priority_queue< data_type, vector<data_type>, compare_type >pq;
+    // ex: priority_queue<int, vector<int>, greater<int> >pq;
+    
     return 0; 
 } 
