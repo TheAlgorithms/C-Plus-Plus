7023161f8eb6b6ff0209dc30aa1e5b57b87020e9
pjkajithkumar <pjkajithkumar@gmail.com>
2019-10-13 12:55:07 +0530

    insertion sort method

---
+++ Sorting/insertnew.cpp
@@ -0,0 +1,39 @@
+
+#include <iostream>
+using namespace std;
+
+void main()
+{
+	int n;
+	cout << "\nEnter the length of ARRAY : ";
+	cin >> n;
+	int Array[n];
+	cout << "\nEnter any " << n << " Numbers for Unsorted Array : ";
+
+	//Input
+	for (int i = 0; i < n; i++)
+	{
+		cin >> Array[i];
+	}
+
+	//Sorting
+	for (int i = 1; i < n; i++)
+	{
+		int temp = Array[i];
+		int j = i - 1;
+		while (j >= 0 && temp < Array[j])
+		{
+			Array[j + 1] = Array[j];
+			j--;
+		}
+		Array[j + 1] = temp;
+	}
+
+	//Output
+	cout << "\nThe Sorted Array is : ";
+	for (int i = 0; i < n; i++)
+	{
+		cout << Array[i] << "\t";
+	}
+	
+}
