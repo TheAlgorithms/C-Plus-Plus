7f8f538f2b71083eb77eb673a31545a2fb507e9b
shubhamdamani <damani.shubham4@gmail.com>
2019-10-15 23:33:39 +0530

    bridges algo

---
+++ Graph/find_bridges.cpp
@@ -0,0 +1,54 @@
+#include <queue>
+#include <vector>
+#include <cstdio>
+#include <iostream>
+using namespace std;
+#define INF 10000010
+
+int n; // number of nodes
+vector<vector<int>> adj; // adjacency list of graph
+
+vector<bool> visited;
+vector<int> tin, low;
+int timer;
+
+void dfs(int v, int p = -1) {
+    visited[v] = true;
+    tin[v] = low[v] = timer++;
+    for (int to : adj[v]) {
+        if (to == p) continue;
+        if (visited[to]) {
+            low[v] = min(low[v], tin[to]);
+        } else {
+            dfs(to, v);
+            low[v] = min(low[v], low[to]);
+            if (low[to] > tin[v])
+                cout<<v<<" "<<to<<endl;
+        }
+    }
+}
+
+void find_bridges() {
+    timer = 0;
+    visited.assign(n, false);
+    tin.assign(n, -1);
+    low.assign(n, -1);
+    for (int i = 0; i < n; ++i) {
+        if (!visited[i])
+            dfs(i);
+    }
+}
+
+int main()
+{
+	int m;
+	cin>>m;
+	for(int i=0;i<m;i++)
+	{
+		int a,b;
+		cin>>a>>b;
+		adj[a].push_back(b);
+		adj[b].push_back(a);
+	}
+	find_bridges();
+}
