3600d7cb42af73a7cf6f095f7c41f0baad64f497
Shubham Gupta <guptashubham8100@gmail.com>
2019-10-21 01:26:59 +0530

    Added Subset Sum Problem Solution

---
+++ Dynamic Programming/Subset Sum.cpp
@@ -0,0 +1,43 @@
+/*
+
+A recursive solution for subset sum problem 
+
+Note:
+
+In the worst case the solution will try all possible subsets. Hence time complexity is exponential.
+The problem is NP-Complete i.e. there is no known polynomial time solution for this problem.
+*/
+
+#include <iostream> 
+
+bool isSubsetSum(int set[], int n, int sum) 
+{ 
+    
+   if (sum == 0) 
+     return true; 
+   if (n == 0 && sum != 0) 
+     return false; 
+  
+   // If last element is greater than sum, then ignore it 
+   if (set[n-1] > sum) 
+     return isSubsetSum(set, n-1, sum); 
+  
+   /* else, check if sum can be obtained by any of the following 
+      (a) including the last element 
+      (b) excluding the last element   */
+   return isSubsetSum(set, n-1, sum) ||  
+                        isSubsetSum(set, n-1, sum-set[n-1]); 
+} 
+  
+// Driver program to test above function 
+int main() 
+{ 
+  int set[] = {3, 34, 4, 12, 5, 2}; 
+  int sum = 9; 
+  int n = sizeof(set)/sizeof(set[0]); 
+  if (isSubsetSum(set, n, sum) == true) 
+     printf("Found a subset with given sum"); 
+  else
+     printf("No subset with given sum"); 
+  return 0; 
+}
