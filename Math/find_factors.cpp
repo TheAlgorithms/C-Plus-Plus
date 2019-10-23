7f23b6c308dd9df28abf9800f41f49c43f06f895
kunal kumar barman <kumar96kunal@gmail.com>
2019-10-23 06:32:51 +0530

    Find factors of any number in Osqrt(n).

---
+++ Math/find_factors.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+       int n = 9;
+       vector<int> vi[1000];
+       for(int i=1;i<=sqrt(n);i++)
+       {
+              if(n%i==0)
+              {
+                  if(i!=sqrt(n))
+                  {
+                      vi[n].push_back(i);
+                      vi[n].push_back(n/i);
+                  }
+                  else
+                  {
+                       vi[n].push_back(i);
+                  }
+                  
+              }
+       }
+       cout<<n<<" -- ";
+       for(auto i=vi[n].begin();i!=vi[n].end();i++)
+            cout<<*i<<" ";
+}
+
+/*
+9 -- 1 9 3 
+*/
