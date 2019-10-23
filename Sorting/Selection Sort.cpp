7b78954cc2a2781c7566f0f3291ae1280c9d007a
Sai-nook73 <43719535+Sai-nook73@users.noreply.github.com>
2019-10-23 08:13:47 +0530

    Update Selection Sort.cpp

--- Sorting/Selection Sort.cpp
+++ Sorting/Selection Sort.cpp
@@ -4,35 +4,36 @@
 using namespace std;
 
 int main()
-{
-	int Array[6];
-	cout << "\nEnter any 6 Numbers for Unsorted Array : ";
+{	
+	int n;
+	cout << "/nEnter the length of array :";
+	cin >> n;
+	int Array[n];
+	cout << "\nEnter Numbers for Unsorted Array : ";
 
 	//Input
-	for (int i = 0; i < 6; i++)
+	for (int i = 0; i < n; i++)
 	{
 		cin >> Array[i];
 	}
 
 	//Selection Sorting
-	for (int i = 0; i < 6; i++)
+	for (int i = 0; i < n; i++)
 	{
-		int min = i;
-		for (int j = i + 1; j < 6; j++)
+		for (int j = i + 1; j < n; j++)
 		{
-			if (Array[j] < Array[min])
+			if (Array[j] < Array[i])
 			{
-				min = j; //Finding the smallest number in Array
+				int temp = Array[j];
+				Array[j] = Array[i];
+				Array[i] = temp;
 			}
 		}
-		int temp = Array[i];
-		Array[i] = Array[min];
-		Array[min] = temp;
 	}
 
 	//Output
 	cout << "\nSorted Array : ";
-	for (int i = 0; i < 6; i++)
+	for (int i = 0; i < n; i++)
 	{
 		cout << Array[i] << "\t";
 	}
