53b70a2b068905c5d9dd0293fbb53913c0ec608e
downfall3597 <shubham24356789@gmail.com>
2019-10-24 20:38:47 +0530

    added queue using stacks

---
+++ Data Structure/queueUsingStack.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Stack
+{
+    queue<int> q1, q2;
+
+    int curr_size;
+
+public:
+    Stack()
+    {
+        curr_size = 0;
+    }
+
+    void push(int x)
+    {
+        curr_size++;
+
+        q2.push(x);
+
+        while (!q1.empty())
+        {
+            q2.push(q1.front());
+            q1.pop();
+        }
+
+        queue<int> q = q1;
+        q1 = q2;
+        q2 = q;
+    }
+
+    void pop()
+    {
+
+        if (q1.empty())
+            return;
+        q1.pop();
+        curr_size--;
+    }
+
+    int top()
+    {
+        if (q1.empty())
+            return -1;
+        return q1.front();
+    }
+
+    int size()
+    {
+        return curr_size;
+    }
+};
+
+int main()
+{
+    Stack s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+
+    cout << "current size: " << s.size()
+         << endl;
+    cout << s.top() << endl;
+    s.pop();
+    cout << s.top() << endl;
+    s.pop();
+    cout << s.top() << endl;
+
+    cout << "current size: " << s.size()
+         << endl;
+    return 0;
+}
