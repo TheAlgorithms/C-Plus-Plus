20973aaf13143776d478817413b67fc10077da30
Ribhav031101 <karthik.ruchi@gmail.com>
2019-10-24 20:11:24 +0530

    Selection Sort Initial Commit

---
+++ goodsorting.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+    int i,j,n,loc,temp,min,a[30];
+    cout<<"Enter the number of elements you want in your list:";
+    cin>>n;
+    cout<<"\nEnter the elements\n";
+ 
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+ 
+    for(i=0;i<n-1;i++)
+    {
+        min=a[i];
+        loc=i;
+        for(j=i+1;j<n;j++)
+        {
+            if(min>a[j])
+            {
+                min=a[j];
+                loc=j;
+            }
+        }
+ 
+        temp=a[i];
+        a[i]=a[loc];
+        a[loc]=temp;
+    }
+ 
+    cout<<"\nSorted list is as follows\n";
+    for(i=0;i<n;i++)
+    {
+        cout<<a[i]<<" ";
+    }
+ 
+    return 0;
+}
