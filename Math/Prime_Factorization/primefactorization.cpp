769769e56f964a2066426658556521ed35c44742
Baranixowy <56301157+Baranixowy@users.noreply.github.com>
2019-11-19 12:43:05 +0100

    Update primefactorization.cpp
    Prime factor finder with O(sqrt(n)) time complexity

--- Math/Prime_Factorization/primefactorization.cpp
+++ Math/Prime_Factorization/primefactorization.cpp
@@ -1,81 +1,41 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <cmath>
 using namespace std;
 
-// Declaring variables for maintaing prime numbers and to check whether a number is prime or not
-bool isprime[1000006];
-vector<int> prime_numbers;
-vector<pair<int, int>> factors;
-
-// Calculating prime number upto a given range
-void SieveOfEratosthenes(int N)
+void primefactors (int n)
 {
-    // initializes the array isprime
-    memset(isprime, true, sizeof isprime);
+    int count = 0;
 
-    for (int i = 2; i <= N; i++)
-    {
-        if (isprime[i])
-        {
-            for (int j = 2 * i; j <= N; j += i)
-                isprime[j] = false;
-        }
+    while (!(n % 2)) {
+        n = n >> 1;
+        count++;
     }
 
-    for (int i = 2; i <= N; i++)
-    {
-        if (isprime[i])
-            prime_numbers.push_back(i);
-    }
-}
+    if (count)
+        cout << 2 << "  " << count << endl;
 
-// Prime factorization of a number
-void prime_factorization(int num)
-{
-
-    int number = num;
-
-    for (int i = 0; prime_numbers[i] <= num; i++)
-    {
-        int count = 0;
-
-        // termination condition
-        if (number == 1)
-        {
-            break;
-        }
-
-        while (number % prime_numbers[i] == 0)
-        {
+    for (int i = 3; i <= sqrt(n); i += 2) {
+        count = 0;
+        while (n % i == 0) {
             count++;
-            number = number / prime_numbers[i];
+            n = n / i;
         }
-
         if (count)
-            factors.push_back(make_pair(prime_numbers[i], count));
+            cout << i << "  " << count << endl;
     }
+
+    if (n > 2)
+        cout << n << "  " << 1 << endl;
 }
 
-/*
-    I added a simple UI.
-*/
 int main()
 {
-    int num;
+    int number;
     cout << "\t\tComputes the prime factorization\n\n";
     cout << "Type in a number: ";
-    cin >> num;
+    cin >> number;
 
-    SieveOfEratosthenes(num);
 
-    prime_factorization(num);
-
-    // Prime factors with their powers in the given number in new line
-    for (auto it : factors)
-    {
-        cout << it.first << " " << it.second << endl;
-    }
-
+    primefactors(number);
     return 0;
 }
