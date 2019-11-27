cb064e5ce12c22356fd71c0841f9a98abc55a74d
Bhaumik Mistry <Bhaumikpmistry@gmail.com>
2019-11-27 10:07:55 -0500

    Update and rename knight-tour.cpp to knight_tour.cpp
    Add information about the file for brief intro

--- Backtracking/knight_tour.cpp
+++ Backtracking/knight_tour.cpp
@@ -1,5 +1,14 @@
 #include<bits/stdc++.h>
 # define n 8
+
+/**
+A knight's tour is a sequence of moves of a knight on a chessboard
+such that the knight visits every square only once. If the knight 
+ends on a square that is one knight's move from the beginning 
+square (so that it could tour the board again immediately, following 
+the same path), the tour is closed; otherwise, it is open.
+**/
+
 using namespace std;
 bool issafe(int x,int y,int sol[n][n])
 {
