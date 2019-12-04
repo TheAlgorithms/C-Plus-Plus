3c761fac847e926ce847063a8efd5e4445121c7f
Christian Clauss <cclauss@me.com>
2019-12-04 08:55:55 +0100

    Trailing whitespace, std::cin, std::cout, std::endl

--- math/prime_numbers.cpp
+++ math/prime_numbers.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-using namespace std;
 
 vector<int> primes(int max) {
     max++;
@@ -8,7 +7,7 @@ vector<int> primes(int max) {
     vector<bool> numbers(max, false);
     for (int i = 2; i < max; i++) {
         if (!numbers[i]) {
-            for (int j = i; j < max; j += i) 
+            for (int j = i; j < max; j += i)
                 numbers[j] = true;
             res.push_back(i);
         }
@@ -17,11 +16,11 @@ vector<int> primes(int max) {
 }
 
 int main() {
-    cout << "Calculate primes up to:\n>> ";
+    std::cout << "Calculate primes up to:\n>> ";
     int n;
-    cin >> n;
+    std::cin >> n;
     vector<int> ans = primes(n);
-    for (int i = 0; i < ans.size(); i++) 
-        cout << ans[i] << ' ';
-    cout << endl;
+    for (int i = 0; i < ans.size(); i++)
+        std::cout << ans[i] << ' ';
+    std::cout << std::endl;
 }
