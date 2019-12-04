e4e5ee6b9dda79db8aaa7f3e3ff89b666210577a
Christian Clauss <cclauss@me.com>
2019-12-04 08:33:00 +0100

    Update kadane.cpp

--- dynamic_programming/kadane.cpp
+++ dynamic_programming/kadane.cpp
@@ -1,12 +1,10 @@
 #include<iostream>
 #include<climits>
 
-int maxSubArraySum(int a[], int size)
-{
+int maxSubArraySum(int a[], int size) {
     int max_so_far = INT_MIN, max_ending_here = 0;
 
-    for (int i = 0; i < size; i++)
-    {
+    for (int i = 0; i < size; i++) {
         max_ending_here = max_ending_here + a[i];
         if (max_so_far < max_ending_here)
             max_so_far = max_ending_here;
@@ -18,17 +16,15 @@ int maxSubArraySum(int a[], int size)
 }
 
 
-int main()
-{
+int main() {
     int n, i;
     std::cout << "Enter the number of elements \n";
     std::cin >> n;
-    int a[n];
-    for(i=0; i<n; i++)
-    {
-        std::cin >> a[i];
+    int kA[n];
+    for(i = 0; i < n; i++) {
+        std::cin >> kA[i];
     }
-    int max_sum = maxSubArraySum(a, n);
+    int max_sum = maxSubArraySum(kA, n);
     std::cout << "Maximum contiguous sum is " << max_sum;
     return 0;
 }
