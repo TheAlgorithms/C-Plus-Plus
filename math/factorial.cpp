ab53b4bfdc455e8e15cf863ce333560adbc0f850
Christian Clauss <cclauss@me.com>
2019-12-05 22:54:31 +0100

    Update factorial.cpp

--- math/factorial.cpp
+++ math/factorial.cpp
@@ -11,6 +11,7 @@ unsigned int factorial(unsigned int n) {
 // Driver code
 int main() {
     int num = 5;
-    std::cout << "Factorial of " << num << " is " << factorial(num) << std::endl;
+    std::cout << "Factorial of " << num << " is " << factorial(num)
+              << std::endl;
     return 0;
 }
