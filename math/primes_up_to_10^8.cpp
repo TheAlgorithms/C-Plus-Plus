1d59fa791749cd47b39398e2314fa00c5af5070e
Christian Clauss <cclauss@me.com>
2019-12-05 19:42:46 +0100

    Update and rename Math/Primeupto10^8.cpp to math/primes_up_to_10^8.cpp

---
+++ math/primes_up_to_10^8.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include <cstring>
+#define ll long long
+char prime[100000000];
+void Sieve(ll n) {
+    memset(prime, '1', sizeof(prime)); //intitize '1' to every index
+    prime[0] = '0';  // 0 is not prime
+    prime[1] = '0';  // 1 is not prime
+    for (int p = 2; p * p <= n; p++) {
+        if (prime[p] == '1') {
+            for (int i = p * p; i <= n; i += p)  // all multiple of p set to false
+                prime[i] = '0';
+        }
+    }
+}
+
+
+int main() {
+    Sieve(100000000);
+    ll n;
+    std:cin >> n;  // 10006187
+    if (prime[n] == '1')
+        std:cout << "YES\n";
+    else
+        std:cout << "NO\n";
+}
