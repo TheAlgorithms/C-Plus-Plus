41e5290578f2f5f54e88d3b553a38c8fc9bb09ad
Christian Clauss <cclauss@me.com>
2019-12-05 22:42:43 +0100

    Update and rename Math/factorial.cpp to math/factorial.cpp

---
+++ math/factorial.cpp
@@ -0,0 +1,16 @@
+// C++ program to find factorial of given number
+#include<iostream>
+
+// function to find factorial of given number
+unsigned int factorial(unsigned int n) {
+    if (n == 0)
+        return 1;
+    return n * factorial(n - 1);
+}
+
+// Driver code
+int main() {
+    int num = 5;
+    std::cout << "Factorial of " << num << " is " << factorial(num) << endl;
+    return 0;
+}
