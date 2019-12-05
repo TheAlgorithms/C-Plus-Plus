5cee00337859efeb1e0f5dc64945f39ce92564bf
Christian Clauss <cclauss@me.com>
2019-12-05 20:27:08 +0100

    cstdint::int64_t

--- math/primes_up_to_10^8.cpp
+++ math/primes_up_to_10^8.cpp
@@ -3,7 +3,7 @@
 
 char prime[100000000];
 
-void Sieve(int64 n) {
+void Sieve(cstdint::int64_t n) {
     memset(prime, '1', sizeof(prime));  // intitize '1' to every index
     prime[0] = '0';  // 0 is not prime
     prime[1] = '0';  // 1 is not prime
@@ -18,7 +18,7 @@ void Sieve(int64 n) {
 
 int main() {
     Sieve(100000000);
-    int64 n;
+    int64_t n;
     std:cin >> n;  // 10006187
     if (prime[n] == '1')
         std:cout << "YES\n";
