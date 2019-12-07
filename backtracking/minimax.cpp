c0cf8ca99c3e2a29cec237debab0d80db554b2a3
Christian Clauss <cclauss@me.com>
2019-12-07 08:19:09 +0100

    Wrap a long line

--- backtracking/minimax.cpp
+++ backtracking/minimax.cpp
@@ -8,13 +8,14 @@ using std::max;
 using std::min;
 using std::vector;
 
-int minimax(int depth, int node_index, bool is_max, vector<int> scores, int height) {
+int minimax(int depth, int node_index, bool is_max, vector<int> scores,
+            int height) {
     if (depth == height)
         return scores[node_index];
 
     int v1 = minimax(depth + 1, node_index * 2, !is_max, scores, height);
     int v2 = minimax(depth + 1, node_index * 2 + 1, !is_max, scores, height);
-    
+
     return is_max ? max(v1, v2) : min(v1, v2);
 }
 
