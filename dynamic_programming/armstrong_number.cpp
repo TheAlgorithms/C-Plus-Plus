f02bdb73902921f5b4e439bb55e94b0771d53480
Masood Momin <47451964+masoodmomin@users.noreply.github.com>
2019-12-11 17:53:41 +0530

    Showing 153 is not an Armstrong number. (#680)
    * Update Armstrong Number.cpp
    
    * Update Armstrong Number.cpp
    
    * Update Armstrong Number.cpp
    
    * Update Armstrong Number.cpp
    
    * Rename Armstrong Number.cpp to armstrong_number.cpp
    
    * Update armstrong_number.cpp
    
    Showing 153 as not an Armstrong number.
    
    * Update armstrong_number.cpp

--- dynamic_programming/armstrong_number.cpp
+++ dynamic_programming/armstrong_number.cpp
@@ -1,16 +1,17 @@
-//program to check whether a number is an armstrong number or not
-#include <iostream.h>
-#include <Math.h>
-int main()
-{
+// Program to check whether a number is an armstrong number or not
+#include <iostream>
+
+using std::cout;
+using std::cin;
+
+int main() {
   int n, k, d, s = 0;
   cout << "Enter a number:";
   cin >> n;
   k = n;
-  while (k != 0)
-  {
+  while (k != 0) {
     d = k % 10;
-    s += (int)pow(d, 3);
+    s += d * d * d;
     k /= 10;
   }
   if (s == n)
