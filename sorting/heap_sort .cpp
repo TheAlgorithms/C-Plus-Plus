2d0f6e9c5df64c72dcb3daf0b81a0947c86246c7
Eric Curtin <ericcurtin17@gmail.com>
2019-12-16 12:06:28 +0000

    cpplint fixes

--- sorting/heap_sort .cpp
+++ sorting/heap_sort .cpp
@@ -1,61 +1,51 @@
 #include <iostream>
 
-void max_heapify(int *a, int i, int n)
-{
-	int j, temp;
-	temp = a[i];
-	j = 2 * i;
-	while (j <= n)
-	{
-		if (j < n && a[j + 1] > a[j])
-			j = j + 1;
-		if (temp > a[j])
-			break;
-		else if (temp <= a[j])
-		{
-			a[j / 2] = a[j];
-			j = 2 * j;
-		}
-	}
-	a[j / 2] = temp;
-	return;
+void max_heapify(int *a, int i, int n) {
+    int j, temp;
+    temp = a[i];
+    j = 2 * i;
+    while (j <= n) {
+        if (j < n && a[j + 1] > a[j])
+            j = j + 1;
+        if (temp > a[j]) {
+            break;
+        } else if (temp <= a[j]) {
+            a[j / 2] = a[j];
+            j = 2 * j;
+        }
+    }
+    a[j / 2] = temp;
+    return;
 }
-void heapsort(int *a, int n)
-{
-	int i, temp;
-	for (i = n; i >= 2; i--)
-	{
-		temp = a[i];
-		a[i] = a[1];
-		a[1] = temp;
-		max_heapify(a, 1, i - 1);
-	}
+void heapsort(int *a, int n) {
+    int i, temp;
+    for (i = n; i >= 2; i--) {
+        temp = a[i];
+        a[i] = a[1];
+        a[1] = temp;
+        max_heapify(a, 1, i - 1);
+    }
 }
-void build_maxheap(int *a, int n)
-{
-	int i;
-	for (i = n / 2; i >= 1; i--)
-	{
-		max_heapify(a, i, n);
-	}
+void build_maxheap(int *a, int n) {
+    int i;
+    for (i = n / 2; i >= 1; i--) {
+        max_heapify(a, i, n);
+    }
 }
-int main()
-{
-	int n, i, x;
-	std::cout << "Enter number of elements of array\n";
-	std::cin >> n;
-	int a[20];
-	for (i = 1; i <= n; i++)
-	{
-		std::cout << "Enter Element " << (i) << endl;
-		std::cin >> a[i];
-	}
-	build_maxheap(a, n);
-	heapsort(a, n);
-	std::cout << "Sorted Output\n";
-	for (i = 1; i <= n; i++)
-	{
-		std::cout << a[i] << endl;
-	}
-	std::getchar();
+int main() {
+    int n, i, x;
+    std::cout << "Enter number of elements of array\n";
+    std::cin >> n;
+    int a[20];
+    for (i = 1; i <= n; i++) {
+        std::cout << "Enter Element " << (i) << endl;
+        std::cin >> a[i];
+    }
+    build_maxheap(a, n);
+    heapsort(a, n);
+    std::cout << "Sorted Output\n";
+    for (i = 1; i <= n; i++) {
+        std::cout << a[i] << endl;
+    }
+    std::getchar();
 }
