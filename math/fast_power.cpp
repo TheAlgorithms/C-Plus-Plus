5b906f6986b2fad7da6b1e7578ea3624b45fdf5b
Bahadir Altun <ibahadiraltun@gmail.com>
2019-12-26 10:00:00 +0300

    Update rand function

--- math/fast_power.cpp
+++ math/fast_power.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdlib>
 #include <cstdint>
 #include <cassert>
 #include <ctime>
@@ -16,7 +17,7 @@ double fast_power_recursive(int64_t a, int64_t b) {
     // negative power. a^b = 1 / (a^-b)
     if (b < 0)
         return 1.0 / fast_power_recursive(a, -b);
-    
+
     if (b == 0) return 1;
     int64_t bottom = fast_power_recursive(a, b >> 1);
     // Since it is integer division b/2 = (b-1)/2 where b is odd.
@@ -38,7 +39,7 @@ double fast_power_linear(int64_t a, int64_t b) {
     // negative power. a^b = 1 / (a^-b)
     if (b < 0)
         return 1.0 / fast_power_linear(a, -b);
-    
+
     double result = 1;
     while (b) {
         if (b & 1) result = result * a;
@@ -54,19 +55,25 @@ int main() {
 
     std::cout << "Testing..." << std::endl;
     for (int i = 0; i < 20; i++) {
-        int a = rand() % 20 - 10;
-        int b = rand() % 20 - 10;
+        unsigned int *rand1, *rand2;
+        int a = rand_r(rand1) % 20 - 10;
+        int b = rand_r(rand2) % 20 - 10;
         std::cout << std::endl << "Calculating " << a << "^" << b << std::endl;
         assert(fast_power_recursive(a, b) == std::pow(a, b));
         assert(fast_power_linear(a, b) == std::pow(a, b));
-        std::cout << "------ " << a << "^" << b << " = "<< fast_power_recursive(a, b) << std::endl;
+
+        std::cout << "------ " << a << "^" << b << " = "<< 
+            fast_power_recursive(a, b) << std::endl;
     }
 
     int64_t a, b;
     std::cin >> a >> b;
 
-    std::cout << a << "^" << b << " = "<< fast_power_recursive(a, b) << std::endl;
-    std::cout << a << "^" << b << " = "<< fast_power_linear(a, b) << std::endl;
+    std::cout << a << "^" << b << " = "<< 
+        fast_power_recursive(a, b) << std::endl;
+
+    std::cout << a << "^" << b << " = "<< 
+        fast_power_linear(a, b) << std::endl;
 
     return 0;
 }
