5c82e5b1d8a4adf6affed2c262cb25d0d63447bf
Lakshika Parihar <31981299+lakshika1064@users.noreply.github.com>
2020-01-10 03:56:03 +0530

    Update interpolation_search.cpp

--- Search/interpolation_search.cpp
+++ Search/interpolation_search.cpp
@@ -1,18 +1,18 @@
 #include<iostream>
 using namespace std;
 
-//function to search the value in an array using interpolation search
+// function to search the value in an array using interpolation search
 int search(int arr[], int value, int len) {
         int low = 0, high, mid;
         high = len-1;
-        while(arr[low] <= value && arr[high] >= value) {
-                        mid = (low + (( value-arr[low])*(high-low)) / (arr[high]-arr[low]));
-                        if(arr[mid] > value)
-                                high = mid-1;
-                        else if (arr[mid] < value)
-                                low = mid+1;
-                        else
-                                return mid; 
+        while (arr[low] <= value && arr[high] >= value) {
+            mid = (low + ((value-arr[low])*(high-low)) / (arr[high]-arr[low]));
+            if (arr[mid] > value)
+                     high = mid-1;
+            else if (arr[mid] < value)
+                     low = mid+1;
+                 else
+                     return mid; 
          }
          if(arr[low] == value)
                 return low;
@@ -20,21 +20,18 @@ int search(int arr[], int value, int len) {
 }
 
 int main() {
-        int n,value,array[100],re;
-        cout<<"Enter the size of array(less than 100) : ";
-        cin>>n;
-        cout<<"Enter the values of array in ascending (increasing) order : "<<endl;
-	
-        for(int i=0;i<n;i++)
-                cin>>array[i];
-
-        cout<<"Enter the value you want to search : ";
-        cin>>value;
-        re=search(array,value,n);
-
-        if(re==0)
-                cout<<"Entered value is not in the array"<<endl;
+        int n ,value ,array[100] ,re;
+        cout << "Enter the size of array(less than 100) : ";
+        cin >> n;
+        cout << "array in ascending (increasing) order : " << endl;
+        for (int i=0; i<n; i++)
+                cin >> array[i];
+        cout << "Enter the value you want to search : ";
+        cin >> value;
+        re = search(array, value, n);
+        if (re == 0)
+                cout << "Entered value is not in the array" << endl;
         else	
-                cout<<"The value is at the position "<<re<<endl;
+                cout << "The value is at the position " << re << endl;
         return 0;
 }
