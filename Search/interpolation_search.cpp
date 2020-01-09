4a1aba33d48835f9fab1cfe10189e0803cda4cdb
Lakshika Parihar <31981299+lakshika1064@users.noreply.github.com>
2020-01-10 04:13:46 +0530

    Update interpolation_search.cpp

--- Search/interpolation_search.cpp
+++ Search/interpolation_search.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-using namespace std;
+using-namespace std;
 
 // function to search the value in an array using interpolation search
 int search(int arr[], int value, int len) {
@@ -9,24 +9,23 @@ int search(int arr[], int value, int len) {
             mid = (low + ((value-arr[low])*(high-low)) / (arr[high]-arr[low]));
             if (arr[mid] > value)
                      high = mid-1;
-            else { 
-                   if (arr[mid] < value)
+            else if (arr[mid] < value)
                      low = mid+1;
-                   else
-                     return mid; 
-                 }
+            else
+                     return mid;
+            }
         }
         if (arr[low] == value)
                 return low;
         return 0;
 }
 
-void main() {
+int main() {
         int n, value, array[100], re;
         std::cout << "Enter the size of array(less than 100) : ";
         std::cin >> n;
         std::cout << "array in ascending (increasing) order : " << std::endl;
-        for (int i=0; i<n; i++)
+        for (int i=0; i < n; i++)
                 std::cin >> array[i];
         std::cout << "Enter the value you want to search : ";
         std::cin >> value;
@@ -35,4 +34,5 @@ void main() {
                 std::cout << "Entered value is not in the array" << std::endl;
         else
                 std::cout << "The value is at the position " << re << std::endl;
+        return 0;
         }
