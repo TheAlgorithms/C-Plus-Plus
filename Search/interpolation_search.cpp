833a68b4f85b4b6e13af2047a07b159c56755b3b
Lakshika Parihar <31981299+lakshika1064@users.noreply.github.com>
2020-01-10 04:07:41 +0530

    Update interpolation_search.cpp

--- Search/interpolation_search.cpp
+++ Search/interpolation_search.cpp
@@ -9,7 +9,8 @@ int search(int arr[], int value, int len) {
             mid = (low + ((value-arr[low])*(high-low)) / (arr[high]-arr[low]));
             if (arr[mid] > value)
                      high = mid-1;
-            else { if (arr[mid] < value)
+            else { 
+                   if (arr[mid] < value)
                      low = mid+1;
                    else
                      return mid; 
@@ -20,19 +21,18 @@ int search(int arr[], int value, int len) {
         return 0;
 }
 
-int main() {
+void main() {
         int n, value, array[100], re;
-        cout << "Enter the size of array(less than 100) : ";
-        cin >> n;
-        cout << "array in ascending (increasing) order : " << endl;
+        std::cout << "Enter the size of array(less than 100) : ";
+        std::cin >> n;
+        std::cout << "array in ascending (increasing) order : " << std::endl;
         for (int i=0; i<n; i++)
-                cin >> array[i];
-        cout << "Enter the value you want to search : ";
-        cin >> value;
+                std::cin >> array[i];
+        std::cout << "Enter the value you want to search : ";
+        std::cin >> value;
         re = search(array, value, n);
         if (re == 0)
-                cout << "Entered value is not in the array" << endl;
+                std::cout << "Entered value is not in the array" << std::endl;
         else
-                cout << "The value is at the position " << re << endl;
-        return 0;
-}
+                std::cout << "The value is at the position " << re << std::endl;
+        }
