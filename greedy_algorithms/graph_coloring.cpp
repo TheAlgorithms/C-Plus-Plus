dd46011d11e0cb8285e6106930b42b4c4c6ed210
Divide-et-impera-11 <54957167+Divide-et-impera-11@users.noreply.github.com>
2020-01-17 15:18:28 +0100

    Update graph_coloring.cpp

--- greedy_algorithms/graph_coloring.cpp
+++ greedy_algorithms/graph_coloring.cpp
@@ -45,35 +45,35 @@ Graph& Graph::addEdge(const uint32_t v0, const uint32_t v1) {
  * chromotic number = the lowest color what enought to make
  * a "good" color on the graph.
 */
-void graph_coloring(Graph &graph) {
+void graph_coloring(Graph *graph) {
 bool *ava_clrs;
 auto srt = [](std::list<uint32_t> &ls0, std::list<uint32_t> &ls1) -> bool {
         return ls0.size() < ls1.size();
 };
-auto chr = [](int32_t &max, int32_t &clr_i) -> int32_t {
+auto ch = [](int32_t &max, int32_t &clr_i) -> int32_t {
         return clr_i > max ? clr_i : max;
 };
-std::sort(graph.adj, graph.adj + graph.vrtx, srt);
-ava_clrs = new bool[graph.adj->size() + 1];
-for (uint32_t off(0); off < graph.vrtx; ++off) {
+std::sort(*graph.adj, *graph.adj + *graph.vrtx, srt);
+ava_clrs = new bool[*graph.adj->size() + 1];
+for (uint32_t off(0); off < *graph.vrtx; ++off) {
   memset(ava_clrs, true, sizeof(ava_clrs) - 1);
-  for (auto itr = graph.adj[off].begin(); itr != graph.adj[off].end(); ++itr) {
-           if (graph.colors[*itr] != graph.BLANK) {
-                  ava_clrs[graph.colors[*itr]] = false;
-           }
-        }
+for (auto itr = *graph.adj[off].begin(); itr != *graph.adj[off].end(); ++itr) {
+         if (*graph.colors[*itr] != *graph.BLANK) {
+                ava_clrs[*graph.colors[*itr]] = false;
+         }
+      }
 for (uint32_t color_idx(0); color_idx < sizeof(ava_clrs) - 1; ++color_idx) {
            if (ava_clrs[color_idx]) {
-               graph.colors[off] = color_idx;
+               *graph.colors[off] = color_idx;
                break;
            }
      }
 }
-graph.chrom = std::accumulate(graph.colors, graph.colors + graph.vrtx, 0, chr);
-for (uint32_t idx(0); idx < graph.vrtx; ++idx) {
-std::cout << "Node index:" << idx << " " << graph.colors[idx] << std::endl;
+*graph.chrom = std::accumulate(*graph.colors,*graph.colors + *graph.vrtx,0,ch);
+for (uint32_t idx(0); idx < *graph.vrtx; ++idx) {
+std::cout << "Node index:" << idx << " " << *graph.colors[idx] << std::endl;
 }
-std::cout << "chromotic (estimation) ~ " << graph.chrom << std::endl;
+std::cout << "chromotic (estimation) ~ " << *graph.chrom << std::endl;
 }
 int main() {
 Graph g1(7);
@@ -86,6 +86,6 @@ g1.addEdge(3, 4);
 g1.addEdge(3, 6);
 g1.addEdge(4, 5);
 g1.addEdge(5, 6);
-graph_coloring(g1);
+graph_coloring(&g1);
 return 0;
 }
