fad68d2c681e7e03d2d6777a4c0e28c2501f1e27
Siddharth M <47861446+siddmohanty@users.noreply.github.com>
2020-02-16 23:33:12 +0530

    Create measure_time_elapsed.cpp

---
+++ others/measure_time_elapsed.cpp
@@ -0,0 +1,25 @@
+//To calculate the time taken by a code to execute
+#include<iostream>
+#include<sys/time.h>
+
+using namespace std;
+
+long getTimeInMicroseconds()
+{
+    struct timeval start;
+    gettimeofday(&start,NULL);
+    return start.tv_sec * 1000000 + start.tv_usec;
+}
+
+//write function sample(args)
+
+int main()
+{
+    //write code 
+    long starttime = getTimeInMicroseconds();
+    //sample(args) function run
+    //Any other functions (if present) run
+    long endtime = getTimeInMicroseconds();
+    long time = endtime - starttime;
+    cout<<time;
+}
