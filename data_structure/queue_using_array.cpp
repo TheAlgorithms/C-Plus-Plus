b7eba1262bb210ace66e0e1899f6d858ecd2e896
Christian Clauss <cclauss@me.com>
2020-03-01 16:03:45 +0100

    Update queue_using_array.cpp

--- data_structure/queue_using_array.cpp
+++ data_structure/queue_using_array.cpp
@@ -15,7 +15,7 @@ class Queue_Array {
     int rear;
     int size;
 
-  public:
+    public:
     Queue_Array() {
         front = -1;
         rear = -1;
@@ -47,16 +47,17 @@ int Queue_Array::dequeue() {
     } else if (front == rear) {
         d = arr[front];
         front = rear = -1;
-    } else
+    } else {
         d = arr[front++];
+    }
 
     return d;
 }
 
 void Queue_Array::display() {
-    if (front == -1)
+    if (front == -1) {
         std::cout << "\nStack is empty";
-    else {
+    } else {
         for (int i = front; i <= rear; i++)
             std::cout << arr[i] << " ";
     }
@@ -81,11 +82,11 @@ int main() {
         } else if (op == 2) {
             data = ob.dequeue();
             std::cout << "\ndequeue element is:\t" << data;
-        } else if (op == 3)
+        } else if (op == 3) {
             ob.display();
-        else if (op == 4)
+        } else if (op == 4) {
             exit(0);
-        else
+        } else {
             std::cout << "\nWrong choice ";
     }
 }
