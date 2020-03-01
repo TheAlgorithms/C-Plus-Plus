27b7f5adfa749ddb7ea6d1de61998322d528c2bf
Christian Clauss <cclauss@me.com>
2020-03-01 15:20:30 +0100

    int64_t

--- others/measure_time_elapsed.cpp
+++ others/measure_time_elapsed.cpp
@@ -1,8 +1,7 @@
 // To calculate the time taken by a code to execute
 #include<sys/time.h>
-#include<iostream>
 
-int64 getTimeInMicroseconds() {
+int64_t getTimeInMicroseconds() {
     struct timeval start;
     gettimeofday(&start, NULL);
     return start.tv_sec * 1000000 + start.tv_usec;
@@ -12,7 +11,7 @@ int64 getTimeInMicroseconds() {
 
 int main() {
     // write code
-    int64 starttime = getTimeInMicroseconds();
+    int64_t starttime = getTimeInMicroseconds();
     // sample(args) function run
     // Any other functions (if present) run
     std:cout << getTimeInMicroseconds() - starttime;
