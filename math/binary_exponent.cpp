7b19550f7fb14ba340bf162936f95ab39eca22b6
Mann Mehta <44433995+mann2108@users.noreply.github.com>
2020-04-02 04:28:03 +0530

    format binary_exponent.cpp

--- math/binary_exponent.cpp
+++ math/binary_exponent.cpp
@@ -1,4 +1,4 @@
-///C++ Program to find Binary Exponent recursively.
+/// C++ Program to find Binary Exponent recursively.
 
 #include<iostream>
 /*
@@ -8,7 +8,7 @@
  * (instead of O(n) multiplications required by the naive approach).
 */
 
-///Function to calculate exponent in O(log(n)) using binary exponent.
+/// Function to calculate exponent in O(log(n)) using binary exponent.
 int binExpo(int a,int b) {
     if (b == 0) return 1;
     int res = binExpo(a,b/2);
@@ -20,7 +20,7 @@ int main() {
     int a,b;
     /// Give two nos as a^b (where '^' denotes power exponent operation)
     std::cin >> a >> b;
-    ///Result of a^b
+    /// Result of a^b
     std::cout << binExpo(a,b) << std::endl;
 }
 
