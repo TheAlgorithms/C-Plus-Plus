bcb3677f7811b9cd78e88974c5a5ddb21e615d27
Mann Mehta <mannmehta2199@gmail.com>
2020-04-04 17:28:19 +0530

    Update math/binary_exponent.cpp
    Co-Authored-By: John Law <johnlaw.po@gmail.com>

--- math/binary_exponent.cpp
+++ math/binary_exponent.cpp
@@ -2,7 +2,7 @@
 
 #include<iostream>
 /*
- * Calculating a^b in O(log(b)) by converting b in binary no.
+ * Calculate a^b in O(log(b)) by converting b to a binary number
  * Binary exponentiation (also known as exponentiation by squaring)
  * is a trick which allows to calculate an using only O(logn) multiplications
  * (instead of O(n) multiplications required by the naive approach).
