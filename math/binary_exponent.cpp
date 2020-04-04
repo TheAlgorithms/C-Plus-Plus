1d4a67c6ea07b12c5d0b6683dcf402ebce9c914d
Mann Mehta <mannmehta2199@gmail.com>
2020-04-04 17:29:11 +0530

    Update math/binary_exponent.cpp
    Co-Authored-By: John Law <johnlaw.po@gmail.com>

--- math/binary_exponent.cpp
+++ math/binary_exponent.cpp
@@ -23,7 +23,7 @@ int binExpo(int a, int b) {
 
 int main() {
     int a, b;
-    /// Give two nos as a^b (where '^' denotes power exponent operation)
+    /// Give two numbers a, b
     std::cin >> a >> b;
     /// Result of a^b
     std::cout << binExpo(a, b) << std::endl;
