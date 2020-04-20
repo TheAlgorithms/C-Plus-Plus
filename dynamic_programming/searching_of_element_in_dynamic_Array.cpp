3621d2ee262760e21ccd7187523493c21ff03f7f
faizan Ahamed <faizanahamed1414@gmail.com>
2020-04-21 01:03:38 +0530

    Update searching_of_element_in_dynamic_Array.cpp

--- dynamic_programming/searching_of_element_in_dynamic_Array.cpp
+++ dynamic_programming/searching_of_element_in_dynamic_Array.cpp
@@ -33,35 +33,36 @@ The element is 5
 */
 #include <iostream>
 int main() {
-long long int r, mr = 0, x, q, i, z;
+int64_t r, mr = 0, x, q, i, z;
 std::cout << "Enter Number of array you want to Store :";
 std::cin >> x;
 std::cout << "Enter Number of Question or Quary you want to do Related to Array :";
 std::cin >> q;
-//create a Array in run time because use can change the size of each array which he/she is going to store
-//create a 2D array
+// create a Array in run time because use can
+// change the size of each array which he/she is going to store
+// create a 2D array
 int** ar = new int* [x]();
 for (r = 0; r < x; r++) {
 std::cout << "Enter number of element in " << r + 1 << " rows :";
 std::cin >> mr;
-//creating a 1D array
+// creating a 1D array
 int* ac = new int[mr]();
 std::cout << "Enter the element of Array ";
 for (i = 0; i < mr; i++) {
-//entering the value of rows in array in Horizontal
+// entering the value of rows in array in Horizontal
 std::cin >> ac[i];
 }
-//Change the position of Array so that new arrays entery will be done
+// Change the position of Array so that new arrays entery will be done
 ar[r] = ac;
 }
 for (z = 0; z < q; z++) {
-long int r1 = 0, q1 = 0;
+int64_t r1 = 0, q1 = 0;
 std::cout << "enter the number of row which element You want to find :";
 std::cin >> r1;
 r1 = r1 - 1;
 std::cout << "enter the position of element which You want to find :";
 std::cin>> q1;
 q1 = q1 - 1;
-//use this to find desire position of element in desire array
+// use this to find desire position of element in desire array
 std::cout <<"The element is "<< ar[r1][q1] <<std::endl;
 }}
