e6874e4d18bced2ad2374edc9b0afe6ea607454f
DarkWarrior703 <56077342+DarkWarrior703@users.noreply.github.com>
2020-04-25 22:28:20 +0300

    Update double_factorial.cpp

--- math/double_factorial.cpp
+++ math/double_factorial.cpp
@@ -2,13 +2,15 @@
 #include <cassert>
 
 /* Double factorial of a non-negative integer n,
-is defined as the product of all the integers from 1 to n
+is defined as the product of 
+all the integers from 1 to n
 that have the same parity (odd or even) as n.
-It is also called as semifactorial of a number and is denoted by !! */
+It is also called as semifactorial 
+of a number and is denoted by !! */
 
 unsigned long long double_factorial_iterative(unsigned int n){
   unsigned long long res = 1;
-  for(unsigned long long i = n; i >= 0; i -= 2){
+  for( unsigned long long i = n; i >= 0; i -= 2 ){
     if(i == 0 || i == 1) return res;
     res *= i;
   }
