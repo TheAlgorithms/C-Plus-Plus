c4f3f915f874dd66f19edec04b95e59d237a0a2d
Christian Clauss <cclauss@me.com>
2020-04-26 08:41:22 +0200

    Update double_factorial.cpp

--- math/double_factorial.cpp
+++ math/double_factorial.cpp
@@ -1,12 +1,9 @@
 #include <iostream>
 #include <cassert>
 
-/* Double factorial of a non-negative integer n,
-is defined as the product of 
-all the integers from 1 to n
-that have the same parity (odd or even) as n.
-It is also called as semifactorial 
-of a number and is denoted by !! */
+/* Double factorial of a non-negative integer n, is defined as the product of 
+all the integers from 1 to n that have the same parity (odd or even) as n.
+It is also called as semifactorial of a number and is denoted by !! */
 
 uint64_t double_factorial_iterative(uint64_t n) {
   uint64_t res = 1;
