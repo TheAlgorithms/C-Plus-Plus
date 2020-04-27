b691536ea93fe5ec8eac3d95d7433693c892884b
Dylan Robertson <31269647+RobotRage@users.noreply.github.com>
2020-04-27 17:31:44 +1000

    spacing
    Co-Authored-By: Christian Clauss <cclauss@me.com>

--- probability/bayes_theorem.cpp
+++ probability/bayes_theorem.cpp
@@ -23,12 +23,8 @@ int main() {
     double A = 0.01;
     double B = 0.1;
     double BgivenA = 0.9;
-
     double AgivenB = bayes_AgivenB(BgivenA, A, B);
-
-
     std::cout << "A given B = " << AgivenB << std::endl;
     std::cout << "B given A = " << bayes_BgivenA(AgivenB, A, B) << std::endl;
-
     return 0;
 }
