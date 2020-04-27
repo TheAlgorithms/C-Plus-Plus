97c556353d3a7146895ca52b7765882e781b19db
Nour B <56294154+nourrrrrrrr@users.noreply.github.com>
2020-04-27 13:46:31 +0100

    Update pigeonholesort.cpp

--- sorting/pigeonholesort.cpp
+++ sorting/pigeonholesort.cpp
@@ -1,13 +1,12 @@
 void pigeonholeSort(int arr[], int n) 
 { 
-    // Find minimum and maximum values in arr[] 
-    int min = arr[0], max = arr[0]; 
-    for (int i = 1; i < n; i++) 
-    { 
-        if (arr[i] < min) 
-            min = arr[i]; 
-        if (arr[i] > max) 
-            max = arr[i]; 
+ int min = arr[0], max = arr[0];
+ for (int i = 1; i < n; i++)
+ {  
+ if (arr[i] < min)
+ min = arr[i];
+ if (arr[i] > max) 
+ max = arr[i];
     } 
     int range = max - min + 1; // Find range 
   
