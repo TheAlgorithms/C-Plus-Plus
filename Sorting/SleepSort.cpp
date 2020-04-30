3f5d22089bf8ddc5485c69579db7c0319639b1a0
Navdeep Kumar <43781596+navdeepkk@users.noreply.github.com>
2020-04-30 15:21:33 +0530

    Update SleepSort.cpp

--- Sorting/SleepSort.cpp
+++ Sorting/SleepSort.cpp
@@ -1,4 +1,4 @@
-//This is the most simple implementation of sleeping sort in c++//
+// This is the most simple implementation of sleeping sort in c++//
 #include<iostream>
 #include<thread>
 #include<vector>
@@ -10,12 +10,12 @@ void sleep(int);
 
 int main()
 {
-	//initialize an array of integers to be sorted
+	// initialize an array of integers to be sorted
 	// Doesn't work for negative numbers
 	int arr[] = { 3,2,1 };
 	int n = sizeof(arr) / sizeof(arr[0]);
 	
-	//find the max element in arr
+	// find the max element in arr
 	//Why to ding max? Read below
 	int max = arr[0];
 	for (int i = 0; i < n; i++) {
@@ -23,12 +23,12 @@ int main()
 	}
 	max++;
 
-	//call the function SLEEPSORT()
+	// call the function SLEEPSORT()
 	sleepSort(arr, n);
 
-	//make the main thread to go to sleep for 1 second more then
-	//the largest number in the list.
-	//this ensures all the threads are over before main finishes execution.
+	// make the main thread to go to sleep for 1 second more then
+	// the largest number in the list.
+	// this ensures all the threads are over before main finishes execution.
 	std::this_thread::sleep_for(std::chrono::seconds(max));
 
 
@@ -36,7 +36,7 @@ int main()
 }
 
 void sleep( int i) {
-	//make the thread to sleep for I seconds.
+	// make the thread to sleep for I seconds.
 	std::this_thread::sleep_for(std::chrono::seconds(i));
 	cout << i << " ";
 }
@@ -45,7 +45,7 @@ void sleepSort(int* arr, int n) {
 	thread* threads;
 	threads = new thread[n];
 	for (int i = 0; i < n; i++) {
-		//a new thread is created for each element of an array, which goes to sleep for arr[i] seconds.
+		// a new thread is created for each element of an array, which goes to sleep for arr[i] seconds.
 		threads[i] = (thread(sleep, arr[i]));
 	}
 }
