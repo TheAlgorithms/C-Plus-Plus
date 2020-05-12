2d71dfaa4d9ff370e303e0b3d612760ce34b4676
Himanshu Airan <62210670+Himanshu-77@users.noreply.github.com>
2020-05-12 22:04:43 +0530

    Update factorial.cpp
    function added

--- math/factorial.cpp
+++ math/factorial.cpp
@@ -2,16 +2,29 @@
 #include<iostream>
 
 // function to find factorial of given number
-unsigned int factorial(unsigned int n) {
+// recursive approach
+unsigned int factorial_rec(unsigned int n) {
     if (n == 0)
         return 1;
-    return n * factorial(n - 1);
+    return n * factorial_rec(n - 1);
+}
+
+// iterative approach
+unsigned int factorial_iter(unsigned int n) {
+    if (n == 0)
+        return 1;
+    
+    unsigned int result = 1 , i;
+    for(i=2 ; i<=n ; ++i)
+        result *= i;
+    
+    return result;
 }
 
 // Driver code
 int main() {
     int num = 5;
-    std::cout << "Factorial of " << num << " is " << factorial(num)
+    std::cout << "Factorial of " << num << " is " << factorial_rec(num)
               << std::endl;
     return 0;
 }
