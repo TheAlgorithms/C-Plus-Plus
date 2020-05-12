ea7237e58f17aed8c81369ca2bfcdf705dfcdb31
Vignesh Ravindran <ravivignesh2000@gmail.com>
2020-05-12 14:40:31 +0530

    added Number of digits of a number in math

---
+++ math/number_of_digits.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<math.h>
+
+unsigned int digits(unsigned int n) {
+	int num_digits=0;
+	while(n>0) {
+		num_digits++;
+		n=n/10;
+	}
+	return num_digits;
+}
+
+unsigned int digits_log(unsigned int n) {
+	return log10(n)+1;
+}
+
+int main() {
+	int num=12345;
+	std::cout << "Number of digits in " << num <<" is : "<<digits(num)<<std::endl;
+	//Efficient approach to find the number of digits of a number
+	std::cout << "Number of digits in " << num <<" is : "<<digits_log(num)<<std::endl;
+}
