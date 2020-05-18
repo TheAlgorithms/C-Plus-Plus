b7d8d72a52b8d2ce15ce868a256b7b1512563b41
Srujan Bharadwaj <55992753+srujangit123@users.noreply.github.com>
2020-05-18 13:09:38 +0530

    Fix wihitespaces and fix if else space issues

--- math/extended_euclid_algorithm.cpp
+++ math/extended_euclid_algorithm.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
-using namespace std;
-
-// Finding coefficients of a and b ie x and y in gcd(a, b) = a * x + b * y 
+// Finding coefficients of a and b ie x and y in gcd(a, b) = a * x + b * y
+// d is gcd(a, b)
+// This is also used in finding Modular multiplicative inverse of a number
+// (A * B)%M == 1 Here B is the MMI of A for given M, so extendedEuclid (A, M) gives B
 
 int d, x, y;
-
 void extendedEuclid(int A, int B) {
-    if(B == 0) {
+    if (B == 0) {
         d = A;
         x = 1;
         y = 0;
-    }// d is gcd(a, b)
-    else {
+    } else {
         extendedEuclid(B, A%B);
         int temp = x;
         x = y;
@@ -19,12 +18,10 @@ void extendedEuclid(int A, int B) {
     }
 }
 
-
-int main(){
+int main() {
     int a, b;
-    cin >> a >> b;
+    std::cin >> a >> b;
     extendedEuclid(a, b);
-    cout << x << " " << y << endl;
-    //gcd(a, b) = a * x + b * y
+    std::cout << x << " " << y << std::endl;
     return 0;
 }
