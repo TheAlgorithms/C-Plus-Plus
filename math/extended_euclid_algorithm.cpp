d459f23d9de95c3fc009beee240fc66aa89f2edf
Srujan Bharadwaj <55992753+srujangit123@users.noreply.github.com>
2020-05-18 17:40:52 +0530

    Update extended_euclid_algorithm.cpp

--- math/extended_euclid_algorithm.cpp
+++ math/extended_euclid_algorithm.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 // Finding coefficients of a and b ie x and y in gcd(a, b) = a * x + b * y
 // d is gcd(a, b)
-// This is also used in finding Modular multiplicative inverse of a number
-// (A * B)%M == 1 Here B is the MMI of A for given M, so extendedEuclid (A, M) gives B
+// This is also used in finding Modular multiplicative inverse of a number.
+// (A * B)%M == 1 Here B is the MMI of A for given M, 
+// so extendedEuclid (A, M) gives B.
 
 int d, x, y;
 void extendedEuclid(int A, int B) {
