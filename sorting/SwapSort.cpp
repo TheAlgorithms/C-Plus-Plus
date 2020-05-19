b9c1545712ee44855183d55e408c9fea82be2976
mishraabhinn <49574460+mishraabhinn@users.noreply.github.com>
2020-05-19 23:32:59 +0530

    Update SwapSort.cpp

--- sorting/SwapSort.cpp
+++ sorting/SwapSort.cpp
@@ -1,74 +1,66 @@
-// C++ program to find minimum number of swaps required to sort an array 
-#include<iostream>
-#include<vector>
-#include<utility>
-#include<algorithm>
+// C++ program to find minimum number of swaps required to sort an array
+#include <algorithm>
+#include <iostream>
+#include <utility>
+#include <vector>
 
+// Function returns the minimum number of swaps
+// required to sort the array
+int minSwaps(int arr[], int n) {
+  // Create an array of pairs where first
+  // element is array element and second element
+  // is position of first element
+  std::pair<int, int> arrPos[n];
+  for (int i = 0; i < n; i++) {
+    arrPos[i].first = arr[i];
+    arrPos[i].second = i;
+  }
 
+  // Sort the array by array element values to
+  // get right position of every element as second
+  // element of pair.
+  std::sort(arrPos, arrPos + n);
 
-// Function returns the minimum number of swaps 
-// required to sort the array 
-int minSwaps(int arr[], int n) 
-{ 
-	// Create an array of pairs where first 
-	// element is array element and second element 
-	// is position of first element 
-	std::pair<int, int> arrPos[n]; 
-	for (int i = 0; i < n; i++) 
-	{ 
-		arrPos[i].first = arr[i]; 
-		arrPos[i].second = i; 
-	} 
+  // To keep track of visited elements. Initialize
+  // all elements as not visited or false.
+  std::vector<bool> vis(n, false);
 
-	// Sort the array by array element values to 
-	// get right position of every element as second 
-	// element of pair. 
-	std::sort(arrPos, arrPos + n); 
+  // Initialize result
+  int ans = 0;
 
-	// To keep track of visited elements. Initialize 
-	// all elements as not visited or false. 
-	std::vector<bool> vis(n, false); 
+  // Traverse array elements
+  for (int i = 0; i < n; i++) {
+    // already swapped and corrected or
+    // already present at correct pos
+    if (vis[i] || arrPos[i].second == i)
+      continue;
 
-	// Initialize result 
-	int ans = 0; 
+    // find out the number of node in
+    // this cycle and add in ans
+    int cycle_size = 0;
+    int j = i;
+    while (!vis[j]) {
+      vis[j] = 1;
 
-	// Traverse array elements 
-	for (int i = 0; i < n; i++) 
-	{ 
-		// already swapped and corrected or 
-		// already present at correct pos 
-		if (vis[i] || arrPos[i].second == i) 
-			continue; 
+      // move to next node
+      j = arrPos[j].second;
+      cycle_size++;
+    }
 
-		// find out the number of node in 
-		// this cycle and add in ans 
-		int cycle_size = 0; 
-		int j = i; 
-		while (!vis[j]) 
-		{ 
-			vis[j] = 1; 
+    // Update answer by adding current cycle.
+    if (cycle_size > 0) {
+      ans += (cycle_size - 1);
+    }
+  }
 
-			// move to next node 
-			j = arrPos[j].second; 
-			cycle_size++; 
-		} 
+  // Return result
+  return ans;
+}
 
-		// Update answer by adding current cycle. 
-		if (cycle_size > 0) 
-		{ 
-			ans += (cycle_size - 1); 
-		} 
-	} 
-
-	// Return result 
-	return ans; 
-} 
-
-//program to test  
-int main() 
-{ 
-	int arr[] = {6, 7 ,8 ,1, 2, 3, 9, 12}; 
-	int n = (sizeof(arr) / sizeof(int)); 
-	std::cout << minSwaps(arr, n); 
-	return 0; 
-} 
+// program to test
+int main() {
+  int arr[] = {6, 7, 8, 1, 2, 3, 9, 12};
+  int n = (sizeof(arr) / sizeof(int));
+  std::cout << minSwaps(arr, n);
+  return 0;
+}
