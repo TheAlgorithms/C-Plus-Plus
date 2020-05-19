ca2a31d1c17b266248feee71141036c11011f4f2
Ivan Dario Arevalo Gutierrez <idarevalog@unal.edu.co>
2020-05-19 16:37:30 +0200

    finbonacci funtion added

--- math/fibonacci.cpp
+++ math/fibonacci.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cassert>
 
 /* Calculate the the Value on
 Fibonnaci's sequence given an
@@ -7,14 +8,13 @@ Fibonacci =  0, 1, 1, 2, 3, 5,
              8, 13, 21, 34, 55,
              89, 144, ...       */
 
-int fibonacci(uint n){
-    int retval =0;
+int fibonacci(uint n) {
+    int retval = 0;
     /*If the input is 0 or 1 just return the same
      This will set the first 2 values of the sequence*/
-    if(n <= 1){
+    if (n <= 1) {
         return n;
-    }
-    else{
+    }else {
         /*Add the last 2 values of the sequence to get next*/
         retval = fibonacci(n-1) + fibonacci(n-2);
     }
@@ -25,5 +25,6 @@ int main() {
     int n;
     std::cin >> n;
     assert(n >= 0);
-    std::cout <<"F("<<n<<")= " << fibonacci(n) << std::endl;
+    std::cout << "F(" << n << ")= " << fibonacci(n) << std::endl;
+
 }
