6f62cf157a2fac171318140565ec9c4fd7676629
Abhinn Mishra <aviabhinn98@gmail.com>
2020-05-20 21:59:02 +0530

    edited

--- graph/longest_path.cpp
+++ graph/longest_path.cpp
@@ -4,12 +4,12 @@
 std::vector<int> arr[100001]; // a vector to store graph.
 int vis[100001]; // an array that maintains that a node is vidited or not.
 int maxd = -1;   // maxd is a variable that store the maximum distance.
-int maxNode;  //maxNode is a varaibale that store the maximum node.
+int maxNode;     // maxNode is a varaibale that store the maximum node.
 void dfs(int node, int d) {
     vis[node] = 1;
     if (d > maxd) {
         maxd = d;
-        maxNode=node;
+        maxNode = node;
     }
 
     for (int child : arr[node]) // tranversing the all nodes inside this node.
@@ -18,20 +18,23 @@ void dfs(int node, int d) {
 }
 
 int main() {
-   
+
     int n, a, b; // n is the number of nodes
     std::cin >> n;
 
     // a and b is used to for input an edge between a and b.
-    for (int i = 1; i < n; i++)
-        {std::cin >> a >> b;
+    for (int i = 1; i < n; i++) {
+
+        std::cin >> a >> b;
         arr[a].push_back(b);
-        arr[b].push_back(a);}
+        arr[b].push_back(a);
+    }
     maxd = -1;
     dfs(1, 0);
 
-    for (int i = 1; i < n; i++)
-       { vis[i] = 0;}
+    for (int i = 1; i < n; i++) {
+        vis[i] = 0;
+    }
     maxd = -1;
 
     dfs(maxNode, 0);
