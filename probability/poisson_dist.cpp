cf78e85612e482775a5e9e59f674c760821fa324
Christian Clauss <cclauss@me.com>
2020-05-21 22:48:06 +0200

    https://en.wikipedia.org/wiki/Poisson_distribution

--- probability/poisson_dist.cpp
+++ probability/poisson_dist.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <cmath>
 
-// The poisson distribution counts
-// how many events occur over a set
-// time interval
+// The Poisson distribution counts how many
+// events occur over a set time interval
+// https://en.wikipedia.org/wiki/Poisson_distribution
 
 // calculate the events per unit time
 // e.g 5 dollars every 2 mins = 5 / 2 = 2.5
@@ -19,7 +19,7 @@ double poisson_expected(double rate, double time) {
     return rate * time;
 }
 
-// finds the factorial of a given number
+// find the factorial of a given number
 
 double fact(double x) {
     double x_fact = x;
@@ -33,13 +33,13 @@ double fact(double x) {
     return x_fact;
 }
 
-// finds the probability of x successes in a poisson dist
+// find the probability of x successes in a Poisson dist
 
 double poisson_x_successes(double expected, double x) {
     return (pow(expected, x) * exp(-expected)) / fact(x);
 }
 
-// probability of a success in range for poisson dist (inclusive, inclusive)
+// probability of a success in range for Poisson dist (inclusive, inclusive)
 
 double poisson_range_successes(double expected, double lower, double upper) {
     double probability = 0;
@@ -52,14 +52,14 @@ double poisson_range_successes(double expected, double lower, double upper) {
 int main() {
     double rate, expected;
     rate = poisson_rate(3, 1);
-    std::cout << "poisson rate : " << rate << std::endl;
+    std::cout << "Poisson rate : " << rate << std::endl;
 
     expected = poisson_expected(rate, 2);
-    std::cout << "poisson expected : " << expected << std::endl;
+    std::cout << "Poisson expected : " << expected << std::endl;
 
-    std::cout << "poisson 0 successes : "
+    std::cout << "Poisson 0 successes : "
         <<poisson_x_successes(expected, 0) <<std::endl;
-    std::cout << "poisson 0-8 successes : "
+    std::cout << "Poisson 0-8 successes : "
         << poisson_range_successes(expected, 0, 8) <<std::endl;
 
     return 0;
