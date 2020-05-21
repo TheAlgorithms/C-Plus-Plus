00e31ff07c645419acffdf29b904e3ea03e97a4a
RAvi Sheoran <ravi.jatt1608@gmail.com>
2020-05-21 07:01:49 +0530

    add_string code added

---
+++ add_strings.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+string add_strings(string a,string b){
+  string space=" ";
+  string newString=a+space+b;
+
+  return newString;
+}
+ int main() {
+   string string1="hello";
+   string string2="world";
+   string newString=add_strings(string1,string2);
+  cout<<newString;
+  return 0;
+}
