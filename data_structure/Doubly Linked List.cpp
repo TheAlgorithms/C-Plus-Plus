eac1f0b7f95a9b205e7048d40344ac6f2f608e31
Khizar Alam <khizar@venturedive.com>
2020-05-22 12:48:37 +0200

    fix: linter issues

--- data_structure/Doubly Linked List.cpp
+++ data_structure/Doubly Linked List.cpp
@@ -1,146 +1,125 @@
 #include <iostream>
 using namespace std;
 
-struct node
-{
-	int val;
-	node *prev;
-	node *next;
+struct node {
+  int val;
+  node *prev;
+  node *next;
 };
 
 node *start;
 
-void insert(int x)
-{
-	node *t = start;
-	if (start != NULL)
-	{
-		while (t->next != NULL)
-		{
-			t = t->next;
-		}
-		node *n = new node;
-		t->next = n;
-		n->prev = t;
-		n->val = x;
-		n->next = NULL;
-	}
-	else
-	{
-		node *n = new node;
-		n->val = x;
-		n->prev = NULL;
-		n->next = NULL;
-		start = n;
-	}
+void insert(int x) {
+  node *t = start;
+  if (start != NULL) {
+    while (t->next != NULL) {
+      t = t->next;
+    }
+    node *n = new node;
+    t->next = n;
+    n->prev = t;
+    n->val = x;
+    n->next = NULL;
+  } else {
+    node *n = new node;
+    n->val = x;
+    n->prev = NULL;
+    n->next = NULL;
+    start = n;
+  }
 }
 
-void remove(int x)
-{
-	node *t = start;
-	while (t && t->val != x)
-	{
-		t = t->next;
-	}
-	if (t){
-		if (!(t->prev || t->next)){
-			start = NULL;
-		}
-		if (t->prev){
-			t->prev->next = t->next;
-		}
-		else{
-			start = t->next;
-		}
-		if (t->next){
-			t->next->prev = t->prev;
-		}
-		delete t;
-	}
+void remove(int x) {
+  node *t = start;
+  while (t && t->val != x) {
+    t = t->next;
+  }
+  if (t) {
+    if (!(t->prev || t->next)) {
+      start = NULL;
+    }
+    if (t->prev) {
+      t->prev->next = t->next;
+    } else {
+      start = t->next;
+    }
+    if (t->next) {
+      t->next->prev = t->prev;
+    }
+    delete t;
+  }
 }
 
-void search(int x)
-{
-	node *t = start;
-	int found = 0;
-	while (t != NULL)
-	{
-		if (t->val == x)
-		{
-			cout << "\nFound";
-			found = 1;
-			break;
-		}
-		t = t->next;
-	}
-	if (found == 0)
-	{
-		cout << "\nNot Found";
-	}
+void search(int x) {
+  node *t = start;
+  int found = 0;
+  while (t != NULL) {
+    if (t->val == x) {
+      cout << "\nFound";
+      found = 1;
+      break;
+    }
+    t = t->next;
+  }
+  if (found == 0) {
+    cout << "\nNot Found";
+  }
 }
 
-void show()
-{
-	node *t = start;
-	while (t != NULL)
-	{
-		cout << t->val << "\t";
-		t = t->next;
-	}
+void show() {
+  node *t = start;
+  while (t != NULL) {
+    cout << t->val << "\t";
+    t = t->next;
+  }
 }
 
-void reverseShow()
-{
-	node *t = start;
-	while (t && t->next != NULL)
-	{
-		t = t->next;
-	}
-	while (t != NULL)
-	{
-		cout << t->val << "\t";
-		t = t->prev;
-	}
+void reverseShow() {
+  node *t = start;
+  while (t && t->next != NULL) {
+    t = t->next;
+  }
+  while (t != NULL) {
+    cout << t->val << "\t";
+    t = t->prev;
+  }
 }
 
-int main()
-{
-	int choice, x;
-	do
-	{
-		cout << "\n1. Insert";
-		cout << "\n2. Delete";
-		cout << "\n3. Search";
-		cout << "\n4. Forward print";
-		cout << "\n5. Reverse print";
-		cout << "\n\nEnter you choice : ";
-		cin >> choice;
-		switch (choice)
-		{
-		case 1:
-			cout << "\nEnter the element to be inserted : ";
-			cin >> x;
-			;
-			insert(x);
-			break;
-		case 2:
-			cout << "\nEnter the element to be removed : ";
-			cin >> x;
-			remove(x);
-			break;
-		case 3:
-			cout << "\nEnter the element to be searched : ";
-			cin >> x;
-			search(x);
-			break;
-		case 4:
-			show();
-			break;
-		case 5:
-			reverseShow();
-			break;
-		}
-	} while (choice != 0);
+int main() {
+  int choice, x;
+  do {
+    cout << "\n1. Insert";
+    cout << "\n2. Delete";
+    cout << "\n3. Search";
+    cout << "\n4. Forward print";
+    cout << "\n5. Reverse print";
+    cout << "\n\nEnter you choice : ";
+    cin >> choice;
+    switch (choice) {
+    case 1:
+      cout << "\nEnter the element to be inserted : ";
+      cin >> x;
+      ;
+      insert(x);
+      break;
+    case 2:
+      cout << "\nEnter the element to be removed : ";
+      cin >> x;
+      remove(x);
+      break;
+    case 3:
+      cout << "\nEnter the element to be searched : ";
+      cin >> x;
+      search(x);
+      break;
+    case 4:
+      show();
+      break;
+    case 5:
+      reverseShow();
+      break;
+    }
+  } while (choice != 0);
 
-	return 0;
+  return 0;
 }
