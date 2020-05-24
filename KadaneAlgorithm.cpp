b23cfb860ee4f372cd7c77e79f198e93cc326271
vanshika1205 <55838567+vanshika1205@users.noreply.github.com>
2020-05-24 20:35:05 +0530

    Update KadaneAlgorithm.cpp

--- KadaneAlgorithm.cpp
+++ KadaneAlgorithm.cpp
@@ -8,17 +8,17 @@ int main()
   int currentsum = 0, maxsum = 0;  // Stores current sum and maximum sum
   std::cin >> n;
   vector <int> a;
-  for ( int i = 0; i < n ; ++i){
+  for ( int i = 0; i < n ; ++i ) {
       int x;
       std::cin >> x;
       a.push_back(x);
   }
-  for ( int i = 0; i < n ; ++i){
+  for ( int i = 0; i < n ; ++i ) {
      currentsum = currentsum + a[i];
      //  If current sum becomes negative make it zero
      if ( currentsum < 0 )
       currentsum = 0;
-     maxsum = max( currentsum , maxsum );
+     maxsum = max(currentsum , maxsum);
   }
   std::cout<< maxsum << endl;
   return 0;
