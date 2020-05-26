59be834836c839db7fba0ab852193252e6fa3eeb
Krishna Vedala <krishna.vedala@ieee.org>
2020-05-26 09:26:40 -0400

    fixed lint
    (cherry picked from commit fb3a8091f9492ccac9b66572612e39e394eed4ad)

--- others/palindrome_of_number.cpp
+++ others/palindrome_of_number.cpp
@@ -1,23 +1,20 @@
-#include <iostream>
 #include <algorithm>
+#include <iostream>
 
-using namespace std;
+int main() {
+    int num;
+    std::cout << "Enter number = ";
+    std::cin >> num;
 
-int main()
-{
-	int num;
-	cout << "Enter number = ";
-	cin >> num;
+    std::string s1 = std::to_string(num);
+    std::string s2 = s1;
 
-	string s1 = to_string(num);
-	string s2 = s1;
+    reverse(s1.begin(), s1.end());
 
-	reverse(s1.begin(), s1.end());
+    if (s1 == s2)
+        std::cout << "true";
+    else
+        std::cout << "false";
 
-	if (s1 == s2)
-		cout << "true";
-	else
-		cout << "false";
-
-	return 0;
+    return 0;
 }
