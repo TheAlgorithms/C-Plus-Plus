b36322c629809375de3abc30d48479efa4acddbf
Krishna Vedala <krishna.vedala@ieee.org>
2020-05-25 22:13:39 -0400

    file rename to standards
    (cherry picked from commit 412d3ff1a2fc491e4c8e0b05db814c186bdf7dc4)

--- math/primes_up_to_billion.cpp
+++ math/primes_up_to_billion.cpp
@@ -1,12 +1,12 @@
-#include<iostream>
 #include <cstring>
+#include <iostream>
 
 char prime[100000000];
 
 void Sieve(int64_t n) {
     memset(prime, '1', sizeof(prime));  // intitize '1' to every index
-    prime[0] = '0';  // 0 is not prime
-    prime[1] = '0';  // 1 is not prime
+    prime[0] = '0';                     // 0 is not prime
+    prime[1] = '0';                     // 1 is not prime
     for (int p = 2; p * p <= n; p++) {
         if (prime[p] == '1') {
             for (int i = p * p; i <= n; i += p)
@@ -14,7 +14,6 @@ void Sieve(int64_t n) {
         }
     }
 }
-
 
 int main() {
     Sieve(100000000);
