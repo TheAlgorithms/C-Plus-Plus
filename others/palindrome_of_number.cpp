a15b5c5a9ba954c112e8e599e7885d9afb297ac0
Krishna Vedala <krishna.vedala@ieee.org>
2020-05-26 10:06:01 -0400

    MSVC does not know cstring-use string-for toString
    (cherry picked from commit fe9d2b9bc66ee9f2e5112ced6b6d5aab7d5e5f05)

--- others/palindrome_of_number.cpp
+++ others/palindrome_of_number.cpp
@@ -1,6 +1,13 @@
 #include <algorithm>
 #include <iostream>
 
+#ifdef _MSC_VER
+// Required to compile std::toString function using MSVC
+#include <string>
+#else
+#include <cstring>
+#endif
+
 int main() {
     int num;
     std::cout << "Enter number = ";
