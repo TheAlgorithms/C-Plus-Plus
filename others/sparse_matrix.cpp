5fddd6c7cd3d8deb2d8e135c3fe3d11586244957
Krishna Vedala <krishna.vedala@ieee.org>
2020-05-26 09:10:28 -0400

    cin accepts only one variable at a time & fixed cpplint
    (cherry picked from commit 2def9abcc2306ab569687f85f1802c4e5493d244)

--- others/sparse_matrix.cpp
+++ others/sparse_matrix.cpp
@@ -1,41 +1,40 @@
-/*A sparse matrix is a matrix which has number of zeroes greater than (m*n)/2,
-where m and n are the dimensions of the matrix.*/
+/** @file
+ * A sparse matrix is a matrix which has number of zeroes greater than
+ * \f$\frac{m*n}{2}\f$, where m and n are the dimensions of the matrix.
+ */
+
 #include <iostream>
-using namespace std;
 
-int main()
-{
-  int m, n;
-  int counterZeros = 0;
-  cout << "Enter dimensions of matrix (seperated with space): ";
-  cin >> m >> n;
-  int a[m][n];
-  cout << "Enter matrix elements:";
-  cout << "\n";
+int main() {
+    int m, n;
+    int counterZeros = 0;
 
-  // reads the matrix from stdin
-  for (int i = 0; i < m; i++)
-  {
-    for (int j = 0; j < n; j++)
-    {
-      cout << "element? ";
-      cin >> a[i][j];
+    std::cout << "Enter dimensions of matrix (seperated with space): ";
+    std::cin >> m;
+    std::cin >> n;
+
+    int a[m][n];
+    std::cout << "Enter matrix elements:";
+    std::cout << "\n";
+
+    // reads the matrix from stdin
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            std::cout << "element? ";
+            std::cin >> a[i][j];
+        }
     }
-  }
 
-  // counts the zero's
-  for (int i = 0; i < m; i++)
-  {
-    for (int j = 0; j < n; j++)
-    {
-      if (a[i][j] == 0)
-        counterZeros++; //Counting number of zeroes
+    // counts the zero's
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            if (a[i][j] == 0) counterZeros++;  // Counting number of zeroes
+        }
     }
-  }
 
-  // makes sure the matrix is a sparse matrix
-  if (counterZeros > ((m * n) / 2)) //Checking for sparse matrix
-    cout << "Sparse matrix";
-  else
-    cout << "Not a sparse matrix";
+    // makes sure the matrix is a sparse matrix
+    if (counterZeros > ((m * n) / 2))  // Checking for sparse matrix
+        std::cout << "Sparse matrix";
+    else
+        std::cout << "Not a sparse matrix";
 }
