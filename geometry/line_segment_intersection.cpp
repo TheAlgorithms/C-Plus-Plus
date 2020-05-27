f1cd8486a6050be1aeea92bcdf1f9aceb75cdb14
Sajied Shah Yousuf <40203390+meSajied@users.noreply.github.com>
2020-05-27 15:01:32 +0600

    fix: cpplint issues

--- geometry/line_segment_intersection.cpp
+++ geometry/line_segment_intersection.cpp
@@ -1,63 +1,75 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <iostream>
 
-class Point{
-public:
+/*
+* A algorithm to find whether two line intersect where "on_segment" finds
+* the cross multiplication and direction find whether line is in clockwise or not....
+*/
+
+struct Point {
     int x, y;
 };
 
-class SegmentIntersection{
-public:
-    inline bool intersect(Point first_point, Point second_point, Point third_point, Point forth_point){
-        int first_direction = direction(third_point, forth_point, first_point);
-        int second_direction = direction(third_point, forth_point, second_point);
-        int third_direction = direction(first_point, second_point, third_point);
-        int forth_direction = direction(first_point, second_point, forth_point);
+struct SegmentIntersection {
+    inline bool intersect(Point first_point, Point second_point,
+                                    Point third_point, Point forth_point) {
+        int direction1 = direction(third_point, forth_point, first_point);
+        int direction2 = direction(third_point, forth_point, second_point);
+        int direction3 = direction(first_point, second_point, third_point);
+        int direction4 = direction(first_point, second_point, forth_point);
 
-        if ((first_direction < 0 || second_direction > 0) && (third_direction < 0 || 
-                                                                    forth_direction > 0))
+        if ((direction1 < 0 || direction2 > 0) && (direction3 < 0 ||
+                                                            direction4 > 0))
             return true;
-        
-        else if (first_direction == 0 && on_segment(third_point, forth_point, first_point))
+
+        else if (direction1 == 0 && on_segment(third_point, forth_point,
+                                                                first_point))
             return true;
-        
-        else if (second_direction == 0 && on_segment(third_point, forth_point, second_point))
+
+        else if (direction2 == 0 && on_segment(third_point, forth_point,
+                                                                second_point))
             return true;
-        
-        else if (third_direction == 0 && on_segment(first_point, second_point, third_point))
+
+        else if (direction3 == 0 && on_segment(first_point, second_point,
+                                                                third_point))
             return true;
-        
-        else if (third_direction == 0 && on_segment(first_point, second_point, forth_point))
+
+        else if (direction3 == 0 && on_segment(first_point, second_point,
+                                                                forth_point))
             return true;
-        
+
         else
             return false;
     }
 
-    inline int direction(Point first_point, Point second_point, Point third_point){
-        return ((third_point.x-first_point.x) * (second_point.y-first_point.y)) - 
-                    ((second_point.x-first_point.x) * (third_point.y-first_point.y));
+    inline int direction(Point first_point, Point second_point,
+                                                    Point third_point) {
+        return ((third_point.x-first_point.x)*(second_point.y-first_point.y))-
+            ((second_point.x-first_point.x) * (third_point.y-first_point.y));
     }
 
-    inline bool on_segment(Point first_point, Point second_point, Point third_point){
-        if (min(first_point.x, second_point.x) <= third_point.x && third_point.x <= 
-            max(first_point.x, second_point.x) && min(first_point.y, second_point.y) <= third_point.y && 
-            third_point.y <= max(first_point.y, second_point.y))
+    inline bool on_segment(Point first_point, Point second_point,
+                                                        Point third_point) {
+        if (std::min(first_point.x, second_point.x) <= third_point.x &&
+            third_point.x <= std::max(first_point.x, second_point.x) &&
+            std::min(first_point.y, second_point.y) <= third_point.y &&
+            third_point.y <= std::max(first_point.y, second_point.y))
             return true;
-        
+
         else
             return false;
     }
 };
 
-int main(){
+int main() {
     SegmentIntersection segment;
     Point first_point, second_point, third_point, forth_point;
 
-    cin >> first_point.x >> first_point.y;
-    cin >> second_point.x >> second_point.y;
-    cin >> third_point.x >> third_point.y;
-    cin >> forth_point.x >> forth_point.y;
+    std::cin >> first_point.x >> first_point.y;
+    std::cin >> second_point.x >> second_point.y;
+    std::cin >> third_point.x >> third_point.y;
+    std::cin >> forth_point.x >> forth_point.y;
 
-    cout << endl << segment.intersect(first_point, second_point, third_point, forth_point) << endl;
+    printf("%d", segment.intersect(first_point, second_point, third_point,
+                                    forth_point));
+    std::cout << std::endl;
 }
