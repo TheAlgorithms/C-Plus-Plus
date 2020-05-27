fb1d69fb1410b092ba66cf53fd473a1473f50d3d
Ayaan Khan <ayaankhan98@gmail.com>
2020-05-27 18:51:05 +0530

    further improved comment blocks

--- sorting/quick_sort.cpp
+++ sorting/quick_sort.cpp
@@ -27,13 +27,13 @@
 #include <iostream>
 
 /**
- * This function takes last element as pivot, places  
- * the pivot element at its correct position in sorted  
- * array, and places all smaller (smaller than pivot)  
- * to left of pivot and all greater elements to right  
- * of pivot 
+ *      This function takes last element as pivot, places  
+ *      the pivot element at its correct position in sorted  
+ *      array, and places all smaller (smaller than pivot)  
+ *      to left of pivot and all greater elements to right  
+ *      of pivot 
  *
- * */
+ */
 
 int partition(int arr[], int low, int high) {
     int pivot = arr[high];    // taking the last element as pivot
