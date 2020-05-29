aeba616ae312ceefd4a95f3e45ba2e9d9229ad2e
Anmol3299 <mittalanmol22@gmail.com>
2020-05-29 23:57:50 +0530

    fix: Modernize trie code and fix memory management

--- data_structure/trie_tree.cpp
+++ data_structure/trie_tree.cpp
@@ -1,90 +1,108 @@
-#include <stdio.h>
-#include <stdbool.h>
-
 #include <iostream>
+#include <memory>
 #include <string>
 
-// structure definition
-typedef struct trie {
-    struct trie * arr[26];
-    bool isEndofWord;
-} trie;
+class Trie {
+   private:
+    static constexpr size_t ALPHABETS = 26;
 
-// create a new node for trie
-trie * createNode() {
-    trie * nn = new trie();
-    for (int i = 0; i < 26; i++)
-        nn -> arr[i] = NULL;
-    nn -> isEndofWord = false;
-    return nn;
-}
+    // Structure of trie node
+    struct TrieNode {
+        std::shared_ptr<TrieNode> character[ALPHABETS]{nullptr};
+        bool isEndOfWord{false};
+    };
 
-// insert string into the trie
-void insert(trie * root, std::string str) {
-    for (int i = 0; i < str.length(); i++) {
-        int j = str[i] - 'a';
-        if (root -> arr[j]) {
-            root = root -> arr[j];
-        } else {
-            root -> arr[j] = createNode();
-            root = root -> arr[j];
+    // Function to check if a node has some children which can form words.
+    inline static bool hasChildren(std::shared_ptr<TrieNode> node) {
+        for (size_t i = 0; i < ALPHABETS; i++) {
+            if (node->character[i]) {
+                return true;
+            }
         }
-    }
-    root -> isEndofWord = true;
-}
-
-// search a string exists inside the trie
-bool search(trie * root, std::string str, int index) {
-    if (index == str.length()) {
-        if (!root -> isEndofWord)
-            return false;
-        return true;
-    }
-    int j = str[index] - 'a';
-    if (!root -> arr[j])
         return false;
-    return search(root -> arr[j], str, index + 1);
-}
-
-/*
-removes the string if it is not a prefix of any  other 
-string, if it is then just sets the endofword to false, else 
-removes the given string
-*/
-bool deleteString(trie * root, std::string str, int index) {
-    if (index == str.length()) {
-        if (!root -> isEndofWord)
-            return false;
-        root -> isEndofWord = false;
-        for (int i = 0; i < 26; i++)
-            return false;
-        return true;
     }
-    int j = str[index] - 'a';
-    if (!root -> arr[j])
-        return false;
-    bool
-    var = deleteString(root, str, index + 1);
-    if (var) {
-        root -> arr[j] = NULL;
-        if (root -> isEndofWord) {
-            return false;
-        } else {
-            int i;
-            for (i = 0; i < 26; i++)
-                if (root -> arr[i])
-                    return false;
-            return true;
+
+    // Helper function to remove a word. The nodes are deleted only when the
+    // string is not a prefix of any other string, else isEndOfWord is set to
+    // false.
+    std::shared_ptr<TrieNode> removeWordHelper(const std::string& word,
+                                               std::shared_ptr<TrieNode> curr,
+                                               size_t index) {
+        if (word.size() == index) {
+            if (curr->isEndOfWord) {
+                curr->isEndOfWord = false;
+            }
+            if (hasChildren(curr)) {
+                return curr;
+            }
+            return nullptr;
         }
+
+        size_t idx = word[index] - 'a';
+        // Throw a runtime error in case the user enters a word which is not
+        // present in the trie.
+        if (!curr->character[idx]) {
+            throw std::runtime_error(std::move(std::string("Word not found.")));
+        }
+
+        curr->character[idx] =
+            removeWordHelper(word, curr->character[idx], index + 1);
+        if (curr->character[idx] || hasChildren(curr)) {
+            return curr;
+        }
+        return nullptr;
     }
-}
+
+   public:
+    // constructor to initialise the root of the trie.
+    Trie() : m_root(std::make_shared<TrieNode>()){};
+
+    // Insert string into the trie.
+    void insert(const std::string& word) {
+        auto curr = m_root;
+        for (char ch : word) {
+            size_t index = ch - 'a';
+            // if a node for current word is not already preset in trie, create
+            // a new node for it.
+            if (!curr->character[index]) {
+                curr->character[index] = std::make_shared<TrieNode>();
+            }
+
+            curr = curr->character[index];
+        }
+        curr->isEndOfWord = true;
+    }
+
+    // Search if a word is present in trie or not.
+    bool search(const std::string& word) {
+        auto curr = m_root;
+        for (char ch : word) {
+            size_t index = ch - 'a';
+            // if any node for a character is not found, then return that the
+            // word cannot be formed.
+            if (!curr->character[index]) {
+                return false;
+            }
+            curr = curr->character[index];
+        }
+        return curr->isEndOfWord;
+    }
+
+    // Main function to remove the word which calls the helper function.
+    void removeWord(const std::string& word) {
+        m_root = removeWordHelper(word, m_root, 0);
+    }
+
+   private:
+    std::shared_ptr<TrieNode> m_root;
+};
 
 int main() {
-    trie * root = createNode();
-    insert(root, "hello");
-    insert(root, "world");
-    int a = search(root, "hello", 0);
-    int b = search(root, "word", 0);
-    printf("%d %d ", a, b);
+    Trie trie;
+    trie.insert("hel");
+    trie.insert("hello");
+    trie.removeWord("hel");
+    std::cout << trie.search("hello") << '\n';
+
     return 0;
 }
