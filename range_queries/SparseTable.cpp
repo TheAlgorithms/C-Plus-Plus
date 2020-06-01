e53d77e58a7143daae77ac25f1e83d06d043de46
Arpan Mukherjee <arpan17007@iiitd.ac.in>
2020-06-01 18:09:48 +0530

    Added Sparse Table for range query

--- range_queries/SparseTable.cpp
+++ range_queries/SparseTable.cpp
@@ -8,10 +8,6 @@
 #include<iostream>
 using namespace std;
 
-#define INF     0x3f3f3f3f
-#define int     long long
-#define pii     pair<int, int>
-
 const int N = 100005;
 const int LOGN = 20;
 
@@ -20,17 +16,21 @@ int A[N];
 int logs[N];            // logs[i] = maximum p s.t. 2^p <= i
 int table[LOGN][N];     // table[i][j] = [j, j+2^i]
 
-void computeLogs(int n) {
+void computeLogs(int n)
+{
     logs[1] = 0;
     for(int i = 2 ; i < n ; i++)
         logs[i] = logs[i/2] + 1;
 }
 
-void buildTable(int n) {
+void buildTable(int n)
+{
     int i, j, curLen;
-    for(i = 0 ; i <= logs[n] ; i++) {
+    for(i = 0 ; i <= logs[n] ; i++)
+    {
         curLen = 1LL<<i;
-        for(j = 0 ; j + curLen < n ; j++) {
+        for(j = 0 ; j + curLen < n ; j++)
+        {
             if(curLen == 1)
                 table[i][j] = A[j];
             else
@@ -39,23 +39,24 @@ void buildTable(int n) {
     }
 }
 
-int getMinimum(int beg, int end) {
+int getMinimum(int beg, int end)
+{
     int p = logs[end - beg + 1];
     int pLen = 1LL << p;
     return min(table[p][beg], table[p][end - pLen + 1]);
 }
 
 
-int32_t main() {
+int main()
+{
     ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 
     int i, n, x;
-    cout << "Enter the array length:";
-    cin >> n;
+    scanf("%d", &n);
     for(i = 0 ; i < n ; i++)
-        cin >> A[i];
+        scanf("%d", &A[i]);
     computeLogs(n);
     buildTable(n);
-    cout << "Minimum of the range [3, 14]: " << getMinimum(2,  13) << endl;
+    printf("Minimum of the range [3, 14]: %d\n", getMinimum(2, 13));
     return 0;
 }
