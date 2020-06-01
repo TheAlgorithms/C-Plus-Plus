5a2615c54e021b3578536f596415c9cef4e6fdc3
Krishna Vedala <7001608+kvedala@users.noreply.github.com>
2020-05-31 23:08:57 -0400

    added sorting algos to namespace sorting

--- sorting/quick_sort.cpp
+++ sorting/quick_sort.cpp
@@ -24,48 +24,53 @@
 #include <cstdlib>
 #include <iostream>
 
-/**
- *      This function takes last element as pivot, places
- *      the pivot element at its correct position in sorted
- *      array, and places all smaller (smaller than pivot)
- *      to left of pivot and all greater elements to right
- *      of pivot
- *
- */
+namespace sorting {
+    /**
+     *      This function takes last element as pivot, places
+     *      the pivot element at its correct position in sorted
+     *      array, and places all smaller (smaller than pivot)
+     *      to left of pivot and all greater elements to right
+     *      of pivot
+     *
+     */
 
-int partition(int arr[], int low, int high) {
-    int pivot = arr[high];  // taking the last element as pivot
-    int i = (low - 1);      // Index of smaller element
+    int partition(int arr[], int low, int high) {
+        int pivot = arr[high];  // taking the last element as pivot
+        int i = (low - 1);      // Index of smaller element
 
-    for (int j = low; j < high; j++) {
-        // If current element is smaller than or
-        // equal to pivot
-        if (arr[j] <= pivot) {
-            i++;  // increment index of smaller element
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
+        for (int j = low; j < high; j++) {
+            // If current element is smaller than or
+            // equal to pivot
+            if (arr[j] <= pivot) {
+                i++;  // increment index of smaller element
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        return (i + 1);
+    }
+
+    /**
+     *      The main function that implements QuickSort
+     *      arr[] --> Array to be sorted,
+     *      low --> Starting index,
+     *      high --> Ending index
+     */
+    void quickSort(int arr[], int low, int high) {
+        if (low < high) {
+            int p = partition(arr, low, high);
+            quickSort(arr, low, p - 1);
+            quickSort(arr, p + 1, high);
         }
     }
-    int temp = arr[i + 1];
-    arr[i + 1] = arr[high];
-    arr[high] = temp;
-    return (i + 1);
-}
 
-/**
- *      The main function that implements QuickSort
- *      arr[] --> Array to be sorted,
- *      low --> Starting index,
- *      high --> Ending index
- */
-void quickSort(int arr[], int low, int high) {
-    if (low < high) {
-        int p = partition(arr, low, high);
-        quickSort(arr, low, p - 1);
-        quickSort(arr, p + 1, high);
-    }
-}
+}  // namespace sorting
+
+using sorting::quickSort;
 
 // prints the array after sorting
 void show(int arr[], int size) {
