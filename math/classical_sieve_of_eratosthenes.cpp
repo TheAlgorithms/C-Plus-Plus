b9dafb8c625430b85979c69e5587d95b9fc8a6e4
Rajiv Singh <rajivperfect007@gmail.com>
2020-06-03 22:22:07 +0530

    added classical_sieve_of_eratosthenes.cpp

---
+++ math/classical_sieve_of_eratosthenes.cpp
@@ -0,0 +1,48 @@
+/*
+ * Classical Sieve of Eratosthenes is an algorithm to find the primes
+ * that is between 2 to N (as defined in main) in fastest way.
+ *
+ * Time Complexity  : O(N * log(log(N)))
+ * Space Complexity : O(N)
+ */
+
+#include <iostream>
+
+#define MAX 10000000
+
+bool isprime[MAX];
+
+/*
+ * This is the function that finds the primes and eliminates
+ * the multiples.
+ */
+void sieve(int N) {
+  isprime[0] = true;
+  isprime[1] = true;
+  for (int i = 2; i * i <= N; ++i) {
+    if (!isprime[i]) {
+      for (int j = i * i; j <= N; j = j + i) {
+        isprime[j] = true;
+      }
+    }
+  }
+}
+
+/*
+ * This function prints out the primes to STDOUT
+ */
+void print(int N) {
+  for (int i = 1; i <= N; i++) {
+    if (!isprime[i]) {
+      std::cout << i << ' ';
+    }
+  }
+  std::cout << '\n';
+}
+
+int main() {
+  fastio();
+  int N = 100;
+  sieve(N);
+  print(N);
+}
