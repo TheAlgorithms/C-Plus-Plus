63342805d52a04857222e6b38799d16b6447da62
Rajiv Singh <rajivperfect007@gmail.com>
2020-06-03 22:28:53 +0530

    removed using namespace std

--- math/sieve_of_eratosthenes.cpp
+++ math/sieve_of_eratosthenes.cpp
@@ -1,5 +1,5 @@
 /*
- * Sieve of Eratosthenes is an algorithm to find the primes 
+ * Sieve of Eratosthenes is an algorithm to find the primes
  * that is between 2 to N (as defined in main).
  *
  * Time Complexity  : O(N * log N)
@@ -7,63 +7,52 @@
  */
 
 #include <iostream>
-using namespace std;
 
 #define MAX 10000000
 
 int isprime[MAX];
 
 /*
- * This is the function that finds the primes and eliminates 
+ * This is the function that finds the primes and eliminates
  * the multiples.
  */
-void sieve(int N)
-{
-    isprime[0] = 0;
-    isprime[1] = 0;
-    for (int i = 2; i <= N; i++)
-    {
-        if (isprime[i])
-        {
-            for (int j = i * 2; j <= N; j += i)
-            {
-                isprime[j] = 0;
-            }
-        }
+void sieve(int N) {
+  isprime[0] = 0;
+  isprime[1] = 0;
+  for (int i = 2; i <= N; i++) {
+    if (isprime[i]) {
+      for (int j = i * 2; j <= N; j += i) {
+        isprime[j] = 0;
+      }
     }
+  }
 }
 
 /*
  * This function prints out the primes to STDOUT
  */
-void print(int N)
-{
-    for (int i = 1; i <= N; i++)
-    {
-        if (isprime[i] == 1)
-        {
-            cout << i << ' ';
-        }
+void print(int N) {
+  for (int i = 1; i <= N; i++) {
+    if (isprime[i] == 1) {
+      std::cout << i << ' ';
     }
-    cout << '\n';
+  }
+  std::cout << '\n';
 }
 
 /*
- * NOTE: This function is important for the 
+ * NOTE: This function is important for the
  * initialization of the array.
  */
-void init()
-{
-    for (int i = 1; i < MAX; i++)
-    {
-        isprime[i] = 1;
-    }
+void init() {
+  for (int i = 1; i < MAX; i++) {
+    isprime[i] = 1;
+  }
 }
 
-int main()
-{
-    int N = 100;
-    init();
-    sieve(N);
-    print(N);
+int main() {
+  int N = 100;
+  init();
+  sieve(N);
+  print(N);
 }
