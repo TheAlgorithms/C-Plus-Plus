cf0dda16b7763281bc3b69af1c0f045b3d45158e
Rajiv Singh <rajivperfect007@gmail.com>
2020-06-05 01:55:42 +0530

    changed comment style

--- math/sieve_of_eratosthenes.cpp
+++ math/sieve_of_eratosthenes.cpp
@@ -1,10 +1,10 @@
-/*
+/**
  * Sieve of Eratosthenes is an algorithm to find the primes
  * that is between 2 to N (as defined in main).
  *
  * Time Complexity: O(N * log N)
  * Space Complexity: O(N)
- */
+**/
 
 #include <iostream>
 
