9b1f5a2b1801cad42b5fcc51e5a4dc03de462390
omkarlanghe <langhe.omkar96@gmail.com>
2020-06-06 23:46:29 +0530

    prime numbers algorithm implemented with optimized logic

--- Math/prime_number_most_optimized_logic.cpp
+++ Math/prime_number_most_optimized_logic.cpp
@@ -3,11 +3,11 @@ int isPrime(int);
 int isPrime(int no) {
 	int isPrime = 1;
 	if(no <= 1) {
-		return(0);
+		return 0;
 	} else if (no == 2) {
-		return(1);
+		return1;
 	} else if ((no&1) == 0) {
-		return(0);
+		return 0;
 	}
 	if(no >= 3) {
 		for(int i = 3 ; i < (no/2+1) ; i = i+2) {
@@ -17,19 +17,19 @@ int isPrime(int no) {
 			}
 		}
 	}
-	return(isPrime);
+	return isPrime;
 }
 
 int main() {
     int n;
-    std::cout<<"Enter the number to check if it is prime or not"<<std::endl;
-    std::cin>>n;
+    std::cout << "Enter the number to check if it is prime or not" << std::endl;
+    std::cin >> n;
     
     int result = isPrime(n);
     if(result) {
-        std::cout<<n<<" is a prime number"<<std::endl;
+        std::cout << n << " is a prime number" << std::endl;
     } else {
-        std::cout<<n<<" is not a prime number"<<std::endl;
+        std::cout << n << " is not a prime number" << std::endl;
     }
-    return(0);
+    return 0;
 }
