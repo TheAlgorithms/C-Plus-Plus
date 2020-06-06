cd46dc859193491f82b6cb62b6e0929e18b34033
Rashi Srivastava <43779027+rashisr19@users.noreply.github.com>
2020-06-06 14:00:04 +0530

    Updated format.
    Thank you for reviewing the PR. I have updated it according to the required norms.

--- others/sub_sequences.cpp
+++ others/sub_sequences.cpp
@@ -1,34 +1,24 @@
-//creating subsequences of a string
-
-#include<iostream>
-#include<cmath>
+// Copyright 2019 by Contributors
+#include <cmath>
+#include <iostream.h>
  
-using namespace std;
-
-
-void SubSequences(char* in,int i,char*out,int j)
-{
-	//base case
-	if(in[i]==0)
-	{
-		out[j]='\0';
-		cout<<out<<endl;
-		return ;
-	}
-	//Recursive case
-	
-	//ignore element of ith index
-	SubSequences(in,i+1,out,j);
-	//consider ith element
-	out[j]=in[i];
-	SubSequences(in,i+1,out,j+1);
+void SubSequences(char *in, int i, char *out, int j) {
+  // base case
+  if (in[i] == 0) {
+    out[j] = '\0';
+    std::cout << out << std::endl;
+    return;
+  }
+  // Recursive case
+  // ignore element of ith index
+  SubSequences(in, i + 1, out, j);
+  // consider ith element
+  out[j] = in[i];
+  SubSequences(in, i + 1, out, j + 1);
 }
-
  
-int main()
-{
-	char in[]="abcd";
-	char out[100];
-	
-	SubSequences(in,0,out,0);
+int main() {
+  char in[] = "abcd";
+  char out[100];
+  SubSequences(in, 0, out, 0);
 }
