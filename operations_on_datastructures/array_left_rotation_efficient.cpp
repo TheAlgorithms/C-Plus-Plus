bc5a4834ce3ed9aad2fd8987666188b42706a48f
Ekansh Bhatnagar <ekanshbhatnagar@outlook.com>
2020-06-07 18:03:04 +0530

    fix: fixed the format and identation according to cpplint

--- operations_on_datastructures/array_left_rotation_efficient.cpp
+++ operations_on_datastructures/array_left_rotation_efficient.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include <iostream>
 
 /*
 This program can be used to rotate an array d times towards left.
@@ -10,39 +10,40 @@ a   : Main array used.
 i   : Loop variable.
 j   : Loop variable.
 
-This program uses a more efficient logic to rotate the array as each element is roated d times in only single iteration.
-This uses less time for more long arrays.
+This program uses a more efficient logic to rotate the array as each element is
+roated d times in only single iteration. This uses less time for more long
+arrays.
 
 */
-int main()
-{
-    int n, d;
-	::std::cout << "Enter size of array=\t";
-	::std::cin >> n;
-	::std::cout << "Enter Number of indeces u want to rotate the array to left=\t";
-        ::std::cin >> d;
-	int a[n];
-	::std::cout << "Enter  elements of array=\t";
-	for (int i = 0; i < n; i++) {
-		::std::cin >> a[i];
-	}
-    int arr[d];
-    for(int i = 0; i < d; i++) {
-        arr[i] = a[i];
-    }
-    int j=0;
-    for(int i = d; i< n; i++) {
+int main() {
+  int n, d;
+  ::std::cout << "Enter size of array=\t";
+  ::std::cin >> n;
+  ::std::cout
+      << "Enter Number of indeces u want to rotate the array to left=\t";
+  ::std::cin >> d;
+  int a[n];
+  ::std::cout << "Enter  elements of array=\t";
+  for (int i = 0; i < n; i++) {
+    ::std::cin >> a[i];
+  }
+  int arr[d];
+  for (int i = 0; i < d; i++) {
+    arr[i] = a[i];
+  }
+  int j = 0;
+  for (int i = d; i < n; i++) {
     a[j] = a[i];
     j++;
-    }
-    j=0;
-    for(int i = n-d; i < n; i++) {
-    	a[i] = arr[j];
-        j++;
-    }
-	cout << "Your rotated array is=\t";
-	for (int j = 0; j < n; j++) {
-	    ::std::cout << a[j] << " ";
-	}
-	return 0;
+  }
+  j = 0;
+  for (int i = n - d; i < n; i++) {
+    a[i] = arr[j];
+    j++;
+  }
+  cout << "Your rotated array is=\t";
+  for (int j = 0; j < n; j++) {
+    ::std::cout << a[j] << " ";
+  }
+  return 0;
 }
