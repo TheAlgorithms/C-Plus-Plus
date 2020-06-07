48e75106aa25570fea7f97066e4498b118b746df
omkarlanghe <langhe.omkar96@gmail.com>
2020-06-07 16:42:49 +0530

    Optimized algorithm to check if number is prime or not.

---
+++ math/prime_number_optimized_logic.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+int is_prime(int no) {
+    int is_prime = 1;
+    if (no <= 1) {
+        return 0;
+    } else if (no == 2) {
+        return 1;
+    } else if (no & 1 == 0) {
+        return 0;
+    }
+    if (no >= 3) {
+        for (int i = 3; i < (no / 2 + 1); i = i + 2) {
+            if (no % i == 0) {
+                is_prime = 0;
+                break;
+            }
+        }
+    }
+    return is_prime;
+}
+
+int main() {
+    int n;
+    std::cout << "enter the number to check if it is prime or not" << std::endl;
+    std::cin >> n;
+    int result = is_prime(n);
+    if (result) {
+        std::cout << n << " is a prime number" << std::endl;
+    } else {
+        std::cout << n << " is not a prime number" << std::endl;
+    }
+}
