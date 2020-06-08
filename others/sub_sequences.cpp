82e1ea82850d8b54fbf999b6ab53ca46df107f65
Rashi Srivastava <43779027+rashisr19@users.noreply.github.com>
2020-06-08 20:08:37 +0530

    Updated as per the requirements.
    Updated header file, commenting style and added comments to briefly explain what is happening in the code.

--- others/sub_sequences.cpp
+++ others/sub_sequences.cpp
@@ -1,24 +1,23 @@
-// Copyright 2019 by Contributors
+/* Copyright 2019 by Contributors */
 #include <cmath>
-#include <iostream.h>
+#include <iostream>
  
-void SubSequences(char *in, int i, char *out, int j) {
-  // base case
+void sub_sequences(char *in, int i, char *out, int j) {
+  /* base case for printing the sequence */
   if (in[i] == 0) {
     out[j] = '\0';
     std::cout << out << std::endl;
     return;
   }
-  // Recursive case
-  // ignore element of ith index
-  SubSequences(in, i + 1, out, j);
-  // consider ith element
+  /* starting to form subsequences from the end */
+  sub_sequences(in, i + 1, out, j);
+  /* forming different sequences by adding rest of the elemets */
   out[j] = in[i];
-  SubSequences(in, i + 1, out, j + 1);
+  sub_sequences(in, i + 1, out, j + 1);
 }
  
 int main() {
   char in[] = "abcd";
   char out[100];
-  SubSequences(in, 0, out, 0);
+  sub_sequences(in, 0, out, 0);
 }
