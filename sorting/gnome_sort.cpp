36cfd7118c23dd1421adee4a9e15fb16521329bc
beqakd <bgoga16@freeuni.edu.ge>
2020-06-08 10:22:45 +0400

    style changes

--- sorting/gnome_sort.cpp
+++ sorting/gnome_sort.cpp
@@ -1,30 +1,29 @@
 #include <iostream>
 
 void gnomesort(int arr[], int size) {
-  // few easy cases
-  if (size <= 1)
-    return;
+    // few easy cases
+    if (size <= 1) return;
 
-  int index = 0; // initialize some variables.
-  while (index < size) {
-    // check for swap
-    if ((index == 0) || (arr[index] >= arr[index - 1])) {
-      index++;
-    } else {
-      std::swap(arr[index], arr[index - 1]); // swap
-      index--;
+    int index = 0;  // initialize some variables.
+    while (index < size) {
+        // check for swap
+        if ((index == 0) || (arr[index] >= arr[index - 1])) {
+            index++;
+        } else {
+            std::swap(arr[index], arr[index - 1]);  // swap
+            index--;
+        }
     }
-  }
 }
+
 // Our main function
 int main() {
-  int arr[] = {-2, -10, 100, 35, 34, 99};
-  int size = sizeof(arr) / sizeof(arr[0]);
+    int arr[] = {-22, 100, 150, 35, -10, 99};
+    int size = sizeof(arr) / sizeof(arr[0]);
 
-  gnomesort(arr, size);
+    gnomesort(arr, size);
 
-  for (int i = 0; i < size; i++)
-    printf("%d ", arr[i]);
+    for (int i = 0; i < size; i++) printf("%d ", arr[i]);
 
-  return 0;
+    return 0;
 }
