7baed740bf2d2b710d3f33c25a90420aff38a383
beqakd <bgoga16@freeuni.edu.ge>
2020-06-09 11:03:27 +0400

    changes

--- math/pythagoras.cpp
+++ math/pythagoras.cpp
@@ -1,28 +1,57 @@
-#include <math.h>
-
+#include <cmath>
 #include <iostream>
+#include <cassert>
 
-// Our class for point coordinates.
+/**
+ * The distance between two points in a three dimensional - 3D - coordinate system.
+ * For this we have struct point where we save coordinates
+ * of our points and later on calculate the distance as mentioned.
+ */
 struct  Point {
     int x;
     int y;
     int z;
 };
 
-// This method just calculate pythagorian distance from a point to b.
+/**
+ * The distance between two points in a three dimensional - 3D - coordinate system
+ */
 int distance(Point a, Point b) {
-    int x_2 = pow(abs(b.x - a.x), 2);
-    int y_2 = pow(abs(b.y - a.y), 2);
-    int z_2 = pow(abs(b.z - a.z), 2);
-    return sqrt(x_2 + y_2 + z_2);
+    int x_2 = std::pow(std::abs(b.x - a.x), 2);
+    int y_2 = std::pow(std::abs(b.y - a.y), 2);
+    int z_2 = std::pow(std::abs(b.z - a.z), 2);
+    return std::sqrt(x_2 + y_2 + z_2);
 }
 
-// Our main function.
+/**
+ * In this method we will test distance between 3D coordinates.
+ */
+void tests() {
+    Point point_fir = {2, 1, 7};
+    Point point_sec = {1, 3, 5};
+    assert(distance(point_fir, point_sec) == 3);
+
+    point_fir = {0, 0, 7};
+    point_sec = {1, 0, 0};
+    assert(distance(point_fir, point_sec) == 7);
+    
+    point_fir = {-7, 0, 7};
+    point_sec = {-1, 0, 1};
+    assert(distance(point_fir, point_sec) == 8);
+    
+    point_fir = {100, 200, 300};
+    point_sec = {400, 500, 600};
+    assert(distance(point_fir, point_sec) == 519);
+    
+    point_fir = {-1, -2, -3};
+    point_sec = {-1, -2, -3};
+    assert(distance(point_fir, point_sec) == 0);
+    
+}
+/**
+ * Our main function from where we will check our method.
+ */
 int main() {
-    // first point
-    Point point_fir = {2, -1, 7};
-    Point point_sec = {1, -3, 5};
-    // Distance should be 3!
-    std::cout << distance(point_fir, point_sec) << std::endl;
+    tests();
     return 0;
 }
