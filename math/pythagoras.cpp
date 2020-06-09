9363da6a46e8fa15d69a36a2e21217fc4f792557
beqakd <bgoga16@freeuni.edu.ge>
2020-06-09 11:06:10 +0400

    style changes

--- math/pythagoras.cpp
+++ math/pythagoras.cpp
@@ -1,20 +1,21 @@
+#include <cassert>
 #include <cmath>
 #include <iostream>
-#include <cassert>
 
 /**
- * The distance between two points in a three dimensional - 3D - coordinate system.
- * For this we have struct point where we save coordinates
- * of our points and later on calculate the distance as mentioned.
+ * The distance between two points in a three dimensional - 3D - coordinate
+ * system. For this we have struct point where we save coordinates of our points
+ * and later on calculate the distance as mentioned.
  */
-struct  Point {
+struct Point {
     int x;
     int y;
     int z;
 };
 
 /**
- * The distance between two points in a three dimensional - 3D - coordinate system
+ * The distance between two points in a three dimensional - 3D - coordinate
+ * system
  */
 int distance(Point a, Point b) {
     int x_2 = std::pow(std::abs(b.x - a.x), 2);
@@ -34,19 +35,18 @@ void tests() {
     point_fir = {0, 0, 7};
     point_sec = {1, 0, 0};
     assert(distance(point_fir, point_sec) == 7);
-    
+
     point_fir = {-7, 0, 7};
     point_sec = {-1, 0, 1};
     assert(distance(point_fir, point_sec) == 8);
-    
+
     point_fir = {100, 200, 300};
     point_sec = {400, 500, 600};
     assert(distance(point_fir, point_sec) == 519);
-    
+
     point_fir = {-1, -2, -3};
     point_sec = {-1, -2, -3};
     assert(distance(point_fir, point_sec) == 0);
-    
 }
 /**
  * Our main function from where we will check our method.
