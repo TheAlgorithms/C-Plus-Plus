3db639094c6981169322271d18da722bb6b425a7
Abhishekajoy <abhiajoy23@gmail.com>
2020-06-11 14:21:29 +0530

    fixed cpp-lint errors

--- sorting/pancake_sort.cpp
+++ sorting/pancake_sort.cpp
@@ -1,90 +1,77 @@
-#include<iostream>  
-using namespace std;  
-  
-/* Reverses arr[0..i] */
-void flip(int arr[], int i)  
-{  
-    int temp, start = 0;  
-    while (start < i)  
-    {  
-        temp = arr[start];  
-        arr[start] = arr[i];  
-        arr[i] = temp;  
-        start++;  
-        i--;  
-    }  
-}  
-  
-/* Returns index of the  
- maximum element in  
- arr[0..n-1]*/
-int findMax(int arr[], int n)  
-{  
-int mi, i;  
-for (mi = 0, i = 0; i < n; ++i)  
-    if (arr[i] > arr[mi])  
-            mi = i;  
-return mi;  
-}  
-  
-/* The main function that  
- sorts given array using  
- flip operations */ 
-void pancakeSort(int *arr, int n)  
-{  
-    /* Start from the complete  
-       array and one by one  
-       reduce current size  
-       by one               */
-    for (int curr_size = n; curr_size > 1; --curr_size)  
-    {  
-        /*Find index of the  
-         maximum element in  
-         arr[0..curr_size-1] */ 
-        int mi = findMax(arr, curr_size);  
-  
-        // Move the maximum  
-        // element to end of  
-        // current array if  
-        // it's not already  
-        // at the end  
-        if (mi != curr_size-1)  
-        {  
-            // To move at the end,  
-            // first move maximum  
-            // number to beginning  
-            flip(arr, mi);  
-  
-            // Now move the maximum  
-            // number to end by  
-            // reversing current array  
-            flip(arr, curr_size-1);  
-        }  
-    }  
-}  
-  
-// A utility function to print  
-void printArray(int arr[], int n)  
-{  
-    for (int i = 0; i < n; ++i)  
-        cout<< arr[i]<<" ";  
-}  
-   
-int main()  
-{  
-    int arr[20];  
-    int n;
-    cout<<"\nEnter the no of elements:";
-    cin>>n;
-    pancakeSort(arr, n); 
-    cout<<"\nEnter the elements:";
-    for(int i = 0; i < n; i++){
-        cin>>arr[i];
+/**
+ * Copyright 2020 @author Abhishek Ajoy Menon
+ *
+ * @file
+ *
+ * A generic implementation of pancake_sort.
+ */
+#include<iostream>
+// Sorting an array of integers in ascending order
+/**
+ * The algorithm finds the largest number in the array
+ * and moves it to the end of the array.
+ * This moving of array to the end is done by reversing the array
+ * from the start to the location of the largest number (which 
+ * brings the largest number to the beginning) and then reversing 
+ * the whole array. The reversing process is analogous to flipping
+ * pancakes, hence the name.
+*/
+// Reverses arr[0..i]
+void flip(int arr[], int i) {
+    int temp, start = 0;
+    while (start < i) {
+        temp = arr[start];
+        arr[start] = arr[i];
+        arr[i] = temp;
+        start++;
+        i--;
     }
-    pancakeSort(arr, n); 
-    cout<<"\nSorted Array "<<endl;  
-    printArray(arr, n);  
-  
-    return 0;  
-}  
-  
+}
+// Returns index of the  maximum element in   arr[0..n-1]
+int findMax(int arr[], int n) {
+    int mi, i;
+    for (mi = 0, i = 0; i < n; ++i)
+    if (arr[i] > arr[mi])
+    mi = i;
+    return mi;
+}
+// The main function that sorts given array using flip operations
+void pancakeSort(int *arr, int n) {
+    // Start from the complete array and
+    // one by one reduce current size by one
+    for (int curr_size = n; curr_size > 1; --curr_size) {
+        // Find index of the  maximum element in  arr[0..curr_size-1]
+        int mi = findMax(arr, curr_size);
+        // Move the maximum element to end of
+        // current array if it's not already at the end
+        if (mi != curr_size-1) {
+            // To move at the end,first move maximum number to beginning
+            flip(arr, mi);
+            // Now move the maximum number to end by reversing current array
+            flip(arr, curr_size-1);
+        }
+    }
+}
+// Sorting an array of integers in ascending order
+/**
+ *Reads the number of elements in the array
+ and then reads the elements. The array is sorted by calling the
+ pancakeSort function. The sorted array is then displayed.
+*/
+int main() {
+    int arr[20];
+    int n;
+    std::cout << "\nEnter the no of elements:";
+    std::cin >> n;
+    pancakeSort(arr, n);
+    std::cout << "\nEnter the elements:";
+    for ( int i = 0; i < n; i++ ) {
+        std::cin >> arr[i];
+    }
+    pancakeSort(arr, n);
+    std::cout << "\nSorted Array " << std::endl;
+    for (int i = 0; i < n; ++i)
+        std::cout << arr[i] << " ";
+    return 0;
+    std::cout << std::endl;
+}
