9a0991ed8a5b4bdabdf107ac045a73a3d5908de9
unknown <ananyasharma1442@gmail.com>
2020-06-12 13:41:39 +0530

    Edited as advised(Applied Generics to OOPS in Linked List)

--- data_structure/linked_list.cpp
+++ data_structure/linked_list.cpp
@@ -1,107 +1,115 @@
 #include <iostream>
-
+template  <class X>
 class node {
-        public:
-            int val;
-            node *next;
+public:
+    X val;
+    node<X> *next;
 };
 
+
+template  <class X>
 class linked_list
 {
 private:
-    node *start;
+    node<X> *start;
 public:
     linked_list()
     {
         start=NULL;
     }
-
-    void insert(int x) {
-            node *t = start;
-            node *n = new node;
-            n->val = x;
-            n->next = NULL;
-            if (start != NULL) {
-                    while (t->next != NULL) {
-                            t = t->next;
-                    }
-                    t->next = n;
-            } else {
-                    start = n;
-            }
-    }
-
-    void remove(int x) {
-            if (start == NULL) {
-                    std::cout << "\nLinked List is empty\n";
-                    return;
-            } else if (start->val == x) {
-                    node *temp = start;
-                    start = start->next;
-                    delete temp;
-                    return;
-            }
-            node *temp = start, *parent = start;
-            while (temp != NULL && temp->val != x) {
-                    parent = temp;
-                    temp = temp->next;
-            }
-            if (temp == NULL) {
-                    std::cout << std::endl << x << " not found in list\n";
-                    return;
-            }
-            parent->next = temp->next;
-            delete temp;
-    }
-
-        void search(int x) {
-            node *t = start;
-            int found = 0;
-            while (t != NULL) {
-                    if (t->val == x) {
-                            std::cout << "\nFound";
-                            found = 1;
-                            break;
-                    }
-                    t = t->next;
-            }
-            if (found == 0) {
-                    std::cout << "\nNot Found";
-            }
-    }
-
-    void show() {
-            node *t = start;
-            while (t != NULL) {
-                    std::cout << t->val << "\t";
-                    t = t->next;
-            }
-    }
-
-    void reverse() {
-            node *first = start;
-            if (first != NULL) {
-                    node *second = first->next;
-                    while (second != NULL) {
-                            node *tem = second->next;
-                            second->next = first;
-                            first = second;
-                            second = tem;
-                    }
-                    start->next = NULL;
-                    start = first;
-            } else {
-                    std::cout << "\nEmpty list";
-            }
-    }
+    void insert (X);
+    void remove (X);
+    void search (X);
+    void show();
+    void reverse();
 };
 
 
+template  <class X>
+void linked_list<X>::insert(X x) {
+        node <X>*t = start;
+        node <X>*n = new node<X>;
+        n->val = x;
+        n->next = NULL;
+        if (start != NULL) {
+                while (t->next != NULL) {
+                        t = t->next;
+                }
+                t->next = n;
+        } else {
+                start = n;
+        }
+}
+template  <class X>
+void linked_list<X>::remove(X x) {
+        if (start == NULL) {
+            std::cout << "\nLinked List is empty\n";
+            return;
+        } else if (start->val == x) {
+            node <X>*temp = start;
+            start = start->next;
+            delete temp;
+            return;
+        }
+        node<X> *temp = start, *parent = start;
+        while (temp != NULL && temp->val != x) {
+            parent = temp;
+            temp = temp->next;
+        }
+        if (temp == NULL) {
+            std::cout << std::endl << x << " not found in list\n";
+            return;
+        }
+        parent->next = temp->next;
+        delete temp;
+}
+template  <class X>
+void linked_list<X>::search(X x) {
+        node<X> *t = start;
+        int found = 0;
+        while (t != NULL) {
+                if (t->val == x) {
+                        std::cout << "\nFound";
+                        found = 1;
+                        break;
+                }
+                t = t->next;
+        }
+        if (found == 0) {
+                std::cout << "\nNot Found";
+        }
+}
+
+template  <class X>
+void linked_list<X>::show() {
+        node <X>*t = start;
+        while (t != NULL) {
+                std::cout << t->val << "\t";
+                t = t->next;
+        }
+}
+template  <class X>
+void linked_list<X>::reverse() {
+    node <X>*first = start;
+    if (first != NULL) {
+            node<X> *second = first->next;
+            while (second != NULL) {
+                    node<X> *tem = second->next;
+                    second->next = first;
+                    first = second;
+                    second = tem;
+            }
+            start->next = NULL;
+            start = first;
+    } else {
+            std::cout << "\nEmpty list";
+    }
+}
 
 
 int main() {
         int choice, x;
-        linked_list object;
+        linked_list<int> object;
         do {
                 std::cout << "\n1. Insert";
                 std::cout << "\n2. Delete";
