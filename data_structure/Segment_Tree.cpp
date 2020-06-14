e6445b7c251651b76ad74d8bc27daaf511e58dd5
LashaBuxo <lashabuxo@gmail.com>
2020-06-14 19:02:49 +0400

    Style Changes

--- data_structure/Segment_Tree.cpp
+++ data_structure/Segment_Tree.cpp
@@ -13,8 +13,6 @@
 #include <iostream>
 #include <vector>
 
-using namespace std;
-
 // Each Node contains it's value and index
 struct Node {
     int val;
@@ -25,8 +23,8 @@ struct Node {
 // the array = 4*N
 struct SegmentTree {
     int N;
-    vector<int> arr;
-    vector<Node> nodes;
+    std::vector<int> arr;
+    std::vector<Node> nodes;
 } ST;
 
 // PreProcessing
@@ -41,10 +39,10 @@ void BuildTree_Procedural(int v, int tl, int tr) {
             ST.nodes[v] = ST.nodes[2 * v];
         else
             ST.nodes[v] = ST.nodes[2 * v + 1];
-    };
+    }
 }
 
-void BuildTree(vector<int> arr) {
+void BuildTree(std::vector<int> arr) {
     ST.N = arr.size();
     ST.arr = arr;
     ST.nodes.resize(ST.N * 4 + 1);
@@ -57,9 +55,9 @@ Node getMaximumOnInterval_Procedural(int v, int tl, int tr, int l, int r) {
     if (tl == l && tr == r) return ST.nodes[v];
     int tm = (tl + tr) / 2;
     Node p, q;
-    p = getMaximumOnInterval_Procedural(2 * v, tl, tm, l, min(r, tm));
-    q = getMaximumOnInterval_Procedural(2 * v + 1, tm + 1, tr, max(l, tm + 1),
-                                        r);
+    p = getMaximumOnInterval_Procedural(2 * v, tl, tm, l, std::min(r, tm));
+    q = getMaximumOnInterval_Procedural(2 * v + 1, tm + 1, tr,
+                                        std::max(l, tm + 1), r);
     if (p.val > q.val)
         return p;
     else
@@ -72,17 +70,17 @@ Node GetMaximumOnInterval(int l, int r) {
 
 int main() {
     // Debug
-    vector<int> exampleArray{17, 3, 190, 2, 34, -90, 19, 100, 9};
+    std::vector<int> exampleArray{17, 3, 190, 2, 34, -90, 19, 100, 9};
 
     BuildTree(exampleArray);
     Node answer = GetMaximumOnInterval(2, 7);
-    cout << "For array {";
+    std::cout << "For array {";
     for (int i = 0; i < exampleArray.size(); i++) {
-        cout << exampleArray[i];
-        if (i != exampleArray.size() - 1) cout << ",";
+        std::cout << exampleArray[i];
+        if (i != exampleArray.size() - 1) std::cout << ",";
     }
-    cout << "} on interval (2,7) Maximum is " << answer.val << " at index "
-         << answer.ind << endl;
+    std::cout << "} on interval (2,7) Maximum is " << answer.val << " at index "
+              << answer.ind << std::endl;
 
     // Tests
     assert(GetMaximumOnInterval(2, 7).val == 190);
