45a7a99ca65554d42f6bfbb02eead1882f342de2
Naqiya Ghulamali <naqiyaghulamali@Naqiyas-MBP.home>
2020-06-14 23:55:19 -0400

    fix: median_search bug

--- search/median_search.cpp
+++ search/median_search.cpp
@@ -15,6 +15,7 @@ void comp(X x)
 {
     if(s1.size()>=x && s1.size()+s2.size()<x)
     {
+        cout<<"Case 1"<<endl;
         cout<<s2[0]<<" is the "<<x+1<<"th element from front";
     }
     else if(s1.size()>x)
@@ -29,8 +30,9 @@ void comp(X x)
     }
     else
     {
-        cout<<x+1<<" is invalid location";
+        cout<<x+1<<" is an invalid location";
     }
+    cout<<endl;
 }
 int main()
 {
@@ -44,28 +46,24 @@ int main()
     }
     int median=rand()%1000;
     cout<<"\nmedian="<<median<<endl;
-    int avg1,avg2,avg3,sum1=0,sum2=0,sum3=0;
     for(int i=0;i<1000;i++)
     {
-        if(v.back()==v[median])
+        if(v.back()==median)
         {
-            avg1=sum1+v.back();
             s2.push_back(v.back());
         }
-        else if(v.back()<v[median])
+        else if(v.back()<median)
         {
-            avg2=sum2+v.back();
             s1.push_back(v.back());
         }
         else
         {
-            avg3=sum3+v.back();
             s3.push_back(v.back());
         }
         v.pop_back();
     }
     int x;
-    cout<<"enter the no. to be searched form begining:- ";
+    cout<<"enter the no. to be searched from beginning: ";
     cin>>x;
     comp(x-1);
     return 0;
