4c261744f6cf3681b8cd9629e12da72c81c6801b
Ayaan Khan <ayaankhan98@gmail.com>
2020-06-19 20:41:13 +0530

    making branch gh-pages empty [deleted all files] (#859)

--- html/search/all_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['merge_1',['merge',['../non__recursive__merge__sort_8cpp.html#aee1ab42c3a838aa040efd8e784f4a9ab',1,'non_recursive_merge_sort.cpp']]]
-];
--- html/search/pages_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['a_20demonstration_20of_20auto_2dgenerated_20documentation_20using_20doxygen_2e_7',['A demonstration of auto-generated documentation using Doxygen.',['../index.html',1,'']]]
-];
