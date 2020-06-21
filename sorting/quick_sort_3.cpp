d2397467730ab6ca86103483a6f72c4db625117c
mohit <rj7555555@gmail.com>
2020-06-21 16:34:03 +0530

    changed quick_sort_3

--- sorting/quick_sort_3.cpp
+++ sorting/quick_sort_3.cpp
@@ -2,86 +2,89 @@
 Author: immortal-j
 Implementation Details
 Quick sort 3 works on Dutch National Flag Algorithm
-The major diffrence between simple quicksort and quick sort 3 comes in the
-function quick_sort_partition3 In quick_sort_partition3 we divide the
-vector/array into 3 parts i.e a[0...q_l]-->less than pivot
-a[q_l+1...q_e-1]-->Equal to pivot
-a[q_e...r]-->Greater than pivot
+The major diffrence between simple quicksort and quick sort 3 comes in the function partition3
+In quick_sort_partition3 we divide the vector/array into 3 parts.
 quick sort 3 works faster in some cases as compared to simple quicksort.
-specially when there are many identical values.
 */
-#include <cstdlib>
 #include <iostream>
-#include <vector>
-
-/*This function takes first element as pivot and places
-  the pivot element at its correct position in sorted
-  array i.e all elements less than pivot before it and greater
-  than pivot after it
-*/
-const std::vector<int> quick_sort_partition3(std::vector<int> &arr, int low,
-                                             int high) {
-  int x = arr[low]; // taking first element as pivot
-  int q_l = low; // We initiate q_l to be the part that is less than the pivot
-  int count = low;
-  int q_e = high; // The part that is greater than the pivot
-  std::vector<int> give_back(
-      2); // this vector is used to return positions of higher and lower
-  while (count <= q_e) {
-    if (arr[count] < x) {
-      std::swap(arr[q_l], arr[count]);
-      q_l++;
-      count++; // increment index
-    } else if (arr[count] == x) {
-      count++; // increment index
-    } else {
-      std::swap(arr[count], arr[q_e]);
-      q_e -= 1;
-    }
-    give_back[0] = q_l;
-    give_back[1] = q_e;
-  }
-  return give_back;
-}
-/*
-arr-->vector to be sorted
-low-->Starting index
-high-->Ending Index
-randomized_quick_sort is used to generate a random number in the range
-low to high to work as a pivot and then the at pivot with element at first
-position
-*/
-const void randomized_quick_sort(std::vector<int> &arr, int low, int high) {
-  if (low >= high) {
-    return;
-  }
-  int random = low + rand_r() % (high - low + 1); // provides a random index;
-  std::swap(arr[low], arr[random]);
-  std::vector<int> markers = quick_sort_partition3(
-      arr, low,
-      high); // Funtion quick_sort_partition3 is called which returns position
-  randomized_quick_sort(
-      arr, low,
-      markers[0] -
-          1); // of elements lower than above value at random index and position
-  randomized_quick_sort(arr, markers[1] + 1,
-                        high); // of all elements greater than above value at
-                               // random index and position
-}
-// Driver program to test above functions
-int main() {
-  int sizeofvector;
-  std::cin >> sizeofvector;
-  std::vector<int> arr(sizeofvector);
-  // Takes input
-  for (size_t i = 0; i < arr.size(); ++i) {
-    std::cin >> arr[i];
-  }
-  randomized_quick_sort(arr, 0, arr.size() - 1);
-  // prints vector/array after sorting
-  for (size_t i = 0; i < arr.size(); ++i) {
-    std::cout << arr[i] << ' ';
-  }
-  std::cout << std::endl;
-  return 0;
+//function to swap elements
+void swap(int *a, int *b) 
+{ 
+    int temp = *a; 
+    *a = *b; 
+    *b = temp; 
+} 
+  
+// function to print an array 
+void print_arr(int arr[], int n) 
+{ 
+    for (int i = 0; i < n; ++i) 
+        std::cout<<arr[i]<<" "; 
+    std::cout<<"\n"; 
+} 
+  
+/* This function partitions arr[] in three parts 
+a) arr[l..i] contains all elements smaller than pivot 
+b) arr[i+1..j-1] contains all occurrences of pivot 
+c) arr[j..r] contains all elements greater than pivot */
+void partition3(int arr[], int low, int high, int &i, int &j) 
+{ 
+    // To handle 2 elements 
+    if (high - low <= 1) 
+    { 
+        if (arr[high] < arr[low]) 
+            swap(&arr[high], &arr[low]); 
+        i = low; 
+        j = high; 
+        return; 
+    } 
+  
+    int mid = low; 
+    int pivot = arr[high]; 
+    while (mid <= high) 
+    { 
+        if (arr[mid]<pivot) 
+            swap(&arr[low++], &arr[mid++]); 
+        else if (arr[mid]==pivot) 
+            mid++; 
+        else if (arr[mid]>pivot) 
+            swap(&arr[mid], &arr[high--]); 
+    } 
+  
+    //update i and j 
+    i = low-1; 
+    j = mid; //or high-1 
+} 
+  
+// 3-way partition based quick sort 
+void quicksort(int arr[], int low, int high) 
+{ 
+    if (low>=high) //1 or 0 elements 
+        return; 
+  
+    int i, j; 
+  
+    // i and j are passed as reference 
+    partition3(arr, low, high, i, j); 
+  
+    // Recur two halves 
+    quicksort(arr, low, i); 
+    quicksort(arr, j, high); 
+} 
+  
+// Driver program for above functions
+int main() 
+{ 
+    int size,i;
+    std::cout<<"Enter Number of elements\n";
+    std::cin>>size;
+    int arr[size];
+    for(i=0;i<size;i++)
+    {
+        std::cin>>arr[i];
+    } 
+    quicksort(arr, 0, size - 1); 
+    std::cout<<"Sorted Array is:";
+    print_arr(arr, size); 
+    return 0; 
 }
