15b09ede91ab2344c11327ec7abf1d43dceb94de
Krishna Vedala <7001608+kvedala@users.noreply.github.com>
2020-06-24 12:43:40 -0400

    Revert "fix: LGTM code quality and Added docs"

--- data_structures/binaryheap.cpp
+++ data_structures/binaryheap.cpp
@@ -13,16 +13,13 @@ class MinHeap {
     int heap_size;  ///< Current number of elements in min heap
 
  public:
-    /** Constructor: Builds a heap from a given array a[] of given size
+    /** Constructor
      * \param[in] capacity initial heap capacity
      */
-    explicit MinHeap(int cap) {
-        heap_size = 0;
-        capacity = cap;
-        harr = new int[cap];
-    }
+    MinHeap(int capacity);
 
-    /** to heapify a subtree with the root at given index */
+    /** to heapify a subtree with the root at given index
+     */
     void MinHeapify(int);
 
     int parent(int i) { return (i - 1) / 2; }
@@ -47,9 +44,14 @@ class MinHeap {
 
     /** Inserts a new key 'k' */
     void insertKey(int k);
-
-    ~MinHeap() { delete[] harr; }
 };
+
+/** Constructor: Builds a heap from a given array a[] of given size */
+MinHeap::MinHeap(int cap) {
+    heap_size = 0;
+    capacity = cap;
+    harr = new int[cap];
+}
 
 // Inserts a new key 'k'
 void MinHeap::insertKey(int k) {
