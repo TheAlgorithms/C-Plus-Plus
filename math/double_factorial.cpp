66eb05e0daf68fdc43e0da3bf4a7b56038191b45
Krishna Vedala <7001608+kvedala@users.noreply.github.com>
2020-06-25 09:59:36 -0400

    added wiki link in file brieff

--- math/double_factorial.cpp
+++ math/double_factorial.cpp
@@ -1,8 +1,9 @@
 /**
  * @file
- * @brief Compute double factorial: \f$n!!\f$
+ * @brief Compute [double
+ * factorial](https://en.wikipedia.org/wiki/Double_factorial): \f$n!!\f$
  *
- * Double factorial of a non-negative integer n, is defined as the product of
+ * Double factorial of a non-negative integer `n`, is defined as the product of
  * all the integers from 1 to n that have the same parity (odd or even) as n.
  * <br/>It is also called as semifactorial of a number and is denoted by
  * \f$n!!\f$
