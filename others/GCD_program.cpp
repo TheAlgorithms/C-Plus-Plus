04cc255a908b10631a18ced74153cde407de7f77
gourav majee <44859494+varuogm@users.noreply.github.com>
2020-06-27 01:22:23 +0530

    Update others/GCD_program.cpp
    Co-authored-by: David Leal <halfpacho@gmail.com>

--- others/GCD_program.cpp
+++ others/GCD_program.cpp
@@ -4,9 +4,10 @@
 #include <iostream>
 using namespace std;
 int gcd(int a, int b) {
-	if (b == 0) 
-		return a; 
-	return gcd(b, a % b); 	
+	if (b == 0) {
+		return a;
+	}
+	return gcd(b, a % b);
 } 
 int main() 
 { 
