912b612d25139d6f2a57e0aa23178e7756ea1ba4
gourav majee <44859494+varuogm@users.noreply.github.com>
2020-06-27 00:46:41 +0530

    find gcd of two numbers
    this is program for finding GCD of two numbers

---
+++ others/GCD_program.cpp
@@ -0,0 +1,19 @@
+
+/*C++ inbuilt function for finding GCD*/
+
+#include<iostream>
+using namespace std;
+int gcd(int a, int b) 
+{ 
+	if (b == 0) 
+		return a; 
+	return gcd(b, a % b); 	
+} 
+int main() 
+{ 
+	int a = 8, b = 6; 
+	cout<<"GCD of two num is "<<gcd(a, b); 
+	return 0; 
+} 
+//also theere is simple way to do gcd by using    __gcd(m, n)  where m ,n are numbers
+
