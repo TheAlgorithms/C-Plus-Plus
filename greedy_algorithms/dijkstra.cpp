a330eb8e8e90b98c293dcc06558445504ee697ac
Ali Mir <ali.mir@mongodb.com>
2020-06-28 17:11:53 -0400

    fix: comments and style in greedy_algorithms/dijkstra.cpp

--- greedy_algorithms/dijkstra.cpp
+++ greedy_algorithms/dijkstra.cpp
@@ -1,29 +1,38 @@
+/**
+ * @file
+ * \brief Implementation of Dijkstra's Algorithm
+ * \details
+ * Dijkstra's Algorithm is a greedy solution for the single-source shortest path problem in a graph whose edges have integer weights.
+ * This solution takes in input to build the graph and outputs distances to nodes from a selected source node.
+ * @author ali-mir
+*/
+
 #include <limits.h>
 #include <iostream>
 
-using namespace std;
-
-// Wrapper class for storing a graph
+/**
+* Class for representing a graph.
+*/
 class Graph {
  public:
     int vertexNum;
     int **edges;
 
-    // Constructs a graph with V vertices and E edges
-    Graph(const int V) {
-        // initializes the array edges.
+    /**
+     * \brief Constructor that initiliazes the graph by creating the edges array.
+     */
+    explicit Graph(const int V) {
         this->edges = new int *[V];
         for (int i = 0; i < V; i++) {
             edges[i] = new int[V];
         }
 
-        // fills the array with zeros.
+        // Fills the array with zeros.
         for (int i = 0; i < V; i++) {
             for (int j = 0; j < V; j++) {
                 edges[i][j] = 0;
             }
         }
-
         this->vertexNum = V;
     }
 
@@ -32,7 +41,14 @@ class Graph {
         this->edges[src][dst] = weight;
     }
 };
-// Utility function to find minimum distance vertex in mdist
+/**
+ * Function to find minimum distance vertex in mdist.
+ * @param mdist[] array of minimum distances to vertices
+ * @param vset[] array of vertices, where vset[i] is a bool that indicates
+ * whether the vertex i is included in the shortest path tree
+ * @param int number of vertices
+ * @return minimum distance vertex
+ */
 int minDistance(int mdist[], bool vset[], int V) {
     int minVal = INT_MAX, minInd = 0;
     for (int i = 0; i < V; i++) {
@@ -45,20 +61,26 @@ int minDistance(int mdist[], bool vset[], int V) {
     return minInd;
 }
 
-// Utility function to print distances
+/**
+ * Function to print minimum distances to vertices.
+ * @param dist[] array of distances to vertices
+ * @param int number of vertices
+ */
 void print(int dist[], int V) {
-    cout << "\nVertex  Distance" << endl;
+    std::cout << "\nVertex  Distance" << std::endl;
     for (int i = 0; i < V; i++) {
         if (dist[i] < INT_MAX)
-            cout << i << "\t" << dist[i] << endl;
+            std::cout << i << "\t" << dist[i] << std::endl;
         else
-            cout << i << "\tINF" << endl;
+            std::cout << i << "\tINF" << std::endl;
     }
 }
 
-// The main function that finds the shortest path from given source
-// to all other vertices using Dijkstra's Algorithm.It doesn't work on negative
-// weights
+/**
+ * The main function that finds the shortest path from given source
+ * to all other vertices using Dijkstra's Algorithm. It doesn't work on negative
+ * weights.
+*/
 void Dijkstra(Graph graph, int src) {
     int V = graph.vertexNum;
     int mdist[V];  // Stores updated distances to vertex
@@ -90,35 +112,34 @@ void Dijkstra(Graph graph, int src) {
     print(mdist, V);
 }
 
-// Driver Function
+/** Main function */
 int main() {
     int V, E, gsrc;
     int src, dst, weight;
-    cout << "Enter number of vertices: ";
-    cin >> V;
-    cout << "Enter number of edges: ";
-    cin >> E;
+    std::cout << "Enter number of vertices: ";
+    std::cin >> V;
+    std::cout << "Enter number of edges: ";
+    std::cin >> E;
     Graph G(V);
     for (int i = 0; i < E; i++) {
-        cout << "\nEdge " << i + 1 << "\nEnter source: ";
-        cin >> src;
-        cout << "Enter destination: ";
-        cin >> dst;
-        cout << "Enter weight: ";
-        cin >> weight;
+        std::cout << "\nEdge " << i + 1 << "\nEnter source: ";
+        std::cin >> src;
+        std::cout << "Enter destination: ";
+        std::cin >> dst;
+        std::cout << "Enter weight: ";
+        std::cin >> weight;
 
-        // makes sure source and destionation are in the proper bounds.
+        // makes sure source and destination are in the proper bounds.
         if (src >= 0 && src < V && dst >= 0 && dst < V) {
             G.addEdge(src, dst, weight);
         } else {
-            cout << "source and/or destination out of bounds" << endl;
+            std::cout << "source and/or destination out of bounds" << std::endl;
             i--;
             continue;
         }
     }
-    cout << "\nEnter source:";
-    cin >> gsrc;
+    std::cout << "\nEnter source:";
+    std::cin >> gsrc;
     Dijkstra(G, gsrc);
-
     return 0;
 }
