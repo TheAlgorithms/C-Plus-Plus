cca3364d9d17a239032cc78a18ab8b804e39c07e
vakhokoto <v.kotoreishvili@gmail.com>
2020-07-07 11:27:38 +0400

    stylistical fixes

--- dynamic_programming/hamiltons-cycle.cpp
+++ dynamic_programming/hamiltons-cycle.cpp
@@ -1,27 +1,33 @@
+#include <stdio.h>
 #include <iostream>
 #include <vector>
 
 typedef bool (*test_func_t)(void);
 
-using namespace std;
-
-
-/* function to takes boolean the matrix of
-    [i, j] is true if there is a road between from i to j */
+/**
+ * The function determines if there is a hamilton's cycle in the graph
+ * 
+ * @param n is the number of vertices in the graph
+ * @param routes nxn boolean matrix of [i, j] where [i, j] is true if there is a road between from i to j
+*/
 bool hamilton_cycle(bool **routes, int n){
-    int height = 1 << n, width = n;
-    bool dp[(1 << n)][n];
+    // height of dp array which is 2^n
+    int height = 1 << n;
+    bool dp[height][n];
     for (int i=0; i<height; i++){
-        for (int j=0; j<width; j++){
+        for (int j=0; j<n; j++){
             dp[i][j] = false;
         }
     }
 
-    dp[1][0] = true;
+    // to fill in the [2^i, i] cells with true
+    for (int i=0; i<n; ++i){
+        dp[1 << i][i] = true;
+    }
     for (int i=1; i<height; i++){
-        vector <int> zeros, ones;
-        /* finding positions with 1s and 0s and separate them */
-        for (int pos=0; pos < width; ++pos){
+        std::vector <int> zeros, ones;
+        // finding positions with 1s and 0s and separate them 
+        for (int pos=0; pos < n; ++pos){
             if ((1 << pos) & i){
                 ones.push_back(pos);
             } else {
@@ -31,25 +37,31 @@ bool hamilton_cycle(bool **routes, int n){
 
         for (int pos = 0; pos < ones.size(); ++pos){
             int o = ones[pos];
-            if (!dp[i][o])
+            if (!dp[i][o]) {
                 continue;
+            }
 
             for (int inner_pos = 0; inner_pos < zeros.size(); inner_pos++){
                 int z = zeros[inner_pos];
-                if (!routes[o][z])
+                if (!routes[o][z]){
                     continue;
+                }
                 dp[i + (1 << z)][z] = 1;
             }
         }
     }
 
     bool is_cycle = false;
-    for (int i=0; i<width; i++){
+    for (int i=0; i<n; i++){
         is_cycle |= dp[height - 1][i];
     }
     return is_cycle;
 }
 
+/**
+ * this test is testing if hamilton_cycle returns true for 
+ * graph: 1 -> 2 -> 3 -> 4
+*/
 bool test1(){
     int n = 4;
 
@@ -68,14 +80,18 @@ bool test1(){
     }
 
     bool ans = hamilton_cycle(b, n);
-    for (int i=0; i<n; ++i){
-        delete b[i];
-    }
-    delete b;
+    delete[] b;
 
     return ans;
 }
 
+/**
+ * this test is testing if hamilton_cycle returns false for 
+ * graph: 1 -> 2 -> 3
+ *             |
+ *             V
+ *             4
+*/
 bool test2(){
     int n = 4;
 
@@ -94,14 +110,15 @@ bool test2(){
     }
 
     bool ans = hamilton_cycle(b, n);
-    for (int i=0; i<n; ++i){
-        delete b[i];
-    }
-    delete b;
+    delete[] b;
 
     return !ans;
 }
 
+/**
+ * this test is testing if hamilton_cycle returns true for
+ * clique with 4 vertices
+*/
 bool test3(){
     int n = 4;
 
@@ -120,15 +137,16 @@ bool test3(){
     }
 
     bool ans = hamilton_cycle(b, n);
-    for (int i=0; i<n; ++i){
-        delete b[i];
-    }
-    delete b;
+    delete[] b;
 
     return ans;
 }
 
-int main() {
+/**
+ * Main function
+*/
+int main(int argc, char **argv) {
+    // array of tests
     test_func_t tests[] = {
         test1,
         test2,
@@ -137,11 +155,12 @@ int main() {
     int test_num = 3;
     for (int t = 0; t < test_num; t++){
         test_func_t cur = tests[t];
-        if (!cur()) {
+        if (cur()) {
             printf("test %d PASSED!\n", t + 1);
         } else {
             printf("test %d FAILED!\n", t + 1);
         }
     }
+    std::cout <<std::endl;
     return 0;
 }
