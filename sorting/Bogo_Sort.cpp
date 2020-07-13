268172aac7979fef70aacc33f0a03858082e9bda
unknown <deepraval2905@gmail.com>
2020-07-13 18:02:33 +0530

    Changed vector to array and changed description format

--- sorting/Bogo_Sort.cpp
+++ sorting/Bogo_Sort.cpp
@@ -1,15 +1,25 @@
-// In computer science, bogosort (also known as permutation sort, stupid sort, slowsort, shotgun sort, random sort,
-//monkey sort, bobosort or shuffle sort) is a highly inefficient sorting algorithm based on the generate and test paradigm. The function
-//successively generates permutations of its input until it finds one that is sorted. It is not useful for sorting, but may be used for
-//educational purposes, to contrast it with more efficient algorithms.
-//  Two versions of this algorithm exist: a deterministic version that enumerates all permutations until it hits a sorted one,
-// and a randomized version that randomly permutes its input. 
-//Randomized version is implemented below.
+/**
+ * @file
+ * @brief Bogo sort algorithm
+ *
+ * Implementation Details -
+ *      In computer science, bogosort (also known as permutation sort, stupid sort, slowsort, 
+ *      shotgun sort, random sort, monkey sort, bobosort or shuffle sort) is a highly inefficient 
+ *      sorting algorithm based on the generate and test paradigm.Two versions of this algorithm 
+ *      exist: a deterministic version that enumerates all permutations until it hits a sorted one,
+ *      and a randomized version that randomly permutes its input.Randomized version is implemented here. 
+ *
+ * Algorithm -      
+ *      
+ *      Shuffle the array untill array is sorted.
+ *
+ */
 #include <iostream>
-#include <vector>
+#include <array>
 #include <cassert>
 //This function checks if array is sorted
-bool is_sorted(const std::vector <int> &arr)
+template<typename T, size_t N>
+bool is_sorted(const std::array <T, N> &arr)
 {
     for(int i = 0; i < arr.size() - 1; i ++)
     {
@@ -22,7 +32,8 @@ bool is_sorted(const std::vector <int> &arr)
     return true;
 }
 //This function prints the array
-void show_array(const std::vector <int> &arr)
+template<typename T, size_t N>
+void show_array(const std::array <T, N> &arr)
 {
     for(int x : arr)
     {
@@ -31,7 +42,8 @@ void show_array(const std::vector <int> &arr)
     std::cout << '\n';
 }
 //This function shuffles the array randomly
-std::vector <int> shuffle(std::vector <int> arr)
+template<typename T, size_t N>
+std::array <T, N> shuffle(std::array <T, N> arr)
 {
     for(int i = 0; i < arr.size(); i ++)
     {
@@ -41,7 +53,8 @@ std::vector <int> shuffle(std::vector <int> arr)
     return arr;
 }
 //Randomized bogo sort
-std::vector <int> randomized_bogosort(std::vector <int> arr)
+template<typename T, size_t N>
+std::array <T, N> randomized_bogosort(std::array <T, N> arr)
 {
     //Untill array is not sorted
     while(!is_sorted(arr))
@@ -53,7 +66,7 @@ std::vector <int> randomized_bogosort(std::vector <int> arr)
 //Driver Code
 int main()
 {
-    std::vector <int> arr = {3, 7, 10, 4, 1}; // Defining array which we want to sort
+    std::array <int, 5> arr = {3, 7, 10, 4, 1}; // Defining array which we want to sort
     arr = randomized_bogosort(arr); //Callling bogo sort on it
     std::cout << "Sorted Array (Randomized Bogosort) : " ;
     show_array(arr); //Printing sorted array
