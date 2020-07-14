4e7b0d904ce30e5193db5799a60b4a353d048942
shoniavika <vshon17@freeuni.edu.ge>
2020-07-14 22:04:56 +0400

    added documentation

--- data_structures/queue_using_two_stacks.cpp
+++ data_structures/queue_using_two_stacks.cpp
@@ -1,3 +1,13 @@
+/*
+    Implementation of a Queue using two Stacks.
+
+    Functions:
+        Push  -> Inserts element to the end of the queue
+        Pop   -> Removes element from the front
+        Peek  -> Returns element from the front (no removing)
+        Empty -> Checks whether the queue is empty
+*/
+
 #include <iostream>
 
 #include "./stack.h"
@@ -5,6 +15,9 @@
 class MyQueue {
  private:
     stack<int> s1, s2;
+
+    /** Helper function for rearranging elements in stacks */
+    void rearrange();
 
  public:
     /** Initializes queue. */
@@ -21,12 +34,16 @@ class MyQueue {
 
     /** Returns whether the queue is empty. */
     bool empty();
-
-    void rearrange();
 };
 
+/**
+ * Appends element to the end of the queue
+ */
 void MyQueue::push(int x) { s1.push(x); }
 
+/**
+ * Removes element from the front of the queue
+ */
 int MyQueue::pop() {
     rearrange();
     int temp = 0;
@@ -39,6 +56,10 @@ int MyQueue::pop() {
     return temp;
 }
 
+/**
+ * Helper function for rearranging elements from
+ * one stack into another.
+ */
 void MyQueue::rearrange() {
     if (s2.isEmptyStack()) {
         while (!s1.isEmptyStack()) {
@@ -49,6 +70,10 @@ void MyQueue::rearrange() {
     }
 }
 
+/**
+ * Returns element in the front.
+ * Does not remove it.
+ */
 int MyQueue::peek() {
     rearrange();
     int temp = 0;
@@ -60,8 +85,14 @@ int MyQueue::peek() {
     return temp;
 }
 
+/**
+ * Checks whether a queue is empty
+ */
 bool MyQueue::empty() { return s2.isEmptyStack() && s1.isEmptyStack(); }
 
+/**
+ * Main function
+ */
 int main() {
     // Test queue functionality
     MyQueue que;
@@ -79,6 +110,5 @@ int main() {
         std::cout << que.peek() << std::endl;
         std::cout << que.pop() << std::endl;
     }
-
     return 0;
 }
