9815805267f6bf30da37e8db1cbde05679fc17f6
shoniavika <vshon17@freeuni.edu.ge>
2020-07-15 00:05:30 +0400

    updated documentation, new func for testing

--- data_structures/queue_using_two_stacks.cpp
+++ data_structures/queue_using_two_stacks.cpp
@@ -1,13 +1,8 @@
-/*
-    Implementation of a Queue using two Stacks.
+/**
+ * Implementation of a Queue using two Stacks.
+ */
 
-    Functions:
-        Push  -> Inserts element to the end of the queue
-        Pop   -> Removes element from the front
-        Peek  -> Returns element from the front (no removing)
-        Empty -> Checks whether the queue is empty
-*/
-
+#include <cassert>
 #include <iostream>
 
 #include "./stack.h"
@@ -16,23 +11,35 @@ class MyQueue {
  private:
     stack<int> s1, s2;
 
-    /** Helper function for rearranging elements in stacks */
+    /**
+     * Helper function for rearranging elements in stacks
+     */
     void rearrange();
 
  public:
-    /** Initializes queue. */
+    /**
+     * Constructor for queue.
+     */
     MyQueue() = default;
 
-    /** Pushes x to the back of queue. */
+    /**
+     * Pushes x to the back of queue.
+     */
     void push(int x);
 
-    /** Removes an element from the front of the queue. */
+    /**
+     * Removes an element from the front of the queue.
+     */
     int pop();
 
-    /** Returns first element, without removing it. */
+    /**
+     * Returns first element, without removing it.
+     */
     int peek();
 
-    /** Returns whether the queue is empty. */
+    /**
+     * Returns whether the queue is empty.
+     */
     bool empty();
 };
 
@@ -91,24 +98,35 @@ int MyQueue::peek() {
 bool MyQueue::empty() { return s2.isEmptyStack() && s1.isEmptyStack(); }
 
 /**
- * Main function
+ * Testing function
  */
-int main() {
-    // Test queue functionality
+void queue_test() {
     MyQueue que;
-
-    que.pop();
-    que.peek();
-
+    std::cout << "Test #1\n";
     que.push(2);
     que.push(5);
     que.push(0);
-    que.push(4);
-    que.push(9);
+    assert(que.peek() == 2);
+    assert(que.pop() == 2);
+    assert(que.peek() == 5);
+    assert(que.pop() == 5);
+    assert(que.peek() == 0);
+    assert(que.pop() == 0);
+    assert(que.empty() == true);
+    std::cout << "PASSED\n";
 
-    while (!que.empty()) {
-        std::cout << que.peek() << std::endl;
-        std::cout << que.pop() << std::endl;
-    }
+    std::cout << "Test #2\n";
+    que.push(-1);
+    assert(que.empty() == false);
+    assert(que.peek() == -1);
+    assert(que.pop() == -1);
+    std::cout << "PASSED\n";
+}
+
+/**
+ * Main function, calls testing function
+ */
+int main() {
+    queue_test();
     return 0;
 }
