28411dbd2fdaf2da3ed4f9b14c8739916018ae47
unknown <deepraval2905@gmail.com>
2020-07-19 11:30:47 +0530

    Changed comment styling and added unnamed namespace

--- ciphers/caesar_cipher.cpp
+++ ciphers/caesar_cipher.cpp
@@ -3,24 +3,20 @@
  * @brief Implementation of [Caesar cipher](https://en.wikipedia.org/wiki/Caesar_cipher) algorithm.
  *
  * @details
- *      In cryptography, a Caesar cipher, also known as Caesar's cipher, the shift cipher, 
- *      Caesar's code or Caesar shift, is one of the simplest and most widely known encryption 
- *      techniques. It is a type of substitution cipher in which each letter in the plaintext 
- *      is replaced by a letter some fixed number of positions down the alphabet. For example, 
- *      with a left shift of 3, D would be replaced by A, E would become B, and so on. 
- *      The method is named after Julius Caesar, who used it in his private correspondence.
+ * In cryptography, a Caesar cipher, also known as Caesar's cipher, the shift cipher, 
+ * Caesar's code or Caesar shift, is one of the simplest and most widely known encryption 
+ * techniques. It is a type of substitution cipher in which each letter in the plaintext 
+ * is replaced by a letter some fixed number of positions down the alphabet. For example, 
+ * with a left shift of 3, D would be replaced by A, E would become B, and so on. 
+ * The method is named after Julius Caesar, who used it in his private correspondence.
  *
- * Algorithm -      
- *      
- *      The encryption can also be represented using modular arithmetic by first transforming 
- *      the letters into numbers, according to the scheme, A → 0, B → 1, ..., Z → 25.
- *      Encryption of a letter x by a shift n can be described mathematically as,
- * 
- *      E(x) = (x + n) mod 26
- * 
- *      while decryption can be described as,
- * 
- *      D(x) = (x - n) mod 26
+ * ### Algorithm
+ * The encryption can also be represented using modular arithmetic by first transforming 
+ * the letters into numbers, according to the scheme, A → 0, B → 1, ..., Z → 25.
+ * Encryption of a letter x by a shift n can be described mathematically as,
+ * \f[ E(x) = (x + n)\;\mbox{mod}\; 26\f]
+ * while decryption can be described as,
+ * \f[ D(x) = (x - n) \;\mbox{mod}\; 26\f]
  * 
  * \note This program implements caesar cipher for only uppercase english latters (i.e. A-Z). 
  * 
@@ -34,27 +30,32 @@
  * \brief Algorithms for encryption and decryption
  */
 namespace ciphers {
-    namespace caesar {
-        /**
-         * This function finds character for given value (i.e.A-Z)
-         * @param x value for which we want character 
-         * @returns  corresponding character for perticular value
-         */        
-        char get_char(const int x) {
-            // By adding 65 we are scaling 0-25 to 65-90. 
-            // Which are in fact ASCII values of A-Z. 
-            return char(x + 65); 
-        }
-        /**
-         * This function finds value for given character (i.e.0-25)
-         * @param c character for which we want value
-         * @returns returns corresponding value for perticular character
-         */  
-        int get_value(const char c) {
-            // A-Z have ASCII values in range 65-90.
-            // Hence subtracting 65 will scale them to 0-25.
-            return int(c - 65);
-        }
+    /** \namespace caesar
+     * \brief Functions for [Caesar cipher](https://en.wikipedia.org/wiki/Caesar_cipher) algorithm.
+     */
+    namespace caesar {   
+        namespace {
+            /**
+             * This function finds character for given value (i.e.A-Z)
+             * @param x value for which we want character 
+             * @returns  corresponding character for perticular value
+             */        
+            inline char get_char(const int x) {
+                // By adding 65 we are scaling 0-25 to 65-90. 
+                // Which are in fact ASCII values of A-Z. 
+                return char(x + 65); 
+            }
+            /**
+             * This function finds value for given character (i.e.0-25)
+             * @param c character for which we want value
+             * @returns returns corresponding value for perticular character
+             */  
+            inline int get_value(const char c) {
+                // A-Z have ASCII values in range 65-90.
+                // Hence subtracting 65 will scale them to 0-25.
+                return int(c - 65);
+            }
+        } // Unnamed namespace
         /**
          * Encrypt given text using caesar cipher.
          * @param text text to be encrypted
