2c2167636a49c805a373a0523fee6e87e9bbc943
Gopal Ramesh Dahale <dahalegopal27@gmail.com>
2020-07-20 15:03:59 +0530

    fixed bug, indented code

--- data_structures/stack_using_two_queues.cpp
+++ data_structures/stack_using_two_queues.cpp
@@ -3,49 +3,43 @@
  * @brief Implementation of stack using two queues
  * @author [Gopal-Dahale](https://github.com/Gopal-Dahale)
  * @details
- * In this alogrithm, stack is implemented by making the pop operation costly. 
- * push(x) operation enqueues x to q1. 
- * pop() operation puts the first n-1 elements from q1 to q2 and then dequeue the last element from q1. 
- * Finally the names q1 and q2 are swapped. 
- * The time Complexity of the algorithm is \f$O(n)\f$ for pop() and \f$O(1)\f$ for push(). 
+ * In this alogrithm, stack is implemented by making the pop operation costly.
+ * push(x) operation enqueues x to q1.
+ * pop() operation puts the first n-1 elements from q1 to q2 and then dequeue
+ * the last element from q1. Finally the names q1 and q2 are swapped. The time
+ * Complexity of the algorithm is \f$O(n)\f$ for pop() and \f$O(1)\f$ for
+ * push().
  */
 
+#include <cassert>
 #include <iostream>
 #include <queue>
-#include <cassert>
 
 using namespace std;
 
 template <typename T>
-class Stack
-{
-private:
+class Stack {
+ private:
     queue<T> q1, q2;
 
-public:
+ public:
     /**
-    * Pushes the given element to top of stack 
-    */
-    void push(T data)
-    {
-        q1.push(data);
-    }
+     * Pushes the given element to top of stack
+     */
+    void push(T data) { q1.push(data); }
 
     /**
-    * Pops the topmost element of stack 
-    */
-    void pop()
-    {
+     * Pops the topmost element of stack
+     */
+    void pop() {
         // push first n-1 elements from q1 to q2
         // pop the last element from q1
         // swap q1 and q2
-        if (q1.size() == 0)
-        {
-            return -1;
+        if (q1.size() == 0) {
+            return;
         }
 
-        while (q1.size() > 1)
-        {
+        while (q1.size() > 1) {
             q2.push(q1.front());
             q1.pop();
         }
@@ -57,16 +51,14 @@ public:
     }
 
     /**
-    * Returns the topmost element of stack. 
-    * Returns -1 if stack is empty
-    */
-    T top()
-    {
+     * Returns the topmost element of stack.
+     * Returns -1 if stack is empty
+     */
+    T top() {
         if (q1.size() == 0)
             return -1;
 
-        while (q1.size() > 1)
-        {
+        while (q1.size() > 1) {
             q2.push(q1.front());
             q1.pop();
         }
@@ -82,30 +74,22 @@ public:
     }
 
     /**
-    * Returns the size of stack
-    */
-    int size()
-    {
-        return q1.size() + q2.size();
-    }
+     * Returns the size of stack
+     */
+    int size() { return q1.size() + q2.size(); }
 
     /**
-    * Returns true if stack is empty, otherwise false 
-    */
-    bool empty()
-    {
-        return (q1.size() == 0);
-    }
+     * Returns true if stack is empty, otherwise false
+     */
+    bool empty() { return (q1.size() == 0); }
 };
 
 /**
- * utility function to print stack 
+ * utility function to print stack
  */
 template <typename T>
-void print(Stack<T> &s)
-{
-    while (!s.empty())
-    {
+void print(Stack<T> &s) {
+    while (!s.empty()) {
         cout << s.top() << " ";
         s.pop();
     }
@@ -115,13 +99,11 @@ void print(Stack<T> &s)
 /**
  * Testing function
  */
-void test()
-{
+void test() {
     Stack<int> s;
 
     // push
-    for (int i = 0; i < 10; i++)
-    {
+    for (int i = 0; i < 10; i++) {
         s.push(i);
     }
 
@@ -131,8 +113,7 @@ void test()
     print(s);
 
     // now stack is empty
-    if (s.empty())
-    {
+    if (s.empty()) {
         cout << "stack is empty\n";
     }
 
@@ -141,8 +122,7 @@ void test()
     // since stack is empty, this will return -1
     cout << s.top() << "\n";
 
-    for (int i = 0; i < 10; i++)
-    {
+    for (int i = 0; i < 10; i++) {
         s.push(i);
     }
 
@@ -152,8 +132,7 @@ void test()
     print(s);
 }
 
-int main()
-{
+int main() {
     test();
     return 0;
 }
