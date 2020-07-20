c99701c627811e2d2a18eb9685770f3a85a1be7e
Gopal Ramesh Dahale <dahalegopal27@gmail.com>
2020-07-20 21:23:42 +0530

    updated the code with namespace, added more test cases

--- data_structures/stack_using_two_queues.cpp
+++ data_structures/stack_using_two_queues.cpp
@@ -14,81 +14,155 @@
 #include <cassert>
 #include <iostream>
 #include <queue>
+#include <string>
 
-template <typename T>
-class Stack {
- private:
-    std::queue<T> q1, q2;
+namespace
+{
+    template <typename T>
+    class Stack
+    {
+    private:
+        std::queue<T> q1, q2;
 
- public:
-    /**
+    public:
+        /**
      * Pushes the given element to top of stack
      */
-    void push(T data) { q1.push(data); }
+        void push(T data) { q1.push(data); }
 
-    /**
+        /**
      * Pops the topmost element of stack
      */
-    void pop() {
-        // push first n-1 elements from q1 to q2
-        // pop the last element from q1
-        // swap q1 and q2
-        if (q1.size() == 0) {
-            return;
-        }
+        void pop()
+        {
+            // push first n-1 elements from q1 to q2
+            // pop the last element from q1
+            // swap q1 and q2
+            if (q1.size() == 0)
+            {
+                return;
+            }
 
-        while (q1.size() > 1) {
-            q2.push(q1.front());
+            while (q1.size() > 1)
+            {
+                q2.push(q1.front());
+                q1.pop();
+            }
+            // pops the last element from q1;
             q1.pop();
+
+            // swaps q1 with q2
+            q1.swap(q2);
         }
-        // pops the last element from q1;
-        q1.pop();
 
-        // swaps q1 with q2
-        q1.swap(q2);
-    }
-
-    /**
+        /**
      * Returns the topmost element of stack.
      * Returns -1 if stack is empty
      */
-    T top() {
-        if (q1.size() == 0) {
-            return -1;
-        }
+        T top()
+        {
+            if (q1.size() == 0)
+            {
+                return T();
+            }
 
-        while (q1.size() > 1) {
+            while (q1.size() > 1)
+            {
+                q2.push(q1.front());
+                q1.pop();
+            }
+
+            T top_element = q1.front();
             q2.push(q1.front());
             q1.pop();
+
+            // swaps q1 with q2
+            q1.swap(q2);
+
+            return top_element;
         }
 
-        T top_element = q1.front();
-        q2.push(q1.front());
-        q1.pop();
+        /**
+     * Returns the size of stack
+     */
+        int size() { return q1.size() + q2.size(); }
 
-        // swaps q1 with q2
-        q1.swap(q2);
+        /**
+     * Returns true if stack is empty, otherwise false
+     */
+        bool empty() { return (q1.size() == 0); }
+    };
 
-        return top_element;
+} // namespace
+
+/**
+ * class Book with book name and it's price
+ */
+class Book
+{
+private:
+    std::string book_name;
+    double book_price;
+
+public:
+    /**
+    * default constructor
+    */
+    Book()
+        : book_name{std::string()}, book_price{float()}
+    {
     }
 
     /**
-     * Returns the size of stack
-     */
-    int size() { return q1.size() + q2.size(); }
+    * parameterized constructor
+    */
+    Book(std::string book_name, double book_price)
+        : book_name{book_name}, book_price{book_price}
+    {
+    }
 
     /**
-     * Returns true if stack is empty, otherwise false
-     */
-    bool empty() { return (q1.size() == 0); }
+    * Returns name of the book
+    */
+    std::string get_book_name() const
+    {
+        return this->book_name;
+    }
+
+    /**
+    * Returns price of the book
+    */
+    double get_book_price() const
+    {
+        return this->book_price;
+    }
+
+    /**
+    * comparison of two books
+    */
+    bool operator==(const Book &b) const
+    {
+        return ((b.get_book_name() == this->book_name) and (b.get_book_price() == this->book_price));
+    }
 };
+
+/**
+ * overloading operator<< to print book details
+ */
+std::ostream &operator<<(std::ostream &os, const Book &book)
+{
+    os << book.get_book_name() << ", " << book.get_book_price() << "\n";
+    return os;
+}
 
 /**
  * utility function to print stack
  */
 template <typename T>
-void print(Stack<T> &s) {
-    while (!s.empty()) {
+void print(Stack<T> s)
+{
+    while (!s.empty())
+    {
         std::cout << s.top() << " ";
         s.pop();
     }
@@ -98,40 +172,70 @@ void print(Stack<T> &s) {
 /**
  * Testing function
  */
-void test() {
+void test()
+{
+    // stack of integers
     Stack<int> s;
 
     // push
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 10; i++)
+    {
         s.push(i);
     }
-
-    assert(s.size() == 10);
 
     // iterating over stack
     print(s);
 
-    // now stack is empty
-    if (s.empty()) {
-        std::cout << "stack is empty\n";
-    }
-
-    assert(s.top() == -1);
-
-    // since stack is empty, this will return -1
+    assert(s.top() == 9);
     std::cout << s.top() << "\n";
 
-    for (int i = 0; i < 10; i++) {
-        s.push(i);
+    // size of stack
+    assert(s.size() == 10);
+    std::cout << "Stack contains " << s.size() << " elements\n";
+
+    // emptying the stack
+    while (!s.empty())
+    {
+        s.pop();
     }
 
-    // size of stack
-    std::cout << "stack contains " << s.size() << " elements\n";
+    assert(s.empty() == true);
+    assert(s.top() == int());
+    std::cout << "Stack is empty: " << s.empty() << "\n";
+    std::cout << "Topmost element is: " << s.top() << "\n";
 
-    print(s);
+    // stack of books
+    Stack<Book> books;
+
+    books.push(Book("The Philosophy of Prayer", 1122.99));
+    books.push(Book("Mahatma Gandhi Autobiography", 114.98));
+    books.push(Book("Wings of Fire", 349.86));
+    books.push(Book("My Gita", 209.95));
+    books.push(Book("The Blue Umbrella", 61.32));
+
+    print(books);
+
+    assert(books.top() == Book("The Blue Umbrella", 61.32));
+    std::cout << books.top() << "\n";
+
+    // size of stack
+    assert(books.size() == 5);
+    std::cout << "Stack contains " << books.size() << " books\n";
+
+    // emptying the stack
+    while (!books.empty())
+    {
+        books.pop();
+    }
+
+    assert(books.empty() == true);
+    assert(books.top() == Book());
+    std::cout << "Stack is empty: " << books.empty() << "\n";
+    std::cout << "Topmost element is: " << books.top() << "\n";
 }
 
-int main() {
+int main()
+{
     test();
     return 0;
 }
