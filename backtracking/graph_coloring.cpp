f9da051c4eeacb7a420a182e46e664ef97384a20
David Leal <halfpacho@gmail.com>
2020-07-31 17:58:12 -0500

    docs: Add long description (graph_coloring.cpp)

--- backtracking/graph_coloring.cpp
+++ backtracking/graph_coloring.cpp
@@ -1,6 +1,19 @@
 /**
  * @file
  * @brief prints the assigned colors
+ * using [Graph Coloring](https://en.wikipedia.org/wiki/Graph_coloring) algorithm
+ *
+ * @details
+ * In graph theory, graph coloring is a special case of graph labeling; 
+ * it is an assignment of labels traditionally called "colors" to elements of a graph subject to certain constraints. 
+ * In its simplest form, it is a way of coloring the vertices of a graph such that no two adjacent vertices are of the same color; 
+ * this is called a vertex coloring. Similarly, an edge coloring assigns
+ * a color to each edge so that no two adjacent edges are of the same color,
+ * and a face coloring of a planar graph assigns a color to each face or
+ * region so that no two faces that share a boundary have the same color.
+ *
+ * @author [Anup Kumar Panwar](https://github.com/AnupKumarPanwar)
+ * @author [David Leal](https://github.com/Panquesito7)
  */
 #include <iostream>
 #include <array>
