9f8cc37124013b7d1bcc4e6fabf24c75a9ca7193
David Leal <halfpacho@gmail.com>
2020-07-31 18:00:41 -0500

    docs: Add long description (sudoku_solve.cpp)

--- backtracking/sudoku_solve.cpp
+++ backtracking/sudoku_solve.cpp
@@ -2,6 +2,14 @@
  * @file
  * @brief [Sudoku Solver](https://en.wikipedia.org/wiki/Sudoku) algorithm.
  *
+ * @details
+ * Sudoku (数独, sūdoku, digit-single) (/suːˈdoʊkuː/, /-ˈdɒk-/, /sə-/, originally called
+ * Number Place)[1] is a logic-based,[2][3] combinatorial[4] number-placement puzzle. 
+ * In classic sudoku, the objective is to fill a 9×9 grid with digits so that each column,
+ * each row, and each of the nine 3×3 subgrids that compose the grid (also called "boxes", "blocks", or "regions")
+ * contain all of the digits from 1 to 9. The puzzle setter provides a
+ * partially completed grid, which for a well-posed puzzle has a single solution.
+ *
  * @author [DarthCoder3200](https://github.com/DarthCoder3200)
  * @author [David Leal](https://github.com/Panquesito7)
  */
