a76bcac887be4f83aaa25d1d145340a6daa5035d
David Leal <halfpacho@gmail.com>
2020-07-31 18:00:22 -0500

    docs: Add long description (minimax.cpp)

--- backtracking/minimax.cpp
+++ backtracking/minimax.cpp
@@ -2,6 +2,18 @@
  * @file
  * @brief returns which is the longest/shortest number
  * using [minimax](https://en.wikipedia.org/wiki/Minimax) algorithm
+ *
+ * @details
+ * Minimax (sometimes MinMax, MM[1] or saddle point[2]) is a decision rule used in
+ * artificial intelligence, decision theory, game theory, statistics,
+ * and philosophy for minimizing the possible loss for a worst case (maximum loss) scenario.
+ * When dealing with gains, it is referred to as "maximin"—to maximize the minimum gain.
+ * Originally formulated for two-player zero-sum game theory, covering both the cases where players take
+ * alternate moves and those where they make simultaneous moves, it has also been extended to more
+ * complex games and to general decision-making in the presence of uncertainty.
+ * 
+ * @author [Gleison Batista](https://github.com/gleisonbs)
+ * @author [David Leal](https://github.com/Panquesito7)
  */
 #include <algorithm>
 #include <cmath>
