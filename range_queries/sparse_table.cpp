d4dac7388336936f6867a10e7ffce5b5cb4e6b6c
Arpan Mukherjee <arpan17007@iiitd.ac.in>
2020-08-07 04:33:33 +0530

    Fixed comments

--- range_queries/sparse_table.cpp
+++ range_queries/sparse_table.cpp
@@ -11,15 +11,22 @@ Range Query : O(1)
 #include<iostream>
 #include<algorithm>
 
-void computeLogs(int n, std::vector<int> &logs) {   // Util function to store pre-computed logs
+
+/**
+ * Util function to store pre-computed logs
+ */
+void computeLogs(int n, std::vector<int> &logs) {
     logs[1] = 0;
     for (int i = 2 ; i < n ; i++) {
         logs[i] = logs[i/2] + 1;
     }
 }
 
-void buildTable(int n, const std::vector<int> &A, const std::vector<int> &logs, std::vector<std::vector<int> > &table) {    // To build Sparse Table data structure
-    int curLen;
+/**
+ * To build Sparse Table data structure
+ */
+void buildTable(int n, const std::vector<int> &A, const std::vector<int> &logs, std::vector<std::vector<int> > &table) {
+    int curLen = 0;
     for (int i = 0 ; i <= logs[n] ; i++) {
         curLen = 1 << i;
         for (int j = 0 ; j + curLen < n ; j++) {
@@ -33,24 +40,30 @@ void buildTable(int n, const std::vector<int> &A, const std::vector<int> &logs, 
     }
 }
 
-int getMinimum(int beg, int end, const std::vector<int> &logs, const std::vector<std::vector<int> > &table) {  // To get the result of the query
+/**
+ * To get the result of the query
+ */
+int getMinimum(int beg, int end, const std::vector<int> &logs, const std::vector<std::vector<int> > &table) {
     int p = logs[end - beg + 1];
     int pLen = 1 << p;
     return std::min(table[p][beg], table[p][end - pLen + 1]);
 }
 
 
+/**
+ * Main function
+ */
 int main() {
-    int n;
+    int n = 0;
     std::cin >> n;
     std::vector<int> A(n+5);
-    std::vector<int> logs(n+5);   // logs[i] = maximum p s.t. 2^p <= i
-    std::vector<std::vector<int> > table(20, std::vector<int>(n+5, 0)); // table[i][j] = [j, j+2^i]
+    std::vector<int> logs(n+5);
+    std::vector<std::vector<int> > table(20, std::vector<int>(n+5, 0));
     for (int i = 0 ; i < n ; i++) {
         std::cin >> A[i];
     }
     computeLogs(n, logs);
     buildTable(n, A, logs, table);
-    std::cout << "Minimum of the range [3, 14]: " << getMinimum(2, 13, logs, table) << "\n";
+    std::cout << "Minimum of the range [3, 14]: " << getMinimum(2, 13, logs, table) << std::endl;
     return 0;
 }
