f54c9759f902fe4c092eaab188b489550afeb949
Arpan Mukherjee <arpan17007@iiitd.ac.in>
2020-08-07 04:02:35 +0530

    Fixed Lint Warnings

--- range_queries/sparse_table.cpp
+++ range_queries/sparse_table.cpp
@@ -5,50 +5,54 @@
 // Range Query : O(1)
 
 
+#include<vector>
 #include<iostream>
 #include<algorithm>
 
 const int N = 100005;
 const int LOGN = 20;
 
-int n;                  // length of the array
-int A[N];
-int logs[N];            // logs[i] = maximum p s.t. 2^p <= i
-int table[LOGN][N];     // table[i][j] = [j, j+2^i]
+int n;                      // length of the array
+std::vector<int> A(N);
+std::vector<int> logs(N);   // logs[i] = maximum p s.t. 2^p <= i
+std::vector<std::vector<int> > table(LOGN, std::vector<int>(N, 0));  // // table[i][j] = [j, j+2^i]
 
 void computeLogs(int n) {
     logs[1] = 0;
-    for (int i = 2 ; i < n ; i++)
+    for (int i = 2 ; i < n ; i++) {
         logs[i] = logs[i/2] + 1;
+    }
 }
 
 void buildTable(int n) {
-    int i, j, curLen;
-    for (i = 0 ; i <= logs[n] ; i++) {
-        curLen = 1LL << i;
-        for (j = 0 ; j + curLen < n ; j++) {
-            if (curLen == 1)
+    int curLen;
+    for (int i = 0 ; i <= logs[n] ; i++) {
+        curLen = 1 << i;
+        for (int j = 0 ; j + curLen < n ; j++) {
+            if (curLen == 1) {
                 table[i][j] = A[j];
-            else
+            }
+            else {
                 table[i][j] = std::min(table[i-1][j], table[i-1][j + curLen/2]);
+            }
         }
     }
 }
 
 int getMinimum(int beg, int end) {
     int p = logs[end - beg + 1];
-    int pLen = 1LL << p;
+    int pLen = 1 << p;
     return std::min(table[p][beg], table[p][end - pLen + 1]);
 }
 
 
 int main() {
-    int i, n, x;
-    scanf("%d", &n);
-    for (i = 0 ; i < n ; i++)
-        scanf("%d", &A[i]);
+    std::cin >> n;
+    for (int i = 0 ; i < n ; i++) {
+        std::cin >> A[i];
+    }
     computeLogs(n);
     buildTable(n);
-    printf("Minimum of the range [3, 14]: %d\n", getMinimum(2, 13));
+    std::cout << "Minimum of the range [3, 14]: " << getMinimum(2, 13) << "\n";
     return 0;
 }
