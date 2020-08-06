3d25978302897ebe7a075bbfded57b90b763e9df
Arpan Mukherjee <arpan17007@iiitd.ac.in>
2020-08-07 04:58:33 +0530

    Fixed variable reference Lint warnings

--- range_queries/sparse_table.cpp
+++ range_queries/sparse_table.cpp
@@ -15,17 +15,20 @@ Range Query : O(1)
 /**
  * Util function to store pre-computed logs
  */
-void computeLogs(int n, std::vector<int>& logs) {
+std::vector<int> computeLogs(int n) {
+    std::vector<int> logs(n+5);
     logs[1] = 0;
     for (int i = 2 ; i < n ; i++) {
         logs[i] = logs[i/2] + 1;
     }
+    return logs;
 }
 
 /**
  * To build Sparse Table data structure
  */
-void buildTable(int n, const std::vector<int>& A, const std::vector<int>& logs, std::vector<std::vector<int> >& table) {
+std::vector<std::vector<int> > buildTable(int n, const std::vector<int>& A, const std::vector<int>& logs) {
+    std::vector<std::vector<int> > table(20, std::vector<int>(n+5, 0));
     int curLen = 0;
     for (int i = 0 ; i <= logs[n] ; i++) {
         curLen = 1 << i;
@@ -38,6 +41,7 @@ void buildTable(int n, const std::vector<int>& A, const std::vector<int>& logs, 
             }
         }
     }
+    return table;
 }
 
 /**
@@ -57,13 +61,11 @@ int main() {
     int n = 0;
     std::cin >> n;
     std::vector<int> A(n+5);
-    std::vector<int> logs(n+5);
-    std::vector<std::vector<int> > table(20, std::vector<int>(n+5, 0));
     for (int i = 0 ; i < n ; i++) {
         std::cin >> A[i];
     }
-    computeLogs(n, logs);
-    buildTable(n, A, logs, table);
+    std::vector<int> logs = computeLogs(n);
+    std::vector<std::vector<int> >  table = buildTable(n, A, logs);
     std::cout << "Minimum of the range [3, 14]: " << getMinimum(2, 13, logs, table) << std::endl;
     return 0;
 }
