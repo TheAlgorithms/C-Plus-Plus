81e43dadf9d138547b96612e09336ea797be52fa
David Leal <halfpacho@gmail.com>
2020-08-07 13:10:47 -0500

    fix: sudoku_solve.cpp improvements

--- backtracking/sudoku_solve.cpp
+++ backtracking/sudoku_solve.cpp
@@ -130,11 +130,17 @@ namespace backtracking {
  */
 int main() {
     const int V = 9;
-    std::array <std::array <int, V>, V> mat = { 5, 3, 0, 0, 7, 0, 0, 0, 0, 6, 0, 0, 1, 9, 5, 0, 0, 0,
-                     0, 9, 8, 0, 0, 0, 0, 6, 0, 8, 0, 0, 0, 6, 0, 0, 0, 3,
-                     4, 0, 0, 8, 0, 3, 0, 0, 1, 7, 0, 0, 0, 2, 0, 0, 0, 6,
-                     0, 6, 0, 0, 0, 0, 2, 8, 0, 0, 0, 0, 4, 1, 9, 0, 0, 5,
-                     0, 0, 0, 0, 8, 0, 0, 7, 9 };
+    std::array <std::array <int, V>, V> mat = { 
+        std::array <int, V> {5, 3, 0, 0, 7, 0, 0, 0, 0},
+        std::array <int, V> {6, 0, 0, 1, 9, 5, 0, 0, 0},
+        std::array <int, V> {0, 9, 8, 0, 0, 0, 0, 6, 0},
+        std::array <int, V> {8, 0, 0, 0, 6, 0, 0, 0, 3},
+        std::array <int, V> {4, 0, 0, 8, 0, 3, 0, 0, 1},
+        std::array <int, V> {7, 0, 0, 0, 2, 0, 0, 0, 6},
+        std::array <int, V> {0, 6, 0, 0, 0, 0, 2, 8, 0},
+        std::array <int, V> {0, 0, 0, 4, 1, 9, 0, 0, 5},
+        std::array <int, V> {0, 0, 0, 0, 8, 0, 0, 7, 9}
+    };
 
     backtracking::printMat<V>(mat, 9);
     std::cout << "Solution " << std::endl;
