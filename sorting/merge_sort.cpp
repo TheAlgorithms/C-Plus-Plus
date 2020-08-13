29e6cf97e82ef7ea5643f2fc1b49a4915d323a8b
@8848hg <53469557+Himalay12@users.noreply.github.com>
2020-08-13 23:28:12 +0530

    Update merge_sort.cpp
    Reduce the extra loop used by changing condition in merge function.

--- sorting/merge_sort.cpp
+++ sorting/merge_sort.cpp
@@ -30,42 +30,28 @@
  *
  * @param l - end index or right index of second half array
  */
-void merge(int *arr, int l, int m, int r) {
+void merge(int &arr, int l, int m, int r) {
     int i, j, k;
     int n1 = m - l + 1;
     int n2 = r - m;
 
-    int *L = new int[n1], *R = new int[n2];
+    int L[n1], R[n2];
 
     for (i = 0; i < n1; i++) L[i] = arr[l + i];
     for (j = 0; j < n2; j++) R[j] = arr[m + 1 + j];
 
     i = 0;
     j = 0;
-    k = l;
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
+    while (i < n1 || j < n2) {
+        if ((i<n1) && (L[i] <= R[j] || j>=n2)) {
             arr[k] = L[i];
             i++;
-        } else {
+        } else{
             arr[k] = R[j];
             j++;
         }
-        k++;
     }
-
-    while (i < n1) {
-        arr[k] = L[i];
-        i++;
-        k++;
-    }
-
-    while (j < n2) {
-        arr[k] = R[j];
-        j++;
-        k++;
-    }
-
+    
     delete[] L;
     delete[] R;
 }
@@ -103,7 +89,7 @@ int main() {
     int size;
     std::cout << "Enter the number of elements : ";
     std::cin >> size;
-    int *arr = new int[size];
+    int arr[size];
     std::cout << "Enter the unsorted elements : ";
     for (int i = 0; i < size; ++i) {
         std::cin >> arr[i];
