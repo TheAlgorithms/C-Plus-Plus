0eb44c18b68454a7f3302361b8ae97cdcb1930b0
pavithransamy10 <68406360+pavithransamy10@users.noreply.github.com>
2020-08-15 16:42:38 +0800

    Display Error message for string input
    Added If else statement in main function so when user inputs anything other than integer error message is shown.I also added a cout method to tell users to enter a Number/Integer.

--- math/sqrt_double.cpp
+++ math/sqrt_double.cpp
@@ -9,41 +9,52 @@
  */
 #include <cassert>
 #include <iostream>
+using namespace std;// Added this so we can use cout
 
-/** Bisection method implemented for the function \f$x^2-a=0\f$
- * whose roots are \f$\pm\sqrt{a}\f$ and only the positive root is returned.
- */
+ /** Bisection method implemented for the function \f$x^2-a=0\f$
+  * whose roots are \f$\pm\sqrt{a}\f$ and only the positive root is returned.
+  */
 double Sqrt(double a) {
-    if (a > 0 && a < 1) {
-        return 1 / Sqrt(1 / a);
-    }
-    double l = 0, r = a;
-    /* Epsilon is the precision.
-    A great precision is
-    between 1e-7 and 1e-12.
-    double epsilon = 1e-12;
-    */
-    double epsilon = 1e-12;
-    while (l <= r) {
-        double mid = (l + r) / 2;
-        if (mid * mid > a) {
-            r = mid;
-        } else {
-            if (a - mid * mid < epsilon) {
-                return mid;
-            }
-            l = mid;
-        }
-    }
-    return -1;
+
+	if (a > 0 && a < 1) {
+		return 1 / Sqrt(1 / a);
+	}
+	double l = 0, r = a;
+	/* Epsilon is the precision.
+	A great precision is
+	between 1e-7 and 1e-12.
+	double epsilon = 1e-12;
+	*/
+	double epsilon = 1e-12;
+	while (l <= r) {
+		double mid = (l + r) / 2;
+		if (mid * mid > a) {
+			r = mid;
+		}
+		else {
+			if (a - mid * mid < epsilon) {
+				return mid;
+			}
+			l = mid;
+		}
+	}
+	return -1;
 }
 
 /** main function */
 int main() {
-    double n{};
-    std::cin >> n;
-    assert(n >= 0);
-    // Change this line for a better precision
-    std::cout.precision(12);
-    std::cout << std::fixed << Sqrt(n);
+	double n{};
+	//This Line Will Tell users to enter A number
+	cout << "Enter A Number :";
+	if (std::cin >> n) {
+		assert(n >= 0);
+		// Change this line for a better precision
+		std::cout.precision(12);
+		std::cout << std::fixed << Sqrt(n);
+	}
+	//If user enters a string an error message will be shown
+	else
+	{
+		cout << "please input an integar value";
+	}
 }
