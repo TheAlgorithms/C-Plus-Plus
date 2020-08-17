71a790c3fcd08ec5eb1a73d9fe3808bc60bd4a93
Lownish Rai Sookha <69192570+Lownish@users.noreply.github.com>
2020-08-18 01:12:09 +0530

    Apply suggestions from code review
    Suggested changes applied
    
    Co-authored-by: David Leal <halfpacho@gmail.com>

--- sorting/pigeonhole_sort.cpp
+++ sorting/pigeonhole_sort.cpp
@@ -1,22 +1,32 @@
 /**
  * @file
  * @brief Implementation of [Pigeonhole Sort algorithm]
- *(https://en.wikipedia.org/wiki/Pigeonhole_sort)
+ * (https://en.wikipedia.org/wiki/Pigeonhole_sort)
  * @author [Lownish](https://github.com/Lownish)
  * @details
- *Pigeonhole sorting is a sorting algorithm that is suitable for sorting lists
- *of elements where the number of elements and the number of possible key values
- *are approximately the same. It requires O(n + Range) time where n is number of
- *elements in input array and ‘Range’ is number of possible values in array.
+ * Pigeonhole sorting is a sorting algorithm that is suitable for sorting lists
+ * of elements where the number of elements and the number of possible key values
+ * are approximately the same. It requires O(n + Range) time where n is number of
+ * elements in input array and ‘Range’ is number of possible values in array.
  *
  * The time Complexity of the algorithm is \f$O(n+N)\f$.
  */
 
 #include <algorithm>  //for std::is_sorted
 #include <array>      //for std::array
-#include <cassert>    //for std::array
+#include <cassert>    //for assert
 #include <iostream>   //for io operations
 
+/**
+ * @namespace sorting
+ * @brief Sorting algorithms
+ */
+namespace sorting {
+    /**
+     * @namespace pigonhole
+     * @brief Functions for [Pigeonhole Sort](https://en.wikipedia.org/wiki/Pigeonhole_sort) algorithm
+     */
+    namespace pigonhole {
 /**
  *Pigeonhole sorting of array with array size n
  */
@@ -64,22 +74,27 @@ void pigeonSort(std::array<int, N> arr, int n) {
         std::cout << arr.at(i) << " ";
     }
 }
+}  // namespace pigonhole
+}  // namespace sorting
+
 
 /**
- *Test function 1 with unsorted array
- *{8, 3, 2, 7, 4, 6, 8}
+ * Test function 1 with unsorted array
+ * {8, 3, 2, 7, 4, 6, 8}
+ * @returns none
  */
 static void test_1() {
     std::array<int, 7> test_array = {8, 3, 2, 7, 4, 6, 8};
 
     int n = sizeof(test_array) / sizeof(test_array[0]);
 
-    pigeonSort(test_array, n);
+    sorting::pigonhole::pigeonSort(test_array, n);
 }
 
 /**
- *Test function 2 with unsorted array
- *{802, 630, 20, 745, 52, 300, 612, 932, 78, 187}
+ * Test function 2 with unsorted array
+ * {802, 630, 20, 745, 52, 300, 612, 932, 78, 187}
+ * @returns none
  */
 static void test_2() {
     std::array<int, 10> test_array = {802, 630, 20,  745, 52,
@@ -87,11 +102,14 @@ static void test_2() {
 
     int n = sizeof(test_array) / sizeof(test_array[0]);
 
-    pigeonSort(test_array, n);
+    sorting::pigonhole::pigeonSort(test_array, n);
 }
 
 /**
  * Main Function
+ */
+/**
+ * Main function
  */
 int main() {
     test_1();
