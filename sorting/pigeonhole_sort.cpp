5515dbcce802a691d9fac038882b11a3a499faaa
lsurface <lownish@hotmail.com>
2020-08-17 21:29:27 +0530

    Modified code structure

--- sorting/pigeonhole_sort.cpp
+++ sorting/pigeonhole_sort.cpp
@@ -1,20 +1,28 @@
-/*
-https://en.wikipedia.org/wiki/Pigeonhole_sort
-Pigeonhole sorting is a sorting algorithm that is suitable for sorting lists of
-elements where the number of elements and the number of possible key values are
-approximately the same. It requires O(n + Range) time where n is number of
-elements in input array and ‘Range’ is number of possible values in array.
-*/
-#include <algorithm>
-#include <array>
-#include <cassert>
-#include <iostream>
+/**
+ * @file
+ * @brief Implementation of [Pigeonhole Sort algorithm]
+ *(https://en.wikipedia.org/wiki/Pigeonhole_sort)
+ * @author [Lownish](https://github.com/Lownish)
+ * @details
+ *Pigeonhole sorting is a sorting algorithm that is suitable for sorting lists
+ *of elements where the number of elements and the number of possible key values
+ *are approximately the same. It requires O(n + Range) time where n is number of
+ *elements in input array and ‘Range’ is number of possible values in array.
+ *
+ * The time Complexity of the algorithm is \f$O(n+N)\f$.
+ */
 
+#include <algorithm>  //for std::is_sorted
+#include <array>      //for std::array
+#include <cassert>    //for std::array
+#include <iostream>   //for io operations
+
+/**
+ *Pigeonhole sorting of array with array size n
+ */
 template <std::size_t N>
 void pigeonSort(std::array<int, N> arr, int n) {
-    /*
-    Finding min and max*
-    */
+    // Finding min and max*
     int min = arr.at(0);
     int max = arr.at(0);
 
@@ -26,23 +34,20 @@ void pigeonSort(std::array<int, N> arr, int n) {
             max = arr[i];
         }
     }
-    /*
-    range refers to the number of holes required
-    */
+
+    // Range refers to the number of holes required
     int range = max - min + 1;
     int* hole = new int[range]();
 
-    /*
-    Copying all array values to pigeonhole
-    */
+    // Copying all array values to pigeonhole
     for (int i = 0; i < n; i++) {
         hole[arr[i] - min] = arr[i];
     }
 
-    /*
-    Deleting elements from list and
-    storing to original array
-    */
+    /**
+     *Deleting elements from list and
+     *storing to original array
+     */
     int count = 0;
     for (int i = 0; i < range; i++) {
         while (hole[i] != '\0') {
@@ -53,14 +58,17 @@ void pigeonSort(std::array<int, N> arr, int n) {
     }
     assert(std::is_sorted(std::begin(arr), std::end(arr)));
     std::cout << "Passed\n";
-    /*
-    Printing sorted array
-    */
+
+    // Printing sorted array
     for (int i = 0; i < n; i++) {
         std::cout << arr.at(i) << " ";
     }
 }
 
+/**
+ *Test function 1 with unsorted array
+ *{8, 3, 2, 7, 4, 6, 8}
+ */
 static void test_1() {
     std::array<int, 7> test_array = {8, 3, 2, 7, 4, 6, 8};
 
@@ -69,6 +77,10 @@ static void test_1() {
     pigeonSort(test_array, n);
 }
 
+/**
+ *Test function 2 with unsorted array
+ *{802, 630, 20, 745, 52, 300, 612, 932, 78, 187}
+ */
 static void test_2() {
     std::array<int, 10> test_array = {802, 630, 20,  745, 52,
                                       300, 612, 932, 78,  187};
@@ -78,6 +90,9 @@ static void test_2() {
     pigeonSort(test_array, n);
 }
 
+/**
+ * Main Function
+ */
 int main() {
     test_1();
     test_2();
