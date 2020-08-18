a4f4330f2d34603b48bb352b362f2cea6d23e47e
hertaioh7 <68407026+hertaioh7@users.noreply.github.com>
2020-08-18 21:47:53 +0800

    fixing errors

--- math/factorial.cpp
+++ math/factorial.cpp
@@ -7,8 +7,6 @@
 using std::cout;
 using std::cin;
 using std::endl;
-using std::string;
-using std::stringstream;
 /** function to find factorial of given number */
 unsigned int factorial(unsigned int n) {
     if (n == 0) {
@@ -20,18 +18,14 @@ unsigned int factorial(unsigned int n) {
 
 /** Main function */
 int main() {
-    string str;
-    int found = str.find("-");
+    
     int num;
     cout << "Please enter an integer to test factorial(0-33):" << endl;
-    cin >> str;
-    if (str[0] == '-') {
+    cin >> num;
+    if (num<0) {
         cout << "Integer cannot be negative" << endl;
     }
     else {
-                stringstream ss;
-                ss << str;
-                ss >> num;
                 cout << "Factorial of " << num << " is " << factorial(num) << endl;    
         
     }
