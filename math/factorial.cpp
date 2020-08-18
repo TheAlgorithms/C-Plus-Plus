e610bbaa5076329f71ff83d9f221ab2241c9a9e4
hertaioh7 <68407026+hertaioh7@users.noreply.github.com>
2020-08-18 21:35:44 +0800

    adding { after if (n == 0)

--- math/factorial.cpp
+++ math/factorial.cpp
@@ -11,8 +11,9 @@ using std::string;
 using std::stringstream;
 /** function to find factorial of given number */
 unsigned int factorial(unsigned int n) {
-    if (n == 0)
-        return 1;
+    if (n == 0) {
+    return 1;
+}
     return n * factorial(n - 1);
 }
 
