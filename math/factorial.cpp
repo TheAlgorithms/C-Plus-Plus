e0700ad2058b1497dc0201266bfbc7eb829c1c5b
hertaioh7 <68407026+hertaioh7@users.noreply.github.com>
2020-08-18 20:56:09 +0800

    Update factorial.cpp
    Can check whether is positive or negative, string's content are numerical or not, also can enter any number to test

--- math/factorial.cpp
+++ math/factorial.cpp
@@ -2,8 +2,9 @@
  * @file
  * @brief C++ program to find factorial of given number
  */
-#include <iostream>
-
+#include <iostream> 
+#include <sstream> 
+using namespace std;
 /** function to find factorial of given number */
 unsigned int factorial(unsigned int n) {
     if (n == 0)
@@ -11,10 +12,35 @@ unsigned int factorial(unsigned int n) {
     return n * factorial(n - 1);
 }
 
+/*check string is number or not*/
+bool check_number(string str) {
+    for (int i = 0; i < str.length(); i++)
+        if (isdigit(str[i]) == false)
+            return false;
+    return true;
+}
+
 /** Main function */
 int main() {
-    int num = 5;
-    std::cout << "Factorial of " << num << " is " << factorial(num)
-              << std::endl;
+    string str;
+    int found = str.find("-");
+    int num;
+    cout << "Please enter an integer to test factorial(0-33):" << endl;
+    cin >> str;
+    if (str[0]=='-') {
+        cout << "Integer cannot be negative" << endl;
+    }
+    else {
+    if (found != 0) {
+        if (check_number(str)) {
+            stringstream ss;
+            ss << str;
+            ss >> num;
+            cout << "Factorial of " << num << " is " << factorial(num) << endl;
+        }
+        else
+            cout << str << " is a string" << endl;
+    }
+    }
     return 0;
 }
