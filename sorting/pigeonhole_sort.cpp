4424320b075f885486b1a9c1440f278e4f7c075a
lsurface <lownish@hotmail.com>
2020-08-25 23:15:21 +0530

    Corrected test functions

--- sorting/pigeonhole_sort.cpp
+++ sorting/pigeonhole_sort.cpp
@@ -69,7 +69,7 @@ static void test_1() {
     const int n = 7;
     std::array<int, n> test_array = {8, 3, 2, 7, 4, 6, 8};
 
-    test_array = sorting::pigeonSort<n>(test_array, n);
+    test_array = sorting::pigeonSort<n>(test_array);
 
     assert(std::is_sorted(std::begin(test_array), std::end(test_array)));
 
@@ -90,7 +90,7 @@ static void test_2() {
     std::array<int, n> test_array = {802, 630, 20,  745, 52,
                                      300, 612, 932, 78,  187};
 
-    test_array = sorting::pigeonSort<n>(test_array, n);
+    test_array = sorting::pigeonSort<n>(test_array);
 
     assert(std::is_sorted(std::begin(test_array), std::end(test_array)));
 
@@ -110,7 +110,7 @@ static void test_3() {
     const int n = 4;
     std::array<int, n> test_array = {11, 13, 12, 14};
 
-    test_array = sorting::pigeonSort<n>(test_array, n);
+    test_array = sorting::pigeonSort<n>(test_array);
 
     assert(std::is_sorted(std::begin(test_array), std::end(test_array)));
 
