c634c0e50b638b35609be45c88f02c06d099a982
Nimish Shah <nimishshah2000@gmail.com>
2020-08-30 23:40:48 +0530

    Improve code and add support for 46+ fibb numbers

--- dynamic_programming/fibonacci_top_down.cpp
+++ dynamic_programming/fibonacci_top_down.cpp
@@ -1,12 +1,17 @@
+/**
+ * @file
+ * @brief C++ program to find nth term in the fibonacci series.
+ */
 #include <iostream>
 using namespace std;
-int arr[1000000];
-int fib(int n) {
-    if (arr[n] == -1) {
-        if (n <= 1)
-            arr[n] = n;
-        else
-            arr[n] = fib(n - 1) + fib(n - 2);
+
+size_t arr[1000000] = {0, 1};
+
+size_t fib(int n) {
+    if (n <= 1)
+        return arr[n];
+    if (!arr[n]) {
+        arr[n] = fib(n - 1) + fib(n - 2);
     }
     return arr[n];
 }
@@ -14,9 +19,6 @@ int main(int argc, char const *argv[]) {
     int n;
     cout << "Enter n: ";
     cin >> n;
-    for (int i = 0; i < n + 1; ++i) {
-        arr[i] = -1;
-    }
     cout << "Fibonacci number is " << fib(n) << endl;
     return 0;
 }
