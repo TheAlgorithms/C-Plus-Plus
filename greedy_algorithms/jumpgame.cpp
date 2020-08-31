0f2ce72be4fe35a029c2dea8c8facaead229c8a8
Rakshaa Viswanathan <46165429+rakshaa2000@users.noreply.github.com>
2020-09-01 00:04:11 +0530

    Created jumpgame.cpp
    An algorithm to check if you can reach the destination

---
+++ greedy_algorithms/jumpgame.cpp
@@ -0,0 +1,21 @@
+//Jump Game:
+/*Given an array of non-negative integers, you are initially positioned at the first index of the array.
+Each element in the array represents your maximum jump length at that position.
+Determine if you are able to reach the last index.*/
+
+#include <vector.h>
+bool canJump(vector<int>& nums) {
+    int lastPos = nums.size() - 1;
+    for (int i = nums.size() - 1; i >= 0; i--) {
+        if (i + nums[i] >= lastPos) {
+            lastPos = i;
+        }
+    }
+    return lastPos == 0;
+}
+
+void main(){
+    //Sample test case
+    vector<int> num={4,3,1,0,5};
+    cout<<canJump(num);  //Should display true, as when you take one step from position 0, you reach position 1, from which 3 steps lead you to the destination
+}
