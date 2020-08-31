331638065c31aaa8b36613352ffd30072b694836
Rakshaa Viswanathan <46165429+rakshaa2000@users.noreply.github.com>
2020-09-01 00:15:34 +0530

    Fixed bug and removed namespace std

--- greedy_algorithms/jumpgame.cpp
+++ greedy_algorithms/jumpgame.cpp
@@ -5,9 +5,9 @@ Determine if you are able to reach the last index.*/
 
 #include <vector>
 #include<iostream>
-using namespace std;
+
 bool canJump(vector<int> nums) {
-    int lastPos = nums.size() - 1;
+    auto lastPos = nums.size() - 1;
     for (auto i = nums.size() - 1; i >= 0; i--) {
         if (i + nums[i] >= lastPos) {
             lastPos = i;
