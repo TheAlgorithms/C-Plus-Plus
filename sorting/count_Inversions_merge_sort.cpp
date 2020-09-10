a7ccb8514360d2b9f16c490fad08d2ac63fdd7a9
Rakshit Raj <rraj.rakshit@gmail.com>
2020-09-10 23:22:57 +0530

    Update count_Inversions_merge_sort.cpp

--- sorting/count_Inversions_merge_sort.cpp
+++ sorting/count_Inversions_merge_sort.cpp
@@ -135,8 +135,10 @@ void show(int arr[], int array_size)
     cout << endl;
 }
 
-int main( void )
+int main(void)
 {
+ /**   
+    // Enter your own array
     int size;
     cout << "Enter number of elements: ";
     cin >> size;
@@ -150,6 +152,9 @@ int main( void )
         cout << "Element "<< (i+1) << ": ";
         cin >> arr[i];
     }
+*/
+    size = 100;
+    arr[] = {100, 99, 98, 97, 96, 95, 94, 93, 92, 82, 81, 80, 79, 78, 77, 76, 75, 74, 73, 72, 71, 70, 69, 68, 67, 66, 65, 64, 63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50, 49, 48, 47, 46, 45, 44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
 
     cout << endl;
     show(arr, size);
@@ -161,7 +166,7 @@ int main( void )
     cout << endl << "Sorted array -->  "<< endl;
     show(arr, size);
 
-    delete[] arr; 
+    delete[] arr;
     return 0;
 }
 /** @rakshitraj */
