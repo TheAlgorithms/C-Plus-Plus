b50219d1663fea8460e1b2aa31ab79feab3168dd
ayush523 <ayushsingh523@gmail.com>
2020-09-16 11:41:28 +0530

    fixed warnings

--- kadane_algorithm/kadane.cpp
+++ kadane_algorithm/kadane.cpp
@@ -1,26 +1,28 @@
+#include <array>
 #include <climits>
 #include <iostream>
 
-// Function for maximum subarray
-int maxSubArray(int arr[], int size) {
+int maxSubArray(const std::array<int, 5> &n) {
     int curr_sum = 0;
     int max_sum = INT_MIN;
-    for (int i = 0; i < size; i++) {
-        curr_sum += arr[i];
+    for (int i = 0; i < n.size(); i++) {
+        curr_sum += n[i];
         max_sum = std::max(max_sum, curr_sum);
         curr_sum = std::max(curr_sum, 0);
     }
     return max_sum;
 }
 int main() {
-    int n, i;
-    std::cout << "Enter the number of elements \n";
-    std::cin >> n;
-    int a[n];
-    for (i = 0; i < n; i++) {
-        std::cin >> a[i];
+    std::array<int, 5> n;
+
+    // taking values of elements from user
+    for (int i = 0; i < n.size(); i++) {
+        std::cout << "Enter value of n[" << i << "]"
+                  << "\n";
+        std::cin >> n[i];
     }
-    int max_sum = maxSubArray(a, n);
+    int max_sum = maxSubArray(n);
     std::cout << "Maximum subarray sum is " << max_sum;
+
     return 0;
 }
