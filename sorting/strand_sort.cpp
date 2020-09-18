9889efb6a68eac4e05b83e7143caa2e23a70b410
Mertcan Davulcu <54983926+mertcandav@users.noreply.github.com>
2020-09-18 21:59:06 +0300

    Update strand_sort.cpp

--- sorting/strand_sort.cpp
+++ sorting/strand_sort.cpp
@@ -1,28 +1,47 @@
+/**
+ * @file strand_sort.cpp
+ * @brief Implementation of [Strand Sort](https://en.wikipedia.org/wiki/Strand_sort) algorithm.
+ *
+ * @details
+ * Strand Sort is a sorting algorithm that works in (O(n)) time if list is already sorted and works in (O(n * n)) in worst case.
+ * 
+ * ### Algorithm
+ * + It is passed over the array to be sorted once and the ascending (sequential) numbers are taken.
+ * + After the first iteration, the sequential sub-array is put on the empty sorted array.
+ * + The main sequence is passed over again and a new sub-sequence is created in order.
+ * + Now that the sorted array is not empty, the newly extracted substring is merged with the sorted array.
+ * + Repeat types 3 and 4 until the sub-sequence and main sequence are empty.
+ * 
+ * @author [Mertcan Davulcu](https://github.com/mertcandav)
+ */
+#include <iostream>
 #include <iostream>
 #include <list>
 
-template <typename T>
-std::list<T> strand_sort(std::list<T> lst) {
-    if (lst.size() < 2) { // Returns list if empty or contains only one element
-        return lst;
-    }
-    std::list<T> result;
-    std::list<T> sorted;
-    while(!lst.empty()) {
-        sorted.push_back(lst.front());
-        lst.pop_front();
-        for (auto it = lst.begin(); it != lst.end(); ) {
-            if (sorted.back() <= *it) {
-                sorted.push_back(*it);
-                it = lst.erase(it);
-            } else {
-                it++;
-            }
+namespace strand {
+    template <typename T>
+    std::list<T> strand_sort(std::list<T> lst) {
+        if (lst.size() < 2) { // Returns list if empty or contains only one element
+            return lst; // Returns list
         }
-        result.merge(sorted);
+        std::list<T> result; // Define new "result" named list instance.
+        std::list<T> sorted; // Define new "sorted" named list instance.
+        while(!lst.empty()) /* if lst is not empty */ {
+            sorted.push_back(lst.front()); // Adds the first element of "lst" list to the bottom of the "sorted" list.
+            lst.pop_front(); // Remove first element of "lst" list.
+            for (auto it = lst.begin(); it != lst.end(); ) { // Return the loop as long as the current iterator is not equal to the last literator of the "lst" list.
+                if (sorted.back() <= *it) { // If the last reference of the "sorted" list is less than or equal to the current iterator reference.
+                    sorted.push_back(*it); // Adds the iterator retrieved in the loop under the "sorted" list.
+                    it = lst.erase(it); // Deletes the element with the current iterator and assigns the deleted element to the iterator.
+                } else {
+                   it++; // Next iterator.
+                }
+            }
+            result.merge(sorted); // Merge "result" list with "sorted" list.
+          }
+        return result; // Returns sorted list
     }
-    return result;
-}
+} // namespace strand
 
 /**
  * @brief Main function
@@ -36,7 +55,7 @@ int main() {
         std::cout << item << " ";
     }
 
-    lst = strand_sort(lst); // Sort list.
+    lst = strand::strand_sort(lst); // Sort list.
 
     std::cout << "\nAfter: ";
     for(auto item: lst) {
