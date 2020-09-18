243aaa5162fcca06b9918341ce6e491f1d9c2391
Mertcan Davulcu <54983926+mertcandav@users.noreply.github.com>
2020-09-18 21:02:34 +0300

    Update strand_sort.cpp

--- strand_sort.cpp
+++ strand_sort.cpp
@@ -11,7 +11,7 @@ std::list<T> strand_sort(std::list<T> lst) {
     while(!lst.empty()) {
         sorted.push_back(lst.front());
         lst.pop_front();
-        for (typename std::list<T>::iterator it = lst.begin(); it != lst.end(); ) {
+        for (auto it = lst.begin(); it != lst.end(); ) {
             if (sorted.back() <= *it) {
                 sorted.push_back(*it);
                 it = lst.erase(it);
@@ -29,14 +29,14 @@ int main() {
     std::list<int> lst = { -333, 525, 1, 0, 94, 52, 33 };
 
     std::cout << "Before: ";
-    for(int item: lst) {
+    for(auto item: lst) {
         std::cout << item << " ";
     }
 
     lst = strand_sort(lst); // Sort list.
 
     std::cout << "\nAfter: ";
-    for(int item: lst) {
+    for(auto item: lst) {
         std::cout << item << " ";
     }
 
