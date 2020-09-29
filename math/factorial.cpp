dc00715680f788e63cf9c84ced5fa87e151d6c20
Nimish Shah <nimishshah2000@gmail.com>
2020-09-30 01:34:02 +0530

    add tests

--- math/factorial.cpp
+++ math/factorial.cpp
@@ -2,10 +2,10 @@
  * @file
  * @brief C++ program to find factorial of given number
  */
-#include <ctime>
+#include <cassert>
 #include <iostream>
-/** function to find factorial of given number */
-unsigned long long int factorial(unsigned long int n) {
+/** function to find factorial of given number using recursive method*/
+uint64_t factorial(unsigned long int n) {
     if (n == 0)
         return 1;
     for (int i = 0; i < 10000; i++) {
@@ -14,18 +14,35 @@ unsigned long long int factorial(unsigned long int n) {
     return n * factorial(n - 1);
 }
 
-unsigned long long int fact(unsigned long long int n) {
-    unsigned long long int ans = 1;
-    for (unsigned long long int i = n; i > 0; i--) {
+/*
+ * compute factorial using iterative method
+ * Recursion is usually costly in terms of computational power usage
+ * */
+uint64_t fact(uint64_t n) {
+    uint64_t ans = 1;
+    for (uint64_t i = n; i > 0; i--) {
         ans *= i;
     }
     return ans;
 }
+
+void test(uint64_t n, uint64_t expected) {
+    assert(factorial(n) == expected);
+    assert(fact(n) == expected);
+}
+
 /** Main function */
 int main() {
-    unsigned long long int num = 60;
-    unsigned long long int a;
+    std::cout << "Test 1:\t n=5\t...";
+    test(5, 120);
+    std::cout << "passed\n";
 
+    std::cout << "Test 2:\t n=10\t...";
+    test(10, 3628800);
+    std::cout << "passed\n";
+
+    uint64_t num = 16;
+    uint64_t a;
     std::cout << "Factorial of " << num << " is :" << fact(num) << std::endl;
     return 0;
 }
