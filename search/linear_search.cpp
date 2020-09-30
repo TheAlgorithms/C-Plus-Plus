2f307cd57612619b991d3403d1767aa29cc23d4a
arsalan7866 <42116394+arsalan7866@users.noreply.github.com>
2020-09-30 21:06:47 +0530

    tried to shorthen the code and reduce the complexity of using pointers helping for beginners to learn code easier

--- search/linear_search.cpp
+++ search/linear_search.cpp
@@ -13,23 +13,14 @@
  * \returns index where the key-value occurs in the array
  * \returns -1 if key-value not found
  */
-int LinearSearch(int *array, int size, int key) {
-    for (int i = 0; i < size; ++i) {
-        if (array[i] == key) {
-            return i;
-        }
-    }
 
-    return -1;
-}
 
 /** main function */
 int main() {
     int size;
     std::cout << "\nEnter the size of the Array : ";
     std::cin >> size;
-
-    int *array = new int[size];
+    int arr[size];
     int key;
 
     // Input array
@@ -40,14 +31,21 @@ int main() {
 
     std::cout << "\nEnter the number to be searched : ";
     std::cin >> key;
-
-    int index = LinearSearch(array, size, key);
+    for(int i=0;i<size;i++)
+    {
+        if(arr[i]==key)
+        {
+            index = i;
+            break;
+        }
+        break;
+    }   
+            
     if (index != -1) {
         std::cout << "\nNumber found at index : " << index;
     } else {
         std::cout << "\nNot found";
     }
 
-    delete[] array;
     return 0;
 }
