06d80692cee9399d42290f95f5fbb8bcd2ed1f79
samruddha patil <45798222+sampatil578@users.noreply.github.com>
2020-10-01 10:44:50 +0530

    Update secant_method.cpp

--- numerical_methods/secant_method.cpp
+++ numerical_methods/secant_method.cpp
@@ -28,33 +28,42 @@ static double eq(double x) {
 //function for finding root in an interval
 static double secant(double x1,double x2,double y1,double y2){
     double diff = x1-x2;
-    if(diff<0)
-    diff= (-1)*diff;
-    if(diff<0.000001){  // 0.000001 is the precision constant.
-        if(y1<0)
-        y1=-y1;
-        if(y2<0)
-        y2=-y2;
-        if(y1<y2)
-        return x1;
-        else
-        return x2;
+    if(diff<0){
+        diff= (-1)*diff;
     }
-    double x3,y3;
+    if(diff<0.000001){  // 0.000001 is the precision constant.
+        if(y1<0){
+            y1=-y1;
+        }
+        if(y2<0){
+            y2=-y2;
+        }
+        if(y1<y2){
+            return x1;
+        }
+        else{
+            return x2;
+        }
+    }
+    double x3=0,y3=0;
     x3 = x1 - (x1-x2)*(y1)/(y1-y2);
     y3 = eq(x3);
     return secant(x2,x3,y2,y3);   //recursive call for next iteration.
 }
 //function for printing roots
 void printRoot(double root,int COUNT){
-    if(COUNT==1)
-    cout << "Your 1st root is : " << root << endl;
-    else if(COUNT==2)
-    cout << "Your 2nd root is : " << root << endl;
-    else if(COUNT==3)
-    cout << "Your 3rd root is : " << root << endl;
-    else
-    cout << "Your "<<COUNT<<"th root is : " << root << endl;
+    if(COUNT==1){
+        cout << "Your 1st root is : " << root << endl;
+    }
+    else if(COUNT==2){
+        cout << "Your 2nd root is : " << root << endl;
+    }
+    else if(COUNT==3){
+        cout << "Your 3rd root is : " << root << endl;
+    }
+    else{
+        cout << "Your "<<COUNT<<"th root is : " << root << endl;
+    }
 }
 
 
