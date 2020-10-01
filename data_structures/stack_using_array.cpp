bfdb314eace36de3f6d1043b937a13880a0141c3
unknown <rajbipin1412@gmail.com>
2020-10-01 12:48:10 +0530

    Use STL in stack_using_array.cpp

--- data_structures/stack_using_array.cpp
+++ data_structures/stack_using_array.cpp
@@ -1,58 +1,60 @@
-#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
 
-int *stack;
-int stack_idx = 0, stack_size;
 
-void push(int x) {
-    if (stack_idx == stack_size) {
-        std::cout << "\nOverflow";
-    } else {
-        stack[stack_idx++] = x;
-    }
-}
-
-void pop() {
-    if (stack_idx == 0) {
-        std::cout << "\nUnderflow";
-    } else {
-        std::cout << "\n" << stack[--stack_idx] << " deleted";
-    }
-}
-
-void show() {
-    for (int i = 0; i < stack_idx; i++) {
-        std::cout << stack[i] << "\n";
-    }
-}
-
-void topmost() { std::cout << "\nTopmost element: " << stack[stack_idx - 1]; }
+//void pop() {
+//    if (stack_idx == 0) {
+//        std::cout << "\nUnderflow";
+//    } else {
+//        std::cout << "\n" << stack[--stack_idx] << " deleted";
+//    }
+//}
+//
+//void show() {
+//    for (int i = 0; i < stack_idx; i++) {
+//        std::cout << stack[i] << "\n";
+//    }
+//}
+//
+//void topmost() { std::cout << "\nTopmost element: " << stack[stack_idx - 1]; }
 int main() {
-    std::cout << "\nEnter stack_size of stack : ";
-    std::cin >> stack_size;
-    stack = new int[stack_size];
+    
+    stack <int> stk;
+    stack <int>s;
     int ch, x;
     do {
-        std::cout << "\n0. Exit";
-        std::cout << "\n1. Push";
-        std::cout << "\n2. Pop";
-        std::cout << "\n3. Print";
-        std::cout << "\n4. Print topmost element:";
-        std::cout << "\nEnter Your Choice : ";
-        std::cin >> ch;
+        cout << "\n0. Exit";
+        cout << "\n1. Push";
+        cout << "\n2. Pop";
+        cout << "\n3. Print";
+        cout << "\n4. Print topmost element:";
+        cout << "\nEnter Your Choice : ";
+        cin >> ch;
         if (ch == 1) {
             std::cout << "\nInsert : ";
             std::cin >> x;
-            push(x);
-        } else if (ch == 2) {
-            pop();
-        } else if (ch == 3) {
-            show();
-        } else if (ch == 4) {
-            topmost();
+            stk.push(x);
+        } 
+		else if (ch == 2) {
+            stk.pop();
+        } 
+		else if (ch == 3) {
+            while(!stk.empty()){
+            	s.push(stk.top());
+            	stk.pop();
+			}
+			while(!s.empty()){
+				cout<<s.top()<<endl;
+				stk.push(s.top());
+				s.pop();
+			}
+        } 
+		else if (ch == 4) {
+            cout<<stk.top();
         }
     } while (ch != 0);
 
-    delete[] stack;
+    
 
     return 0;
 }
