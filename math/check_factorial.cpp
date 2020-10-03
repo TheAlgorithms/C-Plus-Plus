ca7402fb05d43eef9b695dc1bc0c53de77bc658a
divyajyotiuk <divyajyoti.u@gmail.com>
2020-10-03 16:58:21 +0530

    feat: added check_factorial

--- math/check_factorial.cpp
+++ math/check_factorial.cpp
@@ -7,8 +7,6 @@
 #include <cassert>
 #include <iostream>
 
-using namespace std;
-
 /**
  * Function to check if the given number is factorial of some number or not.
  * @param n number to be checked.
@@ -34,25 +32,25 @@ bool is_factorial(int n) {
 
 /** Test function */
 void tests() {
-    cout << "Test 1:\t n=50\n";
+    std::cout << "Test 1:\t n=50\n";
     assert(is_factorial(50) == false);
-    cout << "passed\n";
+    std::cout << "passed\n";
 
-    cout << "Test 2:\t n=720\n";
+    std::cout << "Test 2:\t n=720\n";
     assert(is_factorial(720) == true);
-    cout << "passed\n";
+    std::cout << "passed\n";
 
-    cout << "Test 3:\t n=0\n";
+    std::cout << "Test 3:\t n=0\n";
     assert(is_factorial(0) == false);
-    cout << "passed\n";
+    std::cout << "passed\n";
 
-    cout << "Test 4:\t n=479001600\n";
+    std::cout << "Test 4:\t n=479001600\n";
     assert(is_factorial(479001600) == true);
-    cout << "passed\n";
+    std::cout << "passed\n";
 
-    cout << "Test 5:\t n=-24\n";
+    std::cout << "Test 5:\t n=-24\n";
     assert(is_factorial(-24) == false);
-    cout << "passed\n";
+    std::cout << "passed\n";
 }
 
 /** Main function */
