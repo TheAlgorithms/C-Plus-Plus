88a715db731890b6fa9bd732639c3e22ddd96c1c
anjali1903 <anjali.jha1903@gmail.com>
2020-10-05 00:31:47 +0530

    feat: add longest palindromic subsequence algorithm

---
+++ dynamic_programming/longest_palindromic_subsequence.cpp
@@ -0,0 +1,92 @@
+/**
+ * @file 
+ * @brief Program to find the Longest Palindormic 
+ * Subsequence of a string
+ *
+ * @details 
+ * [Palindrome](https://en.wikipedia.org/wiki/Palindrome) string sequence of  
+ * characters which reads the same backward as forward
+ * [Subsequence](https://en.wikipedia.org/wiki/Subsequence)  is a sequence that 
+ * can be derived from another sequence by deleting some or no elements without 
+ * changing the order of the remaining elements.
+
+ * @author [Anjali Jha](https://github.com/anjali1903)
+ */
+
+#include <iostream>
+#include <algorithm>
+#include <cassert>
+using namespace std;
+
+/**
+* Function that returns the longest palindromic 
+* subsequence of a string
+*/
+string lps(string a) {
+	string b = a;
+	reverse(b.begin(), b.end());
+	int m = a.length();
+	int res[m+1][m+1];
+	
+	//Finding the length of the longest
+	// palindromic subsequence and storing
+	// in a 2D array in bottoms-up manner
+	for(int i = 0; i <= m; i++) {
+		for(int j = 0; j <= m; j++) {
+			
+			if(i == 0 or j == 0) {
+				res[i][j] = 0;
+			}
+			else if(a[i-1] == b[j-1]) {
+				res[i][j] = res[i-1][j-1] + 1;
+			}
+			else {
+				res[i][j] = max(res[i-1][j], res[i][j-1]);
+			}
+		}
+	}
+	
+	string ans = "";
+	int i = m, j = m;
+	
+	//starting from right-most bottom-most corner
+	// and storing them one by one in ans
+	while(i > 0 and j > 0) {
+		
+		//if current characters in a and b are same
+		// then it is a part of the ans
+		if(a[i-1] == b[j-1]) {
+			ans = a[i-1] + ans;
+			i--;
+			j--;
+		}
+		//If they are not same, find the larger of the 
+		// two and move in that direction
+		else if(res[i-1][j] > res[i][j-1]) {
+			i--;
+		}
+		else {
+			j--;
+		}
+	}
+	
+	return ans;
+}
+
+/** Test function */
+void test() {
+	// lps("radar") return "radar"
+	assert(lps("radar")=="radar");
+	// lps("abbcbaa") return "abcba"
+	assert(lps("abbcbaa")=="abcba");
+	// lps("bbbab") return "bbbb"
+	assert(lps("bbbab")=="bbbb");
+}
+
+/**
+ * Main Function
+ */
+int main() {
+    test(); // execute the tests
+    return 0;
+}
