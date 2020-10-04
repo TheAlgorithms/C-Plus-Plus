1512b3b769053fc629574083b09c53164b8f060a
Pawananjani1A <pawananjanimth1@gmail.com>
2020-10-04 09:33:16 +0530

    Fixed bugs

--- operations_on_datastructures/mergeSortedLinkedLists.cpp
+++ operations_on_datastructures/mergeSortedLinkedLists.cpp
@@ -1,41 +1,8 @@
 #include<bits/stdc++.h>
-#include<ext/pb_ds/assoc_container.hpp>
-using namespace __gnu_pbds;
 using namespace std;
 
 
-#define ff                first
-#define ss                second
-#define int               long long
-#define pb                push_back
-#define mp                make_pair
-#define pii               pair<int,int>
-#define vi                vector<int>
-#define mii               map<int,int>
-#define pqb               priority_queue<int>
-#define pqs               priority_queue<int,vi,greater<int> >
-#define setbits(x)        __builtin_popcountll(x)
-#define zrobits(x)        __builtin_ctzll(x)
-#define mod               1000000007   //10^9 + 7
-#define inf               1e18
-#define ps(x,y)           fixed<<setprecision(y)<<x
-#define mk(arr,n,type)    type*arr = new type[n]
-#define w(x)              int x;cin>>x;while(x--)
-#define rep(i,n)          for(int i=0;i<(n);++i)
-#define repA(i,a,n)       for(int i=a;i<=(n);++i)
-#define repD(i,a,n)       for(int i=a;i>=(n);--i)
-#define itr(type,it,x)    for(type it = x.begin();it!=x.end();it++)
-#define endl              "\n"
-mt19937                  rng(chrono::steady_clock::now().time_since_epoch().count());  // to shuffle an array randomly viz. shuffle(arr,arr+n,rng)
 
-typedef tree<int , null_type , less<int> , rb_tree_tag, tree_order_statistics_node_update> pbds;  //policy based data structure
-
-
-void c_p_c() {
-
-	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-
-}
 
 class node {
 public:
@@ -265,7 +232,8 @@ void buildList(node*&head, int n) {
 
 // Overloaded ">>" operator for convenient input of linked list
 istream& operator>>(istream & is, node*&head) {
-	int n; cin >> n;
+	int n;
+     cin >> n;
 	buildList(head, n);
 	return is;
 }
@@ -303,9 +271,7 @@ node*  merge(node*a, node*b) {
 
 
 
-int32_t main() {
-
-	c_p_c();
+int main() {
 
 	node*head = NULL;
 	node*head2 = NULL;
