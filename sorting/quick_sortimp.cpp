a1ee1b4c40d41193bd95cfe9d9a1f404f411441f
Akash chouhan <akash.c1500@gmail.com>
2020-10-05 18:23:57 +0530

    Update quick_sortimp.cpp

--- sorting/quick_sortimp.cpp
+++ sorting/quick_sortimp.cpp
@@ -6,8 +6,8 @@
 /*The key process in quickSort is partition(). Target of partitions is, given an array and an element x of array as pivot, put x at its correct position in sorted array and put all smaller elements (smaller than x) before x, and put all greater elements (greater than x) after x. All this should be done in linear time.*/
 
 #include <iostream>
-#include <stdlib.h>
-#include <time.h>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 class QuickSort
