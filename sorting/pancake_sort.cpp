bed271679bd29a75951ac38cfe712960e6a20b15
divyansh12323 <divyansh.18je0297@am.iitism.ac.in>
2020-10-05 18:42:51 +0530

    Update pancake_sort.cpp

--- sorting/pancake_sort.cpp
+++ sorting/pancake_sort.cpp
@@ -20,7 +20,7 @@
 #include <iostream>   // for io operations
 #include <vector>     // for std::vector
 #include <algorithm>  // for std::is_sorted
-#include <cassert>     // for std::assert
+#include <cassert>    // for std::assert
 
 /**
  * @namespace sorting
@@ -29,8 +29,8 @@
 namespace sorting {
     /**
     * This implementation is for a C-style array input that gets modified in place.
-    * @param [in,out] arr our array of elements.
-    * @param size size of given array
+    * @param [start,end] arr our vector of elements.
+    * @param n size of given array
     */
     template <typename T>
     void reverse(std::vector <T>& arr, int start, int end) { 
@@ -47,7 +47,7 @@ namespace sorting {
     template <typename T>
     int pancakeSort(std::vector <T>& arr, int n)  {
         for (int i=n; i>1; --i) {
-            int mi = 0, j;
+            int mi = 0, j; //intialize some variables.
             T maxE = 0;
             for(j=0; j<i; j++) {
                 if(arr[j] >= maxE) {
@@ -55,7 +55,8 @@ namespace sorting {
 	                mi = j;
 	            }
 	        }
-		    if (mi != i-1) { 
+		    if (mi != i-1) //check for reversing 
+			 { 
 			    reverse(arr, 0, mi); 
 			    reverse(arr, 0, i-1); 
 		    } 
@@ -68,27 +69,43 @@ namespace sorting {
  */
 static void test() {
     
-    // example 1: int
+    // example 1: vector ofi nt
     const int size1 = 7;
+    std::cout<<"\nTest 1- as std::vector<int>...";
     std::vector <int> arr1 = {23, 10, 20, 11, 12, 6, 7};
     sorting::pancakeSort(arr1, size1);
     assert(std::is_sorted(arr1.begin(), arr1.end()));
     std::cout<<"Passed\n";
     for (int i = 0; i < size1; i++) {
-        std::cout<<arr1[i]<<" ";
+        std::cout<<arr1[i]<<" ,";
     }
     std::cout<<std::endl;
     
-    // example 2: double
-    const int size2 = 7;
-    std::vector <double> arr2 = {23.56, 10.62, 200.78, 111.484, 1.2, 61.77, 79.6};
+    // example 2: vector of double
+    const int size2 = 8;
+    std::cout<<"\nTest 2- as std::vector<double>...";
+    std::vector <double> arr2 = {23.56, 10.62, 200.78, 111.484,3.9, 1.2, 61.77, 79.6};
     sorting::pancakeSort(arr2, size2);
     assert(std::is_sorted(arr2.begin(), arr2.end()));
     std::cout<<"Passed\n";
     for (int i = 0; i < size2; i++) {
-        std::cout<<arr2[i]<<" ";
+        std::cout<<arr2[i]<<", ";
     }
     std::cout<<std::endl;
+  
+    // example 3:vector of float
+      const int size3=7;
+    std::cout<<"\nTest 3- as std::vector<float>...";
+    std::vector <float> arr3 = {6.56, 12.62, 200.78, 768.484, 19.27, 68.87, 9.6};
+    sorting::pancakeSort(arr3, size3);
+    assert(std::is_sorted(arr3.begin(), arr3.end()));
+    std::cout<<"Passed\n";
+    for (int i = 0; i < size3; i++) {
+        std::cout<<arr3[i]<<", ";
+    }
+    std::cout<<std::endl;
+	  
+    
 }
 
 /**
