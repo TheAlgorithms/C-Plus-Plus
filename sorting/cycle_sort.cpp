75d31342d4c6e22613f1a4c732913f5e2c06c04e
dalaoqi <mystes3016@gmail.com>
2020-10-05 22:54:56 +0800

    feat: add cycle sort algorithm

--- sorting/cycle_sort.cpp
+++ sorting/cycle_sort.cpp
@@ -1,11 +1,11 @@
 /**
  * @file
- * @brief Implementation of [Cycle
- *sort](https://en.wikipedia.org/wiki/Cycle_sort) algorithm
+ * @brief Implementation of [Cycle sort](https://en.wikipedia.org/wiki/Cycle_sort) algorithm
  *
  *@details
- *Cycle Sort is a sorting algorithm that works in \f$O(n^2)\f$ time in best case
- *and works in \f$O(n^2)\f$ in worst case.
+ *Cycle Sort is a sorting algorithm that works in \f$O(n^2)\f$ time in best case and works in \f$O(n^2)\f$ in worst case.
+ *If a element is already at its correct  position, do nothing.
+ *If a element is not at its correct position, we then need to move it to its correct position by computing the correct positions.Therefore, we should make sure the duplicate elements.
  *
  *@author [TsungHan Ho](https://github.com/dalaoqi)
  */
@@ -20,67 +20,47 @@ void swap(int *a, int *b) {
     *b = tmp;
 }
 
-// Function sort the array using Cycle sort
-void cycleSort(std::vector<int> *arr, int n) {
-    // count number of memory writes
-    int writes = 0;
-
-    // traverse array elements and put it to on
-    // the right place
-    for (int cycle_start = 0; cycle_start <= n - 2; cycle_start++) {
-        // initialize item as starting point
+void cycleSort(std::vector<int> *arr) {
+    for (int cycle_start = 0; cycle_start <= (*arr).size() - 1; cycle_start++) {
+        // initialize item
         int item = (*arr)[cycle_start];
 
-        // Find position where we put the item. We basically
-        // count all smaller elements on right side of item.
+        // Count the number of elements smaller than item, this  number is the correct index of item.
         int pos = cycle_start;
-        for (int i = cycle_start + 1; i < n; i++)
+        for (int i = cycle_start + 1; i < (*arr).size(); i++)
             if ((*arr)[i] < item)
                 pos++;
 
-        // If item is already in correct position
+        // item is already in correct position
         if (pos == cycle_start)
             continue;
 
-        // ignore all duplicate  elements
+        // duplicate  elements
         while (item == (*arr)[pos]) pos += 1;
+        swap(&item, &(*arr)[pos]);
 
-        // put the item to it's right position
-        if (pos != cycle_start) {
-            swap(&item, &(*arr)[pos]);
-            writes++;
-        }
-
-        // Rotate rest of the cycle
+        // Rest of the  elements
         while (pos != cycle_start) {
             pos = cycle_start;
-
             // Find position where we put the element
-            for (int i = cycle_start + 1; i < n; i++)
+            for (int i = cycle_start + 1; i < (*arr).size(); i++)
                 if ((*arr)[i] < item)
                     pos += 1;
-
-            // ignore all duplicate  elements
+            // duplicate  elements
             while (item == (*arr)[pos]) pos += 1;
-
-            // put the item to it's right position
-            if (item != (*arr)[pos]) {
-                swap(&item, &(*arr)[pos]);
-                writes++;
-            }
+            swap(&item, &(*arr)[pos]);
         }
     }
 }
 
 /* Test function */
 void test() {
-    // [1, 8, 3, 9, 10, 10, 2, 4] return [1, 2, 3, 4, 8, 9, 10, 10]
-    std::vector<int> array = {1, 8, 3, 9, 10, 10, 2, 4};
+    // [506, 48, 123, 79, 0, 362, 951, 500, 0] return [0, 0, 48, 79, 123, 362, 500, 506, 951]
+    std::vector<int> array = {506, 48, 123, 79, 0, 362, 951, 500, 0};
     std::vector<int> *arr = &array;
-    int n = array.size();
-    cycleSort(arr, n);
-    std::cout << "After sort : " << std::endl;
-    for (int i = 0; i < n; i++) std::cout << (*arr)[i] << " ";
+    cycleSort(arr);
+    std::cout << "Sorted : ";
+    for (int i = 0; i < (*arr).size(); i++) std::cout << (*arr)[i] << " ";
 }
 
 /* Main function */
