0e2610c442a72c85a2efe29e5ed5b6e8af9c89e3
Pardeep Bhatt <pardeepbhatt5254@gmail.com>
2020-10-08 19:28:13 +0530

    Update dynamic_programming/0_1_knapsack.cpp
    Co-authored-by: Krishna Vedala <7001608+kvedala@users.noreply.github.com>

--- dynamic_programming/0_1_knapsack.cpp
+++ dynamic_programming/0_1_knapsack.cpp
@@ -6,7 +6,7 @@
  * @details
  * Given weights and values of n items, put these items in a knapsack of
  * capacity W to get the maximum total value in the knapsack. In other words,
- * given two integer arrays val[0..n-1] and wt[0..n-1] which represent values
+ * given two integer arrays `val[0..n-1]` and `wt[0..n-1]` which represent values
  * and weights associated with n items respectively. Also given an integer W
  * which represents knapsack capacity, find out the maximum value subset of
  * val[] such that sum of the weights of this subset is smaller than or equal to
