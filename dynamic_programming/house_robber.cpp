335b92c736c962b2ef139e7af5c68938f69f3a40
Sumukh Bhardwaj <42494776+SumukhBhardwaj@users.noreply.github.com>
2020-10-10 23:27:51 +0530

    Update house_robber.cpp

--- dynamic_programming/house_robber.cpp
+++ dynamic_programming/house_robber.cpp
@@ -1,5 +1,4 @@
-/*
-House Robber - Dynamic Programming
+/*House Robber - Dynamic Programming
 
 Problem Statement: 
 A robber is planning to rob houses along a street. Each house has a 
@@ -9,15 +8,14 @@ system connected and it will automatically contact the police if two
 adjacent houses were broken into on the same night.
 
 Task: 
-Determine the maximum amount of money you can rob tonight without alerting the police
-*/
+Determine the maximum amount of money you can rob tonight without alerting the police*/
 
 
 #include <iostream>
 #include <vector>
 #include <cstring> 
 
-int houseRobber(int dpTable[],vector<int>& money,int n,int pos=0)
+int houseRobber(int dpTable[],std::vector<int>& money,int n,int pos=0)
 {
     if(pos >= n)
         return 0;              // if all houses are done, return 0
@@ -25,29 +23,29 @@ int houseRobber(int dpTable[],vector<int>& money,int n,int pos=0)
     if(dpTable[pos] != -1)
         return dpTable[pos];   // if the result for this position has been already calculated, return it
         
-    return dpTable[pos] = max( 
+    return dpTable[pos] = std::max( 
 	                      money[pos]+houseRobber(dpTable, money, n, pos+2)     // rob the current house & move to next to next one
-						           ,houseRobber(dpTable, money, n, pos+1));             // didn't rob the current house move to next one
+						 ,houseRobber(dpTable, money, n, pos+1));              // didn't rob the current house move to next one
 }
 	
 int main() 
 {
 	int n;
 	
-    cout <<"Enter number of houses ";
-    cin >> n;
+    std::cout <<"Enter number of houses ";
+    std::cin >> n;
 	
-	vector<int> money(n);
+	std::vector<int> money(n);
 	
 	// memoization table
 	int dpTable[n]; 
 
 	memset(dpTable,-1,sizeof(dpTable));
 	
-    cout <<"Enter amount of money in each house: ";
+    std::cout <<"Enter amount of money in each house: ";
     for (int i = 0; i < n; ++i) 
-        cin >> money[i];
+        std::cin >> money[i];
  
-    cout << houseRobber(dpTable,money,n);
+    std::cout << houseRobber(dpTable,money,n);
     return 0;
 }
