8ee04cd27cd254f3ce00eaafbbc1da8ad7d01413
Sumukh Bhardwaj <42494776+SumukhBhardwaj@users.noreply.github.com>
2020-10-11 09:27:53 +0530

    Update house_robber.cpp
    added test cases and solution description

--- dynamic_programming/house_robber.cpp
+++ dynamic_programming/house_robber.cpp
@@ -1,52 +1,92 @@
 /*House Robber - Dynamic Programming
+
 Problem Statement: 
 A robber is planning to rob houses along a street. Each house has a 
 certain  amount  of money stashed, the only constraint stopping you 
 from  robbing  each of them  is that  adjacent houses have security 
 system connected and it will automatically contact the police if two
 adjacent houses were broken into on the same night.
+
 Task: 
-Determine the maximum amount of money you can rob tonight without alerting the police*/
+Determine the maximum amount of money you can rob tonight without alerting the police
+
+Solution: 
+For every house we have two options either to rob it, if we do so we have to
+move to next to next huse as we cannot rob two adjacent houses. If we don't rob this
+house then we can move to the next one. The maximum of above two cases will be the maximum profit.
+We store our preiously calculated results in a table (dpTable) if we found that we have already
+calculated the ans then we simply return it.
+*/
 
 
 #include <iostream>
 #include <vector> 
+#include <cassert>
 
-std::vector<int> money;
-int houseRobber(std::vector<int>dpTable, int n, int pos=0)
+int houseRobber(std::vector<int>dpTable, std::vector<int> money, int n, int pos=0)
 {
     if(pos >= n)
 	{
         return 0;  
 	}		
-        
+
     if(dpTable[pos] != -1)
 	{
         return dpTable[pos];   // if the result for this position has been already calculated
 	}
-        
+
 	// rob the current house & move to next to next one or didn't rob the current house move to next one
-    return dpTable[pos] = std::max(money[pos] + houseRobber(dpTable, n, pos + 2), houseRobber(dpTable, n, pos + 1));              				 
+    return dpTable[pos] = std::max(money[pos] + houseRobber(dpTable, money, n, pos + 2), houseRobber(dpTable, money, n, pos + 1));              				 
 
 }
-	
+
+/**
+ * @brief Function to test above algorithm
+ * @returns void
+ */
+ 
+static void test()
+{
+  // Test 1
+  {
+   std::vector<int> num1 = {1, 2, 3, 1};
+   int n = num1.size();
+   std::vector<int>dpTable(n, -1);
+
+   int output = houseRobber(dpTable, num1, n);
+   assert(output == 4);
+   std::cout << "Input: ";
+   for(auto i: num1)
+   {
+     std::cout << i << " ";
+   }
+   std::cout << "Output: 4" << std::endl;
+  }
+  
+  // Test 2
+  {
+   std::vector<int> num1 = {2, 7, 9, 3, 1};
+   int n = num1.size();
+   std::vector<int>dpTable(n, -1);
+
+   int output = houseRobber(dpTable, num1, n);
+   assert(output == 12);
+   std::cout << "Input: ";
+   for(auto i: num1)
+   {
+    std::cout << i << " ";
+   }
+   std::cout << "Output: 12" << std::endl;
+  }
+}
+
+/**
+ * @brief Main function
+ * @returns 0 on exit
+ */
+ 
 int main() 
 {
-	int n = 0, val = 0;
-	
-    std::cout <<"Enter number of houses ";
-    std::cin >> n;
-	
-	// memoization table
-	std::vector<int>dpTable(n,-1);
-
-    std::cout <<"Enter amount of money in each house: ";
-    for (int i = 0; i < n; ++i) 
-	{
-        std::cin >> val;
-		money.push_back(val);
-	}
- 
-    std::cout << houseRobber(dpTable, n);
+	test();
     return 0;
 }
