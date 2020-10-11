62417761f27344173e8b8ce895d0747e1d689299
Sumukh Bhardwaj <42494776+SumukhBhardwaj@users.noreply.github.com>
2020-10-11 09:53:26 +0530

    Update wildcard_string_matching.cpp

--- dynamic_programming/wildcard_string_matching.cpp
+++ dynamic_programming/wildcard_string_matching.cpp
@@ -1,18 +1,30 @@
 /*
 WildCard Matching - Backtracking
+
 Problem Statement: 
 Given an matching string and a pattern, implement wildcard pattern matching with support for '?' and '*'.
 '?' Matches any single character.
 '*' Matches any sequence of characters (including the empty sequence).
 The matching should cover the entire matching string (not partial).
+
 Task: 
 Determine if the pattern matches with the matching string
+
+Solution:  we start comparing the two string from left, if the character at ith position in first
+string matches with the jth character of the second string we will just move ahead in both the string,
+if they are not same and character at jth position of second string is neither '?' nor '*' then matching
+is not possible. If it is '?' then we will convert this symbol to charcter present in the second string and
+move ahead. If it is '*' the we have choice either to match zero or multiple characters thus we have two
+recursive calls here. If both string are done ans is true, but if the pattern is not finished yet but the
+matching string is then the pattern string can only have '*' character so that we can match empty sequence
+in return to it.
 */
 
 
 #include <iostream>
 #include <vector> 
 #include <string>
+#include <cassert>
 
 std::vector<std::vector<int>>dpTable(1000, std::vector<int>(1000, -1));
 int wildcardMatching(std::string s, std::string p, int n, int m, int pos1, int pos2)
@@ -72,18 +84,46 @@ int wildcardMatching(std::string s, std::string p, int n, int m, int pos1, int p
         }
 }
 
+static void test()
+{
+  // Test 1
+  {
+   std::vector<std::vector<int>>tempTable(1000, std::vector<int>(1000, -1));
+   std::string matching = "acdcb" ;
+   std::string pattern = "a*c?b";
+   int n = matching.size(), m = pattern.size();
+   dpTable = tempTable;
+   
+   int output = wildcardMatching(matching, pattern, n, m, 0, 0);
+   assert(output == 0);
+   std::cout << "Input: ";
+   std::cout << matching << " " << pattern << std::endl;
+   std::cout << "Output: 0" << std::endl;
+  }
+  
+  // Test 2
+  {
+   std::vector<std::vector<int>>tempTable(1000, std::vector<int>(1000, -1));
+   std::string matching = "adceb";
+   std::string pattern = "*a*b";
+   int n = matching.size(), m = pattern.size();
+   dpTable = tempTable;
+   
+   int output = wildcardMatching(matching, pattern, n, m, 0, 0);
+   assert(output == 1);
+   std::cout << "Input: ";
+   std::cout << matching << " " << pattern << std::endl;
+   std::cout << "Output: 1" << std::endl;
+  }
+}
+
+/**
+ * @brief Main function
+ * @returns 0 on exit
+ */
+ 
 int main() 
 {
-	  std::string matching = "", pattern = "";
-
-    std::cout <<"Enter the matching string: ";
-    std::cin >> matching;
-
-    std::cout <<"Enter the pattern string ";
-	  std::cin >> pattern;
-
-    int matching_length = matching.length(), pattern_length = pattern.length();
-
-    std::cout << wildcardMatching(matching, pattern, matching_length, pattern_length, 0, 0);
+	test();
     return 0;
 }
