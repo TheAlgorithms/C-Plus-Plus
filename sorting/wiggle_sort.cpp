8b85386d89de6e86d07eea32ed62213e9b7eacb3
Roshan Kanwar <rkanwar0708@gmail.com>
2020-10-12 21:39:54 +0530

    used array instead of vector

--- sorting/wiggle_sort.cpp
+++ sorting/wiggle_sort.cpp
@@ -8,16 +8,15 @@
  *
  *  \details
  *  Wiggle Sort sorts the array into a wave like array.
- *  An array ‘arr[0..n-1]’ is sorted in wave form,
- *  if arr[0] >= arr[1] <= arr[2] >= arr[3] <= arr[4] >= …..
+ *  An array â€˜arr[0..n-1]â€™ is sorted in wave form,
+ *  if arr[0] >= arr[1] <= arr[2] >= arr[3] <= arr[4] >= â€¦..
  *
  *  \example
- *  vec = [1,1,5,6,1,4], after wiggle sort vec will become equal to [1,1,6,1,5,4]
- *  vec = [2,8,9,1,7], after wiggle sort vec will become equal to [8,2,9,1,7]
+ *  arr = [1,1,5,6,1,4], after wiggle sort arr will become equal to [1,1,6,1,5,4]
+ *  arr = [2,8,9,1,7], after wiggle sort arr will become equal to [8,2,9,1,7]
  */
 
 #include <iostream>
-#include <vector>
 
 /**
  *
@@ -39,22 +38,22 @@ void swap(int *x, int *y)
 /**
  *
  *  The displayElements() function is used for printing the elements.
- *  Prints elements of the vector after they're sorted using wiggle sort algorithm.
+ *  Prints elements of the arr after they're sorted using wiggle sort algorithm.
  *
- *  @param vec - vector containing the sorted elements
+ *  @param arr - arr containing the sorted elements
  *
  */
 
-void displayElements(std::vector<int> vec) {
+void displayElements(int *arr,int size) {
 
-    std::cout << "Sorted elements are as follows: \n";
+    std::cout << "Sorted elements are as follows: ";
 
     std::cout << "[";
 
-    for(int i = 0 ; i < vec.size() ; i++ ) {
+    for(int i = 0 ; i < size ; i++ ) {
 
-        std::cout << vec[i] ;
-        if(i != vec.size() - 1) {
+        std::cout << arr[i] ;
+        if(i != size - 1) {
             std::cout << ", " ;
         }
 
@@ -69,11 +68,11 @@ void displayElements(std::vector<int> vec) {
  *  The wiggleSort() function is used for sorting the elements in wave form.
  *  Checking whether the even indexed elements are greater than
  *  their adjacent odd elements.
- *  Traversing all even indexed elements of the input vector.
+ *  Traversing all even indexed elements of the input arr.
  *  If current element is smaller than the previous odd element, swap them.
  *  If current element is smaller than the next odd element, swap them.
  *
- *  @param vec - input vector (unsorted elements)
+ *  @param arr - input arr (unsorted elements)
  *
  */
 
@@ -81,16 +80,16 @@ class Algorithm {
 
    public:
 
-   void wiggleSort(std::vector <int>& vec) {
+   void wiggleSort(int *arr, int size) {
 
-       for(int i = 0; i < vec.size() ; i +=2) {
+       for(int i = 0; i < size ; i +=2) {
 
-           if(i > 0 && vec[i-1] > vec[i]) {
-               swap(&vec[i],&vec[i-1]);
+           if(i > 0 && arr[i-1] > arr[i]) {
+               swap(arr+i,arr+i-1);
            }
 
-           if(i < vec.size() - 1 && vec[i] < vec[i+1]) {
-               swap(&vec[i],&vec[i+1]);
+           if(i < size - 1 && arr[i] < arr[i+1]) {
+               swap(arr+i,arr+i+1);
            }
 
        }
@@ -103,20 +102,23 @@ int main() {
 
     int size;
 
-    std::cout << "\nEnter the number of elements : ";
+    std::cout << "Enter the number of elements : ";
 
     std::cin >> size;
 
-    std::vector<int> v(size);
+    int *arr = new int[size];
 
-    std::cout << "\nEnter the unsorted elements : ";
+    std::cout << "Enter the unsorted elements : ";
 
     for (int i = 0; i < size; ++i) {
-        std::cout << "\n";
-        std::cin >> v[i];
+        std::cin >> arr[i];
     }
 
     Algorithm ob;
-    (ob.wiggleSort(v));
-    displayElements(v);
+    (ob.wiggleSort(arr,size));
+    displayElements(arr,size);
+    delete[] arr;
+    return 0;
 }
+
+/** @} */
