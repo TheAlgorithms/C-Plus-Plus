8633f4dbb7fe337bff40ccee4786858146a42646
x0rld <thomas.sam78@gmail.com>
2020-10-12 13:38:01 +0200

    remove using namespace std;

--- operations_on_datastructures/array_left_rotation.cpp
+++ operations_on_datastructures/array_left_rotation.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
-using namespace std;
 int main() {
     int n, k;
-    cout << "Enter size of array=\t";
-    cin >> n;
-    cout << "Enter Number of indeces u want to rotate the array to left=\t";
-    cin >> k;
+    std::cout << "Enter size of array=\t";
+    std::cin >> n;
+    std::cout << "Enter Number of indeces u want to rotate the array to left=\t";
+    std::cin >> k;
     int a[n];
-    cout << "Enter  elements of array=\t";
+    std::cout << "Enter  elements of array=\t";
     for (int i = 0; i < n; i++) {
-        cin >> a[i];
+        std::cin >> a[i];
     }
     int temp = 0;
     for (int i = 0; i < k; i++) {
@@ -22,9 +21,9 @@ int main() {
             }
         }
     }
-    cout << "Your rotated array is=\t";
+    std::cout << "Your rotated array is=\t";
     for (int j = 0; j < n; j++) {
-        cout << a[j] << " ";
+        std::cout << a[j] << " ";
     }
     getchar();
     return 0;
