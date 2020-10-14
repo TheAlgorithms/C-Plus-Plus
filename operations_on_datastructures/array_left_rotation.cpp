4007f4ff364cf3d202e32e13a7b1d289502e70cc
vinay6023 <53485613+vinay6023@users.noreply.github.com>
2020-10-14 11:10:01 +0530

    Update array_left_rotation.cpp
    improves time complexity form k*n( (number of indices to be rotated )*(length of array) ) to n( the length of the array).

--- operations_on_datastructures/array_left_rotation.cpp
+++ operations_on_datastructures/array_left_rotation.cpp
@@ -1,31 +1,43 @@
 #include <iostream>
 using namespace std;
+int GCD(int a,int b)
+{
+    if(a==0)
+        return b;
+    return GCD(b%a,a);
+}
 int main() {
     int n, k;
     cout << "Enter size of array=\t";
     cin >> n;
-    cout << "Enter Number of indeces u want to rotate the array to left=\t";
+    cout << "Enter Number of indices u want to rotate the array to left=\t";
     cin >> k;
     int a[n];
     cout << "Enter  elements of array=\t";
     for (int i = 0; i < n; i++) {
         cin >> a[i];
     }
-    int temp = 0;
-    for (int i = 0; i < k; i++) {
-        temp = a[0];
-        for (int j = 0; j < n; j++) {
-            if (j == n - 1) {
-                a[n - 1] = temp;
-            } else {
-                a[j] = a[j + 1];
-            }
-        }
-    }
+
+    if(k==0)
+         k=n;
+     int gcd= GCD(k%n,n);
+     int l,temp;
+     for(int i=0;i<gcd;i++)
+     {
+         l=(i+k)%n;
+         temp=a[i];
+         for(int j=0;j<n/gcd;j++)
+         {
+             temp+=a[l];
+             a[l]=temp-a[l];
+             temp=temp-a[l];
+             l=(l+k)%n;
+          }
+      }
     cout << "Your rotated array is=\t";
     for (int j = 0; j < n; j++) {
         cout << a[j] << " ";
     }
-    getchar();
     return 0;
 }
+
