496b14becbd9fa613b27ac83e779e1e4850b70d2
Aman Garg <43947335+aman-garg0001@users.noreply.github.com>
2020-10-14 22:41:27 +0530

    Update Min Max Stack.cpp

--- data_structures/Min Max Stack.cpp
+++ data_structures/Min Max Stack.cpp
@@ -1,25 +1,7 @@
 /*____Garg's Code_____*/
 #include <bits/stdc++.h>
-#include <string.h>
+
 using namespace std;
-#define int long long
-#define ll long long
-#define vec vector < ll >
-#define pb push_back
-#define po pop_back
-#define mp make_pair
-#define mt make_tuple
-#define F  first
-#define S  second
-#define f(i,x,n) for(ll i=x;i<n;i++)
-#define rf(i,x,n) for(ll i = x; i >= x; i--)
-#define all(c) c.begin(),c.end()
-#define fast() ios_base::sync_with_stdio(false);cin.tie(NULL)
-const ll MAX = LLONG_MAX, MOD = 1e9 + 7;
-const ll MIN = LLONG_MIN;
-const long double PI = 3.1415926535;
-const ll N = 1e5 + 10;
-const ll inf = 1e16;
 
 template<class T>
 class Node
@@ -142,7 +124,7 @@ class Stack
     }
 };
   
-int32_t main()
+int main()
 {
   Stack<int> st;
   st.push(1);
