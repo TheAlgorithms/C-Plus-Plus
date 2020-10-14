8adb4a43aab4caa696dae4c314da42ad7ff76266
vinay6023 <53485613+vinay6023@users.noreply.github.com>
2020-10-14 13:01:45 +0530

    Update array_right_rotation.cpp
    improves time complexity  from n*k to n

--- operations_on_datastructures/array_right_rotation.cpp
+++ operations_on_datastructures/array_right_rotation.cpp
@@ -1,27 +1,59 @@
 #include <iostream>
-using namespace std;
-int main() {
-    int n, k;
-    cout << "Enter size of array=\t";
-    cin >> n;
-    cout << "Enter Number of indices u want to rotate the array to right=\t";
-    cin >> k;
-    int a[n];
-    cout << "Enter elements of array=\t";
-    for (int i = 0; i < n; i++) cin >> a[i];
-    int temp = 0;
-    for (int i = 0; i < k; i++) {
-        temp = a[n - 1];
-        for (int j = n - 1; j >= 0; j--) {
-            if (j == 0) {
-                a[j] = temp;
-            } else {
-                a[j] = a[j - 1];
-            }
-        }
+#include <vector>
+int GCD(int a,int b)
+{
+    if(a==0)
+    {
+        return b;
     }
-    cout << "Your rotated array is=\t";
+    return GCD(b%a,a);
+}
+/* this code shifts the element to it's desried index in one go
+eg 1- say n=5,k=2
+gcd(2,5)=1
+a[0]->a[2] ; a[2]->a[4] ; a[4]->a[1] ;a[1]->a[3] ; a[3]->a[0] ;
+
+eg 2- say n=6,k=2
+gcd(2,6)=2
+a[0]->a[2] ;a[2]->a[4] ;a[4]->a[0] ;
+
+a[1]->a[3] ;a[3]->a[5] ;a[5]->a[1] ;
+*/
+int main() {
+    int n=0;
+    int k=0;
+    int temp=0;
+    std::cout << "Enter size of array=\t";
+    std::cin >> n;
+    std::cout << "Enter Number of indices u want to rotate the array to right=\t";
+    std::cin >> k;
+    std::vector<int> a(n);
+    std::cout << "Enter elements of array=\t";
+    for (int i = 0; i < n; i++)
+    {
+        std::cin >> temp;
+        a[i]=temp;
+    }
+    if(k==0)
+    {
+      k=n;
+    }
+    int gcd= GCD(k%n,n);
+    int l=0;
+    for (int i = 0; i <gcd; i++) 
+    {
+        l=(i+k)%n;
+        temp=a[i];
+        for(int j=0;j<n/gcd;j++)
+         {
+             temp+=a[l];
+             a[l]=temp-a[l];
+             temp=temp-a[l];
+             l=(l+k)%n;
+          }
+    }
+    std::cout << "Your rotated array is=\t";
     for (int i = 0; i < n; i++) {
-        cout << a[i] << " ";
+        std::cout << a[i] << " ";
     }
 }
