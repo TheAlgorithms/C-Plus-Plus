2191343b4327814900d69affa67b0f1a71d3333f
Rudra Prasad Das <rudraiitism@gmail.com>
2020-10-16 08:56:43 +0530

    Changes made as suggested

--- numerical_methods/rungekutta.cpp
+++ numerical_methods/rungekutta.cpp
@@ -1,23 +1,33 @@
 /**
- *  \addtogroup Runge Kutta 4th Method for solving Differential equation
- *  @{
- *  \file
- *  \brief Runge Kutta Method implementation
+ * \addtogroup Runge Kutta 4th Method for solving Differential equation
+ * @{
+ * \file
+ * \brief Runge Kutta Method implementation
  *
- *  \author [Rudra Prasad Das](http://github.com/rudra697)
+ * \author [Rudra Prasad Das](http://github.com/rudra697)
  *
- *  \details
+ * \details
  * It solves the unknown value of y 
  * for a given value of x
- * only first oreder differential equations
- *can be solved
- *  \example
- *  it solves dy/dx=(x-y)/2 at a 
+ * only first order differential equations
+ * can be solved
+ * \example
+ * it solves dy/dx=(x-y)/2 at a 
  * given x for given initial 
  * conditions
  * There can be many such equations 
- * **/
+ */
 #include<iostream> 
+/**
+ * @namespace numerical_methods
+ * @brief Numerical Methods
+ */
+namespace numerical_methods {
+/**
+ * @namespace runge_kutta
+ * @brief Functions for [Runge Kutta](https://en.wikipedia.org/wiki/Runge%E2%80%93Kutta_methods) method
+ */
+namespace runge_kutta {
 /**
  * @brief The change() function is used 
  * to return the updated iterative value corresponding 
@@ -25,14 +35,14 @@
  * @param x is the value corresponding to the x coordinate
  * @param y is the value corresponding to the y coordinate 
  * @returns the computed function value at that call
- * **/
+ */
 double change(double x, double y) 
 { 
 	double val=((x - y)/2.0); 
 	return val;
 } 
 /**
- *  @brief The rungeKutta finds value of y for 
+ * @brief The rungeKutta finds value of y for 
  * a given x using step size h 
  * and initial value initial x at initial y. 
  * @param init_x is the value of initial x and is updated after each call
@@ -40,19 +50,19 @@ double change(double x, double y)
  * @param x is the given value of x
  * @param h is the step value 
  * @returns the value of y at thr required value of x from the initial conditions
- **/
+ */
 double rungeKutta(double init_x, double init_y, double x, double h) 
 { 
-	 /**
-	  * Count number of iterations 
-	  * using step size or 
-	  *  step height h 
-	  * **/
-	 /**
-	  * n calucates the number of iterations
-	  * k1,k2,k3,k4 are the Runge Kutta variables 
-	  * used for calculation of y at each iteration
-	  */
+	 
+	  // Count number of iterations 
+	  //using step size or 
+	  // step height h 
+	  
+	 
+	  // n calucates the number of iterations
+	  // k1,k2,k3,k4 are the Runge Kutta variables 
+	  // used for calculation of y at each iteration
+	  
 	 int n = static_cast<int>((x - init_x) / h); 
     
 	double k1=0.0;
@@ -61,37 +71,39 @@ double rungeKutta(double init_x, double init_y, double x, double h)
 	double k4=0.0;
 	double k5=0.0; 
 
-	/**
-	 * Iterate for number of iterations 
-	 */
+	
+	 // Iterate for number of iterations 
+	 
 	double y = init_y; 
 	for (int i=1; i<=n; i++) 
 	{ 
-		/**
-		 * 		
-		 *  Apply Runge Kutta Formulas 
-		 * to find next value of y 
-		 */
+		
+				
+		 // Apply Runge Kutta Formulas 
+		 // to find next value of y 
+		
 
 		k1 = h*change(init_x, y); 
 		k2 = h*change(init_x + 0.5*h, y + 0.5*k1); 
 		k3 = h*change(init_x + 0.5*h, y + 0.5*k2); 
 		k4 = h*change(init_x + h, y + k3); 
 
-		/**
-		 * Update next value of y 
-		*/
+		
+		// Update next value of y 
+		
 
 		y = y + (1.0/6.0)*(k1 + 2*k2 + 2*k3 + k4);
 
-		/**
-		 *  Update next value of x 
-		 */
+		
+		// Update next value of x 
+		
 		init_x = init_x + h; 
 	} 
 
 	return y; 
 } 
+}  // namespace runge_kutta
+}  // namespace numerical_methods
 
 /**
  * @brief Main function
@@ -103,6 +115,6 @@ int main()
 	double y = 1;
 	double x = 2;
 	double h = 0.2; 
-	std::cout<<"The value of y at x is :"<<rungeKutta(x0, y, x, h); 
+	std::cout << "The value of y at x is :"<< numerical_methods::runge_kutta::rungeKutta(x0, y, x, h); 
 	return 0; 
 } 
