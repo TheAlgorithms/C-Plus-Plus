d683dd4a3661146a19c404913ca5154c7ea5e99d
Roshan Kanwar <rkanwar0708@gmail.com>
2020-10-16 06:06:49 +0530

    update wiggle_sort.cpp

--- sorting/wiggle_sort.cpp
+++ sorting/wiggle_sort.cpp
@@ -1,20 +1,20 @@
 /**
- *  \addtogroup sorting Sorting Algorithms
- *  @{
- *  \file
- *  \brief [Wiggle Sort Algorithm]
- *  (https://khanhng.com/post/wiggle-sort/) Implementation
+ * \addtogroup sorting Sorting Algorithms
+ * @{
+ * \file
+ * \brief [Wiggle Sort Algorithm]
+ * (https://khanhng.com/post/wiggle-sort/) Implementation
  *
- *  \author [Roshan Kanwar](http://github.com/roshan0708)
+ * \author [Roshan Kanwar](http://github.com/roshan0708)
  *
- *  \details
- *  Wiggle Sort sorts the array into a wave like array.
- *  An array ‘arr[0..n-1]’ is sorted in wave form,
- *  if arr[0] >= arr[1] <= arr[2] >= arr[3] <= arr[4] >= …..
+ * \details
+ * Wiggle Sort sorts the array into a wave like array.
+ * An array ‘arr[0..n-1]’ is sorted in wave form,
+ * if arr[0] >= arr[1] <= arr[2] >= arr[3] <= arr[4] >= …..
  *
- *  \example
- *  arr = [1,1,5,6,1,4], after wiggle sort arr will become equal to [1,1,6,1,5,4]
- *  arr = [2,8,9,1,7], after wiggle sort arr will become equal to [8,2,9,1,7]
+ * \example
+ * arr = [1,1,5,6,1,4], after wiggle sort arr will become equal to [1,1,6,1,5,4]
+ * arr = [2,8,9,1,7], after wiggle sort arr will become equal to [8,2,9,1,7]
  */
 
 #include <iostream>
@@ -38,10 +38,10 @@ namespace sorting {
 
                 /**
                 *
-                *  @brief Used for swapping two numbers.
+                * @brief Used for swapping two numbers.
                 *
-                *  @param x first number
-                *  @param y second number
+                * @param x first number
+                * @param y second number
                 *
                 */
                 void swap(int *x, int *y) {
@@ -54,11 +54,11 @@ namespace sorting {
 
                 /**
                 *
-                *  @brief Utility function used for printing the elements.
-                *  Prints elements of the array after they're sorted using wiggle sort algorithm.
+                * @brief Utility function used for printing the elements.
+                * Prints elements of the array after they're sorted using wiggle sort algorithm.
                 *
-                *  @param arr array containing the sorted elements
-                *  @param size - size of the input array
+                * @param arr array containing the sorted elements
+                * @param size - size of the input array
                 *
                 */
                 void displayElements(int *arr,int size) {
@@ -89,7 +89,7 @@ namespace sorting {
                 * Traversing all even indexed elements of the input arr.
                 * If current element is smaller than the previous odd element, swap them.
                 * If current element is smaller than the next odd element, swap them.
-                *
+                * 
                 * @param arr input array (unsorted elements)
                 * @param size - size of the input array
                 *
