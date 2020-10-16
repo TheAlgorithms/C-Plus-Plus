e4c7249edd5d98b21c1e4f0a0b8e82409c59c80b
Roshan Kanwar <rkanwar0708@gmail.com>
2020-10-17 02:17:51 +0530

    update wiggle_sort.cpp

--- sorting/wiggle_sort.cpp
+++ sorting/wiggle_sort.cpp
@@ -3,7 +3,7 @@
  * @{
  * \file
  * \brief [Wiggle Sort Algorithm]
- * (https://khanhng.com/post/wiggle-sort/) Implementation
+ * (https://leetcode.com/problems/wiggle-sort-ii/) Implementation
  *
  * \author [Roshan Kanwar](http://github.com/roshan0708)
  *
@@ -18,6 +18,9 @@
  */
 
 #include <iostream>  /// for io operations
+#include <algorithm>
+#include <vector>
+#include <cassert>
 
 /**
  * @namespace sorting
@@ -26,116 +29,105 @@
 namespace sorting {
     /**
     * @namespace wiggle_sort
-    * @brief Functions for [Wiggle Sort](https://khanhng.com/post/wiggle-sort/) algorithm
+    * @brief Functions for [Wiggle Sort](https://leetcode.com/problems/wiggle-sort-ii/) algorithm
     */
     namespace wiggle_sort {
+
         /**
-        * @brief contains various utility functions for completion of the wiggle sort algorithm
+        *
+        * @brief Function used for sorting the elements in wave form.
+        * @details
+        * Checking whether the even indexed elements are greater than
+        * their adjacent odd elements.
+        * Traversing all even indexed elements of the input arr.
+        * If current element is smaller than the previous odd element, swap them.
+        * If current element is smaller than the next odd element, swap them.
+        * 
+        * @param arr input array (unsorted elements)
+        *
         */
-        class Algorithm {
+        template<typename T> // this allows to have vectors of ints, double, float, etc
+        std::vector<T> wiggleSort(const std::vector<T> &arr) {
 
-            public:
+            uint32_t size = arr->size();
 
-                /**
-                *
-                * @brief Used for swapping two numbers.
-                *
-                * @param x first number
-                * @param y second number
-                *
-                */
-                void swap(int *x, int *y) {
+            std::vector<T> out(arr); // create a copy of input vector. this way, the original input vector does not get modified. a sorted array is is returned.
+            
+            for(int i = 0; i < size ; i +=2) {
 
-                    int temp = *x;
-                    *x = *y;
-                    *y = temp;
-
+                if(i > 0 && out[i-1] > out[i]) {
+                    std::swap(out[i],out[i-1]);
                 }
 
-                /**
-                *
-                * @brief Utility function used for printing the elements.
-                * Prints elements of the array after they're sorted using wiggle sort algorithm.
-                *
-                * @param arr array containing the sorted elements
-                * @param size - size of the input array
-                *
-                */
-                void displayElements(int *arr,int size) {
-
-                    std::cout << "Sorted elements are as follows: ";
-
-                    std::cout << "[";
-
-                    for(int i = 0 ; i < size ; i++ ) {
-
-                        std::cout << arr[i] ;
-                        if(i != size - 1) {
-                            std::cout << ", " ;
-                        }
-
-                    }
-
-                    std::cout << "]"<<std::endl;
-
+                if(i < size - 1 && out[i] < out[i+1]) {
+                    std::swap(arr[i],arr[i+1]);
                 }
 
-                /**
-                *
-                * @brief Function used for sorting the elements in wave form.
-                * @details
-                * Checking whether the even indexed elements are greater than
-                * their adjacent odd elements.
-                * Traversing all even indexed elements of the input arr.
-                * If current element is smaller than the previous odd element, swap them.
-                * If current element is smaller than the next odd element, swap them.
-                * 
-                * @param arr input array (unsorted elements)
-                * @param size - size of the input array
-                *
-                */
-                void wiggleSort(int *arr, int size) {
+            }
 
-                    for(int i = 0; i < size ; i +=2) {
+            return out; //returns the sorted vector
 
-                        if(i > 0 && arr[i-1] > arr[i]) {
-                            swap(arr+i,arr+i-1);
-                        }
-
-                        if(i < size - 1 && arr[i] < arr[i+1]) {
-                            swap(arr+i,arr+i+1);
-                        }
-
-                    }
-
-                }
-        };
+        }
     } // namespace wiggle_sort
 } // namespace sorting
 
 /**
- * @brief Main function
- * @returns 0 on exit
+ *
+ * @brief Utility function used for printing the elements.
+ * Prints elements of the array after they're sorted using wiggle sort algorithm.
+ *
+ * @param arr array containing the sorted elements
+ *
  */
-int main() {
-    int size;
+template<typename T>
+static void displayElements(const std::vector<T> &arr) {
 
-    std::cout << "Enter the number of elements : ";
+    uint32_t size = arr->size();
 
-    std::cin >> size;
+    std::cout << "Sorted elements are as follows: ";
 
-    int *arr = new int[size];
+    std::cout << "[";
 
-    std::cout << "Enter the unsorted elements : ";
+    for(int i = 0 ; i < size ; i++ ) {
 
-    for (int i = 0; i < size; ++i) {
-        std::cin >> arr[i];
+        std::cout << arr[i] ;
+        if(i != size - 1) {
+            std::cout << ", " ;
+        }
+        
     }
 
-    sorting::wiggle_sort::Algorithm ob;
-    (ob.wiggleSort(arr,size));
-    (ob.displayElements(arr,size));
-    delete[] arr;
+    std::cout << "]"<<std::endl;
+    
+}
+
+/**
+ * Test function
+ * @returns void
+ */
+static void test() {
+
+    std::srand(std::time(nullptr)); // initialize random number generator
+
+    std::vector<float> data1(100);
+    for (auto &d: data1)
+        // generate random numbers between -5.0 and 4.99
+        d = float(std::rand() % 1000 - 500) / 100.f;
+
+    std::vector<T> sorted = sorting::wiggle_sort::wiggleSort(data1);
+
+    displayElements(sorted);
+
+    for(uint32_t j = 0; j < data1.size(); j+=2)
+        assert(data1[j] <= data1[j+1] && data1[j+1] >= data1[j+2]); // check the validation condition
+
+    std::cout << "Test 1 passed\n";
+    
+}
+
+/** Driver Code */
+int main() {
+    test();
     return 0;
 }
 
