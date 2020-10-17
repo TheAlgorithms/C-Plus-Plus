413ad24788edf146b9e03e4e23a97c02f78854dc
trishnakalita660 <trishnakalita660@gmail.com>
2020-10-17 23:49:31 +0530

    add solution for maximum subarray product problem

---
+++ others/maximum_product_subarray.cpp
@@ -0,0 +1,40 @@
+
+// Program to find maximum product of a contiguous subarray within an array
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// This function returns the maximum product of a contiguous subarray within the given array.
+
+int maxProduct(vector<int>& InputArray) {
+    int maxProd = INT_MIN;
+    int maxSoFar = 1, minSoFar = 1; // maintaining counter
+
+    for (int i = 0; i < InputArray.size(); i++) {
+       
+        int newMaxSoFar = max({maxSoFar * InputArray[i], InputArray[i],minSoFar * InputArray[i]});
+
+        int newMinSoFar = min({minSoFar * InputArray[i], InputArray[i], maxSoFar * InputArray[i]});
+
+        maxProd = max(newMaxSoFar, maxProd);   
+       
+        maxSoFar = newMaxSoFar;
+        minSoFar = newMinSoFar;
+    }
+
+    return maxProd;
+}
+
+int main() {
+    int sizeOfArray, maxProd, num;
+    cin >> sizeOfArray;
+
+    vector<int> InputArray;
+    for (int i = 0; i < sizeOfArray; i++) {
+        cin >> num;
+        InputArray.push_back(num);
+    }
+    maxProd = maxProduct(InputArray);
+    cout << maxProd;
+    return 0;
+}
