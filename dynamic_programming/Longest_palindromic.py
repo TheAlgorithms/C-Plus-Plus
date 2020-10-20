8a44c8e9030838c579814b571062536e28764e14
Prajul Gupta <prajulgupta2000@gmail.com>
2020-10-20 22:16:08 +0530

    Create Longest_palindromic.py

---
+++ dynamic_programming/Longest_palindromic.py
@@ -0,0 +1,23 @@
+# include<bits/stdc++.h>
+using namespace std
+int max(int x, int y) {return (x > y)? x: y
+                       }
+int lps(char * seq, int i, int j)
+{
+    if (i == j)
+    return 1
+    if (seq[i] == seq[j] & & i + 1 == j)
+    return 2
+    if (seq[i] == seq[j])
+    return lps(seq, i+1, j-1) + 2
+    return max(lps(seq, i, j-1), lps(seq, i+1, j))
+}
+
+int main()
+{
+    char seq[] = "AAKAASU"
+    int n = strlen(seq)
+    cout << "The length of the LPS is "
+         << lps(seq, 0, n-1)
+    return 0
+}
