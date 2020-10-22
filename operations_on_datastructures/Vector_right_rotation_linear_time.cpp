9cc97c1bf857830c4dcf21592042a6cf38e9b1b9
vinay6023 <53485613+vinay6023@users.noreply.github.com>
2020-10-22 14:13:06 +0530

    Update Vector_right_rotation_linear_time.cpp
    added documentation , tests, examples.
    
    this rotates an array of length n by k indices
    eg1-
    a[]={1,2,3,4,5};n=5,k=1
    resultant array is {5,1,2,3,4}
    eg2-
    a[]={1,2,3,4,5};n=5,k=1
    resultant array is {2,3,4,5,1}

--- operations_on_datastructures/Vector_right_rotation_linear_time.cpp
+++ operations_on_datastructures/Vector_right_rotation_linear_time.cpp
@@ -1,44 +1,144 @@
-#include <iostream>
-#include<vector>
-/*
-Lets place place every number of the array at its desired correct position. But while doing this, we will lose the original elements.
-Thus, we need to store the number being replaced in a 'temp' variable. Then, we can place the replaced number(which is stored in temp) 
-at its correct position and so on, n times, where n is the length of array. We have chosen n to be the number of replacements since we 
-have to shift all the elements of the array(which is n). But, there could be a problem with this method, if n % k =0 
-where k = k%n (since a value of k larger than n eventually leads to a k equivalent to k%n). In this case, while picking up numbers to
-be placed at the correct position, we will eventually reach the number from which we originally started. Thus, in such a case, when we
-hit the original number's index again, we start the same process with the index following our starting point.
+/**
+ *@file
+ *
+ *@brief  Rotates an 1D array/vector of length n by k indices in linear time.
+ *
+ *@details To obtain the desired vector in linear time complexity,lets place place every number
+ *of the array at its desired correct position. But while doing this, we will lose the original elements.
+ *Thus, we need to store the number being replaced in a 'temp' variable. Then, we can place the replaced
+ *number(which is stored in temp) at its correct position and so on, n times, where n is the length of array.
+ *We have chosen n to be the number of replacements since we have to shift all the elements of the array(which is n).
+ *But, there could be a problem with this method, if n % k =0
+ *where k = k%n (since a value of k larger than n eventually leads to a k equivalent to k%n). In this case,
+ *while picking up numbers to be placed at the correct position, we will eventually reach the number from which
+ *we originally started. Thus, in such a case, when we
+ *hit the original number's index again, we start the same process with the index following our starting point.
+ *
+ * ### Example:
+ *eg 1- say n=5,k=2
+ *gcd(2,5)=1 (gcd=1 means all shifts an be done in one cycle)
+ *a[0]->a[2] ; a[2]->a[4] ; a[4]->a[1] ;a[1]->a[3] ; a[3]->a[0] ;
+ *eg 2- say n=6,k=2
+ *gcd(2,6)=2 (gcd=2 means we need to have 2 cycles)
+ *a[0]->a[2] ;a[2]->a[4] ;a[4]->a[0] ;
+ *a[1]->a[3] ;a[3]->a[5] ;a[5]->a[1] ;
+ *Note that-
+ *the number of sub sequences are equal to the GCD of and k. If n and k are co-prime
+ *complete process can be completed in one go as the subsequent indices are unique.
+ *But if gcd>1 then the subsequent indices will repeat after n/gcd number of iterations,
+ *hence we need to set our starting index in this case gcd number of times
 */
-//recursive funcion for GCD
-int GCD(int a,int b)
-{
-    if(a==0)
+
+#include <iostream> // to include io
+#include<vector>    // to include vector
+#include <cassert>  // to include assert
+
+/**
+*@ namespace math
+*@ brief math algorithms
+*/
+namespace math{
+
+    /**
+    *@brief calculates Greatest Common Divisor
+    *@param a is an integer
+    *@param b is an integer
+    *@returns an GCD as an integer
+    */
+    int GCD(int a,int b)
     {
-        return b;
+        if(a==0)
+        {
+            return b;
+        }
+        return GCD(b%a,a);
     }
-    return GCD(b%a,a);
+    int MOD(int x,int y)
+    {
+        return (x%y +y)%y;
+    }
+}/// namespace math
+
+/**
+*@ namespace rotate
+*@ brief vector rotation algorithms
+*/
+namespace rotate
+{
+    /**
+    *@brief rotates an array of length n by k indices
+    *@param n is an integer ,contains length of the vector
+    *@param k is an integer ,contains number of indices to be shifted
+    *@param a is a vector, contains integer elements
+    *@returns a rotated vector
+    */
+    std::vector<int> to_right(std::vector<int> a, int n, int k)
+    {
+        if(k==0)
+        {
+            k=n; // rotating an array same number of times as its length makes no changes to the array
+        }
+        k=math::MOD(k,n);
+        int gcd= math::GCD(k,n); // rotating k times and n+k times are the same
+        int l=0;
+        for (int i = 0; i < gcd; i++)
+        {
+            l=(i+k)%n; //here we set the starting element of the subsequence
+            int temp=a[i]; //storing initial element of the sub sequence into temporary variable
+            for (int j = 0; j<n/gcd; j++)
+            {
+                //we put the current element to its approptiate place ,fill the emptied place with element in temp
+                //element in temp store the diaplaced
+                //eg n=5, k=2 ,current element = a[2], so we put temp element into a[2],orignal a[2] element into a[4]
+                //and orignal element of a[4] into temp
+                temp+=a[l];
+                a[l]=temp-a[l];
+                temp=temp-a[l];
+                l=(l+k)%n;
+           }
+         }
+        return a;
+    }
+}///namespace rotate
+
+/**
+*@brief test function;
+*@details tests GCD and to_right function
+*/
+void test()
+{
+    ///Tests the GCD function
+    int gcd=math::GCD(3,5);
+    assert(gcd=1);
+
+    gcd=math::GCD(5,10);
+    assert(gcd=5);
+
+    ///Tests the to_rotate function
+    std::vector<int> a;
+    for(int i=1;i<=5;i++)
+    {
+        a.push_back(i);
+    }
+    std::vector<int> v=rotate::to_right(a,5,1);
+    assert(v[0]==5&&v[4]==4);
+    v=rotate::to_right(a,5,-1);
+    assert(v[0]==2&&v[4]==1);
+
+
 }
 
-/*
-eg 1- say n=5,k=2
-gcd(2,5)=1 (gcd=1 means all shifts an be done in one cycle)
-a[0]->a[2] ; a[2]->a[4] ; a[4]->a[1] ;a[1]->a[3] ; a[3]->a[0] ;
-eg 2- say n=6,k=2
-gcd(2,6)=2 (gcd=2 means we need to have 2 cycles)
-a[0]->a[2] ;a[2]->a[4] ;a[4]->a[0] ;
-a[1]->a[3] ;a[3]->a[5] ;a[5]->a[1] ;
-Note that-
-the number of sub sequences are equal to the GCD of nand k. If n and k are coprime
-complete process can be completed in one go as the subsequent indices are unique.
-But if gcd>1 then the subsequent indices will repeat after n/gcd number of iterations,
-hence we need to set our starting index in this case gcd number of times
-*/
-
+/**
+ * @brief Main Function
+ * @details Asks the user to enter the number of indices using std::cin
+ * @returns 0 on exit
+ */
 int main()
  {
-    int n=0;
-    int k=0;
-    int temp=0;
+    test();
+    int n=0;   //variable for size of the vector
+    int k=0;   //indices to be rotated
+    int temp=0;//temporary variable
     std::cout << "Enter size of array=\t";
     std::cin >> n;
     std::cout << "Enter Number of indices u want to rotate the array to right=\t";
@@ -50,30 +150,12 @@ int main()
         std::cin >> temp;
         a[i]=temp;
     }
-    if(k==0)
-    {
-      k=n; // rotating an array same number of times as its length makes no changes to the array
-    }
-    int gcd= GCD(k%n,n); // rotating k times and n+k times are the same
-    int l=0;
-    for (int i = 0; i < gcd; i++)
-    {
-        l=(i+k)%n; //here we set the starting element of the subsequence
-        temp=a[i]; //storing initial element of the sub sequence into temporary variable
-        for (int j = 0; j<n/gcd; j++)
-        {
-           //we put the current element to its approptiate place ,fill the emptied place with element in temp
-           //element in temp store the diaplaced
-           //eg n=5, k=2 ,current element = a[2], so we put temp element into a[2],orignal a[2] element into a[4]
-           //and orignal element of a[4] into temp
-           temp+=a[l];
-           a[l]=temp-a[l];
-           temp=temp-a[l];
-           l=(l+k)%n;
-        }
-    }
+
+    std::vector<int> res= rotate::to_right(a,n,k);
     std::cout << "Your rotated array is=\t";
     for (int i = 0; i < n; i++) {
-        std::cout << a[i] << " ";
+        std::cout << res[i] << " ";
     }
+    return 0;
 }
+
