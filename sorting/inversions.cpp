ee2c61bce8106da3e6933cdeb69168d7a47ec9f1
Tejas <tejasdeore80@gmail.com>
2020-10-28 22:30:16 +0530

    Fix of isse 1309

--- sorting/inversions.cpp
+++ sorting/inversions.cpp
@@ -1,16 +1,19 @@
 // C++ program to Count Inversions
 // in an array
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
-int getInvCount(int arr[], int n)
+int getInvCount(vector<int>& arr, int n)
 {
 	int inv_count = 0;
-	for (int i = 0; i < n - 1; i++)
-		for (int j = i + 1; j < n; j++)
-			if (arr[i] > arr[j])
-				inv_count++;
+	for (int i = 0; i < n - 1; i++){
+		for (int j = i + 1; j < n; j++){
+			if (arr[i] > arr[j]){
+				inv_count+=1;
+			}
+		}
+	}
 
 	return inv_count;
 }
@@ -22,11 +25,10 @@ int main()
 	cin.tie(nullptr); cout.tie(nullptr);
 	int n;
 	cin>>n;
-	int arr[n];
+	vector<int> arr(n);
 	for(int i = 0;i < n; i++){
 		cin >> arr[i];
 	}
-	cout << " Number of inversions are "
-		<< getInvCount(arr, n);
+	cout << "Number of inversions are " << getInvCount(arr, n) << "\n";
 	return 0;
 }
