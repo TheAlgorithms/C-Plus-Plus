0966e12094ae3e3b1aa983ff7cde96ed794267e3
samruddha patil <45798222+sampatil578@users.noreply.github.com>
2020-10-28 10:51:30 +0530

    Improve numerical_methods/false_position.cpp
    Co-authored-by: David Leal <halfpacho@gmail.com>

--- numerical_methods/false_position.cpp
+++ numerical_methods/false_position.cpp
@@ -107,7 +107,7 @@ int main() {
     double a=0, b=0,i=0,root=0;
     int16_t count=0;
     int32_t range = 100000;        //Range in which we have to find the root. (-range,range)
-    double gap = 0.5;          //interval gap. lesser the gap more the accuracy
+    float gap = 0.5;          // interval gap. lesser the gap more the accuracy
     a = numerical_methods::false_position::eq((-1)*range);
     i=((-1)*range + gap);
     //while loop for selecting proper interval in provided range and with provided interval gap.
