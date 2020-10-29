2a2fbc87fc9ea98ea3fd2f831674536994ec2bf0
Suyash Jaiswal <suyash.18je0864@am.iitism.ac.in>
2020-10-29 17:36:59 +0530

    update radix_sort2.cpp

--- sorting/radix_sort2.cpp
+++ sorting/radix_sort2.cpp
@@ -4,10 +4,10 @@
  * @author [Suyash Jaiswal](https://github.com/Suyashjaiswal)
  * @details
  * Sort the vector of integers using radix sort i.e. sorting digit by digit
- using count sort as subroutine.
+ using [Counting Sort](https://en.wikipedia.org/wiki/Counting_sort) as subroutine.
  * Running time of radix is O(d*(n+b)) where b is the base for representing
  numbers and d in the max digits in input inegers and n is number of integers.
- * consider example for n = 5, array elements = 432,234,143,332,123
+ * consider example for n = 5, aray elements = 432,234,143,332,123
  *sorting digit by digit
  *sorting according to
  *1) 1st digit place
@@ -26,95 +26,62 @@
 // header files
 #include <iostream>  // for io operations
 #include <vector>    // for std::vector
+#include <algorithm> // for collection of functions
 /**
  * @namespace sorting
  * @brief Sorting algorithms
  */
 namespace sorting {
-/**
- * @namespace radix_sort
- * @brief Functions for [Radix sort](https://en.wikipedia.org/wiki/Radix_sort)
- * algorithm
- */
-namespace radix_sort {
-/**
- * @brief Class to create a vector and sort it.
- */
-class radix {
- public:
-    int n;
-    std::vector<int> ar;
     /**
-     * @brief Constructor that initializes the vector on creation.
-     * @param a vector to be sorted.
-     */
-    explicit radix(const std::vector<int>& a) {
-        ar = a;
-        n = ar.size();
-    }
-    /**
-     * @brief Function that returns the max element.
-     * @returns none
-     */
-    int get_max() {
-        int mx = 0;
-        for (int i = mx = 0; i < n; i++) mx = std::max(mx, ar[i]);
-        return mx;
-    }
-    /**
-     * @brief Function that print the elements of vector.
-     * @returns none
-     */
-    void show() {
-        for (int i = 0; i < n; i++) std::cout << ar[i] << " ";
-        std::cout << "\n";
-    }
-    void radix_sort();
-    void step_ith(int dig);
-};
-/**
- * @brief Function to sort vector according to current digit using stable
- * sorting.
- * @returns none
- */
-void radix::step_ith(int cur_digit) {  // sorting according to current digit.
-    std::vector<int> position(10, 0);
-    for (int i = 0; i < n; i++) {
-        position[(ar[i] / cur_digit) %
-                 10]++;  // counting frequency of 0-9 at cur_digit.
-    }
-    int cur = 0;
-    for (int i = 0; i < 10; i++) {
-        int a = position[i];
-        position[i] = cur;  // assingning starting position of 0-9.
-        cur += a;
-    }
-    std::vector<int> temp(n);
-    for (int i = 0; i < n; i++) {
-        temp[position[(ar[i] / cur_digit) % 10]] =
-            ar[i];  // storing ar[i] in ar[i]'s cur_digit expected position of
-                    // this step.
-        position[(ar[i] / cur_digit) %
-                 10]++;  // incrementing ar[i]'s cur_digit position by 1, as
-                         // current place used by ar[i].
-    }
-    for (int i = 0; i < n; i++) {
-        ar[i] = temp[i];
-    }
-}
-/**
- * @brief Function to sort vector digit by digit.
- * @returns none
- */
-void radix::radix_sort() {
-    int max_ele = get_max();
-    for (int i = 1; max_ele / i > 0;
-         i *= 10) {  // loop breaks when i > max_ele because no further digits
-                     // left to makes changes in array.
-        step_ith(i);
-    }
-}
-}  // namespace radix_sort
+    * @namespace radix_sort
+    * @brief Functions for [Radix sort](https://en.wikipedia.org/wiki/Radix_sort)
+    * algorithm
+    */
+    namespace radix_sort {
+        /**
+        * @brief Function to sort vector according to current digit using stable
+        * sorting.
+        * @returns none
+        */
+        void step_ith(int cur_digit,std::vector<int>& ar) {  // sorting according to current digit.
+            int n = ar.size();
+            std::vector<int> position(10, 0);
+            for (int i = 0; i < n; i++) {
+                position[(ar[i] / cur_digit) %
+                         10]++;  // counting frequency of 0-9 at cur_digit.
+            }
+            int cur = 0;
+            for (int i = 0; i < 10; i++) {
+                int a = position[i];
+                position[i] = cur;  // assingning starting position of 0-9.
+                cur += a;
+            }
+            std::vector<int> temp(n);
+            for (int i = 0; i < n; i++) {
+                temp[position[(ar[i] / cur_digit) % 10]] =
+                    ar[i];  // storing ar[i] in ar[i]'s cur_digit expected position of
+                            // this step.
+                position[(ar[i] / cur_digit) %
+                         10]++;  // incrementing ar[i]'s cur_digit position by 1, as
+                                 // current place used by ar[i].
+            }
+            ar = temp;
+        }
+        /**
+        * @brief Function to sort vector digit by digit.
+        * @returns none
+        */
+        void radix(std::vector<int>& ar) {
+            int max_ele = *max_element(ar.begin(), ar.end()); // returns the max element.
+            for (int i = 1; max_ele / i > 0;
+                 i *= 10) {  // loop breaks when i > max_ele because no further digits
+                             // left to makes changes in aray.
+                step_ith(i,ar);
+            }
+            for (int i = 0; i < ar.size(); i++) std::cout << ar[i] << " ";
+            std::cout << "\n";
+        }
+    }  // namespace radix_sort
 }  // namespace sorting
 
 /**
@@ -123,9 +90,7 @@ void radix::radix_sort() {
  */
 static void test1() {
     std::vector<int> ar = {432, 234, 143, 332, 123};
-    sorting::radix_sort::radix obj(ar);
-    obj.radix_sort();
-    obj.show();
+    sorting::radix_sort::radix(ar);
 }
 /**
  * Function to test the above algorithm
@@ -134,9 +99,7 @@ static void test1() {
 static void test2() {
     std::vector<int> ar = {213, 3214, 123, 111, 112, 142,
                            133, 132,  32,  12,  113};
-    sorting::radix_sort::radix obj(ar);
-    obj.radix_sort();
-    obj.show();
+    sorting::radix_sort::radix(ar);
 }
 /**
  * @brief Main function
@@ -153,8 +116,6 @@ int main() {
         std::cin >> x;
         ar.push_back(x);
     }
-    sorting::radix_sort::radix obj(ar);
-    obj.radix_sort();
-    obj.show();
+    sorting::radix_sort::radix(ar);
     return 0;
 }
