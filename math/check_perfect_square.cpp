44f833cf2d169b8abe74c49da16c2c1e23b7e27f
AmanDeep-21 <amandeepsmile21@gmail.com>
2020-10-30 15:33:37 +0530

    added requested changes

--- math/check_perfect_square.cpp
+++ math/check_perfect_square.cpp
@@ -1,13 +1,17 @@
 /**
  * @file
  * @brief This program aims at checking that the given number is perfect square or not
- *
+ * @author [Aman Deep](https://github.com/AmanDeep-21)
  */
 
-#include <cassert>
-#include <iostream>
+#include <cassert>          // for assert
+#include <iostream>         // for io operations
 
-/** Getting the integral square root of the number using binary search **/
+/**
+ * @brief Getting the integral square root of the number using binary search
+ * @param num number to be checked
+ * @returns int integral part of the square root of @param 'num'
+ */
 int get_integral_square_root(int num){
     int low = 0;                        // using inclusion exclusion binary search, low = minimum possible
     int high = num + 1;                 // high = any larger value which could never be the answer
@@ -25,7 +29,11 @@ int get_integral_square_root(int num){
     return low;
 }
 
-/** Method to check that given number is perfect square or not **/
+/**
+ * @brief Method to check that given number is perfect square or not
+ * @param num number to be checked
+ * @returns bool if number is perfect square, it returns @ true, else it returns @ false
+ */
 bool check_perfect_square(int num){
     if(num < 0) return false;           // square of real numbers can never be negative 
     
@@ -33,41 +41,59 @@ bool check_perfect_square(int num){
     return value * value == num;
 }
 
-// test case one
-void test1(){
+/**
+ * @brief 1st test case
+ * @returns void
+ */
+static void test1(){
     int test_case_1 = check_perfect_square(-4);
     assert(test_case_1 == false);
 }
 
-// test case two
-void test2(){
+/**
+ * @brief 2nd test case
+ * @returns void
+ */
+static void test2(){
     int test_case_2 = check_perfect_square(1);
     assert(test_case_2 == true);
 }
 
-// test case three
-void test3(){
+/**
+ * @brief 3rd test case
+ * @returns void
+ */
+static void test3(){
     int test_case_3 = check_perfect_square(2);
     assert(test_case_3 == false);
 }
 
-// test case four
-void test4(){
+/**
+ * @brief 4th test case
+ * @returns void
+ */
+static void test4(){
     int test_case_4 = check_perfect_square(4);
     assert(test_case_4 == true);
 }
 
-// test case five
-void test5(){
+/**
+ * @brief 5th test case
+ * @returns void
+ */
+static void test5(){
     int test_case_5 = check_perfect_square(8);
     assert(test_case_5 == false);
 }
 
 /**
- * Function for testing the check_perfect_square() with
+ * @brief Method for testing the 
+ * check_perfect_square() with 
  * all the test cases.
+ *
+ * @returns void
  */
-void test() {
+static void test() {
     // First test
     test1();
     // Second test
@@ -80,7 +106,10 @@ void test() {
     test5();
 }
 
-/** Main function */
+/**
+ * @brief Main function
+ * @returns 0 on exit
+ */
 int main() {
     test();
     std::cout << "Success." << std::endl;
