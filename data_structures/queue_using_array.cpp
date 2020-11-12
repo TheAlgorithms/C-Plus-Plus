1e0902c76f446579c0e2bfa9faeb8cfe6d281ad4
Farbod <farbodahmadian2014@gmail.com>
2020-11-12 22:10:27 +0330

    docs: Updated based on PR review

--- data_structures/queue_using_array.cpp
+++ data_structures/queue_using_array.cpp
@@ -19,7 +19,7 @@
 #include <iostream> /// for io operations
 #include <vector>   /// for std::vector
 
-constexpr uint8_t max_size{10};
+constexpr uint8_t max_size{10}; ///< Maximum size of the queue
 
 /**
  * @namespace data_structures
