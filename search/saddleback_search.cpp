0084e154d59155437b206f567fe39f1b6196affd
Hashir Hameed Niazi <hameedhashirniazi@gmail.com>
2020-11-30 12:00:30 +0530

    refactor: made suggested changes

--- search/saddleback_search.cpp
+++ search/saddleback_search.cpp
@@ -1,5 +1,5 @@
 /**
- * @file saddleback_search.cpp
+ * @file
  * @brief Implementation of saddleback algorithm for 2D arrays.
  *
  * @details
@@ -9,9 +9,9 @@
  *
  * @author [Hashir Niazi](https://github.com/HashirGJ8842)
  */
-#include <cassert>
-#include <iostream>
-#include <vector>
+#include <cassert>      /// for assert
+#include <iostream>     /// for io operations, and std::pair
+#include <vector>       /// for std::vector
 
 /** \namespace search
  * \brief Algorithms for searching
@@ -22,25 +22,23 @@ namespace search {
  */
 namespace saddleback {
 /**
- * This function implements saddleback
+ * This function implements saddleback algorithm, on a sorted 2D array, and finds the location of the element needed to search
  * @param matrix , a 2D matrix which is sorted on the basis of rows and columns
  * @param element , the element which is needed to be searched
  * @return An std::pair of with row and column populated within it, if the
  * element is present.
  * @return An std::pair with (-1, -1), if the element is not present.
  */
-std::pair<size_t, size_t> saddleback(std::vector<std::vector<int>> matrix,
+std::pair<uint64_t, uint64_t> saddleback(std::vector<std::vector<int>> matrix,
                                int element) {
-    size_t left_index = 0;
-    size_t right_index = matrix[0].size() - 1;  // Start from top right corner
-    while (left_index < matrix.size() &&
-           right_index >=
-               0) {  // Exit once the value of indexes get out of range.
+    uint64_t left_index = 0;
+    uint64_t right_index = matrix[0].size() - 1;  // Start from top right corner
+    while (left_index < matrix.size()) {  // Exit once the value of indexes get out of range.
         if (element ==
             matrix[left_index]
                   [right_index]) {  // If value on this position of matrix is
                                     // equal to element, return (row, column).
-            return std::pair<size_t, size_t>(left_index+1, right_index+1);
+            return std::pair<uint64_t, uint64_t>(left_index+1, right_index+1);
         } else if (element >
                    matrix[left_index]
                          [right_index]) {  // Else if value on this position of
@@ -57,7 +55,7 @@ std::pair<size_t, size_t> saddleback(std::vector<std::vector<int>> matrix,
             else --right_index;
         }
     }
-    return std::pair<size_t, size_t>(
+    return std::pair<uint64_t, uint64_t>(
         0, 0);  // If the program reaches here, that means one of the index
                   // went out of index, hence no element present.
 }
@@ -75,25 +73,25 @@ static void test() {
                                             {4, 40, 400, 4000, 40000},
                                             {5, 50, 500, 5000, 50000}};
 
-    std::pair<size_t, size_t> not_found = std::pair<size_t, size_t>(0, 0);
-    std::pair<size_t, size_t> test_answer;
+    std::pair<uint64_t, uint64_t> not_found = std::pair<uint64_t, uint64_t>(0, 0);
+    std::pair<uint64_t, uint64_t> test_answer;
     // Test 1
-    std::pair<size_t, size_t> answer1 = search::saddleback::saddleback(matrix, 123);
+    std::pair<uint64_t, uint64_t> answer1 = search::saddleback::saddleback(matrix, 123);
     assert(not_found == answer1);
     // Test 2
     answer1 = search::saddleback::saddleback(matrix, 0);
     assert(not_found == answer1);
     // Test 3
     answer1 = search::saddleback::saddleback(matrix, 1);
-    test_answer = std::pair<size_t, size_t>(1, 1);
+    test_answer = std::pair<uint64_t, uint64_t>(1, 1);
     assert(test_answer == answer1);
     // Test 4
     answer1 = search::saddleback::saddleback(matrix, 50000);
-    test_answer = std::pair<size_t, size_t>(5, 5);
+    test_answer = std::pair<uint64_t, uint64_t>(5, 5);
     assert(test_answer == answer1);
     // Test 5
     answer1 = search::saddleback::saddleback(matrix, 300);
-    test_answer = std::pair<size_t, size_t>(3, 3);
+    test_answer = std::pair<uint64_t, uint64_t>(3, 3);
     assert(test_answer == answer1);
 }
 
@@ -102,7 +100,6 @@ static void test() {
  * @returns 0 on exit
  */
 int main() {
-    // Test
-    test();
+    test();     // execute the tests
     return 0;
 }
