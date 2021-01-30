6f82d9c8e43ff4d65c3a714c1fcfc53d57235e70
Abhinn Mishra <aviabhinn98@gmail.com>
2021-01-30 10:11:15 +0530

    updates

--- range_queries/mo_algorithm.cpp
+++ range_queries/mo_algorithm.cpp
@@ -17,21 +17,23 @@ int a[N], bucket[N], cnt[N];
 int bucket_size;
 // Structure to represent a query range
 struct query {
-    int l, r, i;
+	int l, r, i;
 } q[N];
 // variable to maintain the count of unique elements.
 int ans = 0;
 // Function to add elements
 void add(int index) {
-    cnt[a[index]]++;
-    if (cnt[a[index]] == 1)
-        ans++;
+	cnt[a[index]]++;
+	if (cnt[a[index]] == 1) {
+		ans++;
+	}
 }
 // Function to remove elements
 void remove(int index) {
-    cnt[a[index]]--;
-    if (cnt[a[index]] == 0)
-        ans--;
+	cnt[a[index]]--;
+	if (cnt[a[index]] == 0) {
+		ans--;
+	}
 }
 /**
  *Function used to sort all queries so that all queries
@@ -39,65 +41,66 @@ void remove(int index) {
  *queries are sorted in increasing order of R values.
  */
 bool mycmp(query x, query y) {
-    // Different blocks, sort by block.
-    if (x.l / bucket_size != y.l / bucket_size)
-        return x.l / bucket_size < y.l / bucket_size;
-    // Same block, sort by R value
-    return x.r < y.r;
+	// Different blocks, sort by block.
+	if (x.l / bucket_size != y.l / bucket_size) {
+		return x.l / bucket_size < y.l / bucket_size;
+	}
+	// Same block, sort by R value
+	return x.r < y.r;
 }
 
 /**
  * Main Function
  */
 int main() {
-    int n, t, i, j, k = 0;
-    std::cin >> n;
-    for (i = 0; i < n; i++) {
-        std::cin >> a[i]
-    }
-    // Find block size
-    bucket_size = ceil(sqrt(n));
-    std::cin >> t;
-    for (i = 0; i < t; i++) {
-        std::cin >> q[i].l >> q[i].r;
-        q[i].l--;
-        q[i].r--;
-        q[i].i = i;
-    }
-    // Sort all queries so that queries of same blocks are arranged together.
+	int n, t, i, j, k = 0;
+	std::cin >> n;
+	for (i = 0; i < n; i++) {
+		std::cin >> a[i]
+	}
+	// Find block size
+	bucket_size = ceil(sqrt(n));
+	std::cin >> t;
+	for (i = 0; i < t; i++) {
+		std::cin >> q[i].l >> q[i].r;
+		q[i].l--;
+		q[i].r--;
+		q[i].i = i;
+	}
+	// Sort all queries so that queries of same blocks are arranged together.
 
-    std::sort(q, q + t, mycmp);
-    // Initialize current left, current right
-    int left = 0, right = 0;
-    // Traverse through all queries
-    for (i = 0; i < t; i++) {
-        // L and R values of current range
-        int L = q[i].l, R = q[i].r;
-        // Remove extra elements of previous range
-        while (left < L) {
-            remove(left);
-            left++;
-        }
-        // Add Elements of current Range
-        while (left > L) {
-            add(left - 1);
-            left--;
-        }
-        while (right <= R) {
-            add(right);
-            right++;
-        }
+	std::sort(q, q + t, mycmp);
+	// Initialize current left, current right
+	int left = 0, right = 0;
+	// Traverse through all queries
+	for (i = 0; i < t; i++) {
+		// L and R values of current range
+		int L = q[i].l, R = q[i].r;
+		// Remove extra elements of previous range
+		while (left < L) {
+			remove(left);
+			left++;
+		}
+		// Add Elements of current Range
+		while (left > L) {
+			add(left - 1);
+			left--;
+		}
+		while (right <= R) {
+			add(right);
+			right++;
+		}
 
-        // Remove elements of previous range.
-        while (right > R + 1) {
-            remove(right - 1);
-            right--;
-        }
-        bucket[q[i].i] = ans;
-    }
-    // Print unique elements in current range
-    for (i = 0; i < t; i++) {
-        std::cout << bucket[i] << endl;
-    }
-    return 0;
+		// Remove elements of previous range.
+		while (right > R + 1) {
+			remove(right - 1);
+			right--;
+		}
+		bucket[q[i].i] = ans;
+	}
+	// Print unique elements in current range
+	for (i = 0; i < t; i++) {
+		std::cout << bucket[i] << endl;
+	}
+	return 0;
 }
