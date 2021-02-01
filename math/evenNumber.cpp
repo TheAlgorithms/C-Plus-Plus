a1cdf87161320a6111cc268a71fbaeb04db2ecec
Khan Tanveer <khantanveer7977@gmail.com>
2021-02-01 19:51:23 +0530

    Added even number

---
+++ math/evenNumber.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cout << "Enter a range to generate even numbers : ";
+    cin >> a;
+    for (int i = 1; i <= 2 * a; i++) {
+        if (i % 2 == 0)
+            cout << i << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
