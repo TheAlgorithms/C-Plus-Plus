169b680a032118f756c2a17599d1325f151bf1f0
Khan Tanveer <khantanveer7977@gmail.com>
2021-02-01 19:47:10 +0530

    LCM And GCD

---
+++ math/LCM_GCD.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b) {
+    if (a == 0) {
+        return b;
+    }
+    return gcd(b % a, a);
+}
+
+int lcm(int a, int b) { return (a * b) / gcd(a, b); }
+
+int main() {
+    int a, b;
+    cout << "Enter First Number: ";
+    cin >> a;
+    cout << "Enter Second Number: ";
+    cin >> b;
+    cout << "GCD is " << gcd(a, b) << endl;
+    // cout << endl;
+    cout << "LCM is " << lcm(a, b) << endl;
+    // cout << endl;
+    return 0;
+}
