b7551052556a4a8680422075706a535ec7070cea
astronmax <astronmax@yandex.ru>
2021-02-13 22:56:36 +0300

    correction of comments, adding math namespace

--- math/binomial.cpp
+++ math/binomial.cpp
@@ -1,19 +1,25 @@
 /**
  * @file
- * @brief Program to calculate binomial coefficients.
+ * @brief Program to calculate [Binomial
+ * coefficients](https://en.wikipedia.org/wiki/Binomial_coefficient)
  *
  * @author [astronmax](https://github.com/astronmax)
  */
 
-#include <cassert>
-#include <cstdint>
-#include <cstdio>
-#include <cstdlib>
+#include <cassert>  /// for assert
+#include <cstdint>  /// for int32_t type
+#include <cstdio>   /// for printf
+#include <cstdlib>  /// for atoi
 
 /**
- * @brief Function to calculate binomial coefficients.
- * @param n first value to calculate binomial coefficient
- * @param k second value to calculate binomial coefficient
+ * @namespace math
+ * @brief Mathematical algorithms
+ */
+namespace math {
+/**
+ * @brief Function to calculate binomial coefficients
+ * @param n first value
+ * @param k second value
  * @return binomial coefficient for n and k
  */
 size_t binomial(int32_t n, int32_t k) {
@@ -33,29 +39,32 @@ size_t binomial(int32_t n, int32_t k) {
 
     return result;
 }
+}  // namespace math
 
 /**
- * TESTS
+ * @brief Test implementations
+ * @returns void
  */
 
-void binomial_test() {
+static void binomial_test() {
     // test for n from 1 to 62
-    assert(binomial(1, 1) == 1);
-    assert(binomial(57, 57) == 1);
-    assert(binomial(6, 3) == 20);
-    assert(binomial(10, 5) == 252);
-    assert(binomial(20, 10) == 184756);
-    assert(binomial(30, 15) == 155117520);
-    assert(binomial(40, 20) == 137846528820);
-    assert(binomial(50, 25) == 126410606437752);
-    assert(binomial(60, 30) == 118264581564861424);
-    assert(binomial(62, 31) == 465428353255261088);
+    assert(math::binomial(1, 1) == 1);
+    assert(math::binomial(57, 57) == 1);
+    assert(math::binomial(6, 3) == 20);
+    assert(math::binomial(10, 5) == 252);
+    assert(math::binomial(20, 10) == 184756);
+    assert(math::binomial(30, 15) == 155117520);
+    assert(math::binomial(40, 20) == 137846528820);
+    assert(math::binomial(50, 25) == 126410606437752);
+    assert(math::binomial(60, 30) == 118264581564861424);
+    assert(math::binomial(62, 31) == 465428353255261088);
 
     printf("[+] Binomial coefficients test completed\n");
 }
 
 /**
- * Main function
+ * @brief Main function
+ * @returns 0 on exit
  */
 int main(int argc, const char* argv[]) {
     // run test
@@ -69,5 +78,6 @@ int main(int argc, const char* argv[]) {
     int32_t n = atoi(argv[1]);
     int32_t k = atoi(argv[2]);
 
-    printf("%zd\n", binomial(n, k));
+    printf("%zd\n", math::binomial(n, k));
+    return 0;
 }
