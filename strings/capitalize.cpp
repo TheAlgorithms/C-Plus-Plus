653d2ba6a16b7c4915d4a1a9b7f9f240c49fabde
powpow58 <41553768+powpow58@users.noreply.github.com>
2021-03-03 13:40:00 -0400

    added lower and uppercase to "strings" namespace

--- strings/capitalize.cpp
+++ strings/capitalize.cpp
@@ -14,8 +14,7 @@ namespace strings {
  */
 std::string capitalize(std::string str) {
     if (str[0] >= 'a' && str[0] <= 'z') {
-        std::string new_str = str.insert(0, 1, str[0] - 32);
-        return new_str.erase(1, 1);
+        return str.insert(0, 1, str[0] - 32).erase(1, 1);
     } else {
         return str;
     }
