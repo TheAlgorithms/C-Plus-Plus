c8585c9ce6929df307f2c47e7fa04ca7b9fbb0fc
bharath <9440110838bharath@gmail.com>
2021-03-12 20:19:58 -0500

    fix: fixed clang.tidy warnings

--- data_structures/trie_using_hashmap.cpp
+++ data_structures/trie_using_hashmap.cpp
@@ -12,11 +12,11 @@
  */
 
 #include <cassert>        /// for assert
-#include <iostream>       /// for iostream
-#include <memory>         /// for shared pointer
-#include <stack>          /// for stack
-#include <unordered_map>  /// for unordered map
-#include <vector>         /// for vector
+#include <iostream>       /// for IO operations
+#include <memory>         /// for std::shared_ptr
+#include <stack>          /// for std::stack
+#include <unordered_map>  /// for std::unordered_map
+#include <vector>         /// for std::vector
 
 /**
  * @namespace data_structures
@@ -35,20 +35,17 @@ class Trie {
         bool word_end = false;  /// boolean variable to represent the node end
     };
 
-    std::shared_ptr<Node> root_node;  ///< declaring root node of trie
+    std::shared_ptr<Node> root_node =
+        std::make_shared<Node>();  ///< declaring root node of trie
 
  public:
     ///< Constructor
-    Trie() {
-        root_node = std::make_shared<Node>();  ///< intializing root node
-    }
-    ///< Destructor
-    virtual ~Trie() { root_node; }
+    Trie() = default;
 
     /** insert string into the trie
      * @param word string to insert in the tree
      */
-    void insert(std::string word) {
+    void insert(const std::string& word) {
         std::shared_ptr<Node> curr = root_node;
         for (char ch : word) {
             if (curr->children.find(ch) == curr->children.end()) {
@@ -68,7 +65,7 @@ class Trie {
      * @returns `false` if not found
      */
 
-    bool search(std::string word) {
+    bool search(const std::string& word) {
         std::shared_ptr<Node> curr = root_node;
         for (char ch : word) {
             if (curr->children.find(ch) == curr->children.end()) {
@@ -93,7 +90,7 @@ class Trie {
      * @returns `false` if not found
      */
 
-    bool startwith(std::string prefix) {
+    bool startwith(const std::string& prefix) {
         std::shared_ptr<Node> curr = root_node;
         for (char ch : prefix) {
             if (curr->children.find(ch) == curr->children.end()) {
@@ -113,8 +110,6 @@ class Trie {
         int cnt = 0;
         for (char ch : word) {
             if (curr->children.find(ch) == curr->children.end()) {
-                std::cout << "the word doesn't exit" << std::endl;
-
                 return;
             }
             if (curr->word_end) {
@@ -146,7 +141,7 @@ class Trie {
      * @returns list of recommended words
      */
     std::vector<std::string> get_all_words(std::vector<std::string> results,
-                                           std::shared_ptr<Node> element,
+                                           const std::shared_ptr<Node>& element,
                                            std::string prefix) {
         if (element->word_end) {
             results.push_back(prefix);
@@ -154,7 +149,7 @@ class Trie {
         if (element->children.empty()) {
             return results;
         }
-        for (auto const &x : element->children) {
+        for (auto const& x : element->children) {
             std::string key = "";
             key = x.first;
             prefix += key;
@@ -173,14 +168,13 @@ class Trie {
      * @returns list of recommended words
      */
 
-    std::vector<std::string> predict_words(std::string prefix) {
+    std::vector<std::string> predict_words(const std::string& prefix) {
         std::vector<std::string> result;
         std::shared_ptr<Node> curr = root_node;
         /* treversing till the end of given prefix in trie*/
 
         for (char ch : prefix) {
             if (curr->children.find(ch) == curr->children.end()) {
-                std::cout << "word with prefix dosen't exit" << std::endl;
                 return result;
             }
 
@@ -193,7 +187,6 @@ class Trie {
             return result;
         }
 
-        std::string pd_word = "";
         result = get_all_words(
             result, curr,
             prefix);  ///< iteratively and recursively get the recommended words
@@ -209,91 +202,91 @@ class Trie {
  * @returns void
  */
 static void test() {
-    data_structures::Trie *obj = new data_structures::Trie();
+    data_structures::Trie obj;
     /* Inserting data into trie using insert method and testing it with search
      * method */
-    obj->insert("app");
-    obj->insert("abscond");
-    obj->insert("about");
-    obj->insert("apps");
-    obj->insert("apen");
-    obj->insert("apples");
-    obj->insert("apple");
-    obj->insert("approach");
-    obj->insert("bus");
-    obj->insert("buses");
-    obj->insert("Apple");
-    obj->insert("Bounce");
+    obj.insert("app");
+    obj.insert("abscond");
+    obj.insert("about");
+    obj.insert("apps");
+    obj.insert("apen");
+    obj.insert("apples");
+    obj.insert("apple");
+    obj.insert("approach");
+    obj.insert("bus");
+    obj.insert("buses");
+    obj.insert("Apple");
+    obj.insert("Bounce");
 
-    assert(!obj->search("appy"));
+    assert(!obj.search("appy"));
     std::cout << "appy is not a word in trie" << std::endl;
 
-    assert(!obj->search("car"));
+    assert(!obj.search("car"));
     std::cout << "car is not a word in trie" << std::endl;
-    assert(obj->search("app"));
-    assert(obj->search("apple"));
-    assert(obj->search("apples"));
-    assert(obj->search("apps"));
-    assert(obj->search("apen"));
-    assert(obj->search("approach"));
-    assert(obj->search("about"));
-    assert(obj->search("abscond"));
-    assert(obj->search("bus"));
-    assert(obj->search("buses"));
-    assert(obj->search("Bounce"));
-    assert(obj->search("Apple"));
+    assert(obj.search("app"));
+    assert(obj.search("apple"));
+    assert(obj.search("apples"));
+    assert(obj.search("apps"));
+    assert(obj.search("apen"));
+    assert(obj.search("approach"));
+    assert(obj.search("about"));
+    assert(obj.search("abscond"));
+    assert(obj.search("bus"));
+    assert(obj.search("buses"));
+    assert(obj.search("Bounce"));
+    assert(obj.search("Apple"));
 
     std::cout << "All the Inserted words are present in the trie" << std::endl;
 
     /*test for startwith prefix method*/
-    assert(!obj->startwith("approachs"));
-    assert(obj->startwith("approach"));
-    assert(obj->startwith("about"));
-    assert(!obj->startwith("appy"));
-    assert(obj->startwith("abscond"));
-    assert(obj->startwith("bus"));
-    assert(obj->startwith("buses"));
-    assert(obj->startwith("Bounce"));
-    assert(obj->startwith("Apple"));
-    assert(obj->startwith("abs"));
-    assert(obj->startwith("b"));
-    assert(obj->startwith("bus"));
-    assert(obj->startwith("Bo"));
-    assert(obj->startwith("A"));
-    assert(!obj->startwith("Ca"));
+    assert(!obj.startwith("approachs"));
+    assert(obj.startwith("approach"));
+    assert(obj.startwith("about"));
+    assert(!obj.startwith("appy"));
+    assert(obj.startwith("abscond"));
+    assert(obj.startwith("bus"));
+    assert(obj.startwith("buses"));
+    assert(obj.startwith("Bounce"));
+    assert(obj.startwith("Apple"));
+    assert(obj.startwith("abs"));
+    assert(obj.startwith("b"));
+    assert(obj.startwith("bus"));
+    assert(obj.startwith("Bo"));
+    assert(obj.startwith("A"));
+    assert(!obj.startwith("Ca"));
 
-    assert(!obj->startwith("C"));
+    assert(!obj.startwith("C"));
 
     std::cout << "All the tests passed for startwith method" << std::endl;
 
     /*test for predict_words/recommendation of words based on a given prefix*/
 
-    std::vector<std::string> pred_words = obj->predict_words("a");
+    std::vector<std::string> pred_words = obj.predict_words("a");
 
-    for (std::string str : pred_words) {
+    for (const std::string& str : obj.predict_words("a")) {
         std::cout << str << std::endl;
     }
     assert(pred_words.size() == 8);
     std::cout << "Returned all words that start with prefix a " << std::endl;
-    pred_words = obj->predict_words("app");
+    pred_words = obj.predict_words("app");
 
-    for (std::string str : pred_words) {
+    for (const std::string& str : pred_words) {
         std::cout << str << std::endl;
     }
 
     assert(pred_words.size() == 5);
     std::cout << "Returned all words that start with prefix app " << std::endl;
-    pred_words = obj->predict_words("A");
+    pred_words = obj.predict_words("A");
 
-    for (std::string str : pred_words) {
+    for (const std::string& str : pred_words) {
         std::cout << str << std::endl;
     }
 
     assert(pred_words.size() == 1);
     std::cout << "Returned all words that start with prefix A " << std::endl;
-    pred_words = obj->predict_words("bu");
+    pred_words = obj.predict_words("bu");
 
-    for (std::string str : pred_words) {
+    for (const std::string& str : pred_words) {
         std::cout << str << std::endl;
     }
 
@@ -302,12 +295,12 @@ static void test() {
 
     /*tests for delete method */
 
-    obj->delete_word("app");
-    assert(!obj->search("app"));
+    obj.delete_word("app");
+    assert(!obj.search("app"));
     std::cout << "word app is deleted sucessful" << std::endl;
 
-    pred_words = obj->predict_words("app");
-    for (std::string str : pred_words) {
+    pred_words = obj.predict_words("app");
+    for (const std::string& str : pred_words) {
         std::cout << str << std::endl;
     }
     assert(pred_words.size() == 4);
@@ -315,12 +308,12 @@ static void test() {
 
     /*test case for chinese language*/
 
-    obj->insert("苹果");
-    assert(obj->startwith("苹"));
-    pred_words = obj->predict_words("h");
-    for (std::string str : pred_words) {
-        std::cout << str << std::endl;
-    }
+    obj.insert("苹果");
+    assert(obj.startwith("苹"));
+    pred_words = obj.predict_words("h");
+
+    assert(pred_words.size() == 0);
+    std::cout << "No word starts with prefix h in trie" << std::endl;
 
     std::cout << "All tests passed" << std::endl;
 }
