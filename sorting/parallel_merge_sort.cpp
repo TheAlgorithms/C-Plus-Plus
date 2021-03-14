2c3c5e58bd4c117ac71f83ff0e7aa23ab32dd4fa
Anish Kumar Singh <anishsingh935>
2021-03-14 21:33:17 +0530

    bug fix

--- sorting/parallel_merge_sort.cpp
+++ sorting/parallel_merge_sort.cpp
@@ -4,31 +4,33 @@
 
 #include<iostream>
 #include "omp.h"
-using namespace std;
+
 //performs the bottom up combine operation
 void merge(int a[], int l, int m, int r)
 {
 	int temp[m-l+1], temp2[r-m];
 	//copy first part to temporary array
-	for(int i=0; i<(m-l+1); i++)
-		temp[i]=a[l+i];
+	for(int i=0; i<(m-l+1); i++){
+		temp[i]=a[l+i];}
 	//copy second part to temporary arrat]y
 	for(int i=0; i<(r-m); i++)
-		temp2[i]=a[m+1+i];
+	{	temp2[i]=a[m+1+i];}
 	int i=0, j=0, k=l;
 	//combine the arrays on the basis of order
 	while(i<(m-l+1) && j<(r-m))
 	{
-		if(temp[i]<temp2[j])
-			a[k++]=temp[i++];
-		else
+		if(temp[i]<temp2[j]){
+			a[k++]=temp[i++];}
+		else{
 			a[k++]=temp2[j++];
+	}
+
 	}
 	//to combine the remainder of the two arrays
 	while(i<(m-l+1))
-		a[k++]=temp[i++];
+	{	a[k++]=temp[i++];}
 	while(j<(r-m))
-		a[k++]=temp2[j++];
+		{a[k++]=temp2[j++];}
 
 }
 /* 
@@ -60,24 +62,24 @@ void mergeSort(int a[], int l, int r)
 //a simple print function to print all the elements
 void print(int a[], int n)
 {
-	for(int i=0; i<n; i++)
-		cout<<a[i]<<" ";
-	cout<<endl;
+	for(int i=0; i<n; i++){
+		std::cout<<a[i]<<" ";}
+	std::cout<<std::endl;
 }
 int main()
 {
 	int n;
-    cout<<" Enter the size of the array"<<endl;
-	cin>>n;
+    std::cout<<" Enter the size of the array"<<std::endl;
+	std::cin>>n;
 	int a[n];
-    cout<<"Enter the element of tha array"<<endl;
+    std::cout<<"Enter the element of tha array"<<std::endl;
 	for(int i=0; i<n; i++)
-		cin>>a[i];
-	cout<<"\nUnsorted array; ";
+		{std::cin>>a[i];}
+	std::cout<<"\nUnsorted array; ";
 	print(a,n);
 	//call should be to a wrapper function that calls the mergeSort function
 	mergeSort(a,0,n-1);
-	cout<<"\nSorted array: ";
+	std::cout<<"\nSorted array: ";
 	print(a,n);
 	return 0;
-}
+} 
