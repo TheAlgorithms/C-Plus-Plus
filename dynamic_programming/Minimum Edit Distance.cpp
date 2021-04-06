d845d8f146f1a53ad5eb8e96e29aca9857cd2006
Nirjas Jakilim <Nirzak@users.noreply.github.com>
2021-04-06 21:44:03 +0600

    Updated the code According to the guidelines

--- dynamic_programming/Minimum Edit Distance.cpp
+++ dynamic_programming/Minimum Edit Distance.cpp
@@ -1,11 +1,44 @@
-#include<bits/stdc++.h>
-using namespace std;
+/*
+@details
+ Given two strings str1 & str2 and we have to calculate the minimum
+ number of operations (Insert, Delete, Replace) required to convert
+ str1 to str2.
+
+ @Author
+ Nirzak (github.com/nirzak)
+ */
+#include<cassert>
+#include<iostream>
+/*
+ * @namespace dynamic_programming
+ * @brief Dynamic Programming algorithms
+ */
+
+namespace dynamic_programming
+{
+/*
+ * @namespace Minimum Edit Distance
+ * @brief Implementation of 0-1 Knapsack problem
+ */
+
+
+namespace minimum_edit_distance
+{
+/*function to return minimum of three operations
+Insert, Replace or Delete
+*/
 int min(int x, int y, int z)
 {
-    return min(min(x, y), z);
+    if (x <= y && x <= z)
+        return x;
+    if (y <= x && y <= z)
+        return y;
+    else
+        return z;
 }
 
-int editDistDP(string str1, string str2, int m, int n)
+
+int editDistDP(std::string str1, std::string str2, int m, int n)
 {
     // Create a table to store results of subproblems
     int dp[m+1][n+1];
@@ -16,7 +49,7 @@ int editDistDP(string str1, string str2, int m, int n)
         for (int j=0; j<=n; j++)
         {
             // If first string is empty, only option is to
-            // isnert all characters of second string
+            // insert all characters of second string
             if (i==0)
                 dp[i][j] = j;  // Min. operations = j
 
@@ -33,25 +66,45 @@ int editDistDP(string str1, string str2, int m, int n)
             // If the last character is different, consider all
             // possibilities and find the minimum
             else
-                dp[i][j] = 1 + min(dp[i][j-1],  // Insert
-                                   dp[i-1][j],  // Remove
-                                   dp[i-1][j-1]); // Replace
+                dp[i][j] = 1 + min(dp[i][j-1],
+                                   dp[i-1][j],
+                                   dp[i-1][j-1]);
         }
     }
 
     return dp[m][n];
 }
+}
+}
+
+//function to test above algorithm
+static void test()
+{
+    //test 1
+    std::string str1 = "INTENTION";
+    std::string str2 = "EXECUTION";
+    const int expected_output1 = 5;
+    const int output1 = dynamic_programming::
+                        minimum_edit_distance::
+                        editDistDP(str1, str2,str1.length(),str2.length());
+    assert(output1==expected_output1);
+    std::cout << "Minimum Number of Operations Required: "<< output1<<std::endl;
+    //test 2
+    std::string str3 = "SATURDAY";
+    std::string str4 = "SUNDAY";
+    const int expected_output2 = 3;
+    const int output2 = dynamic_programming::
+                        minimum_edit_distance::
+                        editDistDP(str3, str4, str3.length(),str4.length());
+    assert(output2==expected_output2);
+    std::cout << "Minimum Number of Operations Required: "<< output2<<std::endl;
+
+
+}
 
 // Driver program
 int main()
 {
-    // your code goes here
-    string str1, str2;
-    cout<<"Enter 1st String: ";
-    cin>>str1;
-    cout<<"Enter 2nd String: ";
-    cin>>str2;
-    cout << editDistDP(str1, str2, str1.length(), str2.length());
-
+    test();
     return 0;
 }
