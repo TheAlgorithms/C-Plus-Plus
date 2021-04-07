f607cd5ce830502acc24d747a625e1e0640edc9a
Nirjas Jakilim <Nirzak@users.noreply.github.com>
2021-04-07 17:47:04 +0600

    Updated with necessary changed and documentation

--- dynamic_programming/minimum_edit_distance.cpp
+++ dynamic_programming/minimum_edit_distance.cpp
@@ -1,32 +1,59 @@
-/*
-@details
- Given two strings str1 & str2 and we have to calculate the minimum
- number of operations (Insert, Delete, Replace) required to convert
- str1 to str2.
-
- @Author
- Nirzak (github.com/nirzak)
+/**
+ * @file
+ * @brief Implementation of Minimum Edit Distance using Dynamic Programing
+ * (https://en.wikipedia.org/wiki/Edit_distance)
+ *
+ * @details
+ * Given two strings str1 & str2 and we have to calculate the minimum
+ * number of operations (Insert, Remove, Replace) required to convert
+ * str1 to str2.
+ *
+ * @algorithm
+ * We will solve this problem using Naive recursion. But as we are
+ * approaching with a DP solution. So, we will take a DP array to
+ * store the solution of all sub-problems so that we don't have to
+ * perform recursion again and again. Now to solve the problem, We
+ * can traverse all characters from either right side of the strings
+ * or left side. Suppose we will do it from the right side. So, there
+ * are two possibilities for every pair of characters being traversed.
+ * 1. If the last characters of two strings are the same, Ignore
+ * the characters and get the count for the remaining string.
+ * So, we get the solution for lengths m-1 and n-1 in a DP array.
+ *
+ * 2. Else, (If last characters are not the same), we will consider all
+ * three operations (Insert, Remove, Replace) on the last character of
+ * the first string and compute the minimum cost for all three operations
+ * and take the minimum of three values in the DP array.
+ * For Insert: Recur for m and n-1
+ * For Remove: Recur for for m-1 and n
+ * For Replace: Recur for for m-1 and n-1
+ *
+ * @author [Nirzak] (github.com/nirzak)
  */
+
 #include<cassert>
 #include<iostream>
-/*
+/**
  * @namespace dynamic_programming
  * @brief Dynamic Programming algorithms
  */
 
 namespace dynamic_programming
 {
-/*
- * @namespace Minimum Edit Distance
- * @brief Implementation of 0-1 Knapsack problem
- */
 
+/**
+ * @namespace Minimum Edit Distance
+ * @brief Implementation of Minimum Edit Distance algorithm
+ */
 
 namespace minimum_edit_distance
 {
-/*function to return minimum of three operations
+
+/**
+function to return minimum of three operations
 Insert, Replace or Delete
 */
+
 int min(int x, int y, int z)
 {
     if (x <= y && x <= z)
@@ -66,18 +93,22 @@ int editDistDP(std::string str1, std::string str2, int m, int n)
             // If the last character is different, consider all
             // possibilities and find the minimum
             else
-                dp[i][j] = 1 + min(dp[i][j-1],
-                                   dp[i-1][j],
-                                   dp[i-1][j-1]);
+                dp[i][j] = 1 + min(dp[i][j-1], //Insert
+                                   dp[i-1][j], //Remove
+                                   dp[i-1][j-1]); //Delete
         }
     }
 
     return dp[m][n];
 }
-}
-}
+} //namespace minimum_edit_distance
+} //namespace dynamic_programming
 
-//function to test above algorithm
+/**
+ * @brief Function to test above algorithm
+ * @returns void
+ */
+
 static void test()
 {
     //test 1
@@ -89,6 +120,7 @@ static void test()
                         editDistDP(str1, str2,str1.length(),str2.length());
     assert(output1==expected_output1);
     std::cout << "Minimum Number of Operations Required: "<< output1<<std::endl;
+
     //test 2
     std::string str3 = "SATURDAY";
     std::string str4 = "SUNDAY";
@@ -102,9 +134,14 @@ static void test()
 
 }
 
-// Driver program
+/**
+ * @brief Main function
+ * @returns 0 on exit
+ */
+
 int main()
 {
+    // Testing
     test();
     return 0;
 }
