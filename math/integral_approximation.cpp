86b98c95caf6752b1777d4a20e19e989a11097f5
Your Name <bwal@umich.edu>
2021-04-13 18:02:11 -0400

    feat: added integral approximation algorithm

--- math/integral_approximation.cpp
+++ math/integral_approximation.cpp
@@ -5,40 +5,52 @@
  * \author [Benjamin Walton](https://github.com/bwalton24)
  * \author [Shiqi Sheng](https://github.com/shiqisheng00)
  */
-#include <iostream>
 #include <cassert>
-#include <math.h>
- 
-double f(double x) {
-    double y = x*x + cos(x) + exp(x) + log(x)*log(x);
-    return y;
-}
- 
+#include <cmath>
+#include <functional>
+#include <iostream>
+
 /**
  * Computes Integral Approximation
  * @param lb lower bound
  * @param ub upper bound
+ * @param func pass in function
  * @param delta delta
- * @returns 
+ * @returns
  */
-double integral_approx(double lb, double ub, double delta) {
+double integral_approx(double lb, double ub,
+                       const std::function<double(double)>& func,
+                       double delta = .001) {
     assert(lb <= ub);
     double result = 0;
-    for(double i = lb; i < ub; i += delta) {
-        result += 0.5*delta*(f(i) + f(i + delta));
+    for (double i = lb; i < ub; i += delta) {
+        assert(!isnan(func(i)));
+        assert(!isnan(func(i + delta)));
+        result += 0.5 * delta * (func(i) + func(i + delta));
     }
     return result;
 }
- 
+
+double test_func1(double x) {
+    double y = x * x + cos(x) + exp(x) + log(x) * log(x);
+    return y;
+}
+
 /**
  * Function for testing integral_approx function.
  * test cases and assert statement.
  * @returns `void`
  */
 static void test() {
-    std::cout << integral_approx(.05, 2.55, .03) << std::endl;
+    // Design a couple of functions and calculate their Riemann sum by hand,
+    // then assert that with the output
+    std::cout << integral_approx(-1.24, 1.02, [](const double x) {
+        return log(x) + exp(x) + x;
+    }) << std::endl;
+
+    std::cout << integral_approx(1.24, 10.25, test_func1) << std::endl;
 }
- 
+
 /**
  * @brief Main function
  * @returns 0 on exit
@@ -47,6 +59,3 @@ int main() {
     test();  // execute the tests
     return 0;
 }
- 
- 
-
