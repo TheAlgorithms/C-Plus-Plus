aeb01b59feae5347579911df959c51025b49fa39
faizan2700 <syedfaizan824@gmail.com>
2021-04-13 12:36:06 +0530

    feat: dp bitmasking

--- dynamic_programming/dp_bitmasking.cpp
+++ dynamic_programming/dp_bitmasking.cpp
@@ -1,7 +1,6 @@
 /**
  * @file
- * @brief Functions for [Bitmasking DP
- * approach](https://codeforces.com/blog/entry/337) implementation
+ * @brief Functions for [Bitmasking DP approach](https://codeforces.com/blog/entry/337) implementation
  *
  * @details
  * Given Integer N and Array of size 2$\cdot${N}. Make N pairs using each
