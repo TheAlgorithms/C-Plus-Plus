ca10c3bad9741bd53ea2df4a634cc796bd71430b
Aditya Prakash <prakashadityaap@gmail.com>
2021-04-28 19:04:44 +0530

    Reworked documentation

--- sorting/recursive_bubble_sort.cpp
+++ sorting/recursive_bubble_sort.cpp
@@ -1,17 +1,19 @@
 /**
- * @file
- * @brief Recursive Bubble sort algorithm
+ * @file recursive_bubble_sort.cpp
+ * @author Aditya Prakash (adityaprakash.tech)
+ * @brief This is an implementation of recursive version of Bubble sort
+algorithm
  *
+ * @details
  * The working principle of the Bubble sort algorithm:
 
-Bubble sort algorithm is the bubble sorting algorithm. The most important reason
-for calling the bubble is that the largest number is thrown at the end of this
-algorithm. This is all about the logic. In each iteration, the largest number is
-expired and when iterations are completed, the sorting takes place.
+Bubble sort is a simple sorting algorithm used to rearrange a set of elements in
+ascending or descending order. Bubble sort gets its name from the fact that data
+"bubbles" to the top of the dataset.
 
-What is Swap?
+ * What is Swap?
 
-Swap in the software means that two variables are displaced.
+To swap two numbers mean that we interchange their values.
 An additional variable is required for this operation. x = 5, y = 10.
 We want x = 10, y = 5. Here we create the most variable to do it.
 
@@ -25,7 +27,7 @@ When x assigns the value to x, the old value of x is lost.
 That's why we created a variable z to create the first value of the value of x,
 and finally, we have assigned to y.
 
-Recursion
+ * Recursion
 
 While the recursive method does not necessarily have advantages over iterative
 versions, but it is useful to enhance the understanding of the algorithm and
@@ -36,67 +38,122 @@ the last element. This fixes the last element for that sub-array.Formally, for
 elements in the array. Exit condition: n==1; i.e. the sub-array contains only
 one element.
 
-Bubble Sort Algorithm Analysis (Best Case - Worst Case - Average Case)
+ * Complexity
+    Time complexity: O(n) best case; O(n²) average case; O(n²) worst case
+    Space complexity: O(n)
 
-Bubble Sort Worst Case Performance is O (n²). Why is that? Because if you
-remember Big O Notation, we were calculating the complexity of the algorithms in
-the nested loops. The n * (n - 1) product gives us O (n²) performance. In the
-worst case all the steps of the cycle will occur. Bubble Sort (Avarage Case)
-Performance. Bubble Sort is not an optimal algorithm. in average, O (n²)
-performance is taken. Bubble Sort Best Case Performance. O (n). However, you
-can't get the best status in the code we shared above. This happens on the
-optimized bubble sort algorithm. It's right down there.
+We need to traverse the array `n * (n-1)` times. However, if the entire array is
+already sorted, then we need to traverse it only once. Hence, O(n) is the best case
+complexity
 */
 
-#include <iostream>
-#include <vector>
+#include <cassert>   // for assert
+#include <iostream>  // for io operations
+#include <vector>    // for std::vector
+#include <array>     // for std::array
 
-void swap(int *x, int *y) {
-    int temp = *x;
+/**
+ * @namespace sorting
+ * @brief Sorting algorithms
+ */
+namespace sorting {
+
+/**
+ * @brief This is a function for swapping two variables. Alternatively, <algorithm> could 
+ * be included.
+ * 
+ * @tparam T type of data variables to be swapped
+ * @param x first variable to be swapped
+ * @param y second variable to be swapped
+ */
+template <typename T>
+void swap(T *x, T *y) {     
+    T temp = *x;  //!< dereferencing pointer to `x` and assigning it to `temp`
     *x = *y;
     *y = temp;
 }
 
-void recursive_bubble_sort(std::vector<int> *nums, int n) {
-    if (n == 1) {
+/**
+ * @brief This is an implementation of the recursive_bubble_sort. A vector is passed
+ * to the function which is then dereferenced, so that the changes are
+ * reflected in the original vector. It also accepts a second parameter of
+ * type `int` and name `n`, which is the size of the array.
+ * 
+ * @tparam T type of data variables in the array
+ * @param nums our array of elements.
+ * @param n size of the array
+ */
+template <typename T>
+void recursive_bubble_sort(std::vector<T> *nums, int n) {
+    if (n == 1) {  //!< base case; when size of the array is 1
         return;
     }
 
-    for (int i = 0; i < n - 1; i++) {
+    for (int i = 0; i < n - 1; i++) {  //!< iterating over the entire array
+        //!< if a larger number appears before the smaller one, swap them.
         if ((*nums)[i] > (*nums)[i + 1]) {
             swap(&(*nums)[i], &(*nums)[i + 1]);
         }
     }
 
+    //!< calling the function after we have fixed the last element
     recursive_bubble_sort(nums, n - 1);
 }
+}
 
-int main() {
-    int n = 0;
-    bool swap_check = true;
-    std::cout << "Enter the amount of numbers to sort: ";
-    std::cin >> n;
-    std::vector<int> numbers;
-    std::cout << "Enter " << n << " numbers: ";
-    int num = 0;
+/**
+ * @brief function for testing the code
+ * 
+ */
+static void test() {
+    // Example 1. Creating array of int,
+    std::cout << "Test 1 using ints\n";
+    const int size = 6;
+    std::vector<int> arr;
+    //populating the array
+    arr.push_back(22);
+    arr.push_back(46);
+    arr.push_back(94);
+    arr.push_back(12);
+    arr.push_back(37);
+    arr.push_back(63);
 
-    // Input
-    for (int i = 0; i < n; i++) {
-        std::cin >> num;
-        numbers.push_back(num);
+    sorting::recursive_bubble_sort(&arr, size);
+    assert(std::is_sorted(std::begin(arr), std::end(arr)));
+    std::cout << " Passed\n";
+    for (int i = 0; i < size; i++) {
+        std::cout << arr[i] << ", ";
     }
+    std::cout << std::endl;
 
-    // Function call
-    recursive_bubble_sort(&numbers, n);
+    // Example 2. Creating array of doubles.
+    std::cout << "Test 2 using doubles\n";
+    std::vector<double> double_arr;
 
-    // Output
-    std::cout << "\nSorted Array : ";
-    for (int i = 0; i < numbers.size(); i++) {
-        if (i != numbers.size() - 1) {
-            std::cout << numbers[i] << ", ";
-        } else {
-            std::cout << numbers[i] << std::endl;
-        }
+    double_arr.push_back(20.4);
+    double_arr.push_back(62.7);
+    double_arr.push_back(12.2);
+    double_arr.push_back(43.6);
+    double_arr.push_back(74.1);
+    double_arr.push_back(57.9);
+
+    sorting::recursive_bubble_sort(&double_arr, size);
+    assert(std::is_sorted(std::begin(double_arr), std::end(double_arr)));
+    std::cout << " Passed\n";
+    for (int i = 0; i < size; i++) {
+        std::cout << double_arr[i] << ", ";
     }
+    std::cout << std::endl;
+
+}
+
+/**
+ * @brief main method to run the program
+ * 
+ * @return int 
+ */
+int main() { 
+    test();
     return 0;
 }
+
