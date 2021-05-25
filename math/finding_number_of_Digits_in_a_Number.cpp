53c56b829702f16f8c87d6d1b4e80d0f29f9a4e5
amino19 <anshumaankrprasad76@gmail.com>
2021-05-26 01:57:02 +0530

    Changes done with codes by adding more comments

--- math/finding_number_of_Digits_in_a_Number.cpp
+++ math/finding_number_of_Digits_in_a_Number.cpp
@@ -1,10 +1,14 @@
 /**
  * @author amino19
- * @file
+ * @file:[Program to count digits in an
+ * integer](https://www.geeksforgeeks.org/program-count-digits-integer-3-different-methods)
  *
  * \brief Finding number of Digits in a Number
- * \details Basically, its a very basic math of
- *  finding number of digits in a number (input),
+ * \details It is a very basic math of finding
+ *  number of digits in a given number i.e, we
+ *  can use it by inputting values it can be a
+ *  positive/negatie value, lets say integer.
+ *
  *  For more details, refer Algorithms-Explanation
  */
 
@@ -23,14 +27,16 @@ int main() {
     int count = 0;
     std::cout << "Enter an integer: ";
     std::cin >> n;
-
     /* iterate until n becomes 0
      *  remove last digit from n in each iteration
      *  increase count by 1 in each iteration */
     while (n != 0) {
-        n /= 10;  // n = n/10
+        /* we can also use: n = n/10 */
+        n /= 10;
+        /* each time while loop running, count will
+         *  be increment by 1.
+         */
         ++count;
     }
-
     std::cout << "Number of digits: " << count;
 }
