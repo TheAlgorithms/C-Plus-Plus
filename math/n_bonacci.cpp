b1a717c12ace7f14b109e921f08d19aba48f100c
Swastika Gupta <64654203+Swastyy@users.noreply.github.com>
2021-06-25 07:26:43 +0530

    Update n_bonacci.cpp

--- math/n_bonacci.cpp
+++ math/n_bonacci.cpp
@@ -13,7 +13,7 @@
  *
  * @author [Swastika Gupta](https://github.com/swastyy)
  */
-#include <algorithm>  /// for std::is_sorted, std::swap
+#include <algorithm>  /// for std::is_equal, std::swap
 #include <cassert>    /// for assert
 #include <iostream>   /// for io operations
 #include <vector>     /// for std::vector
@@ -34,29 +34,24 @@ namespace n_bonacci {
  * @param n,m m is the number of terms in n-bonacci sequence
  * @returns the n-bonacci sequence as vector array
  */
-template <typename T>
-std::vector<T> N_bonacci(const std::vector<T> &in_arr) {
+
+std::vector<int> N_bonacci(int n, unsigned int m) {
     /**
-     * This function finds N-bonacci series for
-     * @param n value and @param m terms
+     * This function finds N-bonacci series for @param n value and @param m terms
      */
-    int n = in_arr[0];
-    int m = in_arr[1];
+    
     std::vector<int> a(m, 0);  // we create an empty array of size m
                               
     int i = 0;
-    a[n - 1] = 1;  /// we initialise the (n-1)th term as 1 which is the sum of
-                   /// preceding N zeros
-    a[n] = 1;  /// similarily the sum of preceding N zeros and the (N+1)th 1 is
-               /// also 1
+    a[n - 1] = 1;  /// we initialise the (n-1)th term as 1 which is the sum of preceding N zeros
+    a[n] = 1;  /// similarily the sum of preceding N zeros and the (N+1)th 1 is also 1
     for (i = n + 1; i < m; i++) {
-         // this is an optimized solution that works in O(M) time and takes O(M)
-         // extra space here we use the concept of the sliding window the current term
+         // this is an optimized solution that works in O(M) time and takes O(M) extra space here we use the concept of the sliding window the current term
          // can be computed using the given formula
         a[i] = 2 * a[i - 1] - a[i - 1 - n];
     }
     return a;
-}
+
 }
 }  // namespace n_bonacci
 }  // namespace math
@@ -67,34 +62,30 @@ std::vector<T> N_bonacci(const std::vector<T> &in_arr) {
  * @returns void
  */
 static void test() {
-    // n = array1[0] m = array1[1] return [1, 1]
-    std::vector<int> array1 = {1, 1};
+    // n = 1 m = 1 return [1, 1]
     std::cout << "Test 1... ";
-    std::vector<int> arr1 = math::n_bonacci::N_bonacci(array1);
+    std::vector<int> arr1 = math::n_bonacci::N_bonacci(1,1);
     std::vector<int> output_array1 = {1, 1};
     assert(std::equal(std::begin(arr1), std::end(arr1), std::begin(output_array1)));
     std::cout << "passed" << std::endl;
 
-    // n = array2[0] m = array2[1] return [0, 0, 0, 0, 1, 1, 2, 4, 8, 16, 31, 61, 120, 236, 464]
-    std::vector<int> array2 = {5, 15};
+    // n = 5 m = 15 return [0, 0, 0, 0, 1, 1, 2, 4, 8, 16, 31, 61, 120, 236, 464]
     std::cout << "Test 2... ";
-    std::vector<int> arr2 = math::n_bonacci::N_bonacci(array2);
+    std::vector<int> arr2 = math::n_bonacci::N_bonacci(5,15);
     std::vector<int> output_array2 = {0, 0, 0, 0, 1, 1, 2, 4, 8, 16, 31, 61, 120, 236, 464};
     assert(std::equal(std::begin(arr2), std::end(arr2), std::begin(output_array2)));
     std::cout << "passed" << std::endl;
 
-    // n = array3[0] m = array3[1] return [0, 0, 0, 0, 0, 1, 1, 2, 4, 8, 16, 32, 63, 125, 248, 492, 976]
-    std::vector<int> array3 = {6, 17};
+    // n = 6 m = 17 return [0, 0, 0, 0, 0, 1, 1, 2, 4, 8, 16, 32, 63, 125, 248, 492, 976]
     std::cout << "Test 3... ";
-    std::vector<int> arr3 = math::n_bonacci::N_bonacci(array3);
+    std::vector<int> arr3 = math::n_bonacci::N_bonacci(6,17);
     std::vector<int> output_array3 = {0, 0, 0, 0, 0, 1, 1, 2, 4, 8, 16, 32, 63, 125, 248, 492, 976};
     assert(std::equal(std::begin(arr3), std::end(arr3), std::begin(output_array3)));
     std::cout << "passed" << std::endl;
 
-    // n = array4[0] m = array4[1] return [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    std::vector<int> array4 = {56, 15};
+    // n = 56 m = 15 return [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     std::cout << "Test 4... ";
-    std::vector<int> arr4 = math::n_bonacci::N_bonacci(array4);
+    std::vector<int> arr4 = math::n_bonacci::N_bonacci(56,15);
     std::vector<int> output_array4 = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
     assert(std::equal(std::begin(arr4), std::end(arr4), std::begin(output_array4)));
     std::cout << "passed" << std::endl;
