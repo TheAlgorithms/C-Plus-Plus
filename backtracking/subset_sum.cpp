1b82d4991b28d144e517ec148a65bd87701c1ec9
Swastika Gupta <64654203+Swastyy@users.noreply.github.com>
2021-07-06 09:12:21 +0530

    Update backtracking/subset_sum.cpp
    Co-authored-by: Abhinn Mishra <49574460+mishraabhinn@users.noreply.github.com>

--- backtracking/subset_sum.cpp
+++ backtracking/subset_sum.cpp
@@ -1,7 +1,6 @@
 /**
  * @file
- * @brief Program to Count number of subsets (non-continuous
- * subarrays) with a given sum
+ * @brief We are given with an array and a sum value. The algorithms find all the subsets of that array with sum equal to given sum and return such subsets count. This approach will have exponential time complexity. 
  *
  * @details Subset problem (https://en.wikipedia.org/wiki/Subset_sum_problem)
  * @author [Swastika Gupta](https://github.com/swastyy)
