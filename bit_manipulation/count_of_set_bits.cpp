7ee363ff43d18059a2f2e2098c9256ec5916a80c
Swastika Gupta <64654203+Swastyy@users.noreply.github.com>
2021-07-08 09:25:04 +0530

    Update bit_manipulation/count_of_set_bits.cpp
    Co-authored-by: David Leal <halfpacho@gmail.com>

--- bit_manipulation/count_of_set_bits.cpp
+++ bit_manipulation/count_of_set_bits.cpp
@@ -1,6 +1,6 @@
 /**
  * @file
- * @brief Program to Count set bits in an integer
+ * @brief Implementation to [count sets bits](https://www.geeksforgeeks.org/count-set-bits-in-an-integer/) in an integer.
  *
  * @details
  * Time Complexity :- O(log n)
