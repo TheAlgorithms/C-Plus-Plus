99e408544e0ab47eaba7db8a03a10c04ff32331a
David Leal <halfpacho@gmail.com>
2021-07-13 15:22:40 -0500

    Update backtracking/magic_sequence.cpp

--- backtracking/magic_sequence.cpp
+++ backtracking/magic_sequence.cpp
@@ -124,6 +124,10 @@ static void test() {
  * @brief Main function
  * @returns 0 on exit
  */
+/**
+ * @brief Main function
+ * @returns 0 on exit
+ */
 int main() {
     test();  // run self-test implementations
 
