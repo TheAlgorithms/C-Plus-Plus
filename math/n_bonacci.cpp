f30cb377421947b761671b8d539a1984be9d6758
Swastika Gupta <64654203+Swastyy@users.noreply.github.com>
2021-07-22 07:29:28 +0530

    Update math/n_bonacci.cpp
    Co-authored-by: David Leal <halfpacho@gmail.com>

--- math/n_bonacci.cpp
+++ math/n_bonacci.cpp
@@ -30,7 +30,7 @@ namespace math {
  */
 namespace n_bonacci {
 /**
- * @brief Finds the N-Bonacci series for the `n` parameter value and `m` parameter terms.
+ * @brief Finds the N-Bonacci series for the `n` parameter value and `m` parameter terms
  * @param n is in the N-Bonacci series
  * @param m is the number of terms in the N-Bonacci sequence
  * @returns the n-bonacci sequence as vector array
