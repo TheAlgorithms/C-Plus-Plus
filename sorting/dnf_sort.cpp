6bd3a25390b572ec23e04052add245ae56d6f53c
Sujal Gupta <sujalgupta6100@gmail.com>
2021-08-13 13:27:57 +0530

    remove extra empty lines

--- sorting/dnf_sort.cpp
+++ sorting/dnf_sort.cpp
@@ -44,12 +44,8 @@ void printArray(int arr[], int arr_size) {
 int main() {
     int arr[] = {0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1};
     int n = sizeof(arr) / sizeof(arr[0]);
-
     sort012(arr, n);
-
     std::cout << "array after segregation ";
-
     printArray(arr, n);
-
     return 0;
 }
