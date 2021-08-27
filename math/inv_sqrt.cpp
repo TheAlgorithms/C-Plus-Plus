ed4ecad4c65c58ceac0d38aa28198e7e7e5886cf
Bensuperpc <bensuperpc@gmail.com>
2021-08-27 18:46:19 +0200

    Update math/inv_sqrt.cpp
    Update comment
    
    Co-authored-by: David Leal <halfpacho@gmail.com>

--- math/inv_sqrt.cpp
+++ math/inv_sqrt.cpp
@@ -17,7 +17,7 @@
  * This is the function that calculate Fast inverse square root.
  * The following code is the fast inverse square root implementation from Quake
  * III Arena (Adapted for C++)
- * More info: https://en.wikipedia.org/wiki/Fast_inverse_square_root
+ * More information can be found at [Wikipedia](https://en.wikipedia.org/wiki/Fast_inverse_square_root)
  * @tparam T floating type
  * @tparam iterations inverse square root, the greater the number of iterations,
  * the more exact the result will be (1 or 2).
