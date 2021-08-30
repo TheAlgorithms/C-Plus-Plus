83649126ffffd6ae4eda972ba20a23648eb3b21c
Bensuperpc <bensuperpc@gmail.com>
2021-08-30 19:01:32 +0200

    Add tests and improve comment
    Add tests and improve comment
    
    Signed-off-by: Bensuperpc <bensuperpc@gmail.com>

--- math/inv_sqrt.cpp
+++ math/inv_sqrt.cpp
@@ -1,15 +1,17 @@
 /**
  * @file
- * @brief Implementation of [Fast Inverse Square
+ * @brief Implementation of [the inverse square root
  * Root](https://medium.com/hard-mode/the-legendary-fast-inverse-square-root-e51fee3b49d9).
  * @details
  * Two implementation to calculate inverse inverse root,
- * from Quake III Arena (C++ version) and with a standard library (`cmath`)
+ * from Quake III Arena (C++ version) and with a standard library (`cmath`).
+ * This algorithm is used to calculate shadows in Quake III Arena.
  */
 
-#include <cmath>        /// for `std::sqrt`
-#include <iostream>     /// for IO operations
-#include <type_traits>  /// for `static_assert`
+#include <cassert>   /// for assert
+#include <cmath>     /// for `std::sqrt`
+#include <iostream>  /// for IO operations
+#include <limits>    /// for numeric_limits
 
 /**
  * @brief This is the function that calculates the fast inverse square root.
@@ -48,7 +50,7 @@ inline T Fast_InvSqrt(T x) {
  * [LinkedIn](https://www.linkedin.com/pulse/fast-inverse-square-root-still-armin-kassemi-langroodi)
  * @tparam T floating type
  * @param x value to calculate
- * @return T return inverse square root
+ * @return the inverse square root
  */
 template <typename T>
 T Standard_InvSqrt(T number) {
@@ -57,10 +59,30 @@ T Standard_InvSqrt(T number) {
 }
 
 /**
+ * @brief Self-test implementations
+ * @returns void
+ */
+static void test() {
+    const float epsilon = 1e-3f;
+
+    /* Tests with multiple values */
+    assert(std::fabs(Standard_InvSqrt<float>(100.0f) - 0.0998449f) < epsilon);
+    assert(std::fabs(Standard_InvSqrt<double>(36.0f) - 0.166667f) < epsilon);
+    assert(std::fabs(Standard_InvSqrt<double>(12.0f) - 0.288423f) < epsilon);
+    assert(std::fabs(Standard_InvSqrt<double>(5.0f) - 0.447141f) < epsilon);
+
+    assert(std::fabs(Fast_InvSqrt<float>(100.0f) - 0.0998449f) < epsilon);
+    assert(std::fabs(Fast_InvSqrt<double>(36.0f) - 0.166667f) < epsilon);
+    assert(std::fabs(Fast_InvSqrt<double>(12.0f) - 0.288423) < epsilon);
+    assert(std::fabs(Fast_InvSqrt<double>(5.0f) - 0.447141) < epsilon);
+}
+
+/**
  * @brief Main function
  * @returns 0 on exit
  */
 int main() {
+    test();
     std::cout << "The Fast inverse square root of 36 is: "
               << Fast_InvSqrt<float, 1>(36.0f) << std::endl;
     std::cout << "The Fast inverse square root of 36 is: "
