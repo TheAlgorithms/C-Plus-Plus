c6501de47c16af26f8a475e441a2f198476f6f28
Uttam kumar <72701081+helper-uttam@users.noreply.github.com>
2021-09-05 18:22:06 +0530

    Update swap.cpp

--- bit_manipulation/swap.cpp
+++ bit_manipulation/swap.cpp
@@ -3,8 +3,8 @@
  * @brief [swaping two numbers using XOR bitwise]
  * @details
  * Given two numbers as input. The goal is to swap those numbers.
- * What is swaping? 
- * Swaping means exchanging the content.
+ * What is swapping? 
+ * Swapping means exchanging the content.
  * Suppose we have two buckets 'A' and 'B' one has 5 apples and other has 2 apples.
  * Then after performing swap bucket 'A' will have 2apples and bucket 
  * 'B' will have 5.
@@ -12,27 +12,45 @@
  */
  
 #include <iostream>   // for IO operations
+
 using namespace std;
 
+
+/*function to swap two values*/
 void swap_XOR(int *first, int *second){   //passing pointers
       // swaping code
-    *first = *first ^ *second;
-    *second = *first ^ *second;
-    *first = *first ^ *second;
+    *first = *first ^ *second;   //doing XOR of first and second and assigning to first
+    *second = *first ^ *second;  //doing XOR of first and second and assigning to second
+    *first = *first ^ *second;   //doing XOR of first and second and assigning to first
 }
+/*
+As we are using call by referance, so we don't need to return anything after swapping 
+because we are changing the values at the address of orginial variables which will led 
+to change of the values in the original variables inself.
+*/
 
+
+/**
+ * @brief Main function
+ * @returns 0 on exit
+ */
 int main()
 {
-    int a = 20;
-    int b = 30;
+    int a = 20; //initializing first variable named "a" with 20
+    int b = 30; //initializing second variable named "b" with 30
     int *ptr1 = &a;  //pointer pointing to first variable
     int *ptr2 = &b; //pointer pointing to second variable
   
-  cout<<"The Value of A before swaping is = "<<a<<"\nThe Value of B before swaing is = "<<b;
+ //Printing Values of A and B before swapping.
+  cout<<"The Value of A before swaping is = "<<a<<"\n";
+  cout<<"The Value of B before swaing is = "<<b<<"\n;
   
-    swap_XOR(ptr1,ptr2); //passing both pointers
-  /* here we are passing pointers which is also called pass by reference in which we 
+    swap_XOR(ptr1,ptr2);    /* here we are passing two pointers which is also known as pass by reference in which we 
   pass address of variable instead of the value*/
-    cout<<"\n\nThe Value of A after swaping is = "<<a<<"\nThe Value of B after swaing is = "<<b;
-    return 0;
+ 
+ //Printing Values of A and B after swapping.
+   cout<<"The Value of A after swaping is = "<<a<<"\n";
+   cout<<"The Value of B after swaing is = "<<b<<"\n;
+    
+    return 0; 
 }
