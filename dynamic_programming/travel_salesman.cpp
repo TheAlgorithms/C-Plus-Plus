84dcb6065ad2c51a72e574b8ea7b03e4478eb1f9
anshul-sharma-2002 <anshulpuransharma@gmail.com>
2021-09-07 10:28:00 +0530

    feat: Added Travel Salesman Problem

--- dynamic_programming/travel_salesman.cpp
+++ dynamic_programming/travel_salesman.cpp
@@ -1,11 +1,14 @@
-//Travelling Salesman Problem using Dynamic Approach
-//Time Complexity : O(n^2 * 2^n)
-
-/*
-    Travelling Salesman Problem (TSP) : Given a set of cities and distances between every pair of cities, 
-    the problem is to find the shortest possible route that visits every city exactly once and returns to 
-    the starting point.
-*/
+/**
+ * @file
+ * @brief Implementation of [Travel Salesman Problem]
+ * with dynamic approach
+ * @details
+ * Travelling Salesman Problem (TSP) : Given a set of cities and distances between every pair of cities, 
+ * the problem is to find the shortest possible route that visits every city exactly once and returns to 
+ * the starting point.
+ * Time Complexity : O(n^2 * 2^n)
+ * @author [Anshul Sharma](https://github.com/anshul-sharma-2002)
+ */
 
 #include<iostream>
 using namespace std;
@@ -26,7 +29,7 @@ int completed_visit = (1<<n) -1;
 int DP[16][4];
 
 
-int  TSP(int mark,int position){
+int travel_salesman(int mark,int position){
 
     if(mark==completed_visit){      // Initially checking whether all the places are visited or not
         return distan[position][0];
@@ -43,7 +46,7 @@ int  TSP(int mark,int position){
     //minimum shortest path
     for(int city=0;city<n;city++){
         if((mark&(1<<city))==0){
-            int newAnswer = distan[position][city] + TSP( mark|(1<<city),city);
+            int newAnswer = distan[position][city] + travel_salesman( mark|(1<<city),city);
             answer = min(answer, newAnswer);
         }
     }
@@ -59,7 +62,8 @@ int main(){
             DP[i][j] = -1;
         }
     }
-    cout<<"Minimum Distance Travelled : "<< TSP(1,0) << endl;
+    cout<<"Minimum Distance Travelled : "<< travel_salesman(1,0) << endl;
 
     return 0;
 }
+
