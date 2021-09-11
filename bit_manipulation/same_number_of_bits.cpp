72f66a5b9818410974a9a91b3ab745edaafe1aae
Kunal Kumar <61112064+kunalkumar37@users.noreply.github.com>
2021-09-11 21:07:56 +0530

    Update bit_manipulation/same_number_of_bits.cpp
    Co-authored-by: David Leal <halfpacho@gmail.com>

--- bit_manipulation/same_number_of_bits.cpp
+++ bit_manipulation/same_number_of_bits.cpp
@@ -22,7 +22,7 @@ namespace bit_manipulation {
 /**
  * @namespace same_number_of_bits
  * @brief Functions for the [Next higher number with the same number of set
- * bits](https://www.geeksforgeeks.org/next-higher-number-with-same-number-of-set-bits/)
+ * bits](https://www.geeksforgeeks.org/next-higher-number-with-same-number-of-set-bits/) implementation
  * implementation
  */
 namespace same_number_of_bits {
