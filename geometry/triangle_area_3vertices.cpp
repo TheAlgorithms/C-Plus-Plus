fe9b01e98fce5fa65a2911316cb908ae9b7baecd
Mithun Balram <54732254+Mithun691@users.noreply.github.com>
2021-10-01 15:07:23 +0530

    Update triangle_area_3vertices.cpp
    Added few comments, examples & the wiki link

--- geometry/triangle_area_3vertices.cpp
+++ geometry/triangle_area_3vertices.cpp
@@ -1,4 +1,5 @@
 // Compute area of triangle given it's vertices
+// https://en.wikipedia.org/wiki/Heron%27s_formula : Wiki link for reference
 
 #include<iostream>
 #include<cmath>
@@ -6,6 +7,10 @@
 
 double eps = 1e-9;
 
+/*
+@ struct Point
+A Point object corresponds to (x,y) in 2D cartesian co-ordinates
+*/
 struct Point{
     // Data members: x, y co-ordinates of the point
     // Member functions: dist (To find distance to some other Point object)
@@ -17,6 +22,7 @@ struct Point{
     }
     Point(double x1,double y1): x(x1),y(y1){};
     double dist(Point a){
+        // The L2 distance with another Point a
         double delta_x = x-a.x;
         double delta_y = y-a.y;
         return sqrt(delta_x*delta_x+delta_y*delta_y);
@@ -34,6 +40,11 @@ double heron_area(Point a,Point b,Point c){
 }
 
 int main(){
+    /*
+    Examples
+    For (0,0),(2,0),(0,2) the area of triangle must be 2 (0.5*b*h = 0.5*2*2 = 2)
+    For (0,0),(1,1),(2,2) the area of triangle is 0 (as points are collinear)
+    */
     std::cout<<std::setprecision(3)<<'\n';
     Point a,b,c;
     std::cout<<heron_area(a,b,c)<<'\n';
