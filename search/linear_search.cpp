d9457e2ac6de0eef4caab834f447fa32e56748ce
Ritika Mukherjee <40390051+ritikaa17@users.noreply.github.com>
2021-10-02 18:06:13 +0530

    Update linear_search.cpp
    Update readability of code

--- search/linear_search.cpp
+++ search/linear_search.cpp
@@ -6,20 +6,23 @@
 #include <iostream>
 
 /**
- * Algorithm implementation
+ * \brief [Algorithm implementation for linear search]
  * \param [in] array array to search in
  * \param [in] size length of array
  * \param [in] key key value to search for
  * \returns index where the key-value occurs in the array
  * \returns -1 if key-value not found
  */
-int LinearSearch(int *array, int size, int key) {
-    for (int i = 0; i < size; ++i) {
+int LinearSearch(int *array, int size, int key) 
+{
+    for (int i = 0; i < size; ++i) 
+    {
         if (array[i] == key) {
             return i;
         }
     }
 
+    /* We reach here only in case element is not present in array, return an invalid entry in that case*/
     return -1;
 }
 
@@ -32,7 +35,7 @@ int main() {
     int *array = new int[size];
     int key;
 
-    // Input array
+    /*User gets to input array elements*/
     std::cout << "\nEnter the Array of " << size << " numbers : ";
     for (int i = 0; i < size; i++) {
         std::cin >> array[i];
@@ -42,10 +45,13 @@ int main() {
     std::cin >> key;
 
     int index = LinearSearch(array, size, key);
-    if (index != -1) {
+    if (index != -1)
+    {
         std::cout << "\nNumber found at index : " << index;
-    } else {
-        std::cout << "\nNot found";
+    } 
+    else 
+    {
+        std::cout << "\nArray element Not found!";
     }
 
     delete[] array;
