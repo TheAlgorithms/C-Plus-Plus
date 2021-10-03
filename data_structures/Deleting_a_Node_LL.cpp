682575c9fcffca9aedaf4d75a8995fb2ce26886d
Sahastracookcoder1 <sahastrakishore2001@gmail.com>
2021-10-03 13:30:21 +0530

    Added Code for Deleting a node in Linked List

---
+++ data_structures/Deleting_a_Node_LL.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// A linked list node
+class Node{
+public:
+    int data;
+    Node* next;
+};
+ 
+// Given a reference (pointer to pointer)
+// to the head of a list and an int,
+// inserts a new node on the front of the
+// list.
+void push(Node** head_ref, int new_data)
+{
+    Node* new_node = new Node();
+    new_node->data = new_data;
+    new_node->next = (*head_ref);
+    (*head_ref) = new_node;
+}
+ 
+// Given a reference (pointer to pointer)
+// to the head of a list and a key, deletes
+// the first occurrence of key in linked list
+void deleteNode(Node** head_ref, int key)
+{
+     
+    // Store head node
+    Node* temp = *head_ref;
+    Node* prev = NULL;
+     
+    // If head node itself holds
+    // the key to be deleted
+    if (temp != NULL && temp->data == key)
+    {
+        *head_ref = temp->next; // Changed head
+        delete temp;            // free old head
+        return;
+    }
+ 
+    // Else Search for the key to be deleted,
+    // keep track of the previous node as we
+    // need to change 'prev->next' */
+      else
+    {
+    while (temp != NULL && temp->data != key)
+    {
+        prev = temp;
+        temp = temp->next;
+    }
+ 
+    // If key was not present in linked list
+    if (temp == NULL)
+        return;
+ 
+    // Unlink the node from linked list
+    prev->next = temp->next;
+ 
+    // Free memory
+    delete temp;
+    }
+}
+ 
+// This function prints contents of
+// linked list starting from the
+// given node
+void printList(Node* node)
+{
+    while (node != NULL)
+    {
+        cout << node->data << " ";
+        node = node->next;
+    }
+}
+ 
+// Driver code
+int main()
+{
+     
+    // Start with the empty list
+    Node* head = NULL;
+ 
+    // Add elements in linked list
+    push(&head, 7);
+    push(&head, 1);
+    push(&head, 3);
+    push(&head, 2);
+ 
+    puts("Created Linked List: ");
+    printList(head);
+ 
+    deleteNode(&head, 1);
+    puts("\nLinked List after Deletion of 1: ");
+     
+    printList(head);
+     
+    return 0;
+}
