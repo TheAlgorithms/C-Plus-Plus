7679843aa58027b9097aef28916bfbb37f1003e2
Pranjal Borkar <71664360+DedAvocado@users.noreply.github.com>
2021-10-07 01:08:52 +0530

    Create transpose_matrix.cpp

---
+++ math/transpose_matrix.cpp
@@ -0,0 +1,51 @@
+/**
+ * @file
+ * @brief A simple C++ program to find the transpose of a matrix
+ */
+
+#include<iostream>
+#include<conio.h>
+
+/**
+ * @namespace
+ * @brief Mathematical algorithms
+ */
+using namespace std;
+
+/**
+ * @brief Main function
+ * @returns 0 on exit
+ */
+int main() {
+    
+  int a[3][3],t;
+  for(int i=0;i<3;i++)
+  {
+    for(int j=0;j<3;j++)
+    {
+      cout<<"\n Enter values";
+      cin>>a[i][j];
+    }
+  }
+  
+  for(int k=0;k<3;k++)
+  {
+    for(int e=0;e<=k;e++) 
+    {
+      if(e==k)
+        cout<<" ";
+      else 
+        cout<<" "<<a[k][e];
+    }
+    for(int l=k;l<3;l++)
+    {
+      t=a[k][l];
+      a[k][l]=a[l][k];
+      a[l][k]=t;
+      cout<<a[k][l]<<" ";
+    }     
+    cout<<'\n';
+  }
+  
+  return 0;
+}
