3d73c5f10be217b3efa95eb46a466603ffb1f683
Rita Cruz <rcchcz@gmail.com>
2021-10-06 20:33:52 -0300

    Update sorting/radix_with_negatives.cpp
    Co-authored-by: David Leal <halfpacho@gmail.com>

--- sorting/radix_with_negatives.cpp
+++ sorting/radix_with_negatives.cpp
@@ -11,7 +11,7 @@
 #include <utility>
 #include <iomanip>
 #include <iostream>
-/*!
+/**
  * This function implements the Radix Sorting Algorithm based on the **less significant digit** (LSD).
  * @note This is a non-comparison sort algorithm. This version works for any kind of integers.
  * @param v Vector to be sorted.
