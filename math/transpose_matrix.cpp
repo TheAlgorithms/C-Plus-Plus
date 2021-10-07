79104a00a39b381001a22e43cc00cc37d03eb7e0
Pranjal Borkar <71664360+DedAvocado@users.noreply.github.com>
2021-10-07 22:17:27 +0530

    Update transpose_matrix.cpp

--- math/transpose_matrix.cpp
+++ math/transpose_matrix.cpp
@@ -3,7 +3,8 @@
  * @brief A simple C++ program to find the transpose of a matrix
  */
 
-#include<iostream>
+#include <cassert> /// for assert
+#include <iostream>
 
 /**
  * @namespace
@@ -15,36 +16,36 @@ using namespace std;
  * @brief Main function
  * @returns 0 on exit
  */
-int main() {
-    
-  int a[3][3],t;
-  for(int i=0;i<3;i++)
-  {
-    for(int j=0;j<3;j++)
+int main()
+{
+    int a[3][3], t;
+    for (int i = 0; i < 3; i++)
     {
-      cout<<"\n Enter values";
-      cin>>a[i][j];
+        for (int j = 0; j < 3; j++)
+        {
+            cout << "\n Enter values";
+            cin >> a[i][j];
+        }
     }
-  }
-  
-  for(int k=0;k<3;k++)
-  {
-    for(int e=0;e<=k;e++) 
+
+    for (int k = 0; k < 3; k++)
     {
-      if(e==k)
-        cout<<" ";
-      else 
-        cout<<" "<<a[k][e];
+        for (int e = 0; e <= k; e++)
+        {
+            if (e == k)
+                cout << " ";
+            else
+                cout << " " << a[k][e];
+        }
+        for (int l = k; l < 3; l++)
+        {
+            t = a[k][l];
+            a[k][l] = a[l][k];
+            a[l][k] = t;
+            cout << a[k][l] << " ";
+        }
+        cout << '\n';
     }
-    for(int l=k;l<3;l++)
-    {
-      t=a[k][l];
-      a[k][l]=a[l][k];
-      a[l][k]=t;
-      cout<<a[k][l]<<" ";
-    }     
-    cout<<'\n';
-  }
-  
-  return 0;
+
+    return 0;
 }
