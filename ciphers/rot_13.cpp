65e3a4292021569497fd83815d87464916a8af0e
pond-e <56418890+pond-e@users.noreply.github.com>
2021-10-09 14:16:33 +0900

    Update ciphers/rot_13.cpp
    Co-authored-by: David Leal <halfpacho@gmail.com>

--- ciphers/rot_13.cpp
+++ ciphers/rot_13.cpp
@@ -1,6 +1,6 @@
 /**
  * @brief [ROT13 Encoding and
- * Decoding](https://en.wikipedia.org/wiki/ROT13)
+ * Decoding](https://en.wikipedia.org/wiki/ROT13) implementation
  * ROT13 ("rotate by 13 places", sometimes hyphenated ROT-13) is a simple letter
  * substitution cipher that replaces a letter with the 13th letter after it in
  * the alphabet. ROT13 is a special case of the Caesar cipher which was
