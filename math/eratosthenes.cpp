b33f7b68abe0f3c733b283a0881ef8fd96e41ce9
Keval Kapdee <keval@thechubbypanda.net>
2021-10-12 21:58:37 +0100

    Comment sieve

--- math/eratosthenes.cpp
+++ math/eratosthenes.cpp
@@ -19,7 +19,10 @@
  * @returns 0 on exit
  */
 int main(int argc, char *argv[]) {
+	// The largest prime we will check for
     auto end = 10000;
+
+	// Store a boolean for every number wich states if that index is prime or not
 	auto primes = std::vector<bool>(end, true);
 
 	auto start = std::chrono::high_resolution_clock::now();
@@ -27,17 +30,20 @@ int main(int argc, char *argv[]) {
 	primes[0] = false;
 	primes[1] = false;
 
+	// The sieve sets values to false as they are found not prime
 	for (int n = 2; n < end; n++) {
 		for (int multiple = n << 1; multiple < end; multiple += n) {
 			primes[multiple] = false;
 		}
 	}
 
+	// Time difference calculation
 	auto time = std::chrono::duration_cast
 	        <std::chrono::duration<double, std::ratio<1>>>
 	        (std::chrono::high_resolution_clock::now() - start)
 	        .count();
 
+	// Print the primes if we see that "print" was passed as an arg
 	if (argc > 1 && argv[1] == std::string("print")) {
 		for (int i = 0; i < primes.size(); i++) {
 			if (primes[i]) std::cout << i << std::endl;
