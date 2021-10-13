3ba8a009d9e4e00b05d9dcccf512d487ef0a95b0
Anudeep Vattikonda <68660304+anudeep-vattikonda@users.noreply.github.com>
2021-10-13 11:20:31 +0530

    Update prime_factorization.cpp

--- math/prime_factorization.cpp
+++ math/prime_factorization.cpp
@@ -23,25 +23,24 @@ std::vector<std::pair<int, int> > factors;
 void prime_factorization(int num) {
     int number = num;
     int count = 0;
-    while(number%2 == 0){  // first, finding the power of 2 in its prime factorization.
-        count++;
-        number/=2;
+    while (number % 2 == 0) {   // finding power of 2 in prime factorization
+        count ++ ;
+        number = number / 2;
     }
-    factors.push_back(std::make_pair(2, count));
-    for(int i = 3; i*i <= number; i = i + 2)  // at this point, only odd factors are possible. we need not find primes and divide number only with primes 
+    factors.push_back ( std::make_pair ( 2, count) ) ;
+    for ( int i = 3 ; i * i <= number ; i = i + 2)   // at this point, only odd primes will be the factors                             
     {
         count = 0;
-        while(number % i == 0)
+        while ( number % i == 0 ) 
         {
-            count++;
-            number/=i;
+            count ++ ;
+            number = number / i ;
         }
-        if(count > 0)
-            factors.push_back(std::make_pair(i, count));
+        if ( count > 0 )
+            factors.push_back ( std::make_pair ( i , count ) ) ;
     }
-    if(number > 2){  // if the number itself is a prime number, we should include it.
-    	factors.push_back(std::make_pair(number, 1));
-	}
+    if ( number > 2)
+    	factors.push_back ( std::make_pair ( number , 1 ) );
 }
 
 /** Main program */
