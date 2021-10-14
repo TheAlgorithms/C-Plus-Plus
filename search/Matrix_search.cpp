54c6067d889dab45d4f171f4be09b0e9109c5bb4
Khushi Maheshwari <khushimaheshwari112@gmail.com>
2021-10-14 22:22:18 +0530

    Update Matrix_search.cpp

--- search/Matrix_search.cpp
+++ search/Matrix_search.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include<algorithm>
 using namespace std;
+
 int main() {
     int n,m;
     cin>>n>>m;
     
+    // input array
     int  arr[n][m];
     for(int i=0;i<n;i++)
      {
@@ -12,13 +14,14 @@ int main() {
          cin>>arr[i][j];
 
      }
+    // input key to be searched
       int key;
       cin>>key;
 
   int a,b;
-  //fr matching r
+  //for matching rows
   a=0;
-  //fr clmns
+  //for columns
   b=m-1;
   while (a<n)
   {
