2b16a8379c7154fe00f388028d9a4a06c5ea14ac
Anudeep Vattikonda <68660304+anudeep-vattikonda@users.noreply.github.com>
2021-10-14 10:37:55 +0530

    Update prime_factorization.cpp
    this approach requires no extra memory and efficient in terms of time : O(sqrt(n))

--- math/prime_factorization.cpp
+++ math/prime_factorization.cpp
@@ -8,40 +8,6 @@
 #include <vector>
 #include<utility>
 
-/** Declaring variables for maintaing prime numbers and to check whether a
- * number is prime or not
- */
-bool isprime[1000006];
-
-/** list of prime numbers */
-std::vector<int> prime_numbers;
-
-/** list of prime factor-pairs */
-std::vector<std::pair<int, int> > factors;
-
-/** Prime factorization of a number */
-void prime_factorization(int num) {
-    int number = num;
-    int count = 0;
-    while (number % 2 == 0) {   // finding power of 2 in prime factorization
-        count ++ ;
-        number = number / 2;
-    }
-    factors.push_back ( std::make_pair ( 2, count) ) ;
-    for ( int i = 3 ; i * i <= number ; i = i + 2)   // at this point, only odd primes will be the factors                             
-    {
-        count = 0;
-        while ( number % i == 0 ) 
-        {
-            count ++ ;
-            number = number / i ;
-        }
-        if ( count > 0 )
-            factors.push_back ( std::make_pair ( i , count ) ) ;
-    }
-    if ( number > 2)
-    	factors.push_back ( std::make_pair ( number , 1 ) );
-}
 
 /** Main program */
 int main() {
@@ -50,12 +16,27 @@ int main() {
     std::cout << "Type in a number: ";
     std::cin >> num;
 
-    prime_factorization(num);
-
-    // Prime factors with their powers in the given number in new line
-    int size = factors.size();
-	for(int i=0; i< size; i++){
-		std::cout<<factors[i].first<<" "<<factors[i].second<<"\n";
-	}
+    int count  = 0 ;    // for storing the power of a prime in its prime factorization
+    while( num % 2 == 0)
+    {
+	    count ++;
+	    num /= 2;
+    }
+    std::cout << 2 << " " << count << "\n";
+    for( int i = 3; i*i <= num ; i = i + 2)  // at this point, all the prime factors will be odd
+    {
+	    count = 0;
+	    while( num % i == 0){
+		    count ++;
+		    num /= i;
+	    }
+	    if(count > 0)
+	    {
+		    std::cout << i << " " << count << "\n";    // output the prime and its power without storing them. No extra memory needed
+	    }
+    }
+    if( num > 2){
+	    std::cout << num << " " << 1 << "\n";  // check if number is a prime, if yes, output the number itself. This handles if the given number itself is a prime
+    }
     return 0;
 }
