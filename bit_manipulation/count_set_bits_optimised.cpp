9ba51927e9a3cf43ad6d515ff025fc8d79f1f175
KillerAV <abhishekvanjani@gmail.com>
2021-10-16 01:02:18 +0530

    Update count_set_bits_optimised.cpp
    - Updating documentation

--- bit_manipulation/count_set_bits_optimised.cpp
+++ bit_manipulation/count_set_bits_optimised.cpp
@@ -1,9 +1,22 @@
-#include <iostream>
+/**
+ * @file
+ * @brief Optimised way of counting the number of set bits in a number.
+ * @details
+ * There are multiple ways of counting the number of set bits. One naive approach is to 
+ * iterate over all the bits and check if the current bit is set or not.
+ * An optimised way to solve the problem is to iterate over ONLY the set bits instead of all the bits.
+ * One way to do so is to make use of the mathematical property of BITWISE AND operator.
+ * When we do a bitwise AND of a number with it's predecessor, then the right most set bit of the number is unset.
+ * We can use this property to find the number of set bits in a number in an optimised manner.
+ * @author abhishek vanjani(https://github.com/KillerAV)
+ */
+
+#include <iostream> /// For input output
+#include <cassert> /// For asserts
 
 /**
- * Optimised way of counting the number of set bits in a number.
- * The function iterates over ONLY the set bits.
- * Hence, the complexity of the function is exactly equal to the answer itself.
+ * Function to count number of set bits in a number.
+ * @param N: The number whose set bit count is needed to be calculated.
  */ 
 int countSetBitsOptimised(int N) {
     int ans=0;
