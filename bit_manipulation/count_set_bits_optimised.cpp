f7b9d33415779c5109cea04fae925375b2a18496
KillerAV <abhishekvanjani@gmail.com>
2021-10-16 00:16:09 +0530

    Update count_set_bits_optimised.cpp
    - Adding tests.
    - Lint fixes.

--- bit_manipulation/count_set_bits_optimised.cpp
+++ bit_manipulation/count_set_bits_optimised.cpp
@@ -1,22 +1,31 @@
-#include <bits/stdc++.h>
-#define mod 1000000007
-#define ll long long
-using namespace std;
+#include <iostream>
 
+/**
+ * Optimised way of counting the number of set bits in a number.
+ * The function iterates over ONLY the set bits.
+ * Hence, the complexity of the function is exactly equal to the answer itself.
+ */ 
 int countSetBitsOptimised(int N) {
     int ans=0;
+
     while(N) {
+        // This unsets the right most set bit.
         N=N&(N-1);
         ans++;
     }
+
     return ans;
 }
 int main() 
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    for(int i=1;i<=10;i++)
-        cout<<"Number of set bits in "<<i<<" is: "<<countSetBitsOptimised(i)<<endl;    
+    assert(1==countSetBitsOptimised(1));
+    assert(1==countSetBitsOptimised(2));
+    assert(2==countSetBitsOptimised(3));
+    assert(1==countSetBitsOptimised(4));
+    assert(2==countSetBitsOptimised(5));
+    assert(2==countSetBitsOptimised(6));
+    assert(3==countSetBitsOptimised(7));
+    assert(1==countSetBitsOptimised(8));
+    assert(2==countSetBitsOptimised(9));
+    assert(2==countSetBitsOptimised(10));
 }
