d7290c960974d16438306081d0247c7f07dc29f3
KillerAV <abhishekvanjani@gmail.com>
2021-10-16 00:54:08 +0530

    Update map.cpp
    - Updating documentation

--- stl/map.cpp
+++ stl/map.cpp
@@ -1,37 +1,41 @@
-#include <iostream>
-#include <map>
-#include <cassert>
-
 /**
- * The following code makes use of Map STL.
- * Following features of map are depicted in the below code:
+ * @file
+ * @brief This code is a basic implementation for use of map STL in C++.
+ * @details
+ * The below code includes:
  * (a) Insertion in a map
  * (b) Finding an entry in a map
  * (c) Removing elements from a map
  * (d) Iterating over a map
+ * @author abhishek vanjani(https://github.com/KillerAV)
  */
+
+#include <iostream> /// For input output
+#include <map> /// For usage of map
+#include <cassert> /// For asserts
+
 int main() 
 {
     std::map<std::string,int> M;
 
-    //Insert in map
+    // Insert in map
     M["Paris"]=1;
     M["New York"]=2;
 
-    //Find an entry in a map #1
+    // Find an entry in a map #1
     assert(M.count("Paris"));
 
-    //Find an entry in a map #2
+    // Find an entry in a map #2
     auto it=M.find("Paris");
     if(it!=M.end()) {
         assert("Paris"==it->first);
         assert(1==it->second);
     }
 
-    //Remove from map
+    // Remove from map
     M.erase("Paris");
 
-    //Iterate over map
+    // Iterate over map
     for(std::pair<std::string,int> item : M) {
         std::cout<<item.first<<", "<<item.second<<std::endl;
     }
