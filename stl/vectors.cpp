bf5acf3c6e82af73431c8e3c6112c3a85ff78bed
KillerAV <abhishekvanjani@gmail.com>
2021-10-16 00:52:50 +0530

    Update vectors.cpp
    - Updating documentation

--- stl/vectors.cpp
+++ stl/vectors.cpp
@@ -1,29 +1,33 @@
-#include <iostream>
-#include <vector>
-#include <cassert>
-
 /**
- * This code is a basic implementation for use of vector STL in C++.
+ * @file
+ * @brief This code is a basic implementation for use of vector STL in C++.
+ * @details
  * The below code includes:
  * (a) Creation of vectors
  * (b) Traversing vectors
+ * @author abhishek vanjani(https://github.com/KillerAV)
  */
+
+#include <iostream> /// For input output
+#include <vector> /// For usage of vectors
+#include <cassert> /// For asserts
+
 int main() 
 {
     std::vector<int> arr;
 
-    //insert in a vector
+    // insert in a vector
     arr.push_back(1);
     arr.push_back(2);
 
-    //access vector elements
+    // access vector elements
     assert(1==arr[0]);
     assert(2==arr[1]);
 
-    //check size of vector
+    // check size of vector
     assert(2==arr.size());
     
-    //iterating over vector
+    // iterating over vector
     for(int i : arr) {
         std::cout<<i<<" ";
     }
