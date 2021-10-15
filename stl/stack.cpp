4d09fe80dda6be64d8336ec654957491d5e7c2e4
KillerAV <abhishekvanjani@gmail.com>
2021-10-16 00:57:22 +0530

    Update stack.cpp
    - Updating documentation

--- stl/stack.cpp
+++ stl/stack.cpp
@@ -1,29 +1,33 @@
-#include <iostream>
-#include <stack>
-#include <cassert>
-
 /**
- * The following code depicts the usage of C++ STL Stack.
- * The code covers following:
+ * @file
+ * @brief This code is a basic implementation for use of stack STL in C++.
+ * @details
+ * The below code includes:
  * (a) Pushing elements to STL Stack.
  * (b) Popping elements from STL Stack.
  * (c) Checking top element in the STL Stack.
+ * @author abhishek vanjani(https://github.com/KillerAV)
  */
+
+#include <iostream> /// For input output
+#include <stack> /// For usage of stack
+#include <cassert> /// For asserts
+
 int main() 
 {
     std::stack<int> S;
 
-    //Insert in stack
+    // Insert in stack
     for(int i=1;i<10;i++) {
         S.push(i);
     }
 
-    //Traversing from stack
+    // Traversing from stack
     while(!S.empty()){
         // Picking top element from the stack
         std::cout<<S.top()<<" ";
 
-        //Removing elements from the top of stack
+        // Removing elements from the top of stack
         S.pop();
     }
 }
