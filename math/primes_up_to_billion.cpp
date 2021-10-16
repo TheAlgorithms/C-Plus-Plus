dd3da9d2455bb6a5d264445b81fa5acaed099734
g1tman <32861951+g1tman@users.noreply.github.com>
2021-10-16 20:06:33 +0530

    Use bitset for faster runtime
    Use of bitset instead of char array improves runtime by 14%.

--- math/primes_up_to_billion.cpp
+++ math/primes_up_to_billion.cpp
@@ -3,31 +3,31 @@
  * @brief Compute prime numbers upto 1 billion
  * @see prime_numbers.cpp sieve_of_eratosthenes.cpp
  */
-#include <cstring>
 #include <iostream>
+#include <bitset>
 
-/** array to store the primes */
-char prime[100000000];
+/** bitset to store the primes */
+std::bitset<1000000000> prime;
 
 /** Perform Sieve algorithm */
-void Sieve(int64_t n) {
-    memset(prime, '1', sizeof(prime));  // intitize '1' to every index
-    prime[0] = '0';                     // 0 is not prime
-    prime[1] = '0';                     // 1 is not prime
+void Sieve(int64_t n) { 
+    prime[0] = 0;                     // 0 is not prime
+    prime[1] = 0;                     // 1 is not prime
     for (int64_t p = 2; p * p <= n; p++) {
-        if (prime[p] == '1') {
+        if (prime[p] == 1) {
             for (int64_t i = p * p; i <= n; i += p)
-                prime[i] = '0';  // set all multiples of p to false
+                prime[i] = 0;  // set all multiples of p to false
         }
     }
 }
 
 /** Main function */
 int main() {
+	prime.set();  // intitize '1' to every index
     Sieve(100000000);
     int64_t n;
     std::cin >> n;  // 10006187
-    if (prime[n] == '1')
+    if (prime[n])
         std::cout << "YES\n";
     else
         std::cout << "NO\n";
