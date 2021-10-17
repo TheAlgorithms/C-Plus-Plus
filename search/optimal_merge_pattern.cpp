8fcf846c8412bbec724aa0baf26745573f298b8c
Akshad Nayakwadi <73118552+Akshad7829@users.noreply.github.com>
2021-10-17 11:47:50 +0530

    Update optimal_merge_pattern.cpp

--- search/optimal_merge_pattern.cpp
+++ search/optimal_merge_pattern.cpp
@@ -1,3 +1,17 @@
+/**
+Node represents a file with a given size also given nodes are greater than 2 
+
+1. Add all the nodes in a priority queue (Min Heap).{node.weight = file size}
+2.Initialize count = 0 // variable to store file computations.
+3. Repeat while (size of priority Queue is greater than 1) 
+    1. create a new node
+    2. new node = pq.poll().weight+pq.poll().weight;//pq denotes priority queue, remove 1st smallest and 2nd smallest element and add their weights to get a new node
+    3. count += node.weight
+    4. add this new node to priority queue; 
+ 
+4. count is the final answer
+ **/
+
 #include<bits/stdc++.h>
 using namespace std;
 #define int long long
