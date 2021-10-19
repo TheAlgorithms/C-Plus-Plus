263db719a0bda027c9b6e6e116dc43a5f6c33d57
Focusucof <imthefirstfocus@gmail.com>
2021-10-19 11:24:11 -0400

    test: added 3rd test case

--- math/area.cpp
+++ math/area.cpp
@@ -96,35 +96,53 @@ T cylinder_surface_area(T radius, T height) {
 
 static void test() {
     // Testing I/O variables
-    int int_input;
+    int int_length;
+    int int_width;
     int int_expected;
     int int_area;
 
-    float float_input;
+    float float_length;
+    float float_width;
     float float_expected;
     float float_area;
 
 	// 1st test
     std::cout << "SQUARE AREA: " << std::endl << std::endl;
-    int_input = 5;
+
+    int_length = 5;
     int_expected = 25;
-    int_area = square_area(int_input);
-    std::cout << "Input: " << int_input << std::endl;
+    int_area = square_area(int_length);
+    std::cout << "Input Length: " << int_length << std::endl;
     std::cout << "Expected Output: " << int_expected << std::endl;
     std::cout << "Output: " << int_area << std::endl;
     assert(int_area == int_expected);
     std::cout << "TEST PASSED" << std::endl << std::endl;
 
     // 2nd test
-    float_input = 2.5;
+    float_length = 2.5;
     float_expected = 6.25;
-    float_area = square_area(float_input);
+    float_area = square_area(float_length);
 
-    std::cout << "Input: " << float_input << std::endl;
+    std::cout << "Input Length: " << float_length << std::endl;
     std::cout << "Expected Output: " << float_expected << std::endl;
     std::cout << "Output: " << float_area << std::endl;
     assert(float_area == float_expected);
-    std::cout << "TEST PASSED" << std::endl;
+    std::cout << "TEST PASSED" << std::endl << std::endl;
+
+    //3rd test
+    std::cout << "RECTANGLE AREA: " << std::endl << std::endl;
+
+    int_length = 4;
+    int_width = 7;
+    int_expected = 28;
+    int_area = rect_area(4, 7);
+
+    std::cout << "Input Length: " << int_length << std::endl;
+    std::cout << "Input Width: " << int_width << std::endl;
+    std::cout << "Expected Output: " << int_expected << std::endl;
+    std::cout << "Output: " << int_area << std::endl;
+    assert(int_area == int_expected);
+    std::cout << "TEST PASSED" << std::endl << std::endl;
 
 }
 
