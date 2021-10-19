0504270a1a7f9e8f3839cd6e1814f527d5a2c304
Alvin Philips <alvinphilips257@gmail.com>
2021-10-20 05:19:14 +0530

    Added test cases

--- operations_on_datastructures/union_of_2_arrays.cpp
+++ operations_on_datastructures/union_of_2_arrays.cpp
@@ -10,11 +10,10 @@
  * @author [Alvin](https://github.com/polarvoid)
  */
 
-#include <cassert>   /// For assert
-#include <iostream>  /// For IO operations
-#include <vector>    /// For std::vector
-
-using namespace std;
+#include <algorithm>  /// For std::sort
+#include <cassert>    /// For assert
+#include <iostream>   /// For IO operations
+#include <vector>     /// For std::vector
 
 /**
  * @namespace operations_on_datastructures
@@ -27,7 +26,7 @@ namespace operations_on_datastructures {
  * character.
  * @returns void
  */
-void print(std::vector<int32_t> array) {
+void print(std::vector<int32_t> &array) {
     for (int64_t i : array) {
         std::cout << i << " ";  /// Print each value in the array
     }
@@ -45,13 +44,15 @@ void print(std::vector<int32_t> array) {
  * @param second A std::vector of sorted integer values
  * @returns A std::vector of the union of the two arrays, in ascending order
  */
-std::vector<int32_t> get_union(std::vector<int32_t> first,
-                               std::vector<int32_t> second) {
+std::vector<int32_t> get_union(std::vector<int32_t> &first,
+                               std::vector<int32_t> &second) {
     std::vector<int32_t> res;         ///< Vector to hold the union
     size_t f_index = 0;               ///< Index for the first array
     size_t s_index = 0;               ///< Index for the second array
     size_t f_length = first.size();   ///< Length of first array
     size_t s_length = second.size();  ///< Length of second array
+    int32_t next = 0;  ///< Integer to store value of the next element
+
     while (f_index < f_length && s_index < s_length) {
         int32_t next;  ///< Integer to store value of the next element
         if (first[f_index] < second[s_index]) {
@@ -65,16 +66,22 @@ std::vector<int32_t> get_union(std::vector<int32_t> first,
             f_index++;              ///< Increment index of first array
             s_index++;              ///< Increment index of second array too
         }
-        if (next != res.back()) {
+        if ((res.size() == 0) || (next != res.back())) {
             res.push_back(next);  ///< Add the element if it is unique
         }
     }
     while (f_index < f_length) {
-        res.push_back(first[f_index]);  ///< Append remaining elements
+        next = first[f_index];  ///< Add remaining elements
+        if ((res.size() == 0) || (next != res.back())) {
+            res.push_back(next);  ///< Add the element if it is unique
+        }
         f_index++;
     }
     while (s_index < s_length) {
-        res.push_back(second[s_index]);  ///< Append remaining elements
+        next = second[s_index];  ///< Add remaining elements
+        if ((res.size() == 0) || (next != res.back())) {
+            res.push_back(next);  ///< Add the element if it is unique
+        }
         s_index++;
     }
     return res;
@@ -87,17 +94,113 @@ std::vector<int32_t> get_union(std::vector<int32_t> first,
  * @brief Testcases to check Reversal of Binary Tree.
  */
 namespace tests {
-using namespace operations_on_datastructures;
+using operations_on_datastructures::get_union;
+using operations_on_datastructures::print;
+/**
+ * @brief A Test to check an edge case (two empty arrays)
+ */
 void test1() {
+    std::cout << "TEST CASE 1\n";
+    std::cout << "Intialized a = {} b = {}\n";
+    std::cout << "Expected result: {}\n";
     std::vector<int32_t> a = {};
     std::vector<int32_t> b = {};
     std::vector<int32_t> result = get_union(a, b);
-    assert(result == a);
-    print(result);
+    assert(result == a);  ///< Check if result is empty
+    print(result);        ///< Should only print newline
+    std::cout << "TEST PASSED!\n\n";
+}
+/**
+ * @brief A Test to check an edge case (one empty array)
+ */
+void test2() {
+    std::cout << "TEST CASE 2\n";
+    std::cout << "Intialized a = {} b = {2, 3}\n";
+    std::cout << "Expected result: {2, 3}\n";
+    std::vector<int32_t> a = {};
+    std::vector<int32_t> b = {2, 3};
+    std::vector<int32_t> result = get_union(a, b);
+    assert(result == b);  ///< Check if result is equal to b
+    print(result);        ///< Should print 2 3
+    std::cout << "TEST PASSED!\n\n";
+}
+/**
+ * @brief A Test to check correct functionality with a simple test case
+ */
+void test3() {
+    std::cout << "TEST CASE 3\n";
+    std::cout << "Intialized a = {4, 6} b = {2, 3}\n";
+    std::cout << "Expected result: {2, 3, 4, 6}\n";
+    std::vector<int32_t> a = {4, 6};
+    std::vector<int32_t> b = {2, 3};
+    std::vector<int32_t> result = get_union(a, b);
+    std::vector<int32_t> expected = {2, 3, 4, 6};
+    assert(result == expected);  ///< Check if result is correct
+    print(result);               ///< Should print 2 3 4 6
+    std::cout << "TEST PASSED!\n\n";
+}
+/**
+ * @brief A Test to check correct functionality with duplicate values
+ */
+void test4() {
+    std::cout << "TEST CASE 4\n";
+    std::cout << "Intialized a = {4, 6, 6, 7} b = {2, 3, 4}\n";
+    std::cout << "Expected result: {2, 3, 4, 6, 7}\n";
+    std::vector<int32_t> a = {4, 6, 6, 7};
+    std::vector<int32_t> b = {2, 3, 4};
+    std::vector<int32_t> result = get_union(a, b);
+    std::vector<int32_t> expected = {2, 3, 4, 6, 7};
+    assert(result == expected);  ///< Check if result is correct
+    print(result);               ///< Should print 2 3 4 6 7
+    std::cout << "TEST PASSED!\n\n";
+}
+/**
+ * @brief A Test to check correct functionality with a harder test case
+ */
+void test5() {
+    std::cout << "TEST CASE 5\n";
+    std::cout << "Intialized a = {1, 4, 6, 7, 9} b = {2, 3, 5}\n";
+    std::cout << "Expected result: {1, 2, 3, 4, 5, 6, 7, 9}\n";
+    std::vector<int32_t> a = {1, 4, 6, 7, 9};
+    std::vector<int32_t> b = {2, 3, 5};
+    std::vector<int32_t> result = get_union(a, b);
+    std::vector<int32_t> expected = {1, 2, 3, 4, 5, 6, 7, 9};
+    assert(result == expected);  ///< Check if result is correct
+    print(result);               ///< Should print 1 2 3 4 5 6 7 9
+    std::cout << "TEST PASSED!\n\n";
+}
+/**
+ * @brief A Test to check correct functionality with an array sorted using
+ * std::sort
+ */
+void test6() {
+    std::cout << "TEST CASE 6\n";
+    std::cout << "Intialized a = {1, 3, 3, 2, 5, 9, 4, 3, 2} ";
+    std::cout << "b = {11, 3, 7, 8, 6}\n";
+    std::cout << "Expected result: {1, 2, 3, 4, 5, 6, 7, 8, 9, 11}\n";
+    std::vector<int32_t> a = {1, 3, 3, 2, 5, 9, 4, 3, 2};
+    std::vector<int32_t> b = {11, 3, 7, 8, 6};
+    std::sort(a.begin(), a.end());  ///< Sort vector a
+    std::sort(b.begin(), b.end());  ///< Sort vector b
+    std::vector<int32_t> result = get_union(a, b);
+    std::vector<int32_t> expected = {1, 2, 3, 4, 5, 6, 7, 8, 9, 11};
+    assert(result == expected);  ///< Check if result is correct
+    print(result);               ///< Should print 1 2 3 4 5 6 7 8 9 11
+    std::cout << "TEST PASSED!\n\n";
 }
 }  // namespace tests
 
-static void test() { tests::test1(); }
+/**
+ * @brief Function to test the correctness of get_union() function
+ */
+static void test() {
+    tests::test1();
+    tests::test2();
+    tests::test3();
+    tests::test4();
+    tests::test5();
+    tests::test6();
+}
 
 /**
  * @brief main function
