b4e763b0e1866431b99dafa7437bd5a63bb374ff
vicnetto <victorhmnetto@gmail.com>
2021-10-27 10:33:43 -0300

    fix: changing the format of the file with clang-format

--- sorting/sleep_sort.cpp
+++ sorting/sleep_sort.cpp
@@ -1,28 +1,29 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include <array>
 #include <cstdio>
 #include <cstdlib>
 #include <iostream>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <array>
-#include <unistd.h>
 
 /**
- * Basic C++ function to use sleep sort. Basically, this function sort a array in a BIG O of O(max(input)+n),
- * using the sleep function. This type of sort is has only an educational purpose, because it is extremely
- * inefficient.
-**/
+ * Basic C++ function to use sleep sort. Basically, this function sort a array
+ *in a BIG O of O(max(input)+n), using the sleep function. This type of sort is
+ *has only an educational purpose, because it is extremely inefficient.
+ **/
 int main() {
-	// Instantiates a 5-position array.
-	std::array<int, 5> a = {10, 5, 2, 7, 6};
-	int c = sizeof(a) / sizeof(a[0]);
+    // Instantiates a 5-position array.
+    std::array<int, 5> a = {10, 5, 2, 7, 6};
+    int c = sizeof(a) / sizeof(a[0]);
 
-	// Creates a new process using fork and then makes the program sleep.
-	while (--c >= 1 && !fork());
-	sleep(c = a[c]);
+    // Creates a new process using fork and then makes the program sleep.
+    while (--c >= 1 && !fork());
+    sleep(c = a[c]);
 
-	// When the sleep process ends, it prints to the user the current number.  
-	std::cout << c << std::endl;
-	wait(nullptr);
+    // When the sleep process ends, it prints to the user the current number.
+    std::cout << c << std::endl;
+    wait(nullptr);
 
-	return 0;
+    return 0;
 }
