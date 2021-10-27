7af9b8fd58fd613368391e4b733ee60758148ca9
ggkogkou <76820848+ggkogkou@users.noreply.github.com>
2021-10-28 00:08:03 +0300

    Update midpoint_integral_method.cpp

--- numerical_methods/midpoint_integral_method.cpp
+++ numerical_methods/midpoint_integral_method.cpp
@@ -1,6 +1,6 @@
-/*!
+/**
  * @file
- * \brief A numerical method for easy [approximation of
+ * @brief A numerical method for easy [approximation of
  * integrals](https://en.wikipedia.org/wiki/Midpoint_method) \details The idea
  * is to split the interval into N of intervals and use as interpolation points
  * the xi for which it applies that xi = x0 + i*h, where h is a step defined as
