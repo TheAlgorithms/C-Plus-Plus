4b9d4719eb47c74b399f85e270aaaaca70b5947e
diksha137 <68290471+diksha137@users.noreply.github.com>
2021-10-31 21:15:52 +0530

    Removed errors and warnings

--- bit_manipulation/rightmost_set_bit.cpp
+++ bit_manipulation/rightmost_set_bit.cpp
@@ -40,7 +40,7 @@ std::uint64_t rightmostsetbit(std :: int64_t n) { // int64_t is preferred over i
     //Here log2 will return you, number of times we can express that number in power of two.
     //For all binary number containing only rightmost set bit as 1 like 2 , 4, 8, 16, 32….
     //We will find that position of right most set bit is always equals to log2(Number)+1
-    return log2(n & -n) + 1;
+    return (long) log2(n & -n) + 1;
     
 }
 }  // namespace rightmost_set_bit
@@ -48,21 +48,21 @@ std::uint64_t rightmostsetbit(std :: int64_t n) { // int64_t is preferred over i
 
 static void test() {
     // n = 4 return 3
-    assert(bit_manipulation::rightmost_set_bit::countSetBits(4) == 3);
+    assert(bit_manipulation::rightmost_set_bit::rightmostsetbit(4) == 3);
     // n = 6 return 2
-    assert(bit_manipulation::rightmost_set_bit::countSetBits(6) == 2);
+    assert(bit_manipulation::rightmost_set_bit::rightmostsetbit(6) == 2);
     // n = 12 return 3
-    assert(bit_manipulation::rightmost_set_bit::countSetBits(12) == 3);
+    assert(bit_manipulation::rightmost_set_bit::rightmostsetbit(12) == 3);
     // n = 9 return 1
-    assert(bit_manipulation::rightmost_set_bit::countSetBits(9) == 1);
+    assert(bit_manipulation::rightmost_set_bit::rightmostsetbit(9) == 1);
     // n = 15 return 1
-    assert(bit_manipulation::rightmost_set_bit::countSetBits(15) == 1);
+    assert(bit_manipulation::rightmost_set_bit::rightmostsetbit(15) == 1);
     // n = 25 return 1
-    assert(bit_manipulation::rightmost_set_bit::countSetBits(25) == 1);
+    assert(bit_manipulation::rightmost_set_bit::rightmostsetbit(25) == 1);
     // n = 96 return 6
-    assert(bit_manipulation::rightmost_set_bit::countSetBits(96) == 6);
+    assert(bit_manipulation::rightmost_set_bit::rightmostsetbit(96) == 6);
     // n = 64 return 7
-    assert(bit_manipulation::rightmost_set_bit::countSetBits(64) == 7);
+    assert(bit_manipulation::rightmost_set_bit::rightmostsetbit(64) == 7);
     std::cout << "All test cases successfully passed!" << std::endl;
 }
 /**
