b911b37e14a7465aa09ec9acc079bc9abb597e24
Ayush Kedia <83538020+ayushkedia05@users.noreply.github.com>
2021-11-05 12:21:22 +0530

    Update reverse_a_linked_list.cpp
    instead of returning int_32 we can return a bool value and the interger which is to be compared in top,last and be passed as a argument. this is the better way of removing the bugs from the code

--- data_structures/reverse_a_linked_list.cpp
+++ data_structures/reverse_a_linked_list.cpp
@@ -1,32 +1,7 @@
-/**
- * @file
- * @brief Implementation of [Reversing
- * a single linked list](https://simple.wikipedia.org/wiki/Linked_list)
- * @details
- * The linked list is a data structure used for holding a sequence of
- * values, which can be added, displayed, reversed, or removed.
- * ### Algorithm
- * Values can be added by iterating to the end of a list (by following
- * the pointers) starting from the first link. Whichever link points to null
- * is considered the last link and is pointed to the new value.
- *
- * Linked List can be reversed by using 3 pointers: current, previous, and
- * next_node; we keep iterating until the last node. Meanwhile, before changing
- * to the next of current, we store it in the next_node pointer, now we store
- * the prev pointer in the current of next, this is where the actual reversal
- * happens. And then we move the prev and current pointers one step forward.
- * Then the head node is made to point to the last node (prev pointer) after
- * completion of an iteration.
-
- * [A graphic explanation and view of what's happening behind the
- *scenes](https://drive.google.com/file/d/1pM5COF0wx-wermnNy_svtyZquaCUP2xS/view?usp=sharing)
- */
-
 #include <cassert>   /// for assert
 #include <iostream>  /// for I/O operations
 #include <memory>    /// for dynamic memory
 #include <new>       /// for managing  dynamic storage
-
 /**
  * @namespace data_structures
  * @brief Data Structures algorithms
@@ -45,7 +20,6 @@ class Node {
     int32_t val;  /// value of the current link
     Node *next;   /// pointer to the next value on the list
 };
-
 /**
  * A list class containing a sequence of links
  */
@@ -63,18 +37,16 @@ class list {
     void insert(int32_t new_elem);
     void reverseList();
     void display();
-    int32_t top();
-    int32_t last();
-    int32_t traverse(int32_t index);
+    bool top(int checkvalue);
+    bool last(int checkvalue);
+    bool traverse(int32_t index,int positionvaluecheck);
 };
-
 /**
  * @brief Utility function that checks if the list is empty
  * @returns true if the list is empty
  * @returns false if the list is not empty
  */
 bool list::isEmpty() { return head == nullptr; }
-
 /**
  * @brief Utility function that adds a new element at the end of the list
  * @param new_elem element be added at the end of the list
@@ -98,7 +70,6 @@ void list::insert(int32_t n) {
         std::cerr << "bad_alloc detected: " << exception.what() << "\n";
     }
 }
-
 /**
  * @brief Utility function for reversing a list
  * @brief Using the current, previous, and next pointer.
@@ -115,61 +86,65 @@ void list::reverseList() {
     }
     head = prev;
 }
-
 /**
  * @brief Utility function to find the top element of the list
  * @returns the top element of the list
  */
-int32_t list::top() {
+bool list::top(int checkvalue) {
     try {
         if (!isEmpty()) {
-            return head->val;
+             return (head->val==checkvalue)?true:false;
+            
         }
     } catch (const std::exception &e) {
         std::cerr << "List is empty" << e.what() << '\n';
+// Function top should return a value of type int32_t but does not return a value here
     }
+    return false;
 }
+// Function top should return a value of type int32_t but does not return a value here
 /**
  *  @brief Utility function to find the last element of the list
  *  @returns the last element of the list
  */
-int32_t list::last() {
+bool list::last(int checkvalue) {
     try {
         if (!isEmpty()) {
             Node *t = head;
             while (t->next != nullptr) {
                 t = t->next;
             }
-            return t->val;
+             return (t->val==checkvalue)?true:false;
         }
     } catch (const std::exception &e) {
         std::cerr << "List is empty" << e.what() << '\n';
+// Function last should return a value of type int32_t but does not return a value here
     }
+    return false;
+
 }
+// Function last should return a value of type int32_t but does not return a value here
 /**
  *  @brief Utility function to find the i th element of the list
  *  @returns the i th element of the list
  */
-int32_t list::traverse(int index) {
+bool list::traverse(int index,int positonvaluecheck) {
     Node *current = head;
-
     int count = 0;
     while (current != nullptr) {
         if (count == index) {
-            return (current->val);
+            return (current->val==positonvaluecheck)?true:false;
         }
         count++;
         current = current->next;
     }
-
     /* if we get to this line,the caller was asking for a non-existent element
     so we assert fail */
-    assert(0);
+    return false;
+// Function traverse should return a value of type int32_t but does not return a value here
 }
-
 }  // namespace linked_list
 }  // namespace data_structures
-
 /**
  * @brief Self-test implementations
  * @returns void
@@ -184,20 +159,19 @@ static void test() {
     L.insert(-12);
     L.insert(-20);
     L.insert(18);
-    assert(L.top() == 11);
-    assert(L.last() == 18);
+    assert(L.top(11) );
+    assert(L.last(18));
     L.reverseList();
     // Reversal Testing
-    assert(L.top() == 18);
-    assert(L.traverse(1) == -20);
-    assert(L.traverse(2) == -12);
-    assert(L.traverse(3) == 10);
-    assert(L.traverse(4) == 15);
-    assert(L.traverse(5) == 12);
-    assert(L.last() == 11);
+    assert(L.top(18));
+    assert(L.traverse(1,-20));
+    assert(L.traverse(2,-12) );
+    assert(L.traverse(3,10) );
+    assert(L.traverse(4,15) );
+    assert(L.traverse(5,12));
+    assert(L.last(11));
     std::cout << "All tests have successfully passed!" << std::endl;
 }
-
 /**
  * @brief Main function
  * @returns 0 on exit
