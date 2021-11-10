c78167b357a32b11d22bb7bb124dc326ccca63e3
Lajat5 <64376519+Lazeeez@users.noreply.github.com>
2021-11-10 07:42:21 +0530

    Update dynamic_programming/partition_problem.cpp
    Co-authored-by: David Leal <halfpacho@gmail.com>

--- dynamic_programming/partition_problem.cpp
+++ dynamic_programming/partition_problem.cpp
@@ -6,7 +6,7 @@
  * The partition problem, or number partitioning, is the task of deciding
  * whether a given multiset S of positive integers can be partitioned into two
  * subsets S1 and S2 such that the sum of the numbers in S1 equals the sum of
- *the numbers in S2. Although the partition problem is NP-complete, there is a
+ * the numbers in S2. Although the partition problem is NP-complete, there is a
  * pseudo-polynomial time dynamic programming solution, and there are heuristics
  * that solve the problem in many instances, either optimally or approximately.
  * For this reason, it has been called "the easiest hard problem".
