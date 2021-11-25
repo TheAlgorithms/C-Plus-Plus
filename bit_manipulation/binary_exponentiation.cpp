7bc2d5f19ec68b9645d8e3429de13b677d0ec1d3
Jaladi Saai Sishir <54372985+sishir2001@users.noreply.github.com>
2021-11-26 02:38:02 +0530

    feat : add binary_exponentiation algorithm class bit_manipulation

--- bit_manipulation/binary_exponentiation.cpp
+++ bit_manipulation/binary_exponentiation.cpp
@@ -1,32 +1,66 @@
-/*
-    author : @sishir2001
-*/
-#include <cassert>
-#include <iostream>
-using namespace std;
+/**
+ * @bit_manipulation
+ * @brief Binary exponentiation for calculating power
+ * Implemented this algorithm :https://cp-algorithms.com/algebra/binary-exp.html
+ * Time complexity : O(log(n))
+ * Space complexity : O(1)
+ * @author Jaladi Saai Sishir(https://github.com/sishir2001)
+ **/
 
-long binary_exponention(int x, int n) {
-    // @param x : base number
-    // @param n : exponent
-    // T(N) = O(log(n))
-    // S(n) = O(1)
-    long res = 1;
+#include <cassert>   /// for assert
+#include <iostream>  /// for IO
+
+/**
+ * @namespace bit_manipulation
+ * @brief Bit manipulation algorithms
+ */
+namespace bit_manipulation {
+/**
+ * @namespace binary_exponentiation
+ * @brief Functions for the [binary
+ * exponentiation](https://cp-algorithms.com/algebra/binary-exp.html)
+ * implementation
+ */
+namespace binary_exponentiation {
+/**
+ * @brief The main function implements power through binary exponentiation
+ * @param x is the base of the power
+ * @param n is the exponent for the power
+ * @returns power(x,n) i.e x^n of type long (64 bits)
+ */
+long binaryExponentiation(int x, int n) {
+    long res = 1;  // variable for storing the final result
     while (n != 0) {
         if (n % 2 == 1) {
             // last bit is 1
             res *= x;
         }
-        x *= x;
-        n /= 2;
+        x *= x;  // exponentiation
+        n /= 2;  // right shifting
     }
     return res;
 }
+}  // namespace binary_exponentiation
+}  // namespace bit_manipulation
 void test() {
-    assert(binary_exponention(2, 2) == 4);
-    assert(binary_exponention(3, 2) == 9);
-    assert(binary_exponention(4, 2) == 16);
+    assert(bit_manipulation::binary_exponentiation::binaryExponentiation(
+               2, 2) == 4);  // x = 2 n = 2 return 4
+    assert(bit_manipulation::binary_exponentiation::binaryExponentiation(
+               3, 2) == 9);  // x = 3 n = 2 return 9
+    assert(bit_manipulation::binary_exponentiation::binaryExponentiation(
+               4, 2) == 16);  // x = 4 n = 2 return 16
+    assert(bit_manipulation::binary_exponentiation::binaryExponentiation(
+               5, 2) == 25);  // x = 5 n = 2 return 25
+    assert(bit_manipulation::binary_exponentiation::binaryExponentiation(
+               6, 2) == 36);  // x = 6 n = 2 return 36
+    std::cout << "All tests passed"
+              << "\n";
 }
 
+/**
+ * @brief Main function
+ * @returns 0 on exit
+ */
 int main(int argc, char *argv[]) {
     test();
     return 0;
