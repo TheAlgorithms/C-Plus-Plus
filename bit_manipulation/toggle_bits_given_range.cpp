f98d457e98103d94ea28992e9d12edfcfc5465cd
David <Panquesito7@users.noreply.github.com>
2022-01-03 04:06:31 +0000

    clang-format and clang-tidy fixes for e2b0e40d

--- bit_manipulation/toggle_bits_given_range.cpp
+++ bit_manipulation/toggle_bits_given_range.cpp
@@ -5,7 +5,8 @@
  *
  * @details
  * We are given a non-negative number n and two values l and r. The problem is
- * to toggle the bits in the range l to r in the binary representation of n, i.e,
+ * to toggle the bits in the range l to r in the binary representation of n,
+i.e,
  * to toggle bits from the rightmost lth bit to the rightmost rth bit. A toggle
  * operation flips a bit 0 to 1 and a bit 1 to 0.
  * Example:- Input : n = 17, l = 2, r = 3
