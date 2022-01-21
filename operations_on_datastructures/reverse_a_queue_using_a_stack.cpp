786f8b2a0407a149e61d4633a819e926d556efc9
alecrothenberg <alec.rothenberg@ufl.edu>
2022-01-21 16:18:08 -0500

    updated file to match conventions

--- operations_on_datastructures/reverse_a_queue_using_a_stack.cpp
+++ operations_on_datastructures/reverse_a_queue_using_a_stack.cpp
@@ -1,21 +1,45 @@
+/**
+ * @file 
+ * @author Alec Rothenberg 
+ * @brief Reverse a queue using a stack
+ * @version 0.1
+ * @date 2022-01-21
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <cassert>
 #include <queue> 
 #include <stack>
 
+namespace name{
+/**
+ * @brief Using two while loops, a queue is reversed by popping the values and inputting into a 
+ * stack. The stack is then emptied and the values are returned in reverse order
+ * to the queue
+ * 
+ * @tparam T 
+ * @param input 
+ * @return std::queue<T> 
+ */
+        template<class T>
+        std::queue<T> reverse(std::queue<T> input) {
+        // temp stack to store values from queue in
+        std::stack<T> st;
 
-template <typename T>
-std::queue<T> reverseQueue (std::queue<T> input) {
-    // temp stack to store values from queue in
-    std::stack<T> st;
+        while (!input.empty()) {
+            st.push() = input.front();
+            input.pop();
+        }
 
-    while (!input.empty()) {
-        st.push() = input.front();
-        input.pop();
-    }
+        while(!st.empty()) {
+            input.push() = st.top();
+            st.pop();
+        }
+        return input;
 
-    while(!st.empty()) {
-        input.push() = st.top();
-        st.pop();
-    }
-    return input;
-
+        }
 }
+
+
