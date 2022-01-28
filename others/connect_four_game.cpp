a98d0a99480285f5c682721c7313aeaeeac9f7fe
Arunabh <80234521+arunabh-cloud@users.noreply.github.com>
2022-01-28 16:17:50 -0500

    Updated changes to connect_four_game.cpp
    Updated changes to connect_four_game.cpp

--- others/connect_four_game.cpp
+++ others/connect_four_game.cpp
@@ -14,27 +14,19 @@
 */
 #include <iostream>   /// for IO operations
 #include <array>     /// for std::array
-
 using std::cout;
 using std::cin;
 
-// Global constants COLS and ROWS for the standard (row * column) 2D game board
-const int COLS = 8;
-const int ROWS = 6;
-
-// Function prototypes
-void printBoard(std::array<std::array<char, COLS>, ROWS>&);
-void initializeBoard(std::array<std::array<char, COLS>, ROWS>&);
-void playGame(std::array<std::array<char, COLS>, ROWS>&);
-bool checkIfWinner(std::array<std::array<char, COLS>, ROWS>&, int, int, char);
+const int COLS = 8;  /// The number of board columns is 8
+const int ROWS = 6;  /// The number of the board rows is 6
 
 /**
-  * Print the game board
+  * @details 
+  * Method for printing the board with tokens or '-' if a space is yet to be chosen
   * @param board of dimension ROWS * COLS
   * @param ROWS is the vertical dimension of the board
   * @param COLS is the horizontal dimension of the board
   */
-// Method for printing the board with tokens or '-' if a space is yet to be chosen
 void printBoard(std::array<std::array<char, COLS>, ROWS>&board) {
     for (int i = ROWS - 1; i > -1; i--) {
         for (int j = 0; j < COLS; j++) {
@@ -45,14 +37,12 @@ void printBoard(std::array<std::array<char, COLS>, ROWS>&board) {
 }
 
 /**
-  * Set up the 2D array for the game board
+  * @details
+  * Method for initializing and printing the board with "-"
   * @param board of dimension ROWS * COLS
   * @param ROWS is the vertical dimension of the board
   * @param COLS is the horizontal dimension of the board
-  * initialize the board with "-"
-  * print the board
   */
-// Method for initializing the board with "-"
 void initializeBoard(std::array<std::array<char, COLS>, ROWS>&board) {
     for (int i = 0; i < ROWS; i++) {
         for (int j = 0; j < COLS; j++) {
@@ -61,54 +51,45 @@ void initializeBoard(std::array<std::array<char, COLS>, ROWS>&board) {
     }
     printBoard(board);
     std::cout << std::endl;
-
-    // Tell the players about the tokens
-    std::cout << "Player 1: x\n";
-    std::cout << "Player 2: o\n";
+    std::cout << "Player 1: x\n";  ///  Assign the token 'x' to the Player 1 
+    std::cout << "Player 2: o\n";  /// Assign the token 'o' to the Player 2 
     std::cout << std::endl;
 }
 
 /**
-  * Play the game
+  * Play the game till there is a winner or till the game draws
   * @param board of dimension ROWS * COLS
   * @param ROWS is the vertical dimension of the board
   * @param COLS is the horizontal dimension of the board
-  * Play till there is a winner or till the game draws
   */
 void playGame(std::array<std::array<char, COLS>, ROWS>&board){
-    // Initializing game variables
-    int count = 1; // Counts each turn played till the board is full
-    bool win = false; // Checks if there is a winner
-    int num = 0; // Stores the player's number 1 or 2
-    char token; // Assigns 'x' or 'o' to the players 1 and 2 respectively
-
-    // Setting the game in the while loop till there is a winner or a draw
-    // Asking for column option from players
+    /// Initializing game variables
+    int count = 1;     /// Counts each turn played till the board is full
+    bool win = false;   /// Checks if there is a winner
+    int num = 0;  /// Stores the player's number 1 or 2
+    char token; /// Assigns 'x' or 'o' to the players 1 and 2 respectively
     while (!win && count <= ROWS * COLS) {
-        int col = 0;
-        int row = 0;
-
+        int col = 0;  /// for column number chosen by the player
+        int row = 0;  /// for row number selected by the program
         num = (count % 2 == 1) ? 1 : 2;
         token = (count % 2 == 1) ? 'x' : 'o';
+        /// Asking for column option from players
         std::cout << "Player " << num << ": Enter a valid column number :  ";
         std::cin >> col;
         col -= 1;
         std::cout << std::endl;
-        // Checking if the player inputs an invalid column number
+        /// Checking if the player inputs an invalid column number
         while (col <= -1 || col >= COLS) {
             std::cout << "Please enter a valid index. ";
             std::cin >> col;
             col -= 1;
             std::cout << std::endl;
         }
-        // Checking for the highest available row
+        /// Checking for the highest available row
         while (board[row][col] != '-' && row < ROWS) {
             row++;
         }
-
-        // if a valid row is available
-        // replace the '-' with the token in the 2D array / board
-        // print the board
+        /// if a valid row is available replace and print the '-' with the token in the 2D array / board
         if (row != ROWS) {
             board[row][col] = token;
             printBoard(board);
@@ -117,8 +98,7 @@ void playGame(std::array<std::array<char, COLS>, ROWS>&board){
             count++;
         }
     }
-
-    // Declaring the winner or if there is a draw
+    /// Declaring the winner or if there is a draw
     if (count > ROWS * COLS && !win) {
         std::cout << "Draw. Nobody wins.\n";
     }
@@ -129,6 +109,8 @@ void playGame(std::array<std::array<char, COLS>, ROWS>&board){
 
 /**
   * checkIfWinner function checks after every turn if a player is the winner
+  * After a token is added, checks if the specified token / chip type creates four in a row
+  * Horizontally, Vertically, Diagonally Right, Diagonally Left
   * @param board of dimension ROWS * COLS
   * @param ROWS is the vertical dimension of the board
   * @param COLS is the horizontal dimension of the board
@@ -138,57 +120,45 @@ void playGame(std::array<std::array<char, COLS>, ROWS>&board){
   * @returns 'win' = true, if a player has won
   * @returns 'win' = false, if no player has won
   */
-
 bool checkIfWinner(std::array<std::array<char, COLS>, ROWS>&board, int col, int row, char chipType) {
-
     bool win = false;
-    // After a token is added, checks if the specified token / chip type creates four in a row
-    // Horizontally, Vertically, Diagonally Right, Diagonally Left
-
     if(row >= 3) {
         if (board[row - 1][col] == chipType && board[row - 2][col] == chipType && board[row - 3][col] == chipType) {
             win = true;   // Connects the current token with 3 vertically below
         }
     }
-
     if(col <= COLS - 4) {
         if (board[row][col + 1] == chipType && board[row][col + 2] == chipType && board[row][col + 3] == chipType) {
             win = true;   // Connects the current token with 3 horizontally to its right
         }
     }
-
     if(col <= COLS - 3  && col >= 1) {
         if (board[row][col - 1] == chipType && board[row][col + 1] == chipType && board[row][col + 2] == chipType) {
             win = true;   // Connects the current token with 1 horizontally to its left and 2 horizontally to its right
         }
     }
-
     if(col <= COLS - 2  && col >= 2) {
         if (board[row][col - 2] == chipType && board[row][col - 1] == chipType && board[row][col + 1] == chipType) {
             win = true;   // Connects the current token with 2 horizontally to its left and 1 horizontally to its right
         }
     }
-
     if(col <= COLS - 1  && col >= 3) {
         if (board[row][col - 3] == chipType && board[row][col - 2] == chipType && board[row][col - 1] == chipType) {
             win = true;   // Connects the current token with 3 horizontally to its left
         }
     }
-
     if(row >= 3 && col <= COLS - 4) {
         if (board[row - 1][col + 1] == chipType && board[row - 2][col + 2] == chipType &&
             board[row - 3][col + 3] == chipType) {
             win = true;   // Connects the current token with 3 diagonally downward to its right
         }
     }
-
     if(row >= 3 && col <= COLS - 1  && col >= 3) {
         if (board[row - 1][col - 1] == chipType && board[row - 2][col - 2] == chipType &&
             board[row - 3][col - 3] == chipType) {
             win = true;   // Connects the current token with 3 diagonally downward to its left
         }
     }
-
     return win;
 }
 
