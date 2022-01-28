0367e706bf11f763fb4565fdce42e5c3309f140d
ngiridharan <73603346+ngiridharan@users.noreply.github.com>
2022-01-28 16:42:51 -0500

    Create reversing_queue.cpp

---
+++ data_structures/reversing_queue.cpp
@@ -0,0 +1,71 @@
+/**
+ * @file
+ * @brief Reverse a queue by using a stack
+ * @details
+ * Using the Last In First Out property of Stacks
+ * we can reverse the order of a queue
+ * @author [Nikhil](https://github.com/ngiridharan)
+ * @see related_file.cpp, another_file.cpp
+ */
+
+
+
+#include <iostream>
+#include <queue>
+#include <stack>
+using namespace std;
+
+
+template <typename T>
+void printQueue(queue<T>& queue){
+    while (!_queue.empty()){
+        cout << _queue.front()<< " ";
+        _queue.pop();
+    }
+    cout << endl;
+}
+template <typename T>
+queue<T> reverseQueue(queue<T>& q){
+    stack<T> s;
+    queue<T> newQueue;
+    while(!q.empty()){
+        T temp = q.front();
+        s.push(temp);
+        q.pop();
+    }
+    while(!s.empty()){
+        newQueue.push(s.top());
+        s.pop();
+    }
+    return newQueue;
+}
+
+
+int main(){
+
+    //test for ints
+    queue<int> testQueueInt;
+    for(int i = 0; i < 10; i++){
+        testQueueInt.push(rand() % 10);
+    }
+    cout << "Original queue:" << endl;
+    queue<int>temp = testQueueInt;
+    printQueue(temp);
+    cout<<"Reversed queue"<<endl;
+    queue<int> rev = reverseQueue(testQueueInt);
+    printQueue(rev);
+
+
+
+    //test for chars
+    queue<char> testQueueChar;
+    for(int i = 0; i < 10; i++){
+        testQueueChar.push(char(rand() % 100));
+    }
+    cout << "Original queue:" << endl;
+    queue<char>tempChar = testQueueChar;
+    printQueue(tempChar);
+    cout<<"Reversed queue"<<endl;
+    queue<char> revChar = reverseQueue(testQueueChar);
+    printQueue(revChar);
+}
