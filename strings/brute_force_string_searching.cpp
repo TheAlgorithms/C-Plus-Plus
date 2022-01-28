96955a76d3a3f763267eee0da39bcadb453440f6
fynnwilliam <fynnwilliam@gmail.com>
2022-01-27 20:30:11 -0500

    updated txt_l in the if-statement

--- strings/brute_force_string_searching.cpp
+++ strings/brute_force_string_searching.cpp
@@ -22,8 +22,8 @@ int brute_force(const std::string &text, const std::string &pattern) {
     size_t pattern_length = pattern.length();
     size_t text_length    = text.length();
     int index = -1;
-    if (pattern_length <= txt_l) {
-        for (size_t i = 0; i < txt_l - pattern_length + 1; i++) {
+    if (pattern_length <= text_length) {
+        for (size_t i = 0; i < text_length - pattern_length + 1; i++) {
             std::string s = text.substr(i, pattern_length);
             if (s == pattern) {
                 index = i;
