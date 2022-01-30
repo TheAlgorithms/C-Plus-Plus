ebda1b5e36f7655e5e9a0a5b9729f7a9847c0069
stevencabral21 <steven.cabral@ufl.edu>
2022-01-30 21:26:50 +0000

    more tests for recursive linear search

--- search/linear_search_recursive.cpp
+++ search/linear_search_recursive.cpp
@@ -4,6 +4,7 @@
 **/
 
 #include <iostream> //input & output services
+#include <array>
 #include <cassert> //
 
 
@@ -22,33 +23,53 @@ int LinearSearch(int *array, int size, int key){
 }
 static void test() {
     /* tests search on basic arrays */
-    int arrSize = 10;
+    int arrSize = 10; 
     int *arr = new int[arrSize];
     for(int i = 0; i < arrSize; i++){
-        arr[i] = i;
+        arr[i] = i*100;
+        
+    }
+    
+    assert(LinearSearch(arr, arrSize, 0) == 0); // this ensures that the algorithm works as expected
+    assert(LinearSearch(arr, arrSize, 100) == 1);
+    assert(LinearSearch(arr, arrSize, 900) == 9); // can have multiple checks
+
+    
+    arrSize = 100;
+    int *arr2 = new int[arrSize];
+    for(int i = 0; i < arrSize; i++){
+        arr2[i] = i*115;
     }
 
-    assert(LinearSearch(arr, arrSize, 0) == 0); // this ensures that the algorithm works as expected
-    assert(LinearSearch(arr, arrSize, 5) == 5);
-    assert(LinearSearch(arr, arrSize, 9) == 9);
-    // can have multiple checks
+    assert(LinearSearch(arr2, arrSize, 115) == 1);
+    assert(LinearSearch(arr2, arrSize, 5750) == 50);
+
+    arrSize = 3;
+    int *arr3 = new int[arrSize];
+    for(int i = 0; i < arrSize; i++){
+        arr3[i] = i+1000;
+    }
+    assert(LinearSearch(arr3, arrSize, 1000) == 0);
+    assert(LinearSearch(arr3, arrSize, 1002) == 2);
+
+    std::cout << "Preliminary Tests Passed" << std::endl;
+    //free up dynamically allocated memory
+    delete[] arr;
+    delete[] arr2;
+    delete[] arr3;
 }
 
 int main(){
     
     test();
-    //Allows user arrays to be created and searched
+    //Allows user arrays to be created and searched    
     int arrSize = 0;
     std::cout << "Enter the size of array to be searched: ";
+    std::cin.clear();
     std::cin >> arrSize;
-
-    if(arrSize <= 0){
-        std::cout << "Array size must be greater than 0, please select a new size" << std::endl;
-        std::cin >> arrSize;
-    }
-
     int *array = new int[arrSize];    
     std::cout << "Enter all " << arrSize << " numbers: ";
+
     for(int i = 0; i < arrSize; i++){
         std::cin >> array[i];
     }
@@ -56,8 +77,8 @@ int main(){
     int key = 0;
     std::cout << "What number would you like to search? " << std::endl;
     std::cin >> key;
-    
     int result = LinearSearch(array, arrSize, key);
+    
     if(result == -1){
         std::cout << "Element was not found in the given array" << std::endl;
     }
