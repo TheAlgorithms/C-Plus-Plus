00e084bbe0a74d86de8628dc284864b92a0d10ef
kknexi <kknexi@aol.com>
2022-01-29 20:27:41 -0500

    Added a recursive bubble sort function

--- sorting/bubble_sort.cpp
+++ sorting/bubble_sort.cpp
@@ -2,12 +2,13 @@
  * @file
  * @brief Bubble sort algorithm
  *
- * The working principle of the Bubble sort algorithm:
+The working principle of the Bubble sort algorithm:
 
-Bubble sort algorithm is the bubble sorting algorithm. The most important reason
-for calling the bubble is that the largest number is thrown at the end of this
-algorithm. This is all about the logic. In each iteration, the largest number is
-expired and when iterations are completed, the sorting takes place.
+The bubble sorting algorithm works by "bubbling" the largest number to the top
+of the list. This is done every pass until the entire set of numbers is fully
+sorted. The bubbling apsect works by swapping adjacent numbers if the left
+number is larger than the right number. After each adjacent number is checked
+going up the list, this is considered one pass.
 
 What is Swap?
 
@@ -25,6 +26,12 @@ When x assigns the value to x, the old value of x is lost.
 That's why we created a variable z to create the first value of the value of x,
 and finally, we have assigned to y.
 
+What is Recursion?
+
+Recursion is the method of solving a problem by solving smaller instances of
+that same problem. This is most commonly seen with a function calling itself
+until a base case has been reached.
+
 Bubble Sort Algorithm Analysis (Best Case - Worst Case - Average Case)
 
 Bubble Sort Worst Case Performance is O (n²). Why is that? Because if you
@@ -39,45 +46,76 @@ optimized bubble sort algorithm. It's right down there.
 
 #include <iostream>
 #include <vector>
+using namespace std;
 
-int main() {
-    int n;
-    bool swap_check = true;
-    std::cout << "Enter the amount of numbers to sort: ";
-    std::cin >> n;
-    std::vector<int> numbers;
-    std::cout << "Enter " << n << " numbers: ";
-    int num;
-
-    // Input
-    for (int i = 0; i < n; i++) {
-        std::cin >> num;
-        numbers.push_back(num);
-    }
-
-    // Bubble Sorting
-    for (int i = 0; (i < n) && (swap_check); i++) {
-        swap_check = false;
-        for (int j = 0; j < n - 1 - i; j++) {
-            if (numbers[j] > numbers[j + 1]) {
-                swap_check = true;
-                std::swap(numbers[j],
-                          numbers[j + 1]);  // by changing swap location.
-                                            // I mean, j. If the number is
-                                            // greater than j + 1, then it
-                                            // means the location.
-            }
-        }
-    }
-
-    // Output
-    std::cout << "\nSorted Array : ";
+void Ouput(vector<int> numbers) {
+    cout << "Sorted Array : ";
     for (int i = 0; i < numbers.size(); i++) {
         if (i != numbers.size() - 1) {
             std::cout << numbers[i] << ", ";
         } else {
-            std::cout << numbers[i] << std::endl;
+            cout << numbers[i] << std::endl;
         }
     }
+    cout << endl;
+}
+
+vector<int> BubbleSort(vector<int> &numbers) {
+    bool swap_check = true;
+
+    for (int i = 0; (i < numbers.size()) && (swap_check); i++) {
+        swap_check = false;
+        for (int j = 0; j < numbers.size() - 1 - i; j++) {
+            if (numbers[j] > numbers[j + 1]) {
+                swap_check = true;
+                swap(numbers[j], numbers[j + 1]);
+            }
+        }
+    }
+    return numbers;
+}
+
+void RecursiveBubbleSort(vector<int> &numbers, int n) {
+    if (n == 1)
+        return;
+
+    for (int i = 0; i < n - 1; i++)
+        if (numbers[i] > numbers[i + 1])
+            swap(numbers[i], numbers[i + 1]);
+
+    RecursiveBubbleSort(numbers, n - 1);
+}
+
+int main() {
+    // Inputs
+    cout << "Enter amount of numbers to sort: ";
+    int n;
+    cin >> n;
+
+    cout << "Enter " << n << " numbers seperated by space: ";
+    int TempNum;
+    vector<int> numbers;
+    for (int i = 0; i < n; i++) {
+        cin >> TempNum;
+        numbers.push_back(TempNum);
+    }
+
+    // which sort?
+    cout << "Regular or Recursive Bubble Sort? ";
+    string input;
+    cin >> input;
+
+    if (input == "Regular" || input == "regular") {
+        // Bubble Sort
+        BubbleSort(numbers);
+        cout << "\nRegular Bubble ";
+        Ouput(numbers);
+    } else if (input == "Recursive" || input == "recursive") {
+        // Recursive Bubble Sort:
+        RecursiveBubbleSort(numbers, n);
+        cout << "\nRecursive Bubble ";
+        Ouput(numbers);
+    }
+
     return 0;
 }
