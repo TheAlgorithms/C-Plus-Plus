05e8b3b752ddc659fce981b1432a740bb560651f
Jared <janthofer@ufl.edu>
2022-01-30 19:38:20 -0500

    feat: add dice roll algorithm

--- dynamic_programming/dice_roll.cpp
+++ dynamic_programming/dice_roll.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 #include <vector>
-using namespace std;
 
 //Determines the number of possibilities to get a dice sum given number of dice and faces.
 int diceRoll (int x, int y, int z) {
-    vector <int> vect(z + 1);
+    std::vector <int> vect(z + 1);
     vect[0] = 1;
     for (int i = 1; i <= x; i++) {
-        vector <int> vectTemp(z + 1);
+        std::vector <int> vectTemp(z + 1);
         for (int j = 1; j <= y; j++) {
             for (int k = j; k <= z; k++) {
                 vectTemp[k] += vect[k - j];
@@ -19,17 +18,17 @@ int diceRoll (int x, int y, int z) {
 }
 
 int main () {
-    cout << "Dice faces: " << endl;
+    std::cout << "Dice faces: " << std::endl;
     int diceFaces = 0;
-    cin >> diceFaces;
+    std::cin >> diceFaces;
 
-    cout << "Number of dice: " << endl;
+    std::cout << "Number of dice: " << std::endl;
     int numDice = 0;
-    cin >> numDice;
+    std::cin >> numDice;
 
-    cout << "Dice sum: " << endl;
+    std::cout << "Dice sum: " << std::endl;
     int diceSum = 0;
-    cin >> diceSum;
+    std::cin >> diceSum;
 
-    cout << "Number of ways to get sum of " << diceSum << ": " << diceRoll(diceFaces, numDice, diceSum) << endl;
+    std::cout << "Number of ways to get sum of " << diceSum << ": " << diceRoll(diceFaces, numDice, diceSum) << std::endl;
 }
