951fffbecec59a10adeb7cb0c14c21a1129fe7c9
fynnwilliam <fynnwilliam@gmail.com>
2022-01-31 19:27:15 -0500

    deducing type for shiftTable with auto

--- strings/horspool.cpp
+++ strings/horspool.cpp
@@ -57,7 +57,7 @@ std::unordered_map<char, size_t> findShiftTable(const std::string &prototype) {
  * @returns false if text string does not contain prototype string
  */
 bool horspool(const std::string &text, const std::string &prototype) {
-    std::unordered_map<char, size_t> shiftTable = findShiftTable(
+    auto shiftTable = findShiftTable(
         prototype);  // Initialise shift table calling findShiftTable function
 
     auto prototype_size = prototype.size();
