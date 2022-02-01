aa4d6eadac6bdb6ed371611a834b62570e87cba6
fynnwilliam <fynnwilliam@gmail.com>
2022-02-01 13:14:11 -0500

    moved implementation of helper functions into a ".h" making manacher(const std::string&) more visible

--- strings/manacher_algorithm.cpp
+++ strings/manacher_algorithm.cpp
@@ -10,14 +10,7 @@
  * @author [Riti Kumari](https://github.com/riti2409)
  */
 
-#include <cassert>   /// for assert
-#include <iostream>  /// for IO operations
-#include <vector>    /// for std::vector STL
-#ifdef _MSC_VER
-#include <string>  /// for string (required for MS Visual C++)
-#else
-#include <cstring>  /// for string
-#endif
+#include "manacher_algorithm.h"  // contains implementation of helper functions for the main algorithm
 
 /**
  * @namespace strings
@@ -31,119 +24,6 @@ namespace strings {
  * implementation
  */
 namespace manacher {
-
-/**
- * @brief A function to stuff characters between a string to handle cases with
- * even length palindrome
- * @param s text to facilitate stuffing
- * @returns a copy of 's' with stuffed characters
- */
-auto stuffed(const std::string& s) {
-    std::string stuffed_str;
-    for (auto const& c : s) {
-        stuffed_str += c;
-        stuffed_str += '#';
-    }
-
-    return stuffed_str = "@#" + stuffed_str + '&';
-}
-
-/**
- * @brief A function expanding the palindrome across the maximum stored length
- * in the array, centered at i
- * @param s text returned from stuffed(const std::string&)
- * @param max_length current max half length of palindrome
- * @param i center lying within one half of the palindrone
- * @returns void
- */
-auto expand_palindrome(const std::string& s, uint64_t& max_length, uint64_t i) {
-    while (s[i + (max_length + 1)] == s[i - (max_length + 1)]) {
-        ++max_length;
-    }
-}
-
-/**
- * @brief A function to find the min possible half length among the palindrome
- * @param stuffed_string text returned from stuffed(const std::string&)
- * @returns a vector of largest possible half length of palindrome centered at
- * index (say i with respect to the stuffed string). This value will be lower
- * bound of half length since single character is a palindrome in itself.
- */
-auto half_lengths(const std::string& stuffed_string) {
-    auto stuffed_str_size = stuffed_string.size();
-    std::vector<uint64_t> half_lengths(stuffed_str_size);
-
-    uint64_t bigger_center =
-        0;  // index of the center of palindromic substring which would be
-            // considered as the larger palindrome, having symmetric halves
-
-    uint64_t right = 0;  // maximum length of the palindrome from
-                         // 'bigger_center' to the rightmost end
-
-    // i is considered as center lying within one half of the palindrone
-    // which is centered at 'bigger_center'
-    for (uint64_t i = 1; i < stuffed_str_size - 1; ++i) {
-        if (i < right) {
-            auto opposite_to_i =
-                2 * bigger_center - i;  // opposite end of string, if centered
-                                        // at center, and having one end as i
-
-            half_lengths[i] = std::min(half_lengths[opposite_to_i], right - i);
-        }
-
-        auto& current_max_half_length = half_lengths[i];
-        expand_palindrome(stuffed_string, current_max_half_length, i);
-
-        // if palindrome centered at i exceeds the rightmost end of palindrome
-        // centered at 'bigger_center', then i will be made the 'bigger_center'
-        // and right value will also be updated with respect to center i
-        if (i + current_max_half_length > right) {
-            bigger_center = i;
-            right = i + current_max_half_length;
-        }
-    }
-
-    return half_lengths;
-}
-
-/**
- * @brief A helper function to extract indices
- * @param stuffed_str_sz is the length of the stuffed string
- * @param v contains largest possible half length of palindrome
- * @returns a pair, {half length of largest palindrome, and index of its center}
- */
-auto extract_indices(const std::vector<uint64_t>& v, uint64_t stuffed_str_sz) {
-    uint64_t half_length = 0;
-    uint64_t center_index = 0;
-
-    for (uint64_t i{1}; i < stuffed_str_sz - 1; ++i) {
-        if (auto max_half_length = v[i]; max_half_length > half_length) {
-            half_length = max_half_length;
-            center_index = i;
-        }
-    }
-
-    return std::pair{half_length, center_index};
-}
-
-/**
- * @brief A helper function to simplify manacher(const std::string&)
- * @param half_length is half the length of the largest palindrome
- * @param center_index index of center of the largest palindrome
- * @param s main string we extract from
- * @returns the largest palindromic substring
- */
-auto palindromic_substring(uint64_t half_length, uint64_t center_index,
-                           const std::string& s) {
-    auto start = center_index - half_length + 1;  // substring[0]
-    auto end = center_index + half_length - 1;    // substring[n - 1]
-    std::string substring;
-    for (auto index = start; index <= end; index += 2) {
-        substring += s[index];
-    }
-
-    return substring;
-}
 
 /**
  * @brief A function that implements Manacher's algorithm
