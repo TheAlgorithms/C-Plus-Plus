e6b1a0e42654d9bd0020d31c329f10e74d8e5d21
fynnwilliam <fynnwilliam@gmail.com>
2022-02-01 20:44:50 -0500

    renamed find_pat_in_text -> find_pattern_in_text

--- strings/z_function.cpp
+++ strings/z_function.cpp
@@ -50,7 +50,7 @@ std::vector<uint64_t> Z_function(const std::string &pattern) {
  * \param[in] text text in which to search
  * \returns a vector of starting indexes where pattern is found in the text
  */
-std::vector<uint64_t> find_pat_in_text(const std::string &pattern,
+std::vector<uint64_t> find_pattern_in_text(const std::string &pattern,
                                        const std::string &text) {
     uint64_t text_length = text.size(), pattern_length = pattern.size();
     std::vector<uint64_t> z = Z_function(pattern + '#' + text);
@@ -74,7 +74,7 @@ static void test() {
     std::string pattern1 = "abc";
 
     // matching_indexes1 gets the indexes where pattern1 exists in text1
-    std::vector<uint64_t> matching_indexes1 = find_pat_in_text(pattern1, text1);
+    std::vector<uint64_t> matching_indexes1 = find_pattern_in_text(pattern1, text1);
     assert((matching_indexes1 == std::vector<uint64_t>{10, 14, 18, 30, 33}));
 
     // corner case
@@ -82,7 +82,7 @@ static void test() {
     std::string pattern2 = "abc";
 
     // matching_indexes2 gets the indexes where pattern2 exists in text2
-    std::vector<uint64_t> matching_indexes2 = find_pat_in_text(pattern2, text2);
+    std::vector<uint64_t> matching_indexes2 = find_pattern_in_text(pattern2, text2);
     assert((matching_indexes2 == std::vector<uint64_t>{}));
 }
 
