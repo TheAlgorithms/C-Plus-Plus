91b04869ef8dde2f5b32106774f06c15e624d915
fynnwilliam <fynnwilliam@gmail.com>
2022-02-05 17:18:15 -0500

    fix: changed 'int i' -> 'std::size_t i'

--- sorting/bogo_sort.cpp
+++ sorting/bogo_sort.cpp
@@ -34,7 +34,7 @@ namespace sorting {
  */
 template <typename T, size_t N>
 auto shuffle (std::array <T, N>& arr) {
-    for (int i = 0; i < N; i++) {
+    for (size_t i = 0; i < N; i++) {
         // Swaps i'th  index with random index (less than array size)
         std::swap(arr[i], arr[std::rand() % N]);
     }
