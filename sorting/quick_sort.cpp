f0f1584432512ab675f283a0d1c85f825bb0e241
MiracleShadow <1210768621@qq.com>
2022-08-14 23:34:35 +0800

    :label: fix C-style arrays

--- sorting/quick_sort.cpp
+++ sorting/quick_sort.cpp
@@ -23,6 +23,7 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <vector>
 
 namespace sorting {
 /**
@@ -34,7 +35,7 @@ namespace sorting {
  *
  */
 
-int partition(int arr[], int low, int high) {
+int partition(std::vector<int> &arr, int low, int high) {
     int pivot = arr[high];  // taking the last element as pivot
     int i = (low - 1);      // Index of smaller element
 
@@ -60,7 +61,7 @@ int partition(int arr[], int low, int high) {
  *      low --> Starting index,
  *      high --> Ending index
  */
-void quickSort(int arr[], int low, int high) {
+void quickSort(std::vector<int> &arr, int low, int high) {
     if (low < high) {
         int p = partition(arr, low, high);
         quickSort(arr, low, p - 1);
@@ -73,19 +74,19 @@ void quickSort(int arr[], int low, int high) {
 using sorting::quickSort;
 
 // prints the array after sorting
-void show(int arr[], int size) {
+void show(const std::vector<int> &arr, int size) {
     for (int i = 0; i < size; i++) std::cout << arr[i] << " ";
     std::cout << "\n";
 }
 
 /** Driver program to test above functions */
 int main() {
-    int size;
+    int size = 0;
     std::cout << "\nEnter the number of elements : ";
 
     std::cin >> size;
 
-    int *arr = new int[size];
+    std::vector<int> arr(size);
 
     std::cout << "\nEnter the unsorted elements : ";
 
@@ -96,6 +97,5 @@ int main() {
     quickSort(arr, 0, size - 1);
     std::cout << "Sorted array\n";
     show(arr, size);
-    delete[] arr;
     return 0;
 }
