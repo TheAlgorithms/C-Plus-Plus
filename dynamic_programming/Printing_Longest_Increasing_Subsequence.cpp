7b482e5be8db553cebd9b45d3089ddc97bfbe3f6
Nikhil Kumar Das <72371791+NikhilKD@users.noreply.github.com>
2022-10-01 01:28:18 +0530

    Adding Longest Increasing Subsequence code

---
+++ dynamic_programming/Printing_Longest_Increasing_Subsequence.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int longestIncreasingSubsequence(int arr[], int n){
+    
+    vector<int> dp(n,1);
+    
+    for(int i=0; i<=n-1; i++){
+        for(int prev_index = 0; prev_index <=i-1; prev_index ++){
+            
+            if(arr[prev_index]<arr[i]){
+                dp[i] = max(dp[i], 1 + dp[prev_index]);
+            }
+        }
+    }
+    
+    int ans = -1;
+    
+    for(int i=0; i<=n-1; i++){
+        ans = max(ans, dp[i]);
+    }
+    
+    return ans;
+}
+
+int main() {
+	
+	int arr[] = {10,9,2,5,3,7,101,18};
+	
+	int n = sizeof(arr)/sizeof(arr[0]);
+	
+	cout<<"The length of the longest increasing subsequence is "
+        <<longestIncreasingSubsequence(arr,n);
+	
+	return 0;
+}
