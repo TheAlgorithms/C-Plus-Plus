4388552a2c8aaec4958c73c9d7e1fdbb6f3df343
Soumyajit Mondal <mondalsoumya02@gmail.com>
2022-10-03 21:21:45 +0530

    Linked_list using array menu driven program
    I have updated the linked list program to run as a menu driven program.

--- data_structures/linkedlist_implentation_usingarray.cpp
+++ data_structures/linkedlist_implentation_usingarray.cpp
@@ -9,106 +9,98 @@
  * reuse the nodes that are to be deleted by again linking it bacj to the list.
  */
 
-#include <iostream>
-
-struct Node {
-    int data;
-    int next;
+#include<iostream>
+using namespace std;
+struct node{
+int data;
+node*next;
 };
+node *head,*newnode,*temp;
+void traversal(struct node*ptr);
+void insertion();
+void deletion();
+int search();
+int main()
+{
+char ch;
+int choice;
+head = NULL;
+do
+{
 
-Node AvailArray[100];  ///< array that will act as nodes of a linked list.
+cout<<"\n \t\t Menu Driven Program";
+cout<<"\n 1. Traversal \n 2. Insertion \n 3. Deletion \n 4. Searching";
+cout<<"\n Enter your choice:";
+cin>>choice;
+switch(choice)
+{
+        case 1: traversal(head);
+                break;
+        case 2: insertion();
+                break;
+        case 3: deletion();
+                break;
+        case 4: int pos;
+                pos=search();
+                if(pos==-1)
+                cout<<"\n Element not found";
+                else
+                cout<<"\n Element is found at Position:"<<pos;
+                break;
+        default:cout<<"wrong choice";
+}
+        cout<<"\n Do you want to continue?";
+        cout<<"(Press y/n)";
+        cin>>ch;
+        }while(ch=='y');
 
-int head = -1;
-int avail = 0;
-void initialise_list() {
-    for (int i = 0; i <= 98; i++) {
-        AvailArray[i].next = i + 1;
+return 0;
+}
+
+void traversal(struct node*ptr)
+{   while(ptr!=NULL)
+    {
+        cout<<ptr->data<<" ";
+        ptr=ptr->next;
+}
+}
+
+void insertion()
+{
+    newnode=new node();
+    cout<<"\n Enter data:";
+    cin>>newnode->data;
+    newnode->next=NULL;
+    if(head==NULL)
+    head=temp=newnode;
+    else
+    {
+        temp->next=newnode;
+        temp=newnode;
     }
-    AvailArray[99].next = -1;  // indicating the end of the linked list.
 }
-
-/** This will return the index of the first free node present in the avail list
- */
-int getnode() {
-    int NodeIndexToBeReturned = avail;
-    avail = AvailArray[avail].next;
-    return NodeIndexToBeReturned;
+void deletion()
+{
+       if(head==NULL)
+       cout<<"\n List is empty";
+       temp=head;
+       head=head->next;
+       free(temp);
 }
-
-/** This function when called will delete the node with
- * the index presented as an argument, and will put
- * back that node into the array.
- */
-void freeNode(int nodeToBeDeleted) {
-    AvailArray[nodeToBeDeleted].next = avail;
-    avail = nodeToBeDeleted;
-}
-
-/** The function will insert the given data
- * into the front of the linked list.
- */
-void insertAtTheBeginning(int data) {
-    int newNode = getnode();
-    AvailArray[newNode].data = data;
-    AvailArray[newNode].next = head;
-    head = newNode;
-}
-
-void insertAtTheEnd(int data) {
-    int newNode = getnode();
-    int temp = head;
-    while (AvailArray[temp].next != -1) {
-        temp = AvailArray[temp].next;
-    }
-    // temp is now pointing to the end node.
-    AvailArray[newNode].data = data;
-    AvailArray[newNode].next = -1;
-    AvailArray[temp].next = newNode;
-}
-
-void display() {
-    int temp = head;
-    while (temp != -1) {
-        std::cout << AvailArray[temp].data << "->";
-        temp = AvailArray[temp].next;
-    }
-    std::cout << "-1" << std::endl;
-}
-
-/** Main function */
-int main() {
-    initialise_list();
-    int x, y, z;
-    for (;;) {
-        std::cout << "1. Insert At The Beginning" << std::endl;
-        std::cout << "2. Insert At The End" << std::endl;
-        std::cout << "3. Display" << std::endl;
-        std::cout << "4.Exit" << std::endl;
-        std::cout << "Enter Your choice" << std::endl;
-        std::cin >> z;
-        switch (z) {
-        case 1:
-            std::cout << "Enter the number you want to enter" << std::endl;
-            std::cin >> x;
-            insertAtTheBeginning(x);
-            break;
-        case 2:
-            std::cout << "Enter the number you want to enter" << std::endl;
-            std::cin >> y;
-            insertAtTheEnd(y);
-            break;
-        case 3:
-            std::cout
-                << "The linked list contains the following element in order"
-                << std::endl;
-            display();
-            break;
-        case 4:
-            return 0;
-        default:
-            std::cout << "The entered choice is not correct" << std::endl;
+int search()
+{       int element;
+        cout<<"\n Enter element to be searched:";
+        cin>>element;
+        temp=head;
+        int count=0;
+        while(temp!=0)
+        {
+                count++;
+                if(temp->data==element)
+                return count;
+                temp=temp->next;
         }
-    }
+        return -1;
 
-    return 0;
 }
+
