8be172b9d81a709852e3de685717496a7688779b
Aryan Gupta <aryan899gupta@gmail.com>
2022-10-04 21:50:32 +0530

    Add Longest_Alternating_Subsequence in Dynamic Programming

--- dynamic_programming/Longest_Alternating_Subsequence.cpp
+++ dynamic_programming/Longest_Alternating_Subsequence.cpp
@@ -1,3 +1,25 @@
+/*
+A sequence {x1, x2, .. xn} is alternating sequence if its elements satisfy one of the following relations :
+x1 < x2 > x3 < x4 > x5..... or  x1 >x2 < x3 > x4 < x5.....
+Your task is to find the longest such sequence.
+
+Example 1:
+
+Input: nums = {1,5,4}
+Output: 3
+Explanation: The entire sequenece is a 
+alternating sequence.
+
+
+Examplae 2:
+
+Input: nums = {1,17,5,10,13,15,10,5,16,8}
+Output: 7
+Explanation: There are several subsequences
+that achieve this length. 
+One is {1,17,10,13,10,16,8}.
+*/
+
 #include<iostream>
 #include<vector>
 using namespace std;
