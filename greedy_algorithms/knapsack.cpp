350817cc0b70d3f65d1defe84caa07238e82e441
Priyanshuiiitg <98309398+Priyanshuiiitg@users.noreply.github.com>
2022-10-05 15:42:40 +0530

    knapsack.cpp

--- greedy_algorithms/knapsack.cpp
+++ greedy_algorithms/knapsack.cpp
@@ -1,78 +1,172 @@
-#include <iostream>
+#include <bits/stdc++.h>
+#define int long long int
+#define f first
+#define s second
+#define pb push_back
+
 using namespace std;
-
-struct Item {
-    int weight;
-    int profit;
-};
-
-float profitPerUnit(Item x) { return (float)x.profit / (float)x.weight; }
-
-int partition(Item arr[], int low, int high) {
-    Item pivot = arr[high];  // pivot
-    int i = (low - 1);       // Index of smaller element
-
-    for (int j = low; j < high; j++) {
-        // If current element is smaller than or
-        // equal to pivot
-        if (profitPerUnit(arr[j]) <= profitPerUnit(pivot)) {
-            i++;  // increment index of smaller element
-            Item temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-        }
-    }
-    Item temp = arr[i + 1];
-    arr[i + 1] = arr[high];
-    arr[high] = temp;
-    return (i + 1);
-}
-
-void quickSort(Item arr[], int low, int high) {
-    if (low < high) {
-        int p = partition(arr, low, high);
-
-        quickSort(arr, low, p - 1);
-        quickSort(arr, p + 1, high);
-    }
-}
-
-int main() {
-    cout << "\nEnter the capacity of the knapsack : ";
-    float capacity;
-    cin >> capacity;
-    cout << "\n Enter the number of Items : ";
+struct segmentTree
+{
+    vector<int> st;
     int n;
-    cin >> n;
-    Item itemArray[n];
-    for (int i = 0; i < n; i++) {
-        cout << "\nEnter the weight and profit of item " << i + 1 << " : ";
-        cin >> itemArray[i].weight;
-        cin >> itemArray[i].profit;
+    void init(int size)
+    {
+        this->n=size;
+        st.resize(4*n,0);
+        
+    }
+    void build(int start,int end,int node ,vector<int> &v)
+    {
+        if(start==end)
+        {
+            st[node]=v[start];
+            return ;
+
+        }
+
+        int mid=(start+end)/2;
+        build(start,mid,2*node+1,v);
+        build(mid+1,end,2*node+2,v);
+        st[node]=st[2*node+1]+st[2*node+2];
+
+
+    }
+    void build(vector<int> &v)
+    {
+        build(0,v.size()-1,0,v);
+
+    }
+    int query(int start,int end,int l,int r,int node)
+    {
+        if(start>r || end<l)//non overlapping case
+        return 0;
+        if(start>=l && end<=r)//completey overlapping region
+        return st[node];
+//partial case
+        int mid=(start+end)/2;
+        int q1=query(start,mid,l,r,2*node+1);
+        int  q2=query(mid+1,end,l,r,2*node+2);
+        return q1+q2;
+
+
+    }
+    int query(int l,int r)
+    {
+        return query(0,n-1,l,r,0);//first four parameters are for checking overlapping,no-overlapping and partially overlapping conditions
+
+    }
+    void update(int start,int end ,int node,int index ,int value)
+    {
+        if(start==end)
+        {
+            st[node]=value;
+            return ;
+
+        }
+        int mid=(start+end)/2;
+        if(index<=mid)
+        {
+            update(start,mid,2*node+1,index,value);
+        }
+        else
+        {
+            update(mid+1,end,2*node+2,index,value);
+
+        }
+        st[node]=st[2*node +1]+st[2*node+2];
+        return ;
+
+    }
+    void update(int x,int y )//at index 'x ' update the value to 'y'
+    {
+        update(0,n-1,0,x,y);//start from the root node
+
     }
 
-    quickSort(itemArray, 0, n - 1);
 
-    // show(itemArray, n);
+};
+struct fenwick
+{
+    int n;
+    vector<int> fn;
+    void init(int n)
+    {
+        this->n=n+1;
+        fn.resize(this->n,0);
 
-    float maxProfit = 0;
-    int i = n;
-    while (capacity > 0 && --i >= 0) {
-        if (capacity >= itemArray[i].weight) {
-            maxProfit += itemArray[i].profit;
-            capacity -= itemArray[i].weight;
-            cout << "\n\t" << itemArray[i].weight << "\t"
-                 << itemArray[i].profit;
-        } else {
-            maxProfit += profitPerUnit(itemArray[i]) * capacity;
-            cout << "\n\t" << capacity << "\t"
-                 << profitPerUnit(itemArray[i]) * capacity;
-            capacity = 0;
-            break;
+    }
+    void add(int x,int y)
+    {
+        x++;
+        while(x<n)
+        {
+            fn[x]+=y;
+            x+=(x&(-x));
+
         }
     }
+    int sum(int x)
+    {
+        x++;
+        int ans=0;
+        while(x)
+        {
+            ans+=fn[x];
+            x-=(x&(-x));
 
-    cout << "\nMax Profit : " << maxProfit;
+        }
+        return ans;
 
-    return 0;
+    }
+    int sum(int l,int r)
+    {
+        return sum(r)-sum(l-1);
+
+    }
+};
+const int N=100;
+int v[N],w[N],W,n;
+int memoise[N][100000];
+
+int dp(int index,int weight)
+{
+    if(index>=n)
+        return 0;
+    int &ans=memoise[index][weight];
+    if(ans!=-1)
+        return ans;
+
+     ans=dp(index+1,weight);
+if(w[index]+weight<=W)
+{
+    ans=max(ans,v[index]+dp(index+1,weight+w[index]));
+}
+return ans;
+
+}
+
+
+int32_t main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+    memset(memoise,-1,sizeof(memoise));
+
+    cin>>n>>W;
+    for(int i=0;i<n;i++)
+    {
+        cin>>w[i]>>v[i];
+
+    }
+    cout<<dp(0,0)<<endl;
+
+ 
+   
+
+
+
+
+
 }
