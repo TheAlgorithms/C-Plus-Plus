8dc6fb90e6d43157e0e4a0aaee26fe455c1ed5b0
Srusti Gourani <79088553+srusti610@users.noreply.github.com>
2022-10-09 12:20:01 +0530

    Create Dijstras.cpp
    Kindly merge the request.

---
+++ Dijstras.cpp
@@ -0,0 +1,64 @@
+#include
+#include
+using namespace std;
+
+int miniDist(int distance[], bool Tset[]) // finding minimum distance
+{
+    int minimum=INT_MAX,ind;
+              
+    for(int k=0;k<6;k++) 
+    {
+        if(Tset[k]==false && distance[k]<=minimum)      
+        {
+            minimum=distance[k];
+            ind=k;
+        }
+    }
+    return ind;
+}
+
+void DijkstraAlgo(int graph[6][6],int src) // adjacency matrix 
+{
+    int distance[6]; // // array to calculate the minimum distance for each node                             
+    bool Tset[6];// boolean array to mark visited and unvisited for each node
+    
+     
+    for(int k = 0; k<6; k++)
+    {
+        distance[k] = INT_MAX;
+        Tset[k] = false;    
+    }
+    
+    distance[src] = 0;   // Source vertex distance is set 0               
+    
+    for(int k = 0; k<6; k++)                           
+    {
+        int m=miniDist(distance,Tset); 
+        Tset[m]=true;
+        for(int k = 0; k<6; k++)                  
+        {
+            // updating the distance of neighbouring vertex
+            if(!Tset[k] && graph[m][k] && distance[m]!=INT_MAX && distance[m]+graph[m][k]<distance[k])
+                distance[k]=distance[m]+graph[m][k];
+        }
+    }
+    cout<<"Vertex\t\tDistance from source vertex"<<endl;
+    for(int k = 0; k<6; k++)                      
+    { 
+        char str=65+k; 
+        cout<<str<<"\t\t\t"<<distance[k]<<endl;
+    }
+}
+
+int main()
+{
+    int graph[6][6]={
+        {0, 1, 2, 0, 0, 0},
+        {1, 0, 0, 5, 1, 0},
+        {2, 0, 0, 2, 3, 0},
+        {0, 5, 2, 0, 2, 2},
+        {0, 1, 3, 2, 0, 1},
+        {0, 0, 0, 2, 1, 0}};
+    DijkstraAlgo(graph,0);
+    return 0;                           
+}
