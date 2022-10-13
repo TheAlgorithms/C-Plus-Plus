6b2169375865cc10c2230243687fd51924aec6cf
Koushik Chakraborty <72178398+KoushikAD1234@users.noreply.github.com>
2022-10-13 10:55:33 +0530

    Floyed warshall algorithm

---
+++ graph/Floyed_Warshall.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define mod 1e+7
+#define inf 1e18
+#define pb push_back
+#define eb emplace_back
+#define vi vector<ll>
+#define vvi vector<vi>
+#define vs vector<string>
+#define pii pair<ll, ll>
+#define mp map
+#define ump unordered_map
+#define loop(i, a, b) for(ll i=a; i<=b; i++)
+#define looprev(i, a, b) for(ll i=a; i>=b; i--)
+#define mid l+((r-l)/2)
+
+const int N = 1e10+9;
+
+void solve(){
+    ll n, m;
+    cin>>n>>m;
+
+}
+
+int main()
+{
+ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+int t;
+cin>>t;
+while(t--){
+solve();
+}
+return 0;
+}
