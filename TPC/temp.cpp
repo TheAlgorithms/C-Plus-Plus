58dfee0ff82028eb28f6a2ef97af1b34576936cd
Koushik Chakraborty <72178398+KoushikAD1234@users.noreply.github.com>
2022-10-13 10:45:09 +0530

    Adding Template for CP

---
+++ TPC/temp.cpp
@@ -0,0 +1,33 @@
+// #include<bits/stdc++.h> includes all header files...
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll               long long int
+#define mod              1e+7
+#define inf              1e18
+#define pb               push_back
+#define eb               emplace_back
+#define vi               vector<ll>
+#define vvi              vector<vi>
+#define vs               vector<string>
+#define pii              pair<ll, ll>
+#define mp               map
+#define ump              unordered_map
+#define loop(i, a, b)    for (ll i = a; i <= b; i++)
+#define looprev(i, a, b) for (ll i = a; i >= b; i--)
+#define mid              l + ((r - l) / 2)
+
+void solve() {
+    // Your codes starts from here...
+}
+
+int main() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int t;
+    cin >> t;  // t -> test case
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
