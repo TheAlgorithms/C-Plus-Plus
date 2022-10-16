0d22b680c7210d0a146183d60bc5357b202b7b5b
PuneetTri <43619937+PuneetTri@users.noreply.github.com>
2022-10-16 17:44:49 +0530

    Update priority_queue.cpp

--- data_structures/priority_queue.cpp
+++ data_structures/priority_queue.cpp
@@ -17,22 +17,16 @@
  * @author [Puneet Tripathi](https://github.com/PuneetTri)
  */
 
-#include<iostream>
-using namespace std;
+#include<iostream> // For standard I/O
+#include<cassert> // For testcases check
+#include<utility> // For importing swap utility function
 
 // Class 
 class PQ{
     private:
         int capacity; // max capacity of PQ
-        int *arr = NULL; // Pointer to the array of the PQ
+        int *arr; // Pointer to the array of the PQ
         int N; // current size of PQ or total elements currently in PQ
-        
-        // Helper function to swap elements
-        void swap(int *a, int *b) {
-            int temp = *a;
-            *a = *b;
-            *b = temp;
-        }
 
         /* Helper function for the max-heap implementation of PQ
         *  Function would help promte child node to their correct 
@@ -42,7 +36,7 @@ class PQ{
             // Check if parent is smaller than child node
             while(pos > 1 && (arr[pos/2] < arr[pos])) {
                 // In such case swap value of child with parent
-                swap(&arr[pos], &arr[pos/2]);
+                std::swap(arr[pos], arr[pos/2]);
                 pos = pos/2; // Jump to position of parent node
             }
             // Promotion of child node will go on until it becomes smaller than the parent
@@ -60,29 +54,28 @@ class PQ{
                 if(current < N && arr[current] < arr[current+1]) current++;
                 // If the parent node is greater, sink operation is complete. Break the loop
                 if(arr[pos] >= arr[current]) break; 
-                swap(&arr[pos], &arr[current]); // If not exchange the value of parent with child
+                std::swap(arr[pos], arr[current]); // If not exchange the value of parent with child
                 pos = current; // Exchange parent position to child position in the array
             }
         }
     public:
-        PQ() {
+        PQ(int max=11) {
             /* If capacity is not defined, default size of 11 would be used
-            *  capacity=12 because we cant access 0th element of PQ, and to 
-            *  accomodate 11 elements we need capacity to be 12.
+            *  capacity=max+1 because we cant access 0th element of PQ, and to 
+            *  accomodate (max)th elements we need capacity to be max+1.
             *  Parent is at position k, child at position (k*2,k*2+1), if we
             *  use positon 0 in our queue, its child would be at:
             *  (0*2, 0*2+1) -> (0,0). This is why we start at position 1
             */
-            capacity = 12; 
-            arr = new int[capacity];
-            for(int i=0; i<capacity; i++) arr[i] = 0; // Initializing array with value 0
-            N = 0; // Initializing size of PQ 
-        }
-        PQ(int max) {
             capacity = max+1;
             arr = new int[capacity];
             for(int i=0; i<capacity; i++) arr[i] = 0; // Initializing array with value 0
             N = 0; // Initializing size of PQ 
+        }
+
+        // Deallocate memory when PQ is no further in scope
+        ~PQ() {
+            delete[] arr;
         }
         
         // Method to check whether PQ is empty or not
@@ -104,7 +97,7 @@ class PQ{
         void add(int val) {
             // Print overflow message if the capacity is full
             if(N == capacity){
-                cout << "Overflow!" << endl;
+                std::cout << "Overflow!" << std::endl;
                 return;
             }
             arr[++N] = val;
@@ -115,11 +108,11 @@ class PQ{
         int poll() {
             // Print underflow message if the PQ has no elements
             if(N == 0){
-                cout << "Underflow!" << endl;
+                std::cout << "Underflow!" << std::endl;
                 return -1;
             }
             int max = arr[1]; // By defintion of our max-heap, value at arr[1] pos is the greatest
-            swap(&arr[1], &arr[N]);
+            std::swap(arr[1], arr[N]);
             sink(1); // Sink the element in order
             arr[N--] = 0; // Nullify the deleted element from the priority queue
             
@@ -127,28 +120,51 @@ class PQ{
         }    
 };
 
-int main() {
-    PQ pq(20); // Initalizing PQ object
+static void test() {
+    /* Following tests would check multiple methods that are defined
+    *  in PQ, and check if they work correctly
+    */
+    
+    PQ pq; // Initalizing PQ object (By default size = 11)
 
     // Example testcases to check if PQ is working fine
+
     if(pq.isEmpty()) {
-        cout << "Priority queue is empty" << endl;
+        std::cout << "Priority queue is empty" << std::endl;
     } else {
-        cout << "Priority queue is not empty" << endl;
+        std::cout << "Priority queue is not empty" << std::endl;
     }
+    assert(pq.isEmpty() == true); // Test-case #1 
+   
     pq.add(15);
-    cout << "Max element: " << pq.peek() << endl;
+    std::cout << "Max element: " << pq.peek() << std::endl;
+    assert(pq.peek() == 15); // Test-case #2 
+
     if(pq.isEmpty()) {
-        cout << "Priority queue is empty" << endl;
+        std::cout << "Priority queue is empty" << std::endl;
     } else {
-        cout << "Priority queue is not empty" << endl;
+        std::cout << "Priority queue is not empty" << std::endl;
     }
-    cout << "Element removed: " << pq.poll() << endl;
+    assert(pq.isEmpty() == false); // Test-case #3
+
+    int temp = pq.poll(); // Value retrieved from poll operation
+    std::cout << "Element removed: " << temp << std::endl;
+    assert(temp == 15); // Test-case #4
+
     pq.add(12);
     pq.add(10);
     pq.add(8);
-    cout << "Size of PQ: " << pq.size() << endl;
-    cout << "Max element: " << pq.peek() << endl;
+    std::cout << "Size of PQ: " << pq.size() << std::endl;
+    assert(pq.size() == 3); // Test-case #5
+
+    std::cout << "Max element: " << pq.peek() << std::endl;
+    assert(pq.peek() == 12); // Test-case #6
+
     pq.add(17);
-    cout << "Max element: " << pq.peek() << endl;
+    std::cout << "Max element: " << pq.peek() << std::endl;
+    assert(pq.peek() == 17); // Test-case #7
+}
+
+int main() {
+    test(); // Run multiple testcases to test our algorithm
 }
