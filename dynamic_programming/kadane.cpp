11c47bb3fe087d6c5b375bb3044d57f99513a8c3
MONU8OO8 <monu270800@gmail.com>
2022-10-17 03:31:21 +0530

    update kadane algo

--- dynamic_programming/kadane.cpp
+++ dynamic_programming/kadane.cpp
@@ -1,16 +1,20 @@
+#include<bits/stdc++.h>
 #include <climits>
-#include <iostream>
+
+int max(int a,int b){
+    return a>b ? a:b;
+}
 
 int maxSubArraySum(int a[], int size) {
     int max_so_far = INT_MIN, max_ending_here = 0;
 
     for (int i = 0; i < size; i++) {
-        max_ending_here = max_ending_here + a[i];
-        if (max_so_far < max_ending_here)
-            max_so_far = max_ending_here;
+        max_ending_here = max_ending_here+a[i];
+        max_so_far = max(max_so_far,max_ending_here);
 
-        if (max_ending_here < 0)
+        if(max_ending_here<0){
             max_ending_here = 0;
+        }
     }
     return max_so_far;
 }
