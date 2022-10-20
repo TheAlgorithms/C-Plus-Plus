760990af6714d03644e0287e4eca85bfb9599c49
Saqlain Zarjis Ansari <110441918+Mysterious786@users.noreply.github.com>
2022-10-20 23:21:41 +0530

    Update Queue_implementation.cpp

--- data_structures/Queue_implementation.cpp
+++ data_structures/Queue_implementation.cpp
@@ -1,54 +1,73 @@
-// CPP program to implement Queue using
-// two stacks with costly deQueue()
-#include <bits/stdc++.h>
-using namespace std;
+//Stack is an LIFO data structure which means last in first out.
+//Queue is an FIF0 data structure which means first in first out.
+// iostream stands for  standard input-output stream,this header file contains definitions of objects like cin, cout.
+// stacks and queue library are used for implementing inbuilt stacks and queues
+#include <iostream>
+#include <stack>
+#include <queue>
 
-struct Queue {
-	stack<int> s1, s2;
+struct Queue
+{
+    //std is an abbreviation of "standard". std is the "standard namespace". cout , cin are only defined in the “std” namespace. 
 
-	// Enqueue an item to the queue
-	void enQueue(int x)
-	{
-		// Push item into the first stack
-		s1.push(x);
-	}
+    std::stack<int> stack1, stack2;
 
-	// Dequeue an item from the queue
-	int deQueue()
-	{
-		// if both stacks are empty
-		if (s1.empty() && s2.empty()) {
-			cout << "Q is empty";
-			exit(0);
-		}
+    // enqueue --> function used to push element
+    void enqueue(int x)
+    {
+        // Move all elements from s1 to s2
+        while (!stack1.empty())
+        {
+            stack2.push(stack1.top());
+            stack1.pop();
+        }
 
-		// if s2 is empty, move
-		// elements from s1
-		if (s2.empty()) {
-			while (!s1.empty()) {
-				s2.push(s1.top());
-				s1.pop();
-			}
-		}
+        // Push item into s1
+        stack1.push(x);
 
-		// return the top item from s2
-		int x = s2.top();
-		s2.pop();
-		return x;
-	}
+        // Push everything back to s1
+        while (!stack2.empty())
+        {
+            stack1.push(stack2.top());
+            stack2.pop();
+        }
+    }
+
+    // Dequeue(function to remove an element) an item from the queue
+    int dequeue()
+    {
+        // if first stack is empty
+        if (stack1.empty())
+        {
+            std::cout << "queue is Empty";
+            exit(0);
+        }
+
+        // Return the top of stack1
+        int x = stack1.top();
+        stack1.pop();
+        return x;
+    }
 };
 
-// Driver code
 int main()
 {
-	Queue q;
-	q.enQueue(1);
-	q.enQueue(2);
-	q.enQueue(3);
+    Queue q;
+    q.enqueue(3);
+    q.enqueue(4);
+    q.enqueue(5);
+    q.enqueue(6);
+    q.enqueue(7);
 
-	cout << q.deQueue() << '\n';
-	cout << q.deQueue() << '\n';
-	cout << q.deQueue() << '\n';
+    std::cout << q.dequeue() <<" ";
+    std::cout << q.dequeue() <<" ";
+    std::cout << q.dequeue()<<" ";
+    std::cout << q.dequeue()<<" ";
+    std::cout << q.dequeue()<<" ";
 
-	return 0;
+
+    return 0;
 }
+// OUTPUT-->  3 4 5 6 7 
+
+
