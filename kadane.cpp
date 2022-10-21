7dbff966d8ec918d7c9947573bf19b26f89fc3b7
Masoom9587 <93204893+Masoom9587@users.noreply.github.com>
2022-10-21 10:00:39 +0530

    Largest Sum Contiguous Subarray (Kadane’s Algorithm)

---
+++ kadane.cpp
@@ -0,0 +1,30 @@
+// C++ program to print largest contiguous array sum
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxSubArraySum(int a[], int size)
+{
+	int max_so_far = INT_MIN, max_ending_here = 0;
+
+	for (int i = 0; i < size; i++) {
+		max_ending_here = max_ending_here + a[i];
+		if (max_so_far < max_ending_here)
+			max_so_far = max_ending_here;
+
+		if (max_ending_here < 0)
+			max_ending_here = 0;
+	}
+	return max_so_far;
+}
+
+// Driver Code
+int main()
+{
+	int a[] = { -2, -3, 4, -1, -2, 1, 5, -3 };
+	int n = sizeof(a) / sizeof(a[0]);
+
+	// Function Call
+	int max_sum = maxSubArraySum(a, n);
+	cout << "Maximum contiguous sum is " << max_sum;
+	return 0;
+}
