c4819d6094e3bb9fb271898d85965616e5af072c
MALAY SHAH <107268610+MALAY-21@users.noreply.github.com>
2022-10-22 17:42:36 +0530

    Update check_prime.cpp
    Updated the code to check whether a number is Prime or not

--- math/check_prime.cpp
+++ math/check_prime.cpp
@@ -1,62 +1,51 @@
 /**
- * Copyright 2020 @author omkarlanghe
  *
  * @file
  * A simple program to check if the given number if prime or not.
  *
  * @brief
  * Reduced all possibilities of a number which cannot be prime.
- * Eg: No even number, except 2 can be a prime number, hence we will increment
- * our loop with i+6 jumping and check for i or i+2 to be a factor of the
- * number; if it's a factor then we will return false otherwise true after the
- * loop terminates at the terminating condition which is (i*i<=num)
  */
 
-#include <cassert>   /// for assert
-#include <iostream>  /// for IO operations
+#include <bits/stdc++.h> 
+using namespace std;
 
 /**
  * Function to check if the given number is prime or not.
  * @param num number to be checked.
- * @return if number is prime, it returns @ true, else it returns @ false.
+ * @return if number is prime, it returns @ is a Prime, else it returns @ Not a Prime.
+ * if a number is -ve or else 0 or 1 it returns @false
  */
-template <typename T>
-bool is_prime(T num) {
-    bool result = true;
-    if (num <= 1) {
-        return false;
-    } else if (num == 2 || num == 3) {
-        return true;
-    } else if ((num % 2) == 0 || num % 3 == 0) {
-        return false;
-    } else {
-        for (T i = 5; (i * i) <= (num); i = (i + 6)) {
-            if ((num % i) == 0 || (num % (i + 2) == 0)) {
-                result = false;
-                break;
-            }
-        }
-    }
-    return (result);
-}
 
 /**
  * Main function
  */
-int main() {
-    // perform self-test
-    assert(is_prime(50) == false);
-    assert(is_prime(115249) == true);
+int main(){
 
-    int num = 0;
-    std::cout << "Enter the number to check if it is prime or not" << std::endl;
-    std::cin >> num;
-    bool result = is_prime(num);
-    if (result) {
-        std::cout << num << " is a prime number" << std::endl;
-    } else {
-        std::cout << num << " is not a prime number" << std::endl;
-    }
+    long long int n;
+    cin>>n;
 
-    return 0;
+    bool isPrime = 1;
+
+    if (n <= 1) 
+        return false;
+
+     for(int i=2; i<n; i++){
+
+         if(n%i==0){
+             isPrime=0;
+             break;
+         }
+     }
+
+      if(isPrime==0){
+          cout<<"Not a Prime"<<endl;
+      }
+
+       else{
+           cout<<"is a Prime"<<endl;
+       }
+ return 0;
 }
+
+// edited by Malay Shah
