633e5e3cae92e28603ad888f2db2bc0b46073a30
Akshat Bokdia <101403537+AkshatBokdia0602@users.noreply.github.com>
2022-10-22 19:43:13 +0530

    Update stack_using_array.cpp

--- data_structures/stack_using_array.cpp
+++ data_structures/stack_using_array.cpp
@@ -1,62 +1,84 @@
 #include <iostream>
+using namespace std;
+struct Stack
+{
+    int size;
+    int top;
+    int * arr;
+    
+    Stack(int s)
+    {
+        size = s;
+        top = -1;
+        arr = new int[size];
+    }
+};
 
-int *stack;
-int stack_idx = 0, stack_size;
+bool isEmpty(Stack * s)
+{
+    return (s->top == -1);
+}
 
-void push(int x) {
-    if (stack_idx == stack_size) {
-        std::cout << "\nOverflow";
+bool isFull(Stack * s)
+{
+    return (s->top == s->size - 1);
+}
+
+void push(Stack * s, int x) {
+    if (isFull(s)) {
+        cout << "\nOverflow";
     } else {
-        stack[stack_idx++] = x;
+        s->arr[++s->top] = x;
     }
 }
 
-void pop() {
-    if (stack_idx == 0) {
-        std::cout << "\nUnderflow";
+void pop(Stack * s) {
+    if (isEmpty(s)) {
+        cout << "\nUnderflow";
     } else {
-        std::cout << "\n" << stack[--stack_idx] << " deleted";
+        cout << "\n" << s->arr[s->top--] << " deleted";
     }
 }
 
-void show() {
-    for (int i = 0; i < stack_idx; i++) {
-        std::cout << stack[i] << "\n";
+void show(Stack * s) {
+    for (int i = 0; i <= s->top; i++) {
+        cout << s->arr[i] << "\n";
     }
 }
 
-void topmost() { std::cout << "\nTopmost element: " << stack[stack_idx - 1]; }
-void bottom() { std::cout << "\nBottom element: " << stack[0]; } // If we need access to first element without using pop command 
+void topmost(Stack * s) { cout << "\nTopmost element: " << s->arr[s->top]; }
+void bottom(Stack * s) { cout << "\nBottom element: " << s->arr[0]; } // If we need access to first element without using pop command 
 int main() {
-    std::cout << "\nEnter stack_size of stack : ";
-    std::cin >> stack_size;
-    stack = new int[stack_size];
+    int stack_size;
+    cout << "\nEnter stack_size of stack : ";
+    cin >> stack_size;
+    Stack * st = new Stack(stack_size);
     int ch, x;
     do {
-        std::cout << "\n0. Exit";
-        std::cout << "\n1. Push";
-        std::cout << "\n2. Pop";
-        std::cout << "\n3. Print";
-        std::cout << "\n4. Print topmost element:";
-        std::cout << "\n5. Print Bottom element:";
-        std::cout << "\nEnter Your Choice : ";
-        std::cin >> ch;
+        cout << "\n0. Exit";
+        cout << "\n1. Push";
+        cout << "\n2. Pop";
+        cout << "\n3. Print";
+        cout << "\n4. Print topmost element:";
+        cout << "\n5. Print Bottom element:";
+        cout << "\nEnter Your Choice: ";
+        cin >> ch;
         if (ch == 1) {
-            std::cout << "\nInsert : ";
-            std::cin >> x;
-            push(x);
+            cout << "\nInsert : ";
+            cin >> x;
+            push(st,x);
         } else if (ch == 2) {
-            pop();
+            pop(st);
         } else if (ch == 3) {
-            show();
+            show(st);
         } else if (ch == 4) {
-            topmost();
+            topmost(st);
         } else if(ch == 5) {
-            bottom();
+            bottom(st);
         }
     } while (ch != 0);
 
-    delete[] stack;
+    delete[] st;
 
     return 0;
 }
