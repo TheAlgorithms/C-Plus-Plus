4f8d5dd002a8479e4e87e4c62890a033f21ee2db
GurramSaiSupraja <66518111+GurramSaiSupraja@users.noreply.github.com>
2022-10-24 07:33:39 +0530

    Create buy_And_sell_stocks.cpp

---
+++ greedy_algorithms/buy_And_sell_stocks.cpp
@@ -0,0 +1,22 @@
+/*
+You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+
+On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However, you can buy it then immediately sell it on the same day.
+
+Find and return the maximum profit you can achieve.
+*/
+
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int ans=0;
+        for(int i=1;i<prices.size();i++)
+        {
+            if(prices[i]>prices[i-1])
+            {
+                ans+=prices[i]-prices[i-1];
+            }
+        }
+        return ans;
+    }
