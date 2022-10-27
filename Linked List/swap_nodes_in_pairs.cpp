13611e8361415cbe029896c41e4c665931e9c890
Arpita Mohanty <58506260+Arpita1401@users.noreply.github.com>
2022-10-27 08:48:58 +0530

    Update swap_nodes_in_pairs.cpp

--- Linked List/swap_nodes_in_pairs.cpp
+++ Linked List/swap_nodes_in_pairs.cpp
@@ -1,11 +1,13 @@
-//Question link: 
+//Question link: https://leetcode.com/problems/swap-nodes-in-pairs/
+//Problem Description: Given a linked list, swap every two adjacent nodes and return its head.
+//                     You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
 
 
-
-//Problem Approach: pp points to the pointer to the current node.
+//Approach: pp points to the pointer to the current node.
 //So at first, pp points to head, and later it points to the next field of ListNodes.
 //Pointers curr and nxt point to the current node and the next node.
 //We need to go from *pp == curr -> nxt -> (nxt->next) to *pp == nxt -> curr -> (nxt->next).
+
 
 //Solution:
 /**
@@ -21,12 +23,16 @@
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) { 
-        ListNode **pp = &head, *a, *b;
-        while ((a = *pp) && (b = a->next)) {
-            a->next = b->next;
-            b->next = a; 
-            *pp = b; 
-            pp = &(a->next);
+        ListNode **pp = &head, *curr, *nxt;
+        while ((curr = *pp) && (nxt = curr->next)) {
+            
+            //Swapping the nodes
+            curr->next = nxt->next;
+            nxt->next = curr; 
+            *pp = nxt; 
+            
+            //Traversing in the list
+            pp = &(curr->next);
         } 
         return head;
     }
