54616433e9c43c102b13f40f7c6d8439c6d2f750
Sonu Kushwaha <77428175+SonuKushwaha-hub@users.noreply.github.com>
2022-10-29 01:51:56 +0530

    Add files via upload

---
+++ strings/intToRoman.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string intToRoman(int num)
+{
+    string ones[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+    string tens[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+    string hrns[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+    string ths[] = {"", "M", "MM", "MMM"};
+
+    return ths[num / 1000] + hrns[(num % 1000) / 100] + tens[(num % 100) / 10] + ones[num % 10];
+}
+int main()
+{
+    int num = 24;
+    cout << "Enter the number: ";
+    cin >> num;
+    cout << intToRoman(num) << endl;
+}
