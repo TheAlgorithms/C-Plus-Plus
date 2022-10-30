408029d20f1e103729f91372bbc8eef7cfd8d71b
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2022-10-30 23:31:48 +0530

    Made some required changes

--- math/aliquot_sum.cpp
+++ math/aliquot_sum.cpp
@@ -1,37 +1,47 @@
 /**
  * @file
- * \brief Program to return the [Aliquot Sum](https://en.wikipedia.org/wiki/Aliquot_sum) of a number
- * 
+ * @brief Program to return the [Aliquot
+ * Sum](https://en.wikipedia.org/wiki/Aliquot_sum) of a number
+ *
  * \details
  * The Aliquot sum s(n) of a non-negative integer n is the sum of all
  * proper divisors of n, that is, all the divisors of n, other than itself.
  * For example, the Aliquot sum of 18 = 1 + 2 + 3 + 6 + 9 = 21
+ *
+ * @author [SpiderMath](https://github.com/SpiderMath)
  */
 
-#include <cassert>
+#include <cassert>   /// for assert
+#include <iostream>  /// for IO operations
 
+/**
+ * @brief Mathematical algorithms
+ * @namespace math
+ */
+namespace math {
 /**
  * Function to return the aliquot sum of a number
  * @param num The input number
  */
-unsigned long long int aliquot_sum(const unsigned long long int num) {
-    if(num == 0 || num == 1) {
-        return 0; // The aliquot sum for 0 and 1 is 0
+uint64_t aliquot_sum(const uint64_t num) {
+    if (num == 0 || num == 1) {
+        return 0;  // The aliquot sum for 0 and 1 is 0
     }
 
-    unsigned long long int sum = 0;
+    uint64_t sum = 0;
 
-    for (unsigned long long int i = 1; i <= num / 2; i++) {
-        if(num % i == 0) {
+    for (uint64_t i = 1; i <= num / 2; i++) {
+        if (num % i == 0) {
             sum += i;
         }
     }
 
     return sum;
 }
-
+}  // namespace math
 /**
- * Function for testing the aliquot_sum() function with all the test cases
+ * @brief Self-test implementations
+ * @returns void
  */
 void test() {
     // Aliquot sum of 10 is 1 + 2 + 5 = 8
@@ -42,12 +52,16 @@ void test() {
     assert(aliquot_sum(1) == 0);
     // Aliquot sum of 97 is 1 (the aliquot sum of a prime number is 1)
     assert(aliquot_sum(97) == 1);
+
+    std::cout << "All the tests have successfully passed!\n";
 }
 
 /**
- * Main function
+ * @brief Main function
+ * @returns 0 on exit
  */
 int main() {
-    test();
+    test();  // run the self-test implementations
+
     return 0;
 }
