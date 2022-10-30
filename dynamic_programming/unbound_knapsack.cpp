3360198b3a8fb8aeef5a625d821926d497bbea34
Shivesh Singh Rao <94308757+ShiveshSinghRao@users.noreply.github.com>
2022-10-30 18:25:03 +0530

    Create unbound_knapsack.cpp

---
+++ dynamic_programming/unbound_knapsack.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int Un_knapsack(int wt[], int val[], int W, int n) {
+	int t[n + 1][W + 1]; // DP matrix
+
+	for (int i = 0; i <= n; i++) {
+		for (int j = 0; j <= W; j++) {
+			if (i == 0 || j == 0) // base case
+				t[i][j] = 0;
+			else if (wt[i - 1] <= j) { // current wt can fit in bag
+				int val1 = val[i - 1] + t[i][j - wt[i - 1]]; // take current wt
+				int val2 = t[i - 1][j]; // skip current wt
+				t[i][j] = max(val1, val2);
+			}
+			else if (wt[i - 1] > j) // current wt doesn't fit in bag
+				t[i][j] = t[i - 1][j];
+		}
+	}
+
+	return t[n][W];
+}
+
+signed main() {
+	int n; cin >> n; // number of items
+	int val[n], wt[n]; // values and wts array
+	for (int i = 0; i < n; i++)
+		cin >> wt[i];
+	for (int i = 0; i < n; i++)
+		cin >> val[i];
+	int W; cin >> W; // capacity
+
+	cout << Un_knapsack(wt, val, W, n) << endl;
+	return 0;
+}
