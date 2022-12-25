d5964b7a2ea05059b3b6b6eabb2e27ff078f8c4c
xakep <21157775+xakep8@users.noreply.github.com>
2022-12-25 19:19:42 +0530

    Updated the requested changes

--- math/binary_exponentiation.cpp
+++ math/binary_exponentiation.cpp
@@ -1,22 +1,12 @@
 #include<iostream>
 #include<cassert>
 
-
-#define ll long long
-#define pb push_back
-#define rep(i,a,b) for(int i=a;i<b;i++)
-#define sq(a) (a)*(a)
-#define vi vector<int>
-#define mp make_pair
-#define ii pair<int,int>
-#define vii vector<ii>
-
 // the concept is to do exponentiation by converting the power to a sum of powers of 2.
 // example: 3^13  we can convert 13 to 8+4+1. which can be represented by 1101. so to do 3^13 we can do instead
 // 3^8 x 3^4 x 3^1. and so we get 3^13 and since every exponent is square of the old exponent eg: 3^8=(3^4)^2.
 
-ll binary_exponentiation(ll a,ll b){
-  ll res=1;   //
+long long binary_exponentiation(ll a,ll b){
+  long long res=1;   //
   while(b>0){
     if(b&1){
       res=res*a;
@@ -39,7 +29,7 @@ int main(){
   std::cout<<"Please enter the mode you want to use \n (1) Self-Test Mode \n (2) Interactive Test Mode \n";
   std::cin>>mode;
   if(mode==1){
-    ll a,b;
+    long long a,b;
     std::cout<<"Enter the base:\n";
     std::cin>>a;
     std::cout<<"Enter the exponent:\n";
