c6ee338736e4913b407254067723129a7d2d0d10
David Leal <halfpacho@gmail.com>
2022-12-27 15:24:28 -0600

    chore: apply suggestions from code review

--- bit_manipulation/travelling_salesman_using_bit_manipulation.cpp
+++ bit_manipulation/travelling_salesman_using_bit_manipulation.cpp
@@ -77,29 +77,27 @@ std::uint64_t travelling_salesman_using_bit_manipulation(std::vector<std::vector
 } // namespace travelling_salesman_using_bit_manipulation
 } // namespace bit_manipulation
 
-
-
 /**
  * @brief Self-test implementations
  * @returns void
  */
 static void test() {
-    //Testcase 1
+    // 1st test-case
     std::vector<std::vector<uint32_t>> dist = {
         {0, 20, 42, 35}, {20, 0, 30, 34}, {42, 30, 0, 12}, {35, 34, 12, 0}
     };
     uint32_t V = dist.size();
     std::vector<std::vector<uint32_t>> dp(1 << V, std::vector<uint32_t>(V, -1));
     assert(bit_manipulation::travelling_salesman_using_bit_manipulation::travelling_salesman_using_bit_manipulation(dist, 1, 0, V, dp) == 97);
-    std::cout << "Testcase 1 Passed" << "\n";
+    std::cout << "1st test-case: passed!" << "\n";
 
-    //TestCase 2;
+    // 2nd test-case
     dist = {{0, 5, 10, 15}, {5, 0, 20, 30}, {10, 20, 0, 35}, {15, 30, 35, 0}};
     V = dist.size();
     std::vector<std::vector<uint32_t>> dp1(1 << V, std::vector<uint32_t>(V, -1));
     assert(bit_manipulation::travelling_salesman_using_bit_manipulation::travelling_salesman_using_bit_manipulation(dist, 1, 0, V, dp1) == 75);
-    std::cout << "Testcase 2 Passed" << "\n";
-    //TestCase 3
+    std::cout << "2nd test-case: passed!" << "\n";
+    // 3rd test-case
     dist = {
         {0, 10, 15, 20}, {10, 0, 35, 25}, {15, 35, 0, 30}, {20, 25, 30, 0}
     };
@@ -107,9 +105,10 @@ static void test() {
     std::vector<std::vector<uint32_t>> dp2(1 << V, std::vector<uint32_t>(V, -1));
     assert(bit_manipulation::travelling_salesman_using_bit_manipulation::travelling_salesman_using_bit_manipulation(dist, 1, 0, V, dp2) == 80);
 
-    std::cout << "Testcase 3 Passed" << "\n";
+    std::cout << "3rd test-case: passed!" << "\n";
 
 }
+
 /**
  * @brief Main function
  * @returns 0 on exit
