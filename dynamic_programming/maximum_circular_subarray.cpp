ba15ee66eaf9a357cace1212f03a829b4b6e9f1a
Suman Mondal <94859440+sumanbmondal@users.noreply.github.com>
2022-12-30 12:50:54 +0530

    Update maximum_circular_subarray.cpp

--- dynamic_programming/maximum_circular_subarray.cpp
+++ dynamic_programming/maximum_circular_subarray.cpp
@@ -7,9 +7,8 @@
 * [Geeks For Geeks](https://www.geeksforgeeks.org/maximum-contiguous-circular-sum/)
 */
 
-#include <cassert>   /// for assert
-#include <iostream>   /// for IO operations    
-using namespace std;  
+#include <cassert>   // for assert
+#include <iostream>   // for IO operations
 
 int maxCircularSum(int a[], int n)
 {
@@ -29,19 +28,19 @@ int maxCircularSum(int a[], int n)
     // Concept of Kadane's Algorithm
     for (int i = 1; i < n; i++) {
         // Kadane's Algorithm to find Maximum subarray sum.
-        current_max = max(current_max + a[i], a[i]);
-        max_so_far = max(max_so_far, current_max);
+        current_max = std::max(current_max + a[i], a[i]);
+        max_so_far = std::max(max_so_far, current_max);
   
         // Kadane's Algorithm to find Minimum subarray sum.
-        current_min = min(current_min + a[i], a[i]);
-        min_so_far = min(min_so_far, current_min);
+        current_min = std::min(current_min + a[i], a[i]);
+        min_so_far = std::min(min_so_far, current_min);
     }
   
     if (min_so_far == sum)
         return max_so_far;
   
     // returning the maximum value
-    return max(max_so_far, sum - min_so_far);
+    return std::max(max_so_far, sum - min_so_far);
 }
 
 /**
