f1f9a24566cbf81a1bae0b363371e9ff63322241
Tapas Ranjan Nayak <tapasrnk@gmail.com>
2023-01-15 18:39:24 +0530

    fix: handle edge case and test for fibonacci_bottom_up

--- dynamic_programming/fibonacci_bottom_up.cpp
+++ dynamic_programming/fibonacci_bottom_up.cpp
@@ -1,6 +1,34 @@
+/**
+ * @file
+ * @brief Implementation to find n th fibonacci_number
+ * (https://en.wikipedia.org/wiki/Fibonacci_number)
+ *
+ * @details
+ * The Fibonacci sequence is a series of numbers where each number is the
+ * sum of the two preceding ones, starting with 0 and 1.
+ *
+ * ### Algorithm
+ * The idea of the algorithm is to find the ith fibonacci number.
+ * DP state : F(i) = ith fibonacci number
+ * DP state transition :
+ *      F(i) = F(i-1) + F(i-2)
+ * DP base case :
+ *      F(0) = 0
+ *      F(1) = 1
+ * Space reduction :
+ * for i the fibonacci store only i-1 th and i-2 th state.
+ */
+#include <cassert>  /// for assert
 #include <iostream>
+/**
+ * @namespace dynamic_programming
+ * @brief Dynamic Programming algorithms
+ */
 using namespace std;
 int fib(int n) {
+    if (n < 0) {
+        return -1;
+    }
     int res[3];
     res[0] = 0;
     res[1] = 1;
@@ -11,11 +39,13 @@ int fib(int n) {
     }
     return res[1];
 }
+static void test() {
+    assert(fib(-2) == -1);
+    assert(fib(0) == 0);
+    assert(fib(1) == 1);
+    assert(fib(17) == 1597);
+}
 int main(int argc, char const *argv[]) {
-    int n;
-    cout << "Enter n: ";
-    cin >> n;
-    cout << "Fibonacci number is ";
-    cout << fib(n) << endl;
+    test();
     return 0;
 }
