72602d027bed30f4fe30d12e773fdc80d932cfa7
Tapas Ranjan Nayak <tapasrnk@gmail.com>
2023-02-05 23:33:59 +0530

    Update dynamic_programming/fibonacci_bottom_up.cpp

--- dynamic_programming/fibonacci_bottom_up.cpp
+++ dynamic_programming/fibonacci_bottom_up.cpp
@@ -16,14 +16,18 @@
  *      F(0) = 0
  *      F(1) = 1
  * Space reduction :
- * for i the fibonacci store only i-1 th and i-2 th state.
+ *  * for i the fibonacci store only i-1 th and i-2 th state.
+ * @author [Santo Cariotti](https://github.com/dcariotti)
+ * @author [tapasrnk](https://github.com/tapasrnk)
  */
 #include <cassert>   /// for assert
 #include <iostream>  /// for IO operations
+#include <vector>    /// for storing the values of nth fibonacci number
 /**
  * @namespace dynamic_programming
  * @brief Dynamic Programming algorithms
  */
+namespace dynamic_programming {
 int fib(int n) {
     if (n < 0) {
         return -1;
@@ -31,7 +35,7 @@ int fib(int n) {
     if (n == 0) {
         return 0;
     }
-    int res[3];
+    std::vector<int> res(3);
     res[0] = 0;
     res[1] = 1;
     for (int i = 2; i <= n; i++) {
@@ -41,16 +45,20 @@ int fib(int n) {
     }
     return res[1];
 }
+}  // namespace dynamic_programming
+
 /**
  * @brief Self-test implementations
  * @returns void
  */
 static void test() {
-    assert(fib(-2) == -1);
-    assert(fib(0) == 0);
-    assert(fib(1) == 1);
-    assert(fib(17) == 1597);
+    assert(dynamic_programming::fib(-2) == -1);
+    assert(dynamic_programming::fib(0) == 0);
+    assert(dynamic_programming::fib(1) == 1);
+    assert(dynamic_programming::fib(17) == 1597);
+    std::cout << "All tests have successfully passed!\n";
 }
+
 /**
  * @brief Main function
  * @param argc commandline argument count (ignored)
