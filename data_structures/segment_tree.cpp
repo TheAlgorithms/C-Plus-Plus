3cdca4267967277ce176325054c7073593a6b79f
Nishant Chatterjee <nishantc1527@gmail.com>
2023-04-01 12:14:32 -0700

    fix: conformed to guidelines

--- data_structures/segment_tree.cpp
+++ data_structures/segment_tree.cpp
@@ -1,32 +1,64 @@
-// Implementation of the segment tree data structre
-//
-// Can do point updates (updates the value of some position)
-// and range queries, where it gives the value of some associative
-// opperation done on a range
-//
-// Both of these operations take O(log N) time
+/**
+ * @file
+ * @brief A data structure to quickly do operations on ranges: [Segment Tree](https://en.wikipedia.org/wiki/Segment_tree)
+ * @details
+ * Implementation of the segment tree data structre
+ *
+ * Can do point updates (updates the value of some position)
+ * and range queries, where it gives the value of some associative
+ * opperation done on a range
+ *
+ * Both of these operations take O(log N) time
+ * @author [Nishant Chatterjee](https://github.com/nishantc1527)
+ */
 
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <iostream>     // For IO operations
+#include <vector>       // For std::vector
+#include <algorithm>    // For std::min and std::max
+#include <cassert>      // For assert
 
+/*
+ * @namespace data_structures
+ */
+namespace data_structures {
+/**
+ * @brief class representation of the segment tree
+ * @param n The size of the tree
+ * @tparam T The type of the class that goes in the datastructure
+ */
 template <class T>
 class SegmentTree {
 private:
-    // Comb(ID, x) = x
-    const int ID = 0;
-    // Vector that represents the tree
-    std::vector<int> t;
-    // Size of the tree
+    const int ID = 0;   ///< Comb(ID, x) = x
+    std::vector<int> t; ///< Vector to represent the tree
     int size;
 private:
-    // Any associative function that combines x and y
+    /**
+     * @brief Any associative function that combines x and y
+     * @param x The first operand
+     * @param y The second operand
+     * @return Some associative operation applied to these two values. In this case, I used addition
+     */
     T comb(T x, T y) {
         return x + y;
     }
+    /**
+     * @brief Gives the midpoint between two integers
+     * @param l The left endpoint
+     * @param r The right endpoint
+     * @return the middle point between them
+     */
     int mid(int l, int r) {
         return l + (r - l) / 2;
     }
+    /**
+     * @brief Helper method for update method below
+     * @param i The index of the current node
+     * @param l The leftmost node of the current node
+     * @param r The rightmost node of the current node
+     * @param pos The position to update
+     * @param val The value to update it to
+     */
     void update(int i, int l, int r, int pos, int val) {
         if(l == r) t[i] = val;
         else {
@@ -36,6 +68,15 @@ private:
             t[i] = comb(t[i * 2], t[i * 2 + 1]);
         }
     }
+    /**
+     * @brief Helper method for range_comb method below
+     * @param i The current node
+     * @param l The leftmost node of the current node
+     * @param r The rightmost node of the current node
+     * @param tl The left endpoint of the range
+     * @param tr The right endpoint of the range
+     * @return The comb operation applied to all values between tl and tr
+     */
     int range_comb(int i, int l, int r, int tl, int tr) {
         if(l == tl && r == tr) return t[i];
         if(tl > tr) return 0;
@@ -43,27 +84,45 @@ private:
         return comb(range_comb(i * 2, l, m, tl, std::min(tr, m)), range_comb(i * 2 + 1, m + 1, r, std::max(tl, m + 1), tr));
     }
 public:
-    // Creates a seg tree length n
     SegmentTree(int n) : t(n * 4, ID), size(n) {}
-    // Updates the value at pos to val
+    /** 
+     * @brief Updates a value at a certain position
+     * @param pos The position to update
+     * @param val The value to update it to
+     */
     void update(int pos, int val) {
         update(1, 1, size, pos, val);
     }
-    // Returns comb of all values between l and r
+    /**
+     * @brief Returns comb across all values between l and r
+     * @param l The left endpoint of the range
+     * @param r The right endpoint of the range
+     * @return The value of the comb operations
+     */
     int range_comb(int l, int r) {
         return range_comb(1, 1, size, l, r);
     }
 };
+} // namespace data_structures
 
-// Tests the seg tree
-int main() {
-    SegmentTree<int> t(5);
+/**
+ * @brief self test implementations
+ */
+static void test() {
+    data_structures::SegmentTree<int> t(5);
     t.update(1, 1);
     t.update(2, 2);
     t.update(3, 3);
     t.update(4, 4);
     t.update(5, 5);
-    std::cout << t.range_comb(1, 3) << "\n"; // 1 + 2 + 3 = 6
+    assert(t.range_comb(1, 3) == 6); // 1 + 2 + 3 = 6
     t.update(1, 3);
-    std::cout << t.range_comb(1, 3) << "\n"; // 3 + 2 + 3 = 8
+    assert(t.range_comb(1, 3) == 8); // 3 + 2 + 3 = 8
+}
+
+/**
+ * @brief Main function
+ */
+int main() {
+    test();
 }
