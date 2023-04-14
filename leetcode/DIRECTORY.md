c4e557fab7c4e679576ee104f50eacc4fb50898a
Nagesh N Nazare <85761012+nageshnnazare@users.noreply.github.com>
2023-04-14 13:43:20 +0000

    feat: add LeetCode problem 1,2
    Created new directory for leetcode solutions

---
+++ leetcode/DIRECTORY.md
@@ -0,0 +1,9 @@
+
+# LeetCode
+
+### LeetCode Algorithm
+
+|    # | Title| Solution          | Difficulty |
+| ---- | ------------------------------------------------------------------------------------------------------------------------------------------------------ | ----------------- | ---------- |
+|    1 | [Two Sum](https://leetcode.com/problems/two-sum)| [C++](./src/1.cpp)    | Easy       |
+|    2 | [Add Two Numbers](https://leetcode.com/problems/add-two-numbers)| [C++](./src/2.cpp)    | Medium     |
---
+++ leetcode/src/2.cpp
@@ -0,0 +1,37 @@
+/* You are given two non - empty linked lists representing two non - negative integers.
+* The digits are stored in reverse order, and each of their nodes contains a single digit.
+* Add the two numbersand return the sum as a linked list.
+*/
+
+/*
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        int carry = 0;
+        ListNode head;
+        ListNode* sum = &head;
+
+        while (l1 || l2) {
+            int val = carry + (l1 ? l1->val : 0) + (l2 ? l2->val : 0);
+            carry = val / 10;
+            sum->next = new ListNode(val % 10);
+            sum = sum->next;
+            l1 = (l1 ? l1->next : nullptr);
+            l2 = (l2 ? l2->next : nullptr);
+        }
+        if (carry) {
+            sum->next = new ListNode(carry);
+        }
+
+        return head.next;
+    }
+};
