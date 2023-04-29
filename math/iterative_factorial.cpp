b022ffea25f89a7ad0ef17e5ea4c4a5a6c8200d8
Gahjouyooj <tyiwei0@gmail.com>
2023-04-29 15:45:45 +0800

    Feat: iterative_factorial docs: Added documentation to some functions. test: Added exception test.

--- math/iterative_factorial.cpp
+++ math/iterative_factorial.cpp
@@ -3,26 +3,44 @@
  * @brief Iterative implementation of factorial
  *
  * @author [Renjian-buchai](https://github.com/Renjian-buchai)
+ *
+ * @description Calculates factorial iteratively.
+ *
+ * @example
+ * int main() {
+ *     using std::cout;
+ *
+ *     cout << "10! = " math::iterativeFactorial(10);
+ * }
+ *
+ * Output:
+ * 10! = 3,628,800
+ *
  */
-
-#include <stdint.h>
 
 #include <cassert>
 #include <exception>
 #include <iostream>
 
-/// @brief Calculates the factorial iteratively.
-/// @param n Nth factorial.
-/// @return Factorial.
-/// @warning Maximum=20 because 21! returns 1.419e+19, which is not 21! but
-/// (21! % UINT64_MAX) after overflowing twice. No 128-bit variables in C++, so
-/// unless you use [abseil](https://abseil.io) or similar libraries, maximum is
-/// 20!.
+/**
+ * @namespace
+ * @brief Mathematical algorithms
+ */
+namespace math {
+
+/**
+ * @brief Calculates the factorial iteratively.
+ * @param n Nth factorial.
+ * @return Factorial.
+ * @note 0! = 1.
+ * @warning Maximum=20 because there are no 128-bit integers in C++. 21!
+ * returns 1.419e+19, which is not 21! but (21! % UINT64_MAX).
+ */
 unsigned long long iterativeFactorial(unsigned short n) {
     if (n > 20)
         throw new std::invalid_argument("Maximum n value is 20");
 
-    /// 1 because 0! == 1
+    // 1 because it is the identity number of multiplication.
     unsigned long long accumulator = 1;
 
     while (n > 0) {
@@ -33,28 +51,59 @@ unsigned long long iterativeFactorial(unsigned short n) {
     return accumulator;
 }
 
+}  // namespace math
+
+/**
+ * @brief Self-test implementations to test iterativeFactorial function.
+ * @note There is 1 special case: 0! = 1.
+ */
 static void test() {
-    /// Exception case
-    assert(iterativeFactorial(0) == 1);
+    // Special case test
+    std::cout << "Exception case test \n"
+                 "Input: 0 \n"
+                 "Expected output: 1 \n";
+    assert(math::iterativeFactorial(0) == 1);
 
-    /// Base case
-    assert(iterativeFactorial(1) == 1);
+    // Base case
+    std::cout << "Base case test \n"
+                 "Input: 1 \n"
+                 "Expected output: 1 \n";
+    assert(math::iterativeFactorial(1) == 1);
 
-    /// Small case
-    assert(iterativeFactorial(5) == 120);
+    // Small case
+    std::cout << "Small number case test \n"
+                 "Input: 5 \n"
+                 "Expected output: 120 \n";
+    assert(math::iterativeFactorial(5) == 120);
 
-    /// Medium case
-    assert(iterativeFactorial(10) == 3628800);
+    // Medium case
+    std::cout << "Medium number case test \n"
+                 "Input: 10 \n"
+                 "Expected output: 3628800 \n";
+    assert(math::iterativeFactorial(10) == 3628800);
 
-    /// Maximum case
-    unsigned long long f20 = 2432902008176640000;
-    assert(iterativeFactorial(20) == f20);
+    // Maximum case
+    std::cout << "Maximum case test \n"
+                 "Input: 20 \n"
+                 "Expected output: 2432902008176640000";
+    assert(math::iterativeFactorial(20) == 2432902008176640000);
+
+    // Exception test
+    std::cout << "Exception test \n"
+                 "Input: 21 \n"
+                 "Expected output: Exception thrown \n";
+    try {
+        math::iterativeFactorial(21);
+    } catch (std::invalid_argument& e) {
+        std::cout << "Exception thrown successfully \n";
+    }
 }
 
+/**
+ * @brief Main function
+ * @returns 0 on exit
+ */
 int main() {
-    test();
-
-    std::cout << "10! = " << iterativeFactorial(10);
-
+    test();  // Run self-test implementation
     return 0;
 }
