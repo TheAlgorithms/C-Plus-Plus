d396e5e029ecc5e80c8ce097739047a7c7cd8042
ewd00010 <78011234+ewd00010@users.noreply.github.com>
2023-05-10 15:43:30 +0100

    original box_stacking documentation
    start point

---
+++ dynamic_programming/box_stacking.cpp
@@ -0,0 +1,45 @@
+/**
+* @file
+* @brief calculate the maximum height you can achieve given a set of different
+* sized boxes. any box can be stacked on one for which the length and breadth
+* are smaller; you may rotate boxes.
+* https://www.geeksforgeeks.org/box-stacking-problem-dp-22/
+*
+* @details
+* as with a lot of dynamic programming, we will find sub problems and use them to
+* find an overall solution.
+*
+* @author [Ewan Dunn](https://github.com/ewd00010)
+*/
+
+#include <cassert>   /// for assert
+#include <climits>   /// for std::max
+#include <iostream>  /// for IO operations
+#include <vector>    /// for std::vector
+
+/**
+* @namespace dynamic_programming
+* @brief Dynamic Programming algorithms
+*/
+namespace dynamic_programming {
+
+}  // namespace dynamic_programming
+
+/**
+* @brief Self-test implementations
+* @returns void
+*/
+static void tests() {
+    //assert();
+}
+
+/**
+* @brief Main function
+* @param argc commandline argument count (ignored)
+* @param argv commandline array of arguments (ignored)
+* @returns 0 on exit
+*/
+int main(int argc, char const *argv[]) {
+    tests();
+ // use a vector to contain boxes, maybe pull from a file that users can change
+}
