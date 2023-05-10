20f8ccd1d4288edeb9448ff2aff541e8c28387b1
ewd00010 <78011234+ewd00010@users.noreply.github.com>
2023-05-10 12:37:28 +0100

    check_prime_update
    docs: added links to file @brief and @details. moved previous @brief to be with function, made comments easier to read in general and added boilerplate documentation to functions.
    chore: removed bool result and most brackets in is_prime. Moved assert tests to their own function and added more in, test success now returns message.

--- math/check_prime.cpp
+++ math/check_prime.cpp
@@ -2,55 +2,81 @@
  * Copyright 2020 @author omkarlanghe
  *
  * @file
- * A simple program to check if the given number if prime or not.
- *
  * @brief
- * Reduced all possibilities of a number which cannot be prime.
- * Eg: No even number, except 2 can be a prime number, hence we will increment
- * our loop with i+6 jumping and check for i or i+2 to be a factor of the
- * number; if it's a factor then we will return false otherwise true after the
- * loop terminates at the terminating condition which is (i*i<=num)
+ * A simple program to check if the given number if prime or not.
+ * https://en.wikipedia.org/wiki/Primality_test
+ * @details
+ * additional ways to solve the prime check problem
+ * https://cp-algorithms.com/algebra/primality_tests.html#practice-problems
  */
 
 #include <cassert>   /// for assert
 #include <iostream>  /// for IO operations
 
 /**
- * Function to check if the given number is prime or not.
+ * @brief Function to check if the given number is prime or not.
  * @param num number to be checked.
- * @return if number is prime, it returns @ true, else it returns @ false.
+ * @return true if number is a prime
+ * @return false if number is not a prime.
  */
 template <typename T>
 bool is_prime(T num) {
-    bool result = true;
+    /*!
+     * Reduce all possibilities of a number which cannot be prime with the first
+     * 3 if, else if conditionals. Example: Since no even number, except 2 can
+     * be a prime number and the next prime we find after our checks is 5,
+     * we will start the for loop with i = 5. then for each loop we increment
+     * i by +6 and check if i or i+2 is a factor of the number; if it's a factor
+     * then we will return false. otherwise, true will be returned after the
+     * loop terminates at the terminating condition which is i*i <= num
+     */
     if (num <= 1) {
         return false;
     } else if (num == 2 || num == 3) {
         return true;
-    } else if ((num % 2) == 0 || num % 3 == 0) {
+    } else if (num % 2 == 0 || num % 3 == 0) {
         return false;
     } else {
-        for (T i = 5; (i * i) <= (num); i = (i + 6)) {
-            if ((num % i) == 0 || (num % (i + 2) == 0)) {
-                result = false;
-                break;
+        for (T i = 5; i * i <= num; i = i + 6) {
+            if (num % i == 0 || num % (i + 2) == 0) {
+                return false;
             }
         }
     }
-    return (result);
+    return true;
+}
+
+/**
+ * @brief Self-test implementations
+ * @returns void
+ */
+static void tests() {
+    /*!
+     * each assert test checks if the is_prime function returns the expected
+     * boolean value to whether its argument is prime
+     */
+    assert(is_prime(1) == false);
+    assert(is_prime(2) == true);
+    assert(is_prime(3) == true);
+    assert(is_prime(4) == false);
+    assert(is_prime(4) == false);
+    assert(is_prime(7) == true);
+    assert(is_prime(19) == true);
+    assert(is_prime(50) == false);
+    assert(is_prime(115249) == true);
+
+    std::cout << "All tests have successfully passed!" << std::endl;
 }
 
 /**
  * Main function
+ * returns 0 on exit
  */
 int main() {
-    // perform self-test
-    assert(is_prime(50) == false);
-    assert(is_prime(115249) == true);
-
+    tests(); // perform self-tests implementations
     int num = 0;
     std::cout << "Enter the number to check if it is prime or not" << std::endl;
-    std::cin >> num;
+    std::cin >> num; // take user chosen number and store in variable num
     bool result = is_prime(num);
     if (result) {
         std::cout << num << " is a prime number" << std::endl;
