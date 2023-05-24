e17c998a5197136d4eb9bd7e69e19739957a528e
Md. Anisul Haque <mdanisulhaque001@gmail.com>
2023-05-25 01:42:35 +0530

    fix: suggested changes

--- hashing/sha256.cpp
+++ hashing/sha256.cpp
@@ -17,6 +17,7 @@
 #include <cstdint>  /// For uint8_t, uint32_t and uint64_t data types
 #include <iomanip>  /// For std::setfill and std::setw
 #include <sstream>  /// For std::stringstream
+#include <utility>  /// For std::move
 #include <vector>   /// For std::vector
 
 /**
@@ -24,6 +25,17 @@
  * @brief Hashing algorithms
  */
 namespace hashing {
+/**
+ * @namespace SHA-256
+ * @brief Functions for the [SHA-256](https://en.wikipedia.org/wiki/SHA-2)
+ * algorithm implementation
+ */
+namespace sha256 {
+/**
+ * @class Hash
+ * @brief Contains hash array and functions to update it and convert it to a
+ * hexadecimal string
+ */
 class Hash {
     // Initialize array of hash values with first 32 bits of the fractional
     // parts of the square roots of the first 8 primes 2..19
@@ -37,11 +49,91 @@ class Hash {
 };
 
 /**
- * @namespace SHA-256
- * @brief Functions for the [SHA-256](https://en.wikipedia.org/wiki/SHA-2)
- * algorithm implementation
+ * @brief Rotates the bits of a 32-bit unsigned integer
+ * @param n Integer to rotate
+ * @param rotate Number of bits to rotate
+ * @return uint32_t The rotated integer
  */
-namespace sha256 {
+uint32_t right_rotate(uint32_t n, size_t rotate) {
+    return (n >> rotate) | (n << (32 - rotate));
+}
+
+/**
+ * @brief Updates the hash array
+ * @param blocks Message schedule array
+ * @return void
+ */
+void Hash::update(const std::array<uint32_t, 64> &blocks) {
+    // Initialize array of round constants with first 32 bits of the fractional
+    // parts of the cube roots of the first 64 primes 2..311
+    const std::array<uint32_t, 64> round_constants = {
+        0x428A2F98, 0x71374491, 0xB5C0FBCF, 0xE9B5DBA5, 0x3956C25B, 0x59F111F1,
+        0x923F82A4, 0xAB1C5ED5, 0xD807AA98, 0x12835B01, 0x243185BE, 0x550C7DC3,
+        0x72BE5D74, 0x80DEB1FE, 0x9BDC06A7, 0xC19BF174, 0xE49B69C1, 0xEFBE4786,
+        0x0FC19DC6, 0x240CA1CC, 0x2DE92C6F, 0x4A7484AA, 0x5CB0A9DC, 0x76F988DA,
+        0x983E5152, 0xA831C66D, 0xB00327C8, 0xBF597FC7, 0xC6E00BF3, 0xD5A79147,
+        0x06CA6351, 0x14292967, 0x27B70A85, 0x2E1B2138, 0x4D2C6DFC, 0x53380D13,
+        0x650A7354, 0x766A0ABB, 0x81C2C92E, 0x92722C85, 0xA2BFE8A1, 0xA81A664B,
+        0xC24B8B70, 0xC76C51A3, 0xD192E819, 0xD6990624, 0xF40E3585, 0x106AA070,
+        0x19A4C116, 0x1E376C08, 0x2748774C, 0x34B0BCB5, 0x391C0CB3, 0x4ED8AA4A,
+        0x5B9CCA4F, 0x682E6FF3, 0x748F82EE, 0x78A5636F, 0x84C87814, 0x8CC70208,
+        0x90BEFFFA, 0xA4506CEB, 0xBEF9A3F7, 0xC67178F2};
+
+    // Initialize working variables
+    auto a = hash[0];
+    auto b = hash[1];
+    auto c = hash[2];
+    auto d = hash[3];
+    auto e = hash[4];
+    auto f = hash[5];
+    auto g = hash[6];
+    auto h = hash[7];
+
+    // Compression function main loop
+    for (size_t block_num = 0; block_num < 64; ++block_num) {
+        const auto s1 =
+            right_rotate(e, 6) ^ right_rotate(e, 11) ^ right_rotate(e, 25);
+        const auto ch = (e & f) ^ (~e & g);
+        const auto temp1 =
+            h + s1 + ch + round_constants[block_num] + blocks[block_num];
+        const auto s0 =
+            right_rotate(a, 2) ^ right_rotate(a, 13) ^ right_rotate(a, 22);
+        const auto maj = (a & b) ^ (a & c) ^ (b & c);
+        const auto temp2 = s0 + maj;
+
+        h = g;
+        g = f;
+        f = e;
+        e = d + temp1;
+        d = c;
+        c = b;
+        b = a;
+        a = temp1 + temp2;
+    }
+
+    // Update hash values
+    hash[0] += a;
+    hash[1] += b;
+    hash[2] += c;
+    hash[3] += d;
+    hash[4] += e;
+    hash[5] += f;
+    hash[6] += g;
+    hash[7] += h;
+}
+
+/**
+ * @brief Convert the hash to a hexadecimal string
+ * @return std::string Final hash value
+ */
+std::string Hash::to_string() const {
+    std::stringstream ss;
+    for (size_t i = 0; i < 8; ++i) {
+        ss << std::hex << std::setfill('0') << std::setw(8) << hash[i];
+    }
+    return ss.str();
+}
+
 /**
  * @brief Computes size of the padded input
  * @param input Input string
@@ -91,16 +183,6 @@ char get_char(const std::string &input, std::size_t pos) {
     }
     return static_cast<char>(
         extract_byte<size_t>(input_size * 8, padded_input_size - pos - 1));
-}
-
-/**
- * @brief Rotates the bits of a 32-bit unsigned integer
- * @param n Integer to rotate
- * @param rotate Number of bits to rotate
- * @return uint32_t The rotated integer
- */
-uint32_t right_rotate(uint32_t n, size_t rotate) {
-    return (n >> rotate) | (n << (32 - rotate));
 }
 
 /**
@@ -156,96 +238,19 @@ std::string sha256(const std::string &input) {
     return h.to_string();
 }
 }  // namespace sha256
-
-/**
- * @brief Updates the hash array
- * @param blocks Message schedule array
- * @return void
- */
-void Hash::update(const std::array<uint32_t, 64> &blocks) {
-    // Initialize array of round constants with first 32 bits of the fractional
-    // parts of the cube roots of the first 64 primes 2..311
-    const std::array<uint32_t, 64> k = {
-        0x428A2F98, 0x71374491, 0xB5C0FBCF, 0xE9B5DBA5, 0x3956C25B, 0x59F111F1,
-        0x923F82A4, 0xAB1C5ED5, 0xD807AA98, 0x12835B01, 0x243185BE, 0x550C7DC3,
-        0x72BE5D74, 0x80DEB1FE, 0x9BDC06A7, 0xC19BF174, 0xE49B69C1, 0xEFBE4786,
-        0x0FC19DC6, 0x240CA1CC, 0x2DE92C6F, 0x4A7484AA, 0x5CB0A9DC, 0x76F988DA,
-        0x983E5152, 0xA831C66D, 0xB00327C8, 0xBF597FC7, 0xC6E00BF3, 0xD5A79147,
-        0x06CA6351, 0x14292967, 0x27B70A85, 0x2E1B2138, 0x4D2C6DFC, 0x53380D13,
-        0x650A7354, 0x766A0ABB, 0x81C2C92E, 0x92722C85, 0xA2BFE8A1, 0xA81A664B,
-        0xC24B8B70, 0xC76C51A3, 0xD192E819, 0xD6990624, 0xF40E3585, 0x106AA070,
-        0x19A4C116, 0x1E376C08, 0x2748774C, 0x34B0BCB5, 0x391C0CB3, 0x4ED8AA4A,
-        0x5B9CCA4F, 0x682E6FF3, 0x748F82EE, 0x78A5636F, 0x84C87814, 0x8CC70208,
-        0x90BEFFFA, 0xA4506CEB, 0xBEF9A3F7, 0xC67178F2};
-
-    // Initialize working variables
-    auto a = hash[0];
-    auto b = hash[1];
-    auto c = hash[2];
-    auto d = hash[3];
-    auto e = hash[4];
-    auto f = hash[5];
-    auto g = hash[6];
-    auto h = hash[7];
-
-    // Compression function main loop
-    for (size_t block_num = 0; block_num < 64; ++block_num) {
-        const auto s1 = sha256::right_rotate(e, 6) ^
-                        sha256::right_rotate(e, 11) ^
-                        sha256::right_rotate(e, 25);
-        const auto ch = (e & f) ^ (~e & g);
-        const auto temp1 = h + s1 + ch + k[block_num] + blocks[block_num];
-        const auto s0 = sha256::right_rotate(a, 2) ^
-                        sha256::right_rotate(a, 13) ^
-                        sha256::right_rotate(a, 22);
-        const auto maj = (a & b) ^ (a & c) ^ (b & c);
-        const auto temp2 = s0 + maj;
-
-        h = g;
-        g = f;
-        f = e;
-        e = d + temp1;
-        d = c;
-        c = b;
-        b = a;
-        a = temp1 + temp2;
-    }
-
-    // Update hash values
-    hash[0] += a;
-    hash[1] += b;
-    hash[2] += c;
-    hash[3] += d;
-    hash[4] += e;
-    hash[5] += f;
-    hash[6] += g;
-    hash[7] += h;
-}
-
-/**
- * @brief Convert the hash to a hexadecimal string
- * @return std::string Final hash value
- */
-std::string Hash::to_string() const {
-    std::stringstream ss;
-    for (size_t i = 0; i < 8; ++i) {
-        ss << std::hex << std::setfill('0') << std::setw(8) << hash[i];
-    }
-    return ss.str();
-}
 }  // namespace hashing
 
 /**
  * @brief Self-test implementations
  * @returns void
  */
-static void test_compute_padded_size() {
+void test_compute_padded_size() {
     assert(hashing::sha256::compute_padded_size(55) == 64);
     assert(hashing::sha256::compute_padded_size(56) == 128);
     assert(hashing::sha256::compute_padded_size(130) == 192);
 }
 
-static void test_extract_byte() {
+void test_extract_byte() {
     assert(hashing::sha256::extract_byte<uint32_t>(512, 0) == 0);
     assert(hashing::sha256::extract_byte<uint32_t>(512, 1) == 2);
     bool exception = false;
@@ -257,7 +262,7 @@ static void test_extract_byte() {
     assert(exception);
 }
 
-static void test_get_char() {
+void test_get_char() {
     assert(hashing::sha256::get_char("test", 3) == 't');
     assert(hashing::sha256::get_char("test", 4) == '\x80');
     assert(hashing::sha256::get_char("test", 5) == '\x00');
@@ -271,13 +276,13 @@ static void test_get_char() {
     assert(exception);
 }
 
-static void test_right_rotate() {
+void test_right_rotate() {
     assert(hashing::sha256::right_rotate(128, 3) == 16);
     assert(hashing::sha256::right_rotate(1, 30) == 4);
     assert(hashing::sha256::right_rotate(6, 30) == 24);
 }
 
-static void test_sha256() {
+void test_sha256() {
     struct TestCase {
         const std::string input;
         const std::string expected_hash;
@@ -303,16 +308,19 @@ static void test_sha256() {
     }
 }
 
+void test() {
+    test_compute_padded_size();
+    test_extract_byte();
+    test_get_char();
+    test_right_rotate();
+    test_sha256();
+}
+
 /**
  * @brief Main function
  * @returns 0 on exit
  */
 int main() {
-    // Run self-test implementations
-    test_sha256();
-    test_compute_padded_size();
-    test_extract_byte();
-    test_get_char();
-    test_right_rotate();
+    test();  // Run self-test implementations
     return 0;
 }
