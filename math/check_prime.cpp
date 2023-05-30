2898891cd42412a58c0523d5ca4b1a9b4fd460b3
ewd00010 <78011234+ewd00010@users.noreply.github.com>
2023-05-30 14:30:52 +0100

    Update check_prime.cpp

--- math/check_prime.cpp
+++ math/check_prime.cpp
@@ -27,7 +27,7 @@ namespace math {
      * @return true if number is a prime
      * @return false if number is not a prime.
      */
-    bool is_prime(long long num) {
+    bool is_prime(int64_t num) {
         /*!
          * Reduce all possibilities of a number which cannot be prime with the first
          * 3 if, else if conditionals. Example: Since no even number, except 2 can
@@ -44,7 +44,7 @@ namespace math {
         } else if (num % 2 == 0 || num % 3 == 0) {
             return false;
         } else {
-            for (long long i = 5; i * i <= num; i = i + 6) {
+            for (int64_t i = 5; i * i <= num; i = i + 6) {
                 if (num % i == 0 || num % (i + 2) == 0) {
                     return false;
                 }
@@ -59,16 +59,16 @@ namespace math {
  * @returns void
  */
 static void tests() {
-    assert(is_prime(1) == false);
-    assert(is_prime(2) == true);
-    assert(is_prime(3) == true);
-    assert(is_prime(4) == false);
-    assert(is_prime(-4) == false);
-    assert(is_prime(7) == true);
-    assert(is_prime(-7) == false);
-    assert(is_prime(19) == true);
-    assert(is_prime(50) == false);
-    assert(is_prime(115249) == true);
+    assert(math::is_prime(1) == false);
+    assert(math::is_prime(2) == true);
+    assert(math::is_prime(3) == true);
+    assert(math::is_prime(4) == false);
+    assert(math::is_prime(-4) == false);
+    assert(math::is_prime(7) == true);
+    assert(math::is_prime(-7) == false);
+    assert(math::is_prime(19) == true);
+    assert(math::is_prime(50) == false);
+    assert(math::is_prime(115249) == true);
 
     std::cout << "All tests have successfully passed!" << std::endl;
 }
