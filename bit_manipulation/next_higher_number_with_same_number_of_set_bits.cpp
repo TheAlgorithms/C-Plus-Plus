6fa173d2b8c57dbd924c30a420bffacaf3b41688
kunal nayak <87607403+Kunal766@users.noreply.github.com>
2023-06-14 21:02:55 +0530

    Update bit_manipulation/next_higher_number_with_same_number_of_set_bits.cpp
    * added
    
    Co-authored-by: David Leal <halfpacho@gmail.com>

--- bit_manipulation/next_higher_number_with_same_number_of_set_bits.cpp
+++ bit_manipulation/next_higher_number_with_same_number_of_set_bits.cpp
@@ -7,7 +7,7 @@
  * @details
  * Given a number x, find next number with same number of 1 bits in it’s binary representation.
  * For example, consider x = 12, whose binary representation is 1100 (excluding leading zeros on 32 bit machine).
- It contains two logic 1 bits. The next higher number with two logic 1 bits is 17 (100012).
+ * It contains two logic 1 bits. The next higher number with two logic 1 bits is 17 (100012).
  *
  * A binary number consists of two digits. They are 0 & 1. Digit 1 is known as
  * set bit in computer terms.
