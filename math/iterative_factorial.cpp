d0a5879230db6a224c8ac6103e6bd941573d0d9f
Renjian-buchai <tyiwei0@gmail.com>
2023-06-16 21:34:33 +0800

    Fix: Exception test termination bug

--- math/iterative_factorial.cpp
+++ math/iterative_factorial.cpp
@@ -1,6 +1,7 @@
 /**
  * @file
- * @brief Iterative implementation of [Factorial](https://en.wikipedia.org/wiki/Factorial)
+ * @brief Iterative implementation of
+ * [Factorial](https://en.wikipedia.org/wiki/Factorial)
  *
  * @author [Renjian-buchai](https://github.com/Renjian-buchai)
  *
@@ -25,6 +26,7 @@
  */
 
 #include <cassert>    /// for assert
+#include <cstdint>    /// for integral types
 #include <exception>  /// for std::invalid_argument
 #include <iostream>   /// for std::cout
 
@@ -68,31 +70,31 @@ static void test() {
     // Special case test
     std::cout << "Exception case test \n"
                  "Input: 0 \n"
-                 "Expected output: 1 \n";
+                 "Expected output: 1 \n\n";
     assert(math::iterativeFactorial(0) == 1);
 
     // Base case
     std::cout << "Base case test \n"
                  "Input: 1 \n"
-                 "Expected output: 1 \n";
+                 "Expected output: 1 \n\n";
     assert(math::iterativeFactorial(1) == 1);
 
     // Small case
     std::cout << "Small number case test \n"
                  "Input: 5 \n"
-                 "Expected output: 120 \n";
+                 "Expected output: 120 \n\n";
     assert(math::iterativeFactorial(5) == 120);
 
     // Medium case
     std::cout << "Medium number case test \n"
                  "Input: 10 \n"
-                 "Expected output: 3628800 \n";
+                 "Expected output: 3628800 \n\n";
     assert(math::iterativeFactorial(10) == 3628800);
 
     // Maximum case
     std::cout << "Maximum case test \n"
                  "Input: 20 \n"
-                 "Expected output: 2432902008176640000\n";
+                 "Expected output: 2432902008176640000\n\n";
     assert(math::iterativeFactorial(20) == 2432902008176640000);
 
     // Exception test
@@ -101,9 +103,12 @@ static void test() {
                  "Expected output: Exception thrown \n";
     try {
         math::iterativeFactorial(21);
-    } catch (std::invalid_argument& e) {
-        std::cout << "Exception thrown successfully \n";
+    } catch (std::invalid_argument* e) {
+        std::cout << "Exception thrown successfully \nContent: " << e->what()
+                  << "\n\n";
     }
+
+    std::cout << "All tests success.\n";
 }
 
 /**
