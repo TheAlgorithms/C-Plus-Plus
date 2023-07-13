4f761530a0c891b04a02874a639b2e20d92d6b7e
Aryan <polygot882@gmail.com>
2023-07-13 22:33:17 +0530

    Modified the code according to instructions given.

--- others/valid_no_of_subarrays.cpp
+++ others/valid_no_of_subarrays.cpp
@@ -1,65 +1,94 @@
+/**
+ * @brief Finds Total No. of Subarrays whose net product is less than a given No. = k
+ * @file C-Plus-Plus\others\valid_no_of_subarrays.cpp
+ * @param a the array to be used 
+ * @param n the size of the array
+ * @param k the maximum product allowed for subarrays
+ * @return the number of valid subarrays (int)
+ * @author https://github.com/hecker1002
+ 
+ */
+
+
+#include <iostream>  /// for IO operations
+#include <vector>   /// for std::vector
+#include <cassert>  /// for assert
+
 /*
-A C++ (CPP) code to find how many subarrays ( contiguous part of an array) are present in an array whose product is less than a
- given number 'k'
- e.g.-
-  Input array -  arr[5]= {1, 3, 4 ,5 ,2}
-  Input No.   -   k = 5
-  Subarrays of arr having product less than (k=5) are - {1} , {3} , {4} , {2} , {1 , 3}
+A C++ (CPP) code to find how many subarrays (contiguous part of an array) are present in an array whose product is less than a
+given number 'k'
+e.g.-
+Input array -  arr[5]= {1, 3, 4 ,5 ,2}
+Input No.   -   k = 5
+Subarrays of arr having product less than (k=5) are - {1} , {3} , {4} , {2} , {1 , 3}
 */
 
 /*
 Idea - Use Sliding Window Algorithm
 Procedure -
-Go in a loop ( j=0 to n) and create a sliding window each time starting from current element and increase its size (in second loop)
-till both of these  conditions are true  ( product of all elemnts in the window < k and iterator < last_index )
+Go in a loop (j=0 to n) and create a sliding window each time starting from the current element and increase its size (in the second loop)
+till both of these conditions are true  (product of all elements in the window < k and iterator < last_index)
 Proof -
- If at any instance , the product of all elements in the current window (subarray) exceeds k , then all further windows's product>k
- and hence we shift to the element to immediate right and do the same process .
+If at any instance, the product of all elements in the current window (subarray) exceeds k, then all further windows' product > k
+and hence we shift to the element to the immediate right and do the same process.
 */
 
-// Time Complexity of Code - O (n ^2 )  since outer loop -> n times and inner loop (worst case ) -> n times so TC = n * n 
-// Space Complexity of Code - O (1)  since does not use any auxillary space 
+// Time Complexity of Code - O(n^2) since the outer loop -> n times and inner loop (worst case) -> n times so TC = n * n 
+// Space Complexity of Code - O(1) since it does not use any auxiliary space 
+
 
 // Implementation
 
-#include <iostream>
-#include <vector>
-#include <cassert>
-
-using namespace std;
-
 struct Solution {
 
-    // required function
-    // Arguments = a[] -> array , n -> size of array , k -> given counter(number)
-    int count_SubArray_Product_Less_Than_K(int a[], int n , int k ) {
+
+/**
+ * @param a the array to be used 
+ * @param n the size of the array
+ * @param k the maximum product allowed for subarrays
+ * @return the number of valid subarrays (int)
+ * 
+ */
+
+
+    // Required function -
+    int count_SubArray_Product_Less_Than_K(int a[], int n, int k) {
+
         // Actual answer
-    int ans = 0;
-    
-    for (int j = 0; j < n; j++)
-    {
-        // traverse  each element
-        int i = j;
+        int ans = 0;
+        
+        // loop starts s
+        for (int j = 0; j < n; j++) {
 
-        // initialize the product as 1 for current window .
+            // traverse each element 
+            int i = j;
 
-        long long int product = 1;
+            // and with respect to current element of array , initialize the window of size =1.
+            long long int product = 1;
 
-        // making  a window that is expanding continuously till (product < k) AND (counter variable < size of array) .
-        while (i < n && product < k)
-        {
-            product *= a[i];
-            if (product < k)
-            {
-                ans++;
-                i++;
-            }
+            // expanding a window continuously till (product of elements in current window  < k) AND (iterator < last index of array).
+            // if any of these above conditions vio,ated , break the loop and count all the valid subarrays till now.
+            while (i < n && product < k) {
+
+                product *= a[i];
+
+                // checking if product in current window do NOT excedd k .
+                if (product < k) {
+                    ans++; // if NOT , inccrease the no . pf valid subarrays
+                    i++;  // expand the window with respect to current element .
+                }
+
+            } // loop ends 
         }
-    }
-
-    return ans;
+     
+       
+        return ans; // return no. of valid subarrays as the final answer
     }
 };
+
+/**
+ * @brief Self-test implementation
+ */
 
 void test() {
     Solution solution;
@@ -68,23 +97,30 @@ void test() {
     int a1[4] = {1, 3, 4, 2};
     int k1 = 5;
     int expected1 = 5;
-    int result1 = solution.count_SubArray_Product_Less_Than_K(a1,4, k1);
+    int result1 = solution.count_SubArray_Product_Less_Than_K(a1, 4, k1);
     assert(result1 == expected1);
 
     // Test case 2
     int a2[5] = {1, 2, 3, 4, 5};
     int k2 = 10;
     int expected2 = 8;
-    int result2 = solution.count_SubArray_Product_Less_Than_K(a2,5, k2);
+    int result2 = solution.count_SubArray_Product_Less_Than_K(a2, 5, k2);
     assert(result2 == expected2);
-    // Add more test cases here
+
+
+    // You can add more test cases here.
     
-    cout << "All tests passed successfully!" << endl;
+    std::cout << "All tests passed successfully!" << std::endl;
 }
 
+
+/**
+ * @brief Main function
+ * @returns 0 on exit
+ */
+
 int main() {
-    test();
+    test();  // runs self-implemented tests
     
     return 0;
 }
-
