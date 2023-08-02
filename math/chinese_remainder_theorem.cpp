666fb1ff75152ffeaf46847fba905d6835001012
Himesh Kohad <107066424+HimeshKohad@users.noreply.github.com>
2023-08-02 10:59:23 +0530

    Update math/chinese_remainder_theorem.cpp
    Co-authored-by: Piotr Idzik <65706193+vil02@users.noreply.github.com>

--- math/chinese_remainder_theorem.cpp
+++ math/chinese_remainder_theorem.cpp
@@ -1,52 +1,72 @@
 /**
  * @file
- * @brief C++ program to solve a system of linear congruences using the Chinese Remainder Theorem (CRT).
+ * @brief C++ program to solve a system of linear congruences using the Chinese
+ * Remainder Theorem (CRT).
  *
  * @details
- * The Chinese Remainder Theorem (CRT) is a mathematical theorem that provides a solution
- * to a system of linear congruences. It allows us to find a unique solution to the system
- * if the moduli are pairwise coprime (i.e., the greatest common divisor of any two moduli is 1).
+ * The Chinese Remainder Theorem (CRT) is a mathematical theorem that provides a
+ * solution to a system of linear congruences. It allows us to find a unique
+ * solution to the system if the moduli are pairwise coprime (i.e., the greatest
+ * common divisor of any two moduli is 1).
  *
  * This implementation assumes that the moduli are pairwise coprime.
  *
- * @note For the system of congruences to have a solution, the moduli must be pairwise coprime.
- * If they are not, the result obtained may not be valid.
+ * @note For the system of congruences to have a solution, the moduli must be
+ * pairwise coprime. If they are not, the result obtained may not be valid.
  *
  * @author [Himesh](https://github.com/HimeshKohad)
  */
 
 #include <cassert>   /// for assert
 #include <iostream>  /// for IO operations
-using namespace std;
+#include <vector>    /// for std::vector
+
+struct LinearCongruance {
+    const int a;
+    const int m;
+
+    LinearCongruance(int a, int m) : a(a), m(m) {}
+};
+
+int compute_product_of_all_moduli(
+    const std::vector<LinearCongruance> &congruances) {
+    int res = 1;
+    for (const auto congruance : congruances) {
+        res *= congruance.m;
+    }
+    return res;
+}
+
+int compute_inverse(int a, int m) {
+    int inv = 1;
+    while ((a * inv) % m != 1) {
+        inv++;
+    }
+    return inv;
+}
 
 /**
- * @brief Function to solve a system of linear congruences using the Chinese Remainder Theorem (CRT).
+ * @brief Function to solve a system of linear congruences using the Chinese
+ * Remainder Theorem (CRT).
  *
  * @param a Array of integers representing the remainders of the congruences.
- * @param m Array of pairwise coprime integers representing the moduli of the congruences.
+ * @param m Array of pairwise coprime integers representing the moduli of the
+ * congruences.
  * @param n Number of congruences in the system.
  * @return The solution to the system of congruences.
  */
-int chinese_remainder_theorem(int a[], int m[], int n) {
+int chinese_remainder_theorem(
+    const std::vector<LinearCongruance> &congruances) {
     // Calculate the product of all moduli
-    int M = 1;
-    for (int i = 0; i < n; ++i) {
-        M *= m[i];
-    }
+    const auto M = compute_product_of_all_moduli(congruances);
 
     int result = 0;
-    for (int i = 0; i < n; ++i) {
-        // Calculate the factor for each congruence
-        int factor = M / m[i];
-
-        // Calculate the modular inverse of the factor modulo m[i]
-        int inv = 1;
-        while ((factor * inv) % m[i] != 1) {
-            inv++;
-        }
+    for (const auto congruance : congruances) {
+        const auto factor = M / congruance.m;
+        const auto inv = compute_inverse(factor, congruance.m);
 
         // Sum up the partial results
-        result += (a[i] * factor * inv) % M;
+        result += (congruance.a * factor * inv) % M;
     }
 
     // Make sure the result is non-negative
@@ -60,14 +80,11 @@ int chinese_remainder_theorem(int a[], int m[], int n) {
  * @returns void
  */
 void test() {
-    int a[] = {2, 3, 1};
-    int m[] = {3, 4, 5};
-    int n = sizeof(a) / sizeof(a[0]);
+    const std::vector<LinearCongruance> testCase = {
+        LinearCongruance(2, 3), LinearCongruance(3, 4), LinearCongruance(1, 5)};
+    assert(chinese_remainder_theorem(testCase) == 11);
 
-    // chinese_remainder_theorem(a, m, n) returns 11.
-    assert(chinese_remainder_theorem(a, m, n) == 11);
-
-    cout << "All tests have successfully passed!\n";
+    std::cout << "One test has successfully passed!\n";
 }
 
 /**
