aeadc066ca44a681f9dcbb3c7b71059591f9b9ca
flo456123 <zachappella@gmail.com>
2023-08-25 11:13:39 -0400

    day 2
    fix: remove cycle from big tree
    chore: use type alias for adjacency list
    docs: add more documentation to algorithm

--- graph/centroid.cpp
+++ graph/centroid.cpp
@@ -4,31 +4,33 @@
  * @date 2023-08-24
  *
  * @attention
- * This algorithm is mainly applicable to unrooted trees because it
- * will output a node which can be used to root the tree in a
- * blaanced manner.
+ * This algorithm is mainly applicable to unrooted trees
+ * because the center of a tree can be used to root it in
+ * a balanced manner.
  *
  * @details
- * Finding the center(s) of a tree is a rather straightforward task
- * that can be accomplished relatively simply.
+ * Finding the center(s) of a tree is a rather straightforward
+ * task that can be accomplished relatively simply.
  *
- * The method I am using in this algorithm is going to be to *peel*
- * away the outer layer of leaf nodes. First, compute the degree of
- * each node, leaf nodes only have a degree of one. Once leaf nodes
- * have been identified, prune them and update the degree of each
- * node. Repeat this process until the center(s) of the tree have
- * been reached.
+ * The method I am using in this algorithm is going to be to
+ * peel away the outer layer of leaf nodes. First, compute
+ * the degree of each node, leaf nodes only have a degree of
+ * one. Once leaf nodes have been identified, prune them and
+ * update the degree of each node. Repeat this process until
+ * the center(s) of the tree have been reached.
  *
  * @example
- * A good anaology to gain an understanding of this algorithm is to
- * think about peeling away the layers of an onion. As each layer is
- * peeled away, the center of it becomes easier to locate.
+ * A good analogy to gain an understanding of this algorithm
+ * is to think about peeling away the layers of an onion.
+ * As each layer is peeled away, the center of it becomes
+ * easier to reach.
  *
- * The only difference the algorithm has with this analogy is that
- * there is always a possiblity for there to be two centers, however
- * there can never be more than two centers. This is because it is
- * possible for different parts of the graph to have similar roles
- * in terms of their centrality as seen below:
+ * The only difference the algorithm has with this analogy
+ * is that there is always a possibility for there to be
+ * two centers, however there can never be more than two
+ * centers. This is because it is possible for different
+ * parts of the graph to have similar roles in terms of
+ * their centrality as seen below:
  *
  *    A       F
  *     \     /
@@ -36,20 +38,21 @@
  *     /     \
  *    C       G
  *
- * In this graph, both nodes B and D have equidistant from all other
- * nodes on the graph, making two centers of the graph.
+ * In this graph, both nodes B and D have equidistant from
+ * all other nodes on the graph, making two centers of the
+ * graph. On the event that there are two centers, either
+ * one can be used to root the tree.
  */
 #include <cassert>  // for assertions
 #include <vector>   // for std::vector
-
-const int EMPTY_SIZE = 0;
-const int MAX_SIZE = 2;
 
 /**
  * @namespace graph
  * @brief Graph algorithms
  */
 namespace graph {
+
+using adjacency_list = std::vector<std::vector<int>>;
 
 /**
  * @namespace centroid
@@ -58,6 +61,9 @@ namespace graph {
  */
 namespace centroid {
 
+const int EMPTY_SIZE = 0;
+const int MAX_SIZE = 2;
+
 /**
  * Finds the center(s) of a tree.
  *
@@ -65,15 +71,15 @@ namespace centroid {
  * @param n    size of the tree
  * @return std::vector<int> center(s) of the tree
  */
-std::vector<int> treeCenters(const std::vector<std::vector<int>> &tree,
-                             const int n) {
+std::vector<int> treeCenters(const adjacency_list &tree) {
+    const int n = tree.size();
     int degree[n];            // tracks degree of each node
-    std::vector<int> leaves;  // stores nodes to be pruned
+    std::vector<int> leaves;  // stores leaves to be removed
     for (int i = 0; i < n; i++) {
-        degree[i] = tree[i].size();
+        degree[i] = tree[i].size();  // set degree to size number of children
         if (degree[i] == EMPTY_SIZE || degree[i] == 1) {
             leaves.push_back(i);
-            degree[i] = EMPTY_SIZE;  // mark node as visited
+            degree[i] = EMPTY_SIZE;  // mark node as explored
         }
     }
 
@@ -82,15 +88,16 @@ std::vector<int> treeCenters(const std::vector<std::vector<int>> &tree,
         std::vector<int> new_leaves;
         for (int node : leaves) {
             for (int neighbor : tree[node]) {
-                degree[neighbor] = degree[neighbor] - 1;
-                if (degree[neighbor] == 1) {
+                if (--degree[neighbor] == 1) {  // decrement degree of neighbor
+                                                // and check for leaf
                     new_leaves.push_back(neighbor);
                 }
             }
             degree[node] = EMPTY_SIZE;
         }
-        count += new_leaves.size();
-        leaves = new_leaves;
+        count += new_leaves.size();  // add leaves of current iteration
+        leaves = new_leaves;  // update leaves with the current leaves for
+                              // the iteration
     }
     return leaves;
 }
@@ -99,7 +106,8 @@ std::vector<int> treeCenters(const std::vector<std::vector<int>> &tree,
 }  // namespace graph
 
 bool valid_centers(const int size) {
-    return size > EMPTY_SIZE && size <= MAX_SIZE;
+    return size > graph::centroid::EMPTY_SIZE &&
+           size <= graph::centroid::MAX_SIZE;
 }
 
 /**
@@ -110,7 +118,7 @@ bool valid_centers(const int size) {
  */
 void f() {
     const int size = 7;
-    std::vector<std::vector<int>> tree(size);
+    graph::adjacency_list tree(size);
 
     // Construct the tree
     tree[0] = {1, 3, 6};
@@ -121,7 +129,7 @@ void f() {
     tree[5] = {4};
     tree[6] = {0};
 
-    std::vector<int> centers = graph::centroid::treeCenters(tree, size);
+    std::vector<int> centers = graph::centroid::treeCenters(tree);
     assert(valid_centers(centers.size()));
 }
 
@@ -133,7 +141,7 @@ void f() {
  */
 void f2() {
     const int size = 31;
-    std::vector<std::vector<int>> tree(size);
+    graph::adjacency_list tree(size);
 
     // BIG TREE!!!
     tree[0] = {1, 3, 5};
@@ -153,14 +161,14 @@ void f2() {
     tree[14] = {2, 13};
     tree[15] = {16, 17};
     tree[16] = {13, 15};
-    tree[17] = {18, 19};
+    tree[17] = {15, 18, 19};
     tree[18] = {17};
     tree[19] = {17};
     tree[20] = {7};
     tree[21] = {11};
     tree[22] = {11, 24};
     tree[23] = {24, 25, 26};
-    tree[24] = {22, 23, 26};
+    tree[24] = {22, 23};
     tree[25] = {23, 27, 30};
     tree[26] = {23};
     tree[27] = {25, 29};
@@ -168,7 +176,7 @@ void f2() {
     tree[29] = {27, 28};
     tree[30] = {25};
 
-    std::vector<int> centers = graph::centroid::treeCenters(tree, size);
+    std::vector<int> centers = graph::centroid::treeCenters(tree);
     assert(valid_centers(centers.size()));
 }
 
