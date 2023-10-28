c40e8983408d4af6d76e325d17f6a4b780d98d33
YashKesh <89805020+YashKesh@users.noreply.github.com>
2023-10-28 14:09:24 +0530

    added the path sum question of the trees in the folder of trees section with test case

--- trees/pathsum.cpp
+++ trees/pathsum.cpp
@@ -50,20 +50,21 @@ class Solution {
 int main() {
     // Create your binary tree and call the pathSum function here
     // Example usage:
-    // TreeNode* root = new TreeNode(5);
-    // root->left = new TreeNode(4);
-    // root->right = new TreeNode(8);
-    // root->left->left = new TreeNode(11);
-    // root->left->left->left = new TreeNode(7);
-    // root->left->left->right = new TreeNode(2);
-    // root->right->left = new TreeNode(13);
-    // root->right->right = new TreeNode(4);
-    // root->right->right->left = new TreeNode(5);
-    // root->right->right->right = new TreeNode(1);
-    // int targetSum = 22;
+    TreeNode* root = new TreeNode(5);
+    root->left = new TreeNode(4);
+    root->right = new TreeNode(8);
+    root->left->left = new TreeNode(11);
+    root->left->left->left = new TreeNode(7);
+    root->left->left->right = new TreeNode(2);
+    root->right->left = new TreeNode(13);
+    root->right->right = new TreeNode(4);
+    root->right->right->left = new TreeNode(5);
+    root->right->right->right = new TreeNode(1);
+    int targetSum = 22;
 
     Solution solution;
     // Call solution.pathSum(root, targetSum) and process the result as needed
+    solution.pathSum(root, targetSum);
 
     return 0;
 }
