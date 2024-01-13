cf7ca3a9e933c9873dddb89a90ee3d10152bae39
harsh-su <harshsoa542@gmail.com>
2024-01-13 09:36:41 +0530

    Reimplemented using C++ class and fixed memory leaks

--- search/hash_search.cpp
+++ search/hash_search.cpp
@@ -9,130 +9,119 @@
  * conflict, that is, we link a chain list after the data, and store all the
  * records whose keywords are synonyms in the same linear chain list.
  *
- * @warning This program is only for educational purposes. It has serious flaws
- * in implementation with regards to memory management resulting in large
- * amounts of memory leaks.
- * @todo fix the program for memory leaks and better structure in C++ and not C
- * fashion
  */
+
 #include <cstdlib>
 #include <iostream>
 
-#define MAX 6      ///< Determines how much data
-#define HASHMAX 5  ///< Determines the length of the hash table
+#define MAX     6
+#define HASHMAX 5
 
-int data[MAX] = {1, 10, 15, 5, 8, 7};  //!< test data
-
-/**
- * a one-way linked list
- */
-typedef struct list {
-    int key;            //!< key value for node
-    struct list* next;  //!< pointer to next link in the chain
-} node,                 /**< define node as one item list */
-    *link;              ///< pointer to nodes
-
-node hashtab[HASHMAX];  ///< array of nodes
-
-// int counter = 1;
-
-/**
- * Mode of hash detection :
- * Division method
- * \param [in] key to hash
- * \returns hash value for `key`
- */
 int h(int key) { return key % HASHMAX; }
 
-/**
- * The same after the remainder will be added after the same hash header
- * To avoid conflict, zipper method is used
- * Insert elements into the linked list in the header
- * \param [in] key key to add to list
- * \warning dynamic memory allocated to `n` never gets freed.
- * \todo fix memory leak
- */
-void create_list(int key) {  // Construct hash table
-    link p, n;
-    int index;
-    n = (link)malloc(sizeof(node));
-    n->key = key;
-    n->next = NULL;
-    index = h(key);
-    p = hashtab[index].next;
-    if (p != NULL) {
-        n->next = p;
-        hashtab[index].next = n;
-    } else {
-        hashtab[index].next = n;
+class hashnode {
+ public:
+    int key;
+    hashnode* next;
+
+    // Constructor
+    hashnode() {
+        key = 0;
+        next = NULL;
     }
-}
+    hashnode(int key) {
+        this->key = key;
+        this->next = NULL;
+    }
+};
 
-/**
- * Input the key to be searched, and get the hash header position through the H
- * (int key) function, then one-dimensional linear search. If found @return
- * element depth and number of searches If not found @return -1
- */
-int hash_search(int key, int* counter) {  // Hash lookup function
-    link pointer;
-    int index;
+class hashtable {
+ public:
+    hashnode hashtab[HASHMAX];
 
-    *counter = 0;
-    index = h(key);
-    pointer = hashtab[index].next;
-
-    std::cout << "data[" << index << "]:";
-
-    while (pointer != NULL) {
-        counter[0]++;
-        std::cout << "data[" << pointer->key << "]:";
-        if (pointer->key == key)
-            return 1;
-        else
-            pointer = pointer->next;
+    hashtable() {
+        for (int i = 0; i < HASHMAX; i++) {
+            hashtab[i].next = nullptr;
+        }
     }
 
-    return 0;
-}
+    ~hashtable() {
+        for (int i = 0; i < HASHMAX; i++) {
+            hashnode* current = hashtab[i].next;
+            while (current != nullptr) {
+                hashnode* temp = current;
+                current = current->next;
+                delete temp;
+            }
+        }
+    }
 
-/** main function */
+    void create_hashtable(int key) {
+        int index = h(key);
+        hashnode* new_hashnode = new hashnode(key);
+        new_hashnode->next = hashtab[index].next;
+        hashtab[index].next = new_hashnode;
+    }
+
+    int hash_search(int key, int* counter) {
+        hashnode* pointer;
+        int index = h(key);
+        pointer = hashtab[index].next;
+
+        std::cout << "data[" << index << "]: ";
+
+        while (pointer != nullptr) {
+            (*counter)++;
+            std::cout << "data[" << pointer->key << "]: ";
+            if (pointer->key == key)
+                return 1;
+            else
+                pointer = pointer->next;
+        }
+
+        return 0;
+    }
+};
+
 int main() {
-    link p;
-    int key, index, i, counter;  // Key is the value to be found
+    hashtable htable;
+    int key, index, counter = 0;  // Key is the value to be found
     index = 0;
 
-    // You can write the input mode here
-    while (index < MAX) {  // Construct hash table
-        create_list(data[index]);
+    int data[MAX] = {1, 10, 15, 5, 8, 7};  //!< test data
+
+    // Construct hash table
+    while (index < MAX) {
+        htable.create_hashtable(data[index]);
         index++;
     }
 
-    for (i = 0; i < HASHMAX; i++) {  // Output hash table
+    for (int i = 0; i < HASHMAX; i++) {
         std::cout << "hashtab [" << i << "]\n";
 
-        p = hashtab[i].next;
+        hashnode* temp = htable.hashtab[i].next;
 
-        while (p != NULL) {
-            std::cout << "please int key:";
-            if (p->key > 0)
-                std::cout << "[" << p->key << "]";
-            p = p->next;
+        while (temp != nullptr) {
+            std::cout << "[" << temp->key << "] ";
+            temp = temp->next;
         }
         std::cout << std::endl;
     }
 
     while (key != -1) {
-        // You can write the input mode here
-        // test key = 10
-        key = 10;
-        if (hash_search(key, &counter))
-            std::cout << "search time = " << counter << std::endl;
-        else
-            std::cout << "no found!\n";
-        key = -1;  // Exit test
-        /* The test sample is returned as:
-         * data[0]:data[5]:data[15]:data[10]:search time = 3 The search is
-         * successful. There are 10 in this set of data */
+        // Get key from user input or use a test key
+        std::cout << "Enter key to search (-1 to exit): ";
+        std::cin >> key;
+
+        if (key == -1)
+            break;
+
+        if (htable.hash_search(key, &counter)) {
+            std::cout << "Search time = " << counter << std::endl;
+            counter = 0;
+        } else {
+            std::cout << "Not found!\n";
+        }
     }
 
     return 0;
