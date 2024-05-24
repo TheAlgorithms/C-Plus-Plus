dd1462627dd7955a59c1045870073fceaca08fa2
Alexey Elfimov <elfimov@npfratex.ru>
2024-05-24 12:08:22 +0300

    feat: raw pointers are replaced by std::vector

--- numerical_methods/fast_fourier_transform.cpp
+++ numerical_methods/fast_fourier_transform.cpp
@@ -24,6 +24,7 @@
 #include <complex>   /// for storing points and coefficents
 #include <iostream>  /// for IO operations
 #include <vector>    /// for std::vector
+#include <cstdint>   /// for uint8_t and other
 
 /**
  * @namespace numerical_methods
@@ -38,7 +39,11 @@ namespace numerical_methods {
  * @returns p if n==1
  * @returns y if n!=1
  */
-std::complex<double> *FastFourierTransform(std::complex<double> *p, uint8_t n) {
+using ComplVec = std::vector<std::complex<double>>;
+
+ComplVec FastFourierTransform(ComplVec &p) {
+    size_t n = p.size();
+
     if (n == 1) {
         return p;  /// Base Case To return
     }
@@ -48,9 +53,8 @@ std::complex<double> *FastFourierTransform(std::complex<double> *p, uint8_t n) {
     std::complex<double> om = std::complex<double>(
         cos(2 * pi / n), sin(2 * pi / n));  /// Calculating value of omega
 
-    auto *pe = new std::complex<double>[n / 2];  /// Coefficients of even power
-
-    auto *po = new std::complex<double>[n / 2];  /// Coefficients of odd power
+    auto pe = ComplVec(n / 2);  /// Coefficients of even power
+    auto po = ComplVec(n / 2);  /// Coefficients of odd power
 
     int k1 = 0, k2 = 0;
     for (int j = 0; j < n; j++) {
@@ -62,13 +66,11 @@ std::complex<double> *FastFourierTransform(std::complex<double> *p, uint8_t n) {
         }
     }
 
-    std::complex<double> *ye =
-        FastFourierTransform(pe, n / 2);  /// Recursive Call
+    ComplVec ye = FastFourierTransform(pe);  /// Recursive Call
 
-    std::complex<double> *yo =
-        FastFourierTransform(po, n / 2);  /// Recursive Call
+    ComplVec yo = FastFourierTransform(po);  /// Recursive Call
 
-    auto *y = new std::complex<double>[n];  /// Final value representation list
+    ComplVec y = ComplVec(n);  /// Final value representation list
 
     k1 = 0, k2 = 0;
 
@@ -82,13 +84,6 @@ std::complex<double> *FastFourierTransform(std::complex<double> *p, uint8_t n) {
         k2++;
     }
 
-    if (n != 2) {
-        delete[] pe;
-        delete[] po;
-    }
-
-    delete[] ye;  /// Deleting dynamic array ye
-    delete[] yo;  /// Deleting dynamic array yo
     return y;
 }
 
@@ -104,15 +99,15 @@ std::complex<double> *FastFourierTransform(std::complex<double> *p, uint8_t n) {
 static void test() {
     /* descriptions of the following test */
 
-    auto *t1 = new std::complex<double>[2];  /// Test case 1
-    auto *t2 = new std::complex<double>[4];  /// Test case 2
+//    auto *t1 = new std::complex<double>[2];  /// Test case 1
+//    auto *t2 = new std::complex<double>[4];  /// Test case 2
 
-    t1[0] = {1, 0};
-    t1[1] = {2, 0};
-    t2[0] = {1, 0};
-    t2[1] = {2, 0};
-    t2[2] = {3, 0};
-    t2[3] = {4, 0};
+    auto t1 = std::vector<std::complex<double>> {{1, 0}, {2, 0}};
+    auto t2 = std::vector<std::complex<double>> {
+        {1, 0},
+        {2, 0},
+        {3, 0},
+        {4, 0}};
 
     uint8_t n1 = 2;
     uint8_t n2 = 4;
@@ -122,32 +117,21 @@ static void test() {
     std::vector<std::complex<double>> r2 = {
         {10, 0}, {-2, -2}, {-2, 0}, {-2, 2}};  /// True Answer for test case 2
 
-    std::complex<double> *o1 = numerical_methods::FastFourierTransform(t1, n1);
-    std::complex<double> *t3 =
-        o1;  /// Temporary variable used to delete memory location of o1
-    std::complex<double> *o2 = numerical_methods::FastFourierTransform(t2, n2);
-    std::complex<double> *t4 =
-        o2;  /// Temporary variable used to delete memory location of o2
+    numerical_methods::ComplVec o1 = numerical_methods::FastFourierTransform(t1);
+    numerical_methods::ComplVec o2 = numerical_methods::FastFourierTransform(t2);
     for (uint8_t i = 0; i < n1; i++) {
-        assert((r1[i].real() - o1->real() < 0.000000000001) &&
-               (r1[i].imag() - o1->imag() <
+        assert((r1[i].real() - o1[i].real() < 0.000000000001) &&
+               (r1[i].imag() - o1[i].imag() <
                 0.000000000001));  /// Comparing for both real and imaginary
                                    /// values for test case 1
-        o1++;
     }
 
     for (uint8_t i = 0; i < n2; i++) {
-        assert((r2[i].real() - o2->real() < 0.000000000001) &&
-               (r2[i].imag() - o2->imag() <
+        assert((r2[i].real() - o2[i].real() < 0.000000000001) &&
+               (r2[i].imag() - o2[i].imag() <
                 0.000000000001));  /// Comparing for both real and imaginary
                                    /// values for test case 2
-        o2++;
     }
-
-    delete[] t1;
-    delete[] t2;
-    delete[] t3;
-    delete[] t4;
     std::cout << "All tests have successfully passed!\n";
 }
 
