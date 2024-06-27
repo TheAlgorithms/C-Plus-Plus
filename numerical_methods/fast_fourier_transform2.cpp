ec392e8802acfe044346e38e4fbd877d5e4d2528
Alexey Elfimov <elfimov@npfratex.ru>
2024-06-27 11:07:12 +0300

    test: add test-case 3

--- numerical_methods/fast_fourier_transform2.cpp
+++ numerical_methods/fast_fourier_transform2.cpp
@@ -60,43 +60,70 @@ Container fft(const Container& in_data, const unsigned int log2n)
 static void test() {
     using ComplVec = std::vector<std::complex<double>>;
 
-    /* descriptions of the following test */
-
-    //    auto *t1 = new std::complex<double>[2];  /// Test case 1
-    //    auto *t2 = new std::complex<double>[4];  /// Test case 2
-
-    auto t1 = std::vector<std::complex<double>> {{1, 0}, {2, 0}};
-    auto t2 = std::vector<std::complex<double>> {
+    auto t1 = ComplVec {{1, 0}, {2, 0}};
+    auto t2 = ComplVec {
         {1, 0},
         {2, 0},
         {3, 0},
         {4, 0}};
+    auto t3 = ComplVec {
+        {0, 0},
+        {1, 1},
+        {3, 3},
+        {4, 4},
+        {4, 4},
+        {3, 3},
+        {1, 1},
+        {0, 0}};
 
-    uint8_t n1 = 2;
-    uint8_t n2 = 4;
+    uint8_t logN1 = 1;
+    uint8_t logN2 = 2;
+    uint8_t logN3 = 3;
+
+    uint8_t n1 = 1 << logN1;
+    uint8_t n2 = 1 << logN2;
+    uint8_t n3 = 1 << logN3;
+
     // The result was verified using the following
     // online tool: https://scistatcalc.blogspot.com/2013/12/fft-calculator.html
-    std::vector<std::complex<double>> r1 = {
-        {3, 0}, {-1, 0}};  /// True Answer for test case 1
+    // True Answer for test case 1
+    ComplVec r1 = {{3, 0}, {-1, 0}};
 
-    std::vector<std::complex<double>> r2 = {
-        {10, 0}, {-2, 2}, {-2, 0}, {-2, -2}};  /// True Answer for test case 2
+    // True Answer for test case 2
+    ComplVec r2 = {{10, 0}, {-2, 2}, {-2, 0}, {-2, -2}};
 
-    ComplVec o1 = fft(t1, 1);
-    ComplVec o2 = fft(t2, 2);
+    // True Answer for test case 3
+    ComplVec r3 = {
+        {16, 16},
+        {-4.82843, -11.6569},
+        {0, 0},
+        {-0.343146, 0.828427},
+        {0, 0},
+        {0.828427, -0.343146},
+        {0, 0},
+        {-11.6569, -4.82843}};
+
+    ComplVec o1 = fft(t1, logN1);
     for (short i = 0; i < n1; i++) {
         // Comparing for both real and imaginary values for test case 1
         assert((r1[i].real() - o1[i].real() < 0.00000001) &&
                (r1[i].imag() - o1[i].imag() < 0.00000001));
     }
 
-    std::cout << std::endl;
-
+    ComplVec o2 = fft(t2, logN2);
     for (short i = 0; i < n2; i++) {
         // Comparing for both real and imaginary values for test case 2
         assert((r2[i].real() - o2[i].real() < 0.00000001) &&
                (r2[i].imag() - o2[i].imag() < 0.00000001));
     }
+
+    ComplVec o3 = fft(t3, logN3);
+    for (short i = 0; i < n3; i++) {
+        // Comparing for both real and imaginary values for test case 3
+        assert((r3[i].real() - o3[i].real() < 0.00000001) &&
+               (r3[i].imag() - o3[i].imag() < 0.00000001));
+    }
+
     std::cout << "All tests have successfully passed!\n";
 }
 
