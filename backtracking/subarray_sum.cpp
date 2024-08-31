6629fd20592a97c5a459787205214ba74b965427
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2024-08-31 10:45:49 +0530

    fix: add <cstdint> to subarray_sum.cpp

--- backtracking/subarray_sum.cpp
+++ backtracking/subarray_sum.cpp
@@ -14,6 +14,7 @@
  */
 
 #include <cassert>        /// for assert
+#include <cstdint>        /// for integral typedefs
 #include <iostream>       /// for IO operations
 #include <unordered_map>  /// for unordered_map
 #include <vector>         /// for std::vector
