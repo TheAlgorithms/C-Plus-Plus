9620859abc6f7c15682a0c6b76c50c5dff72c1e7
Monalisa0311 <100229305+Monalisa0311@users.noreply.github.com>
2024-09-11 17:13:19 +0530

    Update kruskals_minimum_spanning_tree.cpp
    Formatting changed

--- greedy_algorithms/kruskals_minimum_spanning_tree.cpp
+++ greedy_algorithms/kruskals_minimum_spanning_tree.cpp
@@ -73,77 +73,77 @@ constexpr uint32_t INFINITY_UINT32 = UINT32_MAX;
  * @returns void
  */
 static void test() {
-    // Test case with interger values
+    // Test case with integer values
     std::cout << "\nTest Case 1 :\n";
     std::array<std::array<int, 6>, 6> graph1{
              0,            4,            1,             4,        INFINITY_INT,   INFINITY_INT,
              4,            0,            3,             8,             3,         INFINITY_INT,
              1,            3,            0,        INFINITY_INT,       1,         INFINITY_INT,
              4,            8,       INFINITY_INT,        0,            5,              7,
-        INFINITY_INT,      3,            1,              5,            0,        INFINITY_INT,
+        INFINITY_INT,      3,            1,              5,            0,         INFINITY_INT,
         INFINITY_INT, INFINITY_INT, INFINITY_INT,        7,       INFINITY_INT,       0};
     greedy_algorithms::findMinimumEdge(INFINITY_INT, graph1);
 
     // Test case with floating values
     std::cout << "\nTest Case 2 :\n";
      std::array<std::array<float, 3>, 3> graph2{
-        0.0f,           2.5f, INFINITY_FLOAT,
-        2.5f,           0.0f, 3.2f,
-        INFINITY_FLOAT, 3.2f, 0.0f};
+             0.0f,           2.5f,  INFINITY_FLOAT,
+             2.5f,           0.0f,       3.2f,
+        INFINITY_FLOAT,      3.2f,       0.0f};
     greedy_algorithms::findMinimumEdge(INFINITY_FLOAT, graph2);
 
     // Test case with double values
     std::cout << "\nTest Case 3 :\n";
    std::array<std::array<double, 5>, 5> graph3{
-        0.0, 10.5, INFINITY_DOUBLE, 6.7, 3.3,
-        10.5, 0.0, 8.1, 15.4, INFINITY_DOUBLE,
-        INFINITY_DOUBLE, 8.1, 0.0, INFINITY_DOUBLE, 7.8,
-        6.7, 15.4, INFINITY_DOUBLE, 0.0, 9.9,
-        3.3, INFINITY_DOUBLE, 7.8, 9.9, 0.0};
+               0.0,             10.5,       INFINITY_DOUBLE,        6.7,              3.3,
+               10.5,            0.0,              8.1,              15.4,       INFINITY_DOUBLE,
+        INFINITY_DOUBLE,        8.1,              0.0,        INFINITY_DOUBLE,         7.8,
+               6.7,             15.4,       INFINITY_DOUBLE,        0.0,               9.9,
+               3.3,       INFINITY_DOUBLE,        7.8,              9.9,               0.0};
     greedy_algorithms::findMinimumEdge(INFINITY_DOUBLE, graph3);
 
     // Test Case with negative weights
     std::cout << "\nTest Case 4 :\n";
     std::array<std::array<int, 3>, 3> graph_neg{
-        0, -2, 4,
-        -2, 0, 3,
-        4, 3, 0};
+        0,   -2,   4,
+       -2,    0,   3,
+        4,    3,   0};
     greedy_algorithms::findMinimumEdge(INFINITY_INT, graph_neg);
 
     // Test Case with Self-Loops
     std::cout << "\nTest Case 5 :\n";
     std::array<std::array<int, 3>, 3> graph_self_loop{
-        2, 1, INFINITY_INT,
-        INFINITY_INT, 0, 4,
-        INFINITY_INT, 4, 0};
+               2,       1, INFINITY_INT,
+        INFINITY_INT,   0,        4,
+        INFINITY_INT,   4,        0};
     greedy_algorithms::findMinimumEdge(INFINITY_INT, graph_self_loop);
 
     // Test Case with no edges
     std::cout << "\nTest Case 6 :\n";
     std::array<std::array<int, 4>, 4> no_edges{
-        0, INFINITY_INT, INFINITY_INT, INFINITY_INT,
-        INFINITY_INT, 0, INFINITY_INT, INFINITY_INT,
-        INFINITY_INT, INFINITY_INT, 0, INFINITY_INT,
-        INFINITY_INT, INFINITY_INT, INFINITY_INT, 0};
+               0,       INFINITY_INT,   INFINITY_INT,   INFINITY_INT,
+        INFINITY_INT,         0,        INFINITY_INT,   INFINITY_INT,
+        INFINITY_INT,   INFINITY_INT,          0,       INFINITY_INT,
+        INFINITY_INT,   INFINITY_INT,   INFINITY_INT,           0};
     greedy_algorithms::findMinimumEdge(INFINITY_INT, no_edges);
 
     // Test Case with a non-connected graph
     std::cout << "\nTest Case 7:\n";
     std::array<std::array<int, 4>, 4> partial_graph{
-        0, 2, INFINITY_INT, 6,
-        2, 0, 3, INFINITY_INT,
-        INFINITY_INT, 3, 0, 4,
-        6, INFINITY_INT, 4, 0};
+                0,             2,    INFINITY_INT,         6,
+                2,             0,            3,     INFINITY_INT,
+        INFINITY_INT,          3,            0,             4,
+                6,     INFINITY_INT,         4,             0};
     greedy_algorithms::findMinimumEdge(INFINITY_INT, partial_graph);
 
-    // Test Case with Directed weighted graph. Krushkal algorithm does not give
+    // Test Case with Directed weighted graph. The Krushkal algorithm does not give
     // optimal answer
     std::cout << "\nTest Case 8:\n";
     std::array<std::array<int, 4>, 4> directed_graph{
-        0, 3, 7, INFINITY_INT,  // Vertex 0 has edges to Vertex 1 and Vertex 2
-        INFINITY_INT, 0, 2, 5,  // Vertex 1 has edges to Vertex 2 and Vertex 3
-        INFINITY_INT, INFINITY_INT, 0, 1,  // Vertex 2 has an edge to Vertex 3
-        INFINITY_INT, INFINITY_INT, INFINITY_INT, 0};  // Vertex 3 has no outgoing edges
+                0,            3,            7,   INFINITY_INT,  // Vertex 0 has edges to Vertex 1 and Vertex 2
+        INFINITY_INT,         0,            2,           5,  // Vertex 1 has edges to Vertex 2 and Vertex 3
+        INFINITY_INT, INFINITY_INT,         0,           1,  // Vertex 2 has an edge to Vertex 3
+        INFINITY_INT, INFINITY_INT, INFINITY_INT,        0};  // Vertex 3 has no outgoing edges
     greedy_algorithms::findMinimumEdge(INFINITY_INT, directed_graph);
 
     // Test case with wrong input passed
@@ -154,7 +154,7 @@ static void test() {
         5,        5,        5,        5};
     greedy_algorithms::findMinimumEdge(INFINITY_INT, graph9);
 
-    // Test case with all same values between every edges
+    // Test case with all the same values between every edge
     std::cout << "\nTest Case 10:\n";
     std::array<std::array<int, 5>, 5> graph10{
         0,        5,        5,        5,        5,
