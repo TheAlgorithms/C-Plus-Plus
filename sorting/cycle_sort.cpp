768534b253adbab6226a6e1816ac32332c198b79
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2024-09-30 05:54:10 +0530

    Update sorting/cycle_sort.cpp

--- sorting/cycle_sort.cpp
+++ sorting/cycle_sort.cpp
@@ -13,7 +13,7 @@
 
 #include <algorithm>  /// for std::is_sorted, std::swap
 #include <cassert>    /// for assert
-#include <cstdint>    /// for integral typedefs
+#include <cstdint>
 #include <iostream>   /// for io operations
 #include <vector>     /// for std::vector
 
