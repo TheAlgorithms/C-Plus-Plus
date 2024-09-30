9c35a066eaf8ca2b311c9ea5fa3ccafa2de8369a
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2024-09-30 05:43:05 +0530

    Update backtracking/subset_sum.cpp

--- backtracking/subset_sum.cpp
+++ backtracking/subset_sum.cpp
@@ -10,7 +10,7 @@
  */
 
 #include <cassert>   /// for assert
-#include <cstdint>   /// for integral typedefs
+#include <cstdint>
 #include <iostream>  /// for IO operations
 #include <vector>    /// for std::vector
 
