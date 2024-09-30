eb9043c8754f14013d18f011412430b80fb269ec
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2024-09-30 05:54:53 +0530

    Update search/interpolation_search.cpp

--- search/interpolation_search.cpp
+++ search/interpolation_search.cpp
@@ -31,7 +31,7 @@
 
 #include <algorithm>  /// for std::sort function
 #include <cassert>    /// for std::assert
-#include <cstdint>    /// for integral typedefs
+#include <cstdint>
 #include <iostream>   /// for IO operations
 #include <vector>     /// for std::vector
 
