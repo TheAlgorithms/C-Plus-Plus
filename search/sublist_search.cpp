01e263510e0a5155ba3792336ded5fab6f0c2a54
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2024-09-30 05:54:23 +0530

    Update search/sublist_search.cpp

--- search/sublist_search.cpp
+++ search/sublist_search.cpp
@@ -26,7 +26,7 @@
  */
 
 #include <cassert>   /// for assert
-#include <cstdint>   /// for integral typedefs
+#include <cstdint>
 #include <iostream>  /// for IO operations
 #include <vector>    /// for std::vector
 
