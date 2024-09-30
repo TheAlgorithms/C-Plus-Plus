f60bc8b25b4227e0faa7841bf54b5a00bbd10068
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2024-09-30 05:42:48 +0530

    Update search/binary_search.cpp

--- search/binary_search.cpp
+++ search/binary_search.cpp
@@ -36,7 +36,7 @@
 
 #include <algorithm>  /// for std::sort function
 #include <cassert>    /// for std::assert
-#include <cstdint>    /// for integral typedefs
+#include <cstdint>
 #include <iostream>   /// for IO operations
 #include <vector>     /// for std::vector
 /******************************************************************************
