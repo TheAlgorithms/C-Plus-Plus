04c9f15da8655ae6278e6c86c3a14c93e4af2219
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2024-09-30 05:46:35 +0530

    Update data_structures/list_array.cpp

--- data_structures/list_array.cpp
+++ data_structures/list_array.cpp
@@ -16,7 +16,7 @@
 
 #include <array>     /// for std::array
 #include <cassert>   /// for assert
-#include <cstdint>   /// for integral typedefs
+#include <cstdint>
 #include <iostream>  /// for io operations
 
 /**
