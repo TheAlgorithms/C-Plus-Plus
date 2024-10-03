32593116bd0f25d4a058164d71cebc7197cc9d02
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2024-10-03 17:43:25 +0530

    fix: typo in function name

--- physics/ground_to_ground_projectile_motion.cpp
+++ physics/ground_to_ground_projectile_motion.cpp
@@ -31,7 +31,7 @@ namespace ground_to_ground_projectile_motion {
  * @returns Angle in degrees
  */
 
-double degree_to_radians(double degree){
+double degrees_to_radians(double degree){
     double radians = degree * (M_PI / 180);
     return radians;
 }
