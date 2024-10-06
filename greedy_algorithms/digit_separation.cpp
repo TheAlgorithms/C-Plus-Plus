ba5688080645088482b5002ab07d9dcd2a4d9fb9
Muhammad Junaid Khalid <mjk22071998@gmail.com>
2024-10-06 08:02:08 +0500

    fowardOrder to forwardOrder on line 122
    Co-authored-by: realstealthninja <68815218+realstealthninja@users.noreply.github.com>

--- greedy_algorithms/digit_separation.cpp
+++ greedy_algorithms/digit_separation.cpp
@@ -119,7 +119,7 @@ static void tests() {
     expectedForward = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 1, 2, 3, 4, 5};
     reverseOrder = ds.digitSeparationReverseOrder(number);
     assert(reverseOrder == expectedReverse);
-    fowardOrder = ds.digitSeparationForwardOrder(number);
+    forwardOrder = ds.digitSeparationForwardOrder(number);
     assert(forwardOrder == expectedForward);
 }
 
