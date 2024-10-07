a9c309e1d40383f1a53c0f746a0cc6440b2d252f
Naman Jain <103953760+namanmodi65@users.noreply.github.com>
2024-10-07 20:05:54 +0530

    Longest Increasing subsequence using binary search most optimal approach for this problem(done)

--- search/Longest_Increasing_Subsequence_using_binary_search.cpp
+++ search/Longest_Increasing_Subsequence_using_binary_search.cpp
@@ -1,3 +1,54 @@
+
+/**
+ * @file
+ * @brief find the length of the Longest Increasing Subsequence (LIS)
+ * using Binary Search(https://en.wikipedia.org/wiki/Longest_increasing_subsequence)
+ * @details
+ * Given an integer array nums, return the length of the longest strictly
+ * increasing subsequence.
+ * The longest increasing subsequence is described as a subsequence of an array
+ * where: All elements of the subsequence are in increasing order. This subsequence
+ * itself is of the longest length possible.
+
+ * For solving this problem we have Three Approaches :-
+
+ * Approach 1 :- Using Brute Force
+ * The first approach that came to your mind is the Brute Force approach where we
+ * generate all subsequences and then manually filter the subsequences whose
+ * elements come in increasing order and then return the longest such subsequence.
+ * Time Complexity :- O(2^n)
+ * It's time complexity is exponential. Therefore we will try some other
+ * approaches.
+
+ * Approach 2 :- Using Dynamic Programming
+ * To generate all subsequences we will use recursion and in the recursive logic we
+ * will figure out a way to solve this problem. Recursive Logic to solve this
+ * problem:-
+ * 1. We only consider the element in the subsequence if the element is grater then
+ * the last element present in the subsequence
+ * 2. When we consider the element we will increase the length of subsequence by 1
+ * Time Complexity: O(N*N)
+ * Space Complexity: O(N*N) + O(N)
+
+ * This approach is better then the previous Brute Force approach so, we can
+ * consider this approach.
+
+ * But when the Constraints for the problem is very larger then this approach fails
+
+ * Approach 3 :- Using Binary Search
+ * Other approaches use additional space to create a new subsequence Array.
+ * Instead, this solution uses the existing nums Array to build the subsequence
+ * array. We can do this because the length of the subsequence array will never be
+ * longer than the current index.
+
+ * Time complexity: O(n∗log(n))
+ * Space complexity: O(1)
+
+ * This approach consider Most optimal Approach for solving this problem
+
+ * @author [Naman Jain](https://github.com/namanmodi65)
+ */
+
 #include <cassert>   /// for std::assert
 #include <iostream>  /// for IO operations
 #include <vector>    /// for std::vector
