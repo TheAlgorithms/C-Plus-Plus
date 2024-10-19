3cce6fd3516be65057567b6c0835acf57b806a69
Shree Harish S <sreenivasulushreeharish@gmail.com>
2024-10-19 16:15:56 +0530

    updated comments for readability and placed tables at top

--- hashing/ripemd_160.cpp
+++ hashing/ripemd_160.cpp
@@ -12,6 +12,18 @@
  * others. RIPEMD-160 is used in some cryptocurrencies and blockchain
  * technologies as part of their hashing mechanisms.
  *
+ * The r and r' table specifies the order in which 4-byte words of the current
+ * 512-bits block are accessed and processed in each step of the compression
+ * function. It introduces non-linearity to the code. The r table is utilised by
+ * one of the parallel path and the r' table by the other.
+ *
+ * The s and s' table dtermines the number of bits to cyclically shift (left
+ * rotate) the result of each step. The different shift values prevent patterns
+ * from emerging in the output, which is crucial for achieving properties like
+ * the avalanche effect (where a small change in the input results in a
+ * large change in the output). The s table is utilised by
+ * one of the parallel path and these s' table by the other.
+ *
  * Produces a fixed-size output of 160 bits (20 bytes), which is commonly
  * represented as a 40-character hexadecimal string.
  *
@@ -45,6 +57,46 @@ namespace hashing {
  */
 class RIPEMD160 {
  private:
+    /**
+     * @brief Implements the r table
+     */
+    static constexpr int r[80] = {
+        0, 1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15,
+        7, 4,  13, 1,  10, 6,  15, 3,  12, 0, 9,  5,  2,  14, 11, 8,
+        3, 10, 14, 4,  9,  15, 8,  1,  2,  7, 0,  6,  13, 11, 5,  12,
+        1, 9,  11, 10, 0,  8,  12, 4,  13, 3, 7,  15, 14, 5,  6,  2,
+        4, 0,  5,  9,  7,  12, 2,  10, 14, 1, 3,  8,  11, 6,  15, 13};
+
+    /**
+     * @brief Implements the r' table
+     */
+    static constexpr int r_dash[80] = {
+        5,  14, 7,  0, 9, 2,  11, 4,  13, 6,  15, 8,  1,  10, 3,  12,
+        6,  11, 3,  7, 0, 13, 5,  10, 14, 15, 8,  12, 4,  9,  1,  2,
+        15, 5,  1,  3, 7, 14, 6,  9,  11, 8,  12, 2,  10, 0,  4,  13,
+        8,  6,  4,  1, 3, 11, 15, 0,  5,  12, 2,  13, 9,  7,  10, 14,
+        12, 15, 10, 4, 1, 5,  8,  7,  6,  2,  13, 14, 0,  3,  9,  11};
+
+    /**
+     * @brief Implements the s table
+     */
+    static constexpr int s[80] = {
+        11, 14, 15, 12, 5,  8,  7,  9,  11, 13, 14, 15, 6,  7,  9,  8,
+        7,  6,  8,  13, 11, 9,  7,  15, 7,  12, 15, 9,  11, 7,  13, 12,
+        11, 13, 6,  7,  14, 9,  13, 15, 14, 8,  13, 6,  5,  12, 7,  5,
+        11, 12, 14, 15, 14, 15, 9,  8,  9,  14, 5,  6,  8,  6,  5,  12,
+        9,  15, 5,  11, 6,  8,  13, 12, 5,  12, 13, 14, 11, 8,  5,  6};
+
+    /**
+     * @brief Implements the s' table
+     */
+    static constexpr int s_dash[80] = {
+        8,  9,  9,  11, 13, 15, 15, 5,  7,  7,  8,  11, 14, 14, 12, 6,
+        9,  13, 15, 7,  12, 8,  9,  11, 7,  7,  12, 7,  6,  15, 13, 11,
+        9,  7,  15, 11, 8,  6,  6,  14, 12, 13, 5,  14, 13, 13, 7,  5,
+        15, 5,  8,  11, 14, 14, 6,  14, 6,  9,  12, 9,  12, 5,  15, 8,
+        8,  5,  12, 9,  12, 5,  14, 6,  8,  13, 6,  5,  15, 13, 11, 11};
+
     /**
      * @brief Implements f(j,B,C,D)
      * @param j Round number j / 16
@@ -114,68 +166,6 @@ class RIPEMD160 {
                 throw std::invalid_argument("j value out of bound");
         }
     }
-
-    /**
-     * @brief Specifies r value for a given j.
-     *
-     * @details Specifies the order in which 4-byte words of the current
-     * 512-bits block are accessed and processed in each step of the compression
-     * function. Introduces non-linearity to the code. It is used by one of the
-     * parallel path.
-     */
-    static constexpr int r[80] = {
-        0, 1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15,
-        7, 4,  13, 1,  10, 6,  15, 3,  12, 0, 9,  5,  2,  14, 11, 8,
-        3, 10, 14, 4,  9,  15, 8,  1,  2,  7, 0,  6,  13, 11, 5,  12,
-        1, 9,  11, 10, 0,  8,  12, 4,  13, 3, 7,  15, 14, 5,  6,  2,
-        4, 0,  5,  9,  7,  12, 2,  10, 14, 1, 3,  8,  11, 6,  15, 13};
-
-    /**
-     * @brief Specifies the r' value a given j.
-     *
-     * @details Specifies the order in which 4-byte words of the current
-     * 512-bits block are accessed and processed in each step of the compression
-     * function. Introduces non-linearity to the code. It is used in the other
-     * parallel path.
-     */
-    static constexpr int r_dash[80] = {
-        5,  14, 7,  0, 9, 2,  11, 4,  13, 6,  15, 8,  1,  10, 3,  12,
-        6,  11, 3,  7, 0, 13, 5,  10, 14, 15, 8,  12, 4,  9,  1,  2,
-        15, 5,  1,  3, 7, 14, 6,  9,  11, 8,  12, 2,  10, 0,  4,  13,
-        8,  6,  4,  1, 3, 11, 15, 0,  5,  12, 2,  13, 9,  7,  10, 14,
-        12, 15, 10, 4, 1, 5,  8,  7,  6,  2,  13, 14, 0,  3,  9,  11};
-
-    /**
-     * @brief Specifies the s value for a given j.
-     *
-     * @details Determines the number of bits to cyclically shift (left rotate)
-     * the result of each step. The different shift values prevent patterns from
-     * emerging in the output, which is crucial for achieving properties like
-     * the avalanche effect (where a small change in the input results in a
-     * large change in the output). It is used by one of the parallel path.
-     */
-    static constexpr int s[80] = {
-        11, 14, 15, 12, 5,  8,  7,  9,  11, 13, 14, 15, 6,  7,  9,  8,
-        7,  6,  8,  13, 11, 9,  7,  15, 7,  12, 15, 9,  11, 7,  13, 12,
-        11, 13, 6,  7,  14, 9,  13, 15, 14, 8,  13, 6,  5,  12, 7,  5,
-        11, 12, 14, 15, 14, 15, 9,  8,  9,  14, 5,  6,  8,  6,  5,  12,
-        9,  15, 5,  11, 6,  8,  13, 12, 5,  12, 13, 14, 11, 8,  5,  6};
-
-    /**
-     * @brief Specifies the s' value for a given j.
-     *
-     * @details Determines the number of bits to cyclically shift (left rotate)
-     * the result of each step. The different shift values prevent patterns from
-     * emerging in the output, which is crucial for achieving properties like
-     * the avalanche effect (where a small change in the input results in a
-     * large change in the output). It is used in the other parallel path.
-     */
-    static constexpr int s_dash[80] = {
-        8,  9,  9,  11, 13, 15, 15, 5,  7,  7,  8,  11, 14, 14, 12, 6,
-        9,  13, 15, 7,  12, 8,  9,  11, 7,  7,  12, 7,  6,  15, 13, 11,
-        9,  7,  15, 11, 8,  6,  6,  14, 12, 13, 5,  14, 13, 13, 7,  5,
-        15, 5,  8,  11, 14, 14, 6,  14, 6,  9,  12, 9,  12, 5,  15, 8,
-        8,  5,  12, 9,  12, 5,  14, 6,  8,  13, 6,  5,  15, 13, 11, 11};
 
     /**
      * @brief converts string data to vector of uint32_t (4 byte words)
