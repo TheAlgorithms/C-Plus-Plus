1a9e177b1d39496cf3187245b95dcba383362be5
David Otazo <davidotazo2008@gmail.com>
2025-06-17 20:19:09 -0700

    Add insertion sort algorithm with comments

---
+++ search/insertion_sort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+// Function that sorts an array using the insertion sort algorithm.
+
+void insertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];       // Current element to be inserted
+        int j = i - 1;
+
+        // Move elements of arr[0..i-1], that are greater than key,
+        // to one position ahead of their current position
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;  // Insert the key at the correct position
+    }
+}
+
+int main() {
+    int arr[] = {5, 2, 9, 1, 5, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    insertionSort(arr, n);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
