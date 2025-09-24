5d04574a120f12b5878bf36d52459eb40a6dccab
Rohith A K <rohithakkankol1234@gmail.com>
2025-09-24 14:57:54 +0530

    Added necessary documentation and removed the using namespace

--- sorting/bubble_sort_optimized.cpp
+++ sorting/bubble_sort_optimized.cpp
@@ -1,37 +1,62 @@
+#include <iostream>  /// for IO operations
+#include <vector>    /// for std::vector
+#include <cassert>   /// for assert
 
-/*This version of bubble sort will reduce the number of passes needed if average cases and also the number od swaps needed in each pass which optimize the algorithm*/
+namespace sorting {
 
-
-#include <iostream>
-#include <vector>
-using namespace std;
-
-vector<int>& bubble_sort(vector<int>& nums){
-    int flag=0;
-    int temp;
-    for(int i=0; i<nums.size()-1;i++){
-        flag=0;
-        for(int j=0; j<nums.size()-1-i;j++){
-             if(nums[j]>nums[j+1]){
-                temp=nums[j];
-                nums[j]=nums[j+1];
-                nums[j+1]=temp;
-                flag=1;
-             }
+    /**
+     * @brief Sorts the given vector using the bubble sort algorithm.
+     * Optimized by reducing the number of passes in average cases.
+     * @param nums the vector of integers to be sorted
+     * @return reference to the sorted vector
+     */
+    std::vector<int>& bubble_sort(std::vector<int>& nums) {
+        int flag=0;
+        int temp;
+        for(int i=0; i<nums.size()-1;i++){
+            flag=0;
+            for(int j=0; j<nums.size()-1-i;j++){
+                if(nums[j]>nums[j+1]){
+                    temp=nums[j];
+                    nums[j]=nums[j+1];
+                    nums[j+1]=temp;
+                    flag=1;
+                }
+            }
+            if(flag==0){
+                break;
+            }
         }
-        if(flag==0){
-            break;
-        }
+        return nums;
     }
-    return nums;
+
+    /**
+     * @brief Self-test implementations
+     * @returns void
+     */
+    static void tests() {
+        std::vector<int> v1 = {2,8,1,6,2,0,3,6};
+        std::vector<int> expected1 = {0,1,2,2,3,6,6,8};
+        assert(bubble_sort(v1) == expected1);
+
+        std::vector<int> v2 = {5,4,3,2,1};
+        std::vector<int> expected2 = {1,2,3,4,5};
+        assert(bubble_sort(v2) == expected2);
+
+        std::vector<int> v3 = {1,2,3,4,5};
+        std::vector<int> expected3 = {1,2,3,4,5};
+        assert(bubble_sort(v3) == expected3);
+
+        std::cout << "All tests have successfully passed!\n";
+    }
 }
 
-
+/**
+ * @brief Main function
+ * @returns 0 on exit
+ */
 int main() {
-    vector<int> v={2,8,1,6,2,0,3,6};
-    v=bubble_sort(v);
-    for(int i=0;i<v.size();i++){
-        cout<<v.at(i)<<endl;
-    }
+    sorting::tests(); // run self-test implementations
     return 0;
 }
+   
