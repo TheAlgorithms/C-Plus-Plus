46bc050f0444eeb36a17274544c5a184d021dd21
Vanshraj Tandon <vrajtandon@gmail.com>
2026-05-01 03:21:44 +0530

    Added linear Search implementation

---
+++ search/linear_searching.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int linearSearch(int arr[], int n, int key) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == key)
+            return i;
+    }
+    return -1;
+}
