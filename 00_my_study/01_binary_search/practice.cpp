428070b2c4719c8752b68046df18c782bad3923d
陳奕愷 <j83859000@gmail.com>
2026-07-07 17:24:44 +0800

    feat: 完成二分搜尋法

--- 00_my_study/01_binary_search/practice.cpp
+++ 00_my_study/01_binary_search/practice.cpp
@@ -2,15 +2,15 @@
 #include <cstdint>
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
-uint64_t binarySearch(vector<long long>& arr, long long val) {
+uint64_t binarySearch(const vector<long long>& arr, uint64_t val) {
     uint64_t low = 0;
     uint64_t high = arr.size() - 1;
 
     while (low <= high) {
-        
-        uint64_t m = low + (high - low) / 2;
-        if (val == arr[m]) {
+        long long m = low + (high - low) / 2;
+        if (arr[m] == val) {
             return m;
         } else if (val < arr[m]) {
             high = m - 1;
@@ -22,10 +22,12 @@ uint64_t binarySearch(vector<long long>& arr, long long val) {
 }
 
 int main() {
-    vector<long long> v = {1, 2, 3, 4, 5};
+    vector<long long> v = {1, 2, 3, 4, 5 , 6 , 7};
+    sort(v.begin(), v.end());
     int val = 2;
-    int expected_ans = 1;
+    uint64_t expected_ans = 1;
     uint64_t res = binarySearch(v, val);
+    cout << res << " " << expected_ans << "\n";
     assert(res == expected_ans);
     cout << "Pass!\n";
     return 0;
