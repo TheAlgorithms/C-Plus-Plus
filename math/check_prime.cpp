34119189461187d3a806d2444b46b7c850e23c10
yasmeenMohammed <mwyasmeen@gmail.com>
2026-07-20 13:00:07 +0530

    docs/refactor: clean check_prime.cpp variable names and guidelines

--- math/check_prime.cpp
+++ math/check_prime.cpp
@@ -1,17 +1,18 @@
 /**
  * @file
- * @brief
- * A simple program to check if the given number is
- * [Prime](https://en.wikipedia.org/wiki/Primality_test) or not.
+ * @brief A simple program to check if a given number is [Prime](https://en.wikipedia.org/wiki/Primality_test) or not.
  * @details
- * A prime number is any number that can be divided only by itself and 1. It
- * must be positive and a whole number, therefore any prime number is part of
- * the set of natural numbers. The majority of prime numbers are even numbers,
- * with the exception of 2. This algorithm finds prime numbers using this
- * information. additional ways to solve the prime check problem:
- * https://cp-algorithms.com/algebra/primality_tests.html#practice-problems
+ * A prime number is any natural number greater than 1 that has no positive divisors
+ * other than 1 and itself. The only even prime number is 2; all other prime numbers
+ * are odd numbers. This algorithm utilizes trial division optimized by skipping
+ * multiples of 2 and 3.
+ *
+ * Additional resources for primality tests:
+ * https://cp-algorithms.com/algebra/primality_tests.html
+ *
  * @author [Omkar Langhe](https://github.com/omkarlanghe)
  * @author [ewd00010](https://github.com/ewd00010)
+ * @author [Yasmeen](https://github.com/yasmeenMohammed)
  */
 
 #include <cassert>   /// for assert
@@ -19,64 +20,65 @@
 
 /**
  * @brief Mathematical algorithms
- * @namespace
+ * @namespace math
  */
 namespace math {
 /**
  * @brief Function to check if the given number is prime or not.
- * @param num number to be checked.
- * @return true if number is a prime
- * @return false if number is not a prime.
+ * @param number The 64-bit integer to be checked for primality.
+ * @return true if the number is a prime number.
+ * @return false if the number is not a prime number.
+ * @note Time Complexity: O(sqrt(number)) due to trial division optimization up to the square root.
+ * @note Space Complexity: O(1) as it uses a constant amount of extra memory space.
  */
-bool is_prime(int64_t num) {
-    /*!
-     * Reduce all possibilities of a number which cannot be prime with the first
-     * 3 if, else if conditionals. Example: Since no even number, except 2 can
-     * be a prime number and the next prime we find after our checks is 5,
-     * we will start the for loop with i = 5. then for each loop we increment
-     * i by +6 and check if i or i+2 is a factor of the number; if it's a factor
-     * then we will return false. otherwise, true will be returned after the
-     * loop terminates at the terminating condition which is i*i <= num
-     */
-    if (num <= 1) {
+bool is_prime(int64_t number) {
+    // Numbers less than or equal to 1 are not prime
+    if (number <= 1) {
         return false;
-    } else if (num == 2 || num == 3) {
+    } 
+    // 2 and 3 are prime numbers
+    if (number == 2 || number == 3) {
         return true;
-    } else if (num % 2 == 0 || num % 3 == 0) {
+    } 
+    // Exclude all even numbers and multiples of 3
+    if (number % 2 == 0 || number % 3 == 0) {
         return false;
-    } else {
-        for (int64_t i = 5; i * i <= num; i = i + 6) {
-            if (num % i == 0 || num % (i + 2) == 0) {
-                return false;
-            }
+    } 
+    
+    // Check potential factors starting from 5, skipping multiples of 2 and 3 (i.e., step of 6)
+    for (int64_t factor = 5; factor * factor <= number; factor += 6) {
+        if (number % factor == 0 || number % (factor + 2) == 0) {
+            return false;
         }
     }
+    
     return true;
 }
 }  // namespace math
 
 /**
- * @brief Self-test implementations
+ * @brief Self-test implementations to validate the correctness of the primality check.
  * @returns void
  */
 static void tests() {
-    assert(math::is_prime(1) == false);
-    assert(math::is_prime(2) == true);
-    assert(math::is_prime(3) == true);
-    assert(math::is_prime(4) == false);
-    assert(math::is_prime(-4) == false);
-    assert(math::is_prime(7) == true);
-    assert(math::is_prime(-7) == false);
-    assert(math::is_prime(19) == true);
-    assert(math::is_prime(50) == false);
-    assert(math::is_prime(115249) == true);
+    assert(math::is_prime(0) == false);     /// Test boundary edge case 0
+    assert(math::is_prime(1) == false);     /// Test boundary edge case 1
+    assert(math::is_prime(2) == true);      /// Test smallest even prime
+    assert(math::is_prime(3) == true);      /// Test smallest odd prime
+    assert(math::is_prime(4) == false);     /// Test composite even number
+    assert(math::is_prime(-4) == false);    /// Test negative composite number
+    assert(math::is_prime(7) == true);      /// Test standard odd prime
+    assert(math::is_prime(-7) == false);    /// Test negative odd number
+    assert(math::is_prime(19) == true);     /// Test medium prime number
+    assert(math::is_prime(50) == false);    /// Test large composite number
+    assert(math::is_prime(115249) == true); /// Test a large prime number
 
-    std::cout << "All tests have successfully passed!" << std::endl;
+    std::cout << "All primality checks have successfully passed!" << std::endl;
 }
 
 /**
- * @brief Main function
- * @returns 0 on exit
+ * @brief Main function to execute the self-test suite.
+ * @returns 0 on clean exit
  */
 int main() {
     tests();  // perform self-tests implementations
