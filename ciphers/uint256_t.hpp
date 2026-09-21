6d81fa37622b89811d51319ce3d39abf5c217baf
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2026-09-21 19:15:29 +0530

    fix: Remove undefined specialisation in uint128_t and uint256_t (#3232)
    * fix: remove undefined specialisations
    
    * fix: remove undefined behaviour of specialising std
    
    * clang-format and clang-tidy fixes for 57f60874
    
    * chore: bump freeglut
    
    * chore: typo in sha256
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- ciphers/uint256_t.hpp
+++ ciphers/uint256_t.hpp
@@ -17,15 +17,6 @@
 
 class uint256_t;
 
-template <>
-struct std::is_integral<uint256_t> : std::true_type {};
-
-template <>
-struct std::is_arithmetic<uint256_t> : std::true_type {};
-
-template <>
-struct std::is_unsigned<uint256_t> : std::true_type {};
-
 /**
  * @class uint256_t
  * @brief class for 256-bit unsigned integer
